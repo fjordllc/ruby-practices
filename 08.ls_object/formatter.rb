1b0e099fc527bef1586e60e6a5f6c5bdf363cf80
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-12-09 19:21:44 +0900

    レビュー指摘箇所を修正

--- 08.ls_object/formatter.rb
+++ 08.ls_object/formatter.rb
@@ -8,8 +8,28 @@ require './file_data'
 module Ls
   class Formatter
     def simple(files)
-      files.each_slice(3) do |separated_files|
-        puts separated_files.map { |n| n.to_s.ljust(15) }.join
+      reform_files = []
+      files.each_slice(5) do |separated_files|
+        reform_files << case separated_files.size
+                        when 1
+                          separated_files.push(nil, nil, nil, nil, nil)
+                        when 2
+                          separated_files.push(nil, nil, nil, nil)
+                        when 3
+                          separated_files.push(nil, nil, nil)
+                        when 4
+                          separated_files.push(nil, nil)
+                        when 5
+                          separated_files.push(nil)
+                        else
+                          separated_files
+                        end
+      end
+      reform_files.transpose.each do |array_files|
+        array_files.each do |file|
+          print file.to_s.ljust(15)
+        end
+        print "\n"
       end
     end
 
