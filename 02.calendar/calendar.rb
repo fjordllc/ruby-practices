7b7388b90cd8fcf15798ac7853da1c7ad06d5d43
Toshiyasu Nonohara <hh0648075@gmail.com>
2020-12-18 11:54:08 +0900

    引数から年と月を取得し文字列として表示

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,3 +5,14 @@ options = ARGV.getopts('m:y:')
 year = options["y"]
 month = options["m"]
 
+if year && month
+  puts "　　　　#{month}月　#{year}"
+  puts "日　月　火　水　木　金　土"
+else
+  today = Date.today.to_s.split("-")
+  this_year = today[0]
+  this_month = today[1]
+  this_day = today[2]
+  puts "　　　　#{this_month}月 #{this_year}"
+  puts "日　月　火　水　木　金　土"
+end
