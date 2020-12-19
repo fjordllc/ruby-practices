ba9f3f3a265ad284849f285d0210db483c02ad38
HirokiMiyakura <miyakurahiroki@gmail.com>
2020-12-19 23:27:12 +0900

    re-re-commit ls

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -71,7 +71,7 @@ if option_hash[:l]
     owner_name = get_owner_name(stat.uid).name
     group_name = get_group_name(stat.gid).name
     formatted_time = change_time_format(stat.mtime)
-    print "#{file_type}#{file_mode} #{stat.nlink} #{owner_name} #{group_name} #{stat.size} #{formatted_time}
+    print "#{file_type}#{file_mode} #{stat.nlink} #{owner_name} #{group_name} #{stat.size} #{formatted_time} \
 #{file_or_directory}\n"
   end
 else
