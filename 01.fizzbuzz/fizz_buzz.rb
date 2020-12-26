f3c8adda1e0c8087117c68c15951c3703e1d584f
Shota <s.ito@wiz-net.jp>
2020-12-26 23:08:41 +0900

    fizzbuzz課題の再提出２

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -1,12 +1,12 @@
 (1..100).each do |i|
 
- if i % 15 == 0
+  if i % 15 == 0
      puts 'Fizz Buzz'
- elsif i % 3 ==0
+  elsif i % 3 ==0
      puts 'Fizz'
   elsif i % 5 == 0
      puts 'Buzz'
- else
-    puts i
- end
+  else
+     puts i
+  end
 end
