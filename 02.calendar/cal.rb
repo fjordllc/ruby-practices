f59f7a9cf67e1a61f5f3ddf503e1dc8af2714926
fronts1231 <takumi.maeomote1231@gmail.com>
2021-01-06 20:49:42 +0900

    1st submit

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,17 +1,22 @@
 require 'optparse'
-opt = OptionParser.new
+require 'date'
 
-opt.on('-y', '--year', 'input year')
-opt.on('-m', '--month', 'input month')
+options = ARGV.getopts('y:', 'm:')
 
-opt.parse!(ARGV)
-year = ARGV[0].to_i
-month = ARGV[1].to_i
+if options["y"].nil?
+  year = Date.today.year.to_i
+else
+  year = options["y"].to_i
+end
+if options["m"].nil?
+  month = Date.today.mon.to_i
+else
+  month = options["m"].to_i
+end
 
 puts "      " + month.to_s + "月 " + year.to_s
 puts "日 月 火 水 木 金 土"
 
-require 'date'
 first = Date.new(year, month, 1)
 final = Date.new(year, month, -1)
 
@@ -19,9 +24,6 @@ days = []
 x = first - 1
 while x < final
   x += 1
-  # if x.cwday == 6
-  #   puts "saturday"
-  # end
   days.push(x)
 end
 
@@ -42,7 +44,7 @@ days.each do |y|
       when 5
         print "                " + y.mday.to_s + " "
       when 7
-        print "                      " + y.mday.to_s + " "
+        print " " + y.mday.to_s + " "
       end
     else
       if y.cwday == 6 && y.mday < 10
@@ -54,7 +56,11 @@ days.each do |y|
           if y.mday < 10
             print " " + y.mday.to_s + " "
           else
-            print y.mday.to_s + " "
+            if y == final
+              print y.mday.to_s + "\n"
+            else
+              print y.mday.to_s + " "
+            end
           end
         end
       end
