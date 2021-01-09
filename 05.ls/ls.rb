4492520f0276acf37ae06f2f4aa9feade8881933
CHICALOG <66663821+CHICALOG@users.noreply.github.com>
2021-01-09 22:09:48 +0900

    Update ls.rb

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,9 +2,9 @@
 # frozen_string_literal: true
 
 #ディレクトリの中身を取得(ls)
-p Dir.glob("*",).sort
+p Dir.glob("*").sort
 #ディレクトリの中身を逆から取得(ls -r)
-p Dir.glob("*",).sort.reverse
+p Dir.glob("*").sort.reverse
 
 #ディレクトリの隠しファイルを含めた中身を取得(ls -a)
 p Dir.glob("*", File::FNM_DOTMATCH).sort
@@ -12,8 +12,14 @@ p Dir.glob("*", File::FNM_DOTMATCH).sort
 p Dir.glob("*", File::FNM_DOTMATCH).sort.reverse
 
 #ディレクトリの詳細を含めた中身を取得(考え中)
-#           | | usr  |      |   |   | |mtime| ファイルの名前   
-#drwxr-xr-x  3 chica  staff   96 12  4 21:14 09.wc_object
-#-rw-r--r--@ 1 chica  staff  393  1  4 19:51 a.rtf
-#mtime => 最終更新時刻
-#p Dir.glob("*",)
+#ディレクトリ内のファイルの詳細を出力するようにする
+fs = File::Stat.new($0) 
+p fs.rdev_major
+p fs.dev
+p fs.mode
+p fs.nlink
+p fs.uid
+p fs.gid
+p fs.size
+p fs.mtime
+#ディレクトリの名前を取得(考え中)
