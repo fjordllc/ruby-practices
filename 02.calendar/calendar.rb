855ff46b80b7af5154df4edb81a8c23f18a5843a
ryotori <ko_re_zo_n@yahoo.co.jp>
2021-01-11 22:19:37 +0900

    日付をいれる変数名を変更。曜日の出力をputsに変更。カレンダー出力処理を簡略化。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -11,50 +11,39 @@ month = params.values[1].to_i
 
 #コマンドラインで受け取った月と年を元にDateインスタンスを生成
 #月の初日の情報を取得
-date = Date.new(year, month, 1)
+date_first = Date.new(year, month, 1)
 #月の最終日の情報を取得
-date2 = Date.new(year, month, -1)
+date_last = Date.new(year, month, -1)
 
-day_temp = date.day
-date_temp = date
+day_first_temp = date_first.day
+date_first_temp = date_first
 
 date_hash = {}
 
 #日付をキー、曜日の番号を値としたハッシュを作成
-while day_temp <= date2.day
-    date_hash[:"#{day_temp}"] = date_temp.wday
-    date_temp += 1
-    day_temp += 1
+while day_first_temp <= date_last.day
+    date_hash[:"#{day_first_temp}"] = date_first_temp.wday
+    date_first_temp += 1
+    day_first_temp += 1
 end
 
 #カレンダーを出力
-print('      ' + month.to_s + '月 ' + year.to_s)
-print("\n")
-print('日', ' 月 ', '火', ' 水 ', '木', ' 金 ', '土')
-print("\n")
+puts '      ' + month.to_s + '月 ' + year.to_s
+puts '日' + ' 月 ' + '火' + ' 水 ' + '木' + ' 金 ' + '土'
 date_hash.each do |key, value|
     if key.to_s.length == 1
         #日付が一桁の場合は、整合をとるために前にも空白を付与する
+        pre_branks = ' '
         if key.to_s == "1"
-            #1日だけは曜日番号に応じて空白の数を調整して出力位置をずらす
-            case date_hash[:"1"]
-            when 0
-                print(' ' + key.to_s + ' ')    
-            when 1
-                print('    ' + key.to_s + ' ')
-            when 2
-                print('       ' + key.to_s + ' ')
-            when 3
-                print('          ' + key.to_s + ' ')
-            when 4
-                print('             ' + key.to_s + ' ')
-            when 5
-                print('                ' + key.to_s + ' ')
-            when 6
-                print('                   ' + key.to_s + ' ')
+            #初日が日曜日以外の場合、そのまま出力すると曜日と日付が合わないので
+            #曜日番号に応じて前に付与する空白の数を変えて出力する位置をずらす
+            value.times do 
+                #曜日番号が1増えるごとに、空白を3つ増やす
+                pre_branks += '   '
             end
+            print(pre_branks + key.to_s + ' ')
         else
-            print(' ' + key.to_s + ' ')
+            print(pre_branks + key.to_s + ' ')
         end
     elsif
         print(key.to_s + ' ')
