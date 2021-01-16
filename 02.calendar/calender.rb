1dd74f995246dd6a24d2f038ce2708b324d3d586
ToshiroKoba <t-koba@ruby-dev.jp>
2021-01-16 13:30:45 +0900

    カレンダープログラムの回答を記述

---
+++ 02.calendar/calender.rb
@@ -0,0 +1,41 @@
+require 'date'
+require 'optparse'
+
+def write(year, month, total_days)
+  wday_number = Date.new(year, month, 1).wday
+  puts "      #{month}月 #{year}"
+  puts "日 月 火 水 木 金 土"
+  print '   ' * wday_number
+  (1..(total_days)).each do | day |
+    print ' ' if day.to_s.size == 1
+    print "#{day} "
+    print "\n" if (wday_number + day) % 7 == 0
+  end
+end
+
+params = ARGV.getopts("m:", 'y:')
+
+month = 
+  if params["m"].nil?
+    Date.today.month
+  else
+    params["m"].to_i
+  end
+
+year = 
+  if params["y"].nil?
+    Date.today.year
+  else
+    params["y"].to_i
+  end
+
+begin
+  BaseDay = Date.new(year, month, 15)
+rescue => e
+  puts "#{year}年#{month}月は存在しません。\n西暦を入力してください\n例) ./calender.rb -m 1 -y 2021"
+  exit
+end
+
+total_days = Date.new(year, month, -1).mday
+
+write(year, month, total_days)
