038044542a5955ca19607b549558d6e38f72706b
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-01-18 07:59:12 +0900

    フレーム数が10フレームになるように仕様変更。

--- 04.bowling/04.bowling.rb
+++ 04.bowling/04.bowling.rb
@@ -13,26 +13,44 @@ pins.each_with_index do |pin, n|
 end
 
 frames = []
-shots.each_slice(2) do |shot|
-  frames << shot
-end
-
-frame_score = []
-frames.each_with_index do |frame, n|
-  frame_score <<
-    if frame[0] == 10 && n < 9
-      if (frames[n + 1])[0] == 10 && (frames[n + 2])[0] == 10
-        30
-      elsif n < 8 && (frames[n + 1])[0] == 10
-        20 + (frames[n + 2])[0]
-      else
-        10 + frames[n + 1].sum
-      end
-    elsif frame.sum == 10 && n < 9
-      10 + (frames[n + 1])[0]
-    else
-      frame.sum
+one_frame = []
+shots.each_with_index do |shot, n|
+  if frames.size < 9
+    one_frame << shot
+    if one_frame.size == 2
+      frames << one_frame
+      one_frame = []
     end
+  else
+    one_frame << shot unless shots[n - 1] == 10
+    frames << one_frame if n == shots.size - 1
+  end
 end
 
-puts frame_score.sum
+total_score = frames.each_with_index.sum do |frame, n|
+  if n < 8 && frame[0] == 10
+    if (frames[n + 1])[0] == 10 && (frames[n + 2])[0] == 10
+      30
+    elsif (frames[n + 1])[0] == 10
+      20 + (frames[n + 2])[0]
+    else
+      10 + (frames[n + 1]).sum
+    end
+  elsif n == 8 && frame[0] == 10
+    if frames[9][0] == 10
+      if frames[9][1] == 10
+        30
+      else
+        20 + (frames[9])[1]
+      end
+    else
+      10 + (frames[9])[0] + (frames[9])[1]
+    end
+  elsif n < 8 && frame.sum == 10
+    10 + (frames[n + 1])[0]
+  else
+    frame.sum
+  end
+end
+
+puts total_score
