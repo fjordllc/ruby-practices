b8fc5dea7aed110f11f29a6428f4b996ac2f806e
ryotori <ko_re_zo_n@yahoo.co.jp>
2021-02-05 22:52:24 +0900

    バグ修正中(原因は分かったが、解決方法分からず)

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -23,29 +23,31 @@ point = 0
 frames_index = 0
 
 frames.each do |frame|
-  if frames_index == 9 || frames_index == 10
+  p (frames_index + 1).to_s + "フレーム目"
+  if frames_index >= 9
       # 10フレーム目のみは単純に加算する
       p "10フレーム目だよ"
-      point = point + frame.sum
+      point += frame.sum
   elsif frame.sum == 10 && frame[0] != 10
-      p "スペアの場合"
       # スペアの場合
       # 次フレームの1投目の得点を追加で加算
-      point = point + frame.sum + frames[frames_index + 1][0]
+      p "スペア"
+      point +=frame.sum + frames[frames_index + 1][0]
   elsif frame[0] == 10
-      p "ストライクの場合"
       # ストライクの場合
       # 次のフレームの1,2投目の合計を追加で加算
-      point = point +  frame.sum + frames[frames_index + 1][0] + frames[frames_index + 1][1];
+      p "ストライク"
+      point +=frame.sum + frames[frames_index + 1][0] + frames[frames_index + 1][1];
   else
-      p "スペア、ストライク以外"
       #スペアでもストライクでもない場合は、単純に1フレームの合計値を加算
-      point = point + frame.sum
+      p "スペアでもストライクでもなし"
+      point += frame.sum
   end
+  p (frames_index + 1).to_s + "フレーム目の得点"
+  p point
   # フレームを1つ進める
-#  p (frames_index + 1).to_s + "フレーム目の得点"
-#  p point
-  frames_index = frames_index + 1
+  frames_index += 1
 end
 
+p "総合得点"
 p point
