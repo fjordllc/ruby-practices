ac8048a6da7d50cc37599bc244f875a39167ea94
ryotori <ko_re_zo_n@yahoo.co.jp>
2021-02-09 22:20:03 +0900

    ボウリングプログラム完成(rubocop警告は未対応)

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,53 +1,62 @@
 #!/usr/bin/env ruby
 score = ARGV[0]
-scores = score.chars
-shots = []
-scores.each do |s|
-  if s == 'X' # strike
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
+scores = score.chars.map! {|n| n == 'X' ? 10 : n.to_i}
+frame = []
+frames = []
+frame_count = 0
+
+scores.each do |score|
+  frame << score
+  if frame_count + 1 <= 9 
+    if frame.length == 1 && frame[0] == 10
+      # 1投目で1本倒した(つまりストライク)の場合は、後ろに0を追加して
+      # 配列framesに格納
+      frame << 0
+      frames << frame
+      frame = []
+      frame_count += 1
+    elsif frame.length == 2
+      # 1フレーム分の得点がまとまった場合は、配列framesに格納
+      frames << frame
+      frame = []
+      frame_count += 1
+    end
+  elsif frame_count + 1 == 10
+    # 10フレーム目はそのまま配列framesに代入するだけ
+    frames << frame
+    frame_count += 1
   end
 end
-
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
-
-p frames
 
 point = 0
 #フレーム数を表すインデックス変数
 frames_index = 0
 
 frames.each do |frame|
-  p (frames_index + 1).to_s + "フレーム目"
-  if frames_index >= 9
+  if frames_index + 1 == 10
       # 10フレーム目のみは単純に加算する
-      p "10フレーム目だよ"
       point += frame.sum
   elsif frame.sum == 10 && frame[0] != 10
       # スペアの場合
       # 次フレームの1投目の得点を追加で加算
-      p "スペア"
-      point +=frame.sum + frames[frames_index + 1][0]
+      point += frame.sum + frames[frames_index + 1][0]
   elsif frame[0] == 10
       # ストライクの場合
       # 次のフレームの1,2投目の合計を追加で加算
-      p "ストライク"
-      point +=frame.sum + frames[frames_index + 1][0] + frames[frames_index + 1][1];
+      if frames[frames_index + 1][1] == 0 && frames_index + 1 != 9
+        # ストライクが連続した場合は、計算を合わせるために2フレーム先の得点を足す
+        # ただし9フレーム目のみは2フレーム先はないので行わない
+        point += frame.sum + frames[frames_index + 1][0] + frames[frames_index + 2][0]
+      else
+        point += frame.sum + frames[frames_index + 1][0] + frames[frames_index + 1][1]
+      end
   else
       #スペアでもストライクでもない場合は、単純に1フレームの合計値を加算
-      p "スペアでもストライクでもなし"
       point += frame.sum
   end
-  p (frames_index + 1).to_s + "フレーム目の得点"
-  p point
   # フレームを1つ進める
   frames_index += 1
 end
 
-p "総合得点"
+# 総合得点を出力
 p point
