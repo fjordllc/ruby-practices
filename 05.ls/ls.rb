bceb245f31b07dd759345f0202037893a2c8d3e6
e6i24v <e6i24v@gmail.com>
2021-02-28 17:55:36 +0900

    modified1

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,76 +1,54 @@
-##!/bin/sh
-exec ruby -x "$0" "$@"
-##!ruby
-
+#! /usr/bin/env ruby
 # frozen_string_literal: true
 
 require 'optparse'
 require_relative './mystat'
 require 'pathname'
 
-opt = OptionParser.new
+def dir_stat(pathname, opt)
+  stat = []
+  arr = []
+  arr << pathname.join('*')
+  arr << pathname.join('.*') if opt
+  pathname.glob(arr) do |pathname_|
+    stat << MyStat.new(pathname_)
+  end
+  stat
+end
+
+def file_dir(input, opt)
+  args = []
+  if input == false
+    ARGV.each do |arg|
+      Pathname.glob(arg) do |pathname|
+        stat = []
+        if pathname.directory?
+          stat = dir_stat(pathname, opt)
+          args << [:other, arg, stat]
+        else
+          stat << MyStat.new(pathname)
+          args << [:first, nil, stat]
+        end
+      end
+    end
+  else
+    stat = dir_stat(Pathname('./'), opt)
+    args << [:first, nil, stat]
+  end
+  args
+end
+
+option = OptionParser.new
 
 options = {}
 
-opt.on('-a') { |v| v }
-opt.on('-l') { |v| v }
-opt.on('-r') { |v| v }
+option.on('-a') { |v| v }
+option.on('-l') { |v| v }
+option.on('-r') { |v| v }
 
-opt.parse!(ARGV, into: options)
+option.parse!(ARGV, into: options)
 
-file_dir_info = if !ARGV.empty? && !options[:a]
-                  args = []
-                  ARGV.each do |arg|
-                    Pathname.glob(arg) do |pathname|
-                      stat = []
-                      if pathname.directory?
-                        Pathname.glob(pathname.join('*')) do |pathname_|
-                          stat << MyStat.new(pathname_)
-                        end
-                        args << [:other, arg, stat]
-                      else
-                        stat << MyStat.new(pathname)
-                        args << [:first, nil, stat]
-                      end
-                    end
-                  end
-                  args
-                elsif !ARGV.empty? && options[:a]
-                  args = []
-                  ARGV.each do |arg|
-                    Pathname.glob(arg) do |pathname|
-                      stat = []
-                      if pathname.directory?
-                        arr = []
-                        arr << pathname.join('*')
-                        arr << pathname.join('.*')
-                        Pathname.glob(arr) do |pathname_|
-                          stat << MyStat.new(pathname_)
-                        end
-                        args << [:other, arg, stat]
-                      else
-                        stat << MyStat.new(pathname)
-                        args << [:first, nil, stat]
-                      end
-                    end
-                  end
-                  args
-                elsif ARGV.empty? && options[:a]
-                  args = []
-                  stat = []
-                  Pathname.glob(['./*', './.*']) do |pathname|
-                    stat << MyStat.new(pathname)
-                  end
-                  args << [:first, nil, stat]
-                elsif ARGV.empty? && !options[:a]
-                  args = []
-                  stat = []
-
-                  Pathname.glob('./*') do |pathname|
-                    stat << MyStat.new(pathname)
-                  end
-                  args << [:first, nil, stat]
-                end
+file_dir_info = file_dir(ARGV.empty?, options[:a])
 
 file_dir_info.sort! do |a, b|
   (a[0] <=> b[0]).nonzero? || (a[1] <=> b[1])
@@ -103,27 +81,26 @@ else
   file_dir_info.each do |fd|
     puts "#{fd[1]} :" unless fd[0] == :first
 
-    display = []
+    datas = []
     div = if (fd[2].size % 3).zero?
             fd[2].size / 3
           else
             fd[2].size / 3 + 1
           end
 
-    display_child = []
-    i = 0
-    fd[2].each do |d|
-      display_child << File.basename(d.path)
+    data_child = []
+
+    fd[2].each.with_index do |d, i|
+      data_child << File.basename(d.path)
       if ((i + 1) % div).zero?
-        display << display_child
-        display_child = []
+        datas << data_child
+        data_child = []
       end
-      i += 1
     end
-    display << display_child
-    (0...display[0].size).each do |col|
-      (0...display.size).each do |row|
-        printf('%<display>-30s ', display: display[row][col]) unless display[row][col].nil?
+    datas << data_child
+    datas.each do |dataset|
+      dataset.each do |item|
+        printf('%<display>-30s ', display: item) unless item.nil?
       end
       puts
     end
