d5ed6e5bc73fe9ef42b5e54f0bc4e2e2914d5092
rosh-1228 <sho.watanabe1228@gmail.com>
2021-03-05 13:39:14 +0900

    fizzbuzz問題の答え

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+i = 0
+100.times do
+  i += 1
+  if i % 3 == 0 && i % 5 == 0 then
+    puts "FizzBuzz"
+  elsif i % 3 == 0 then
+    puts "Fizz"
+  elsif i % 5 == 0 then
+    puts "Buzz"
+  else
+    puts i
+  end
+end
