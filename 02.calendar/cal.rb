0ac09f004b17c950d6f4e720d85cf80ce96d08f8
rosh-1228 <sho.watanabe1228@gmail.com>
2021-03-11 21:53:22 +0900

    Calederクラス作成完了

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,56 +2,49 @@ require 'date'
 require 'optparse'
 
 class Calender
+  CALENDER_TODAY = Date.today
+  CALENDER_WEEK = ["日","月","火","水","木","金","土"]
   
-  def get_day(i)
-    @get_day = Date.new(Time.now.year, Time.now.month, i)
+  def self.get_day(i)
+    Date.new(Time.now.year, Time.now.month, i)
   end
 
-  def create_space(x)
-    @space = self.get_day(1).strftime("%e").rjust(x)
+  def self.create_space(x)
+    get_day(1).strftime("%e").rjust(x)
+  end
+  
+  def self.first_day_search
+    if get_day(1).monday? then
+      print(create_space(5))
+    elsif get_day(1).tuesday? then
+      print(create_space(8))
+    elsif get_day(1).wednesday? then
+      print(create_space(11))
+    elsif get_day(1).thursday? then
+      print(create_space(14))
+    elsif get_day(1).friday? then
+      print(create_space(17))
+    end
   end
 
-  # 月初日の曜日を判定する
-  def first_day_search
-    if self.get_day(1).monday? then
-      print(self.create_space(5))
-    elsif self.get_day(1).tuesday? then
-      print(self.create_space(8))
-    elsif self.get_day(1).wednesday? then
-      print(self.create_space(11))
-    elsif self.get_day(1).thursday? then
-      print(self.create_space(14))
-    elsif self.get_day(1).friday? then
-      print(self.create_space(17))
+  CALENDER_START = get_day(1).strftime("%e").to_i
+  CALENDER_END = get_day(-1).strftime("%e").to_i
+
+  def self.show_calender
+    puts CALENDER_TODAY.strftime("     %-m月 %Y")
+    puts CALENDER_WEEK.join(" ")
+    (CALENDER_START..CALENDER_END).each do |x|
+      if get_day(x).strftime("%e").to_i == CALENDER_START then
+        print(first_day_search)
+      elsif get_day(x).strftime("%e").to_i == CALENDER_END || get_day(x).saturday? then
+        puts get_day(x).strftime("%e").rjust(3)
+      elsif get_day(x).sunday?
+        print(get_day(x).strftime("%e"))
+      else
+        print(get_day(x).strftime("%e").rjust(3))
+      end
     end
   end
 end
 
-
-# 今日を取得
-today = Date.today
-
-# 月と年を取得して表示
-puts today.strftime("     %-m月 %Y")
-
-# 曜日を表示
-week = ["日","月","火","水","木","金","土"]
-puts week.join(" ")
-
-cal = Calender.new
-start_month = cal.get_day(1).strftime("%e").to_i
-end_month = cal.get_day(-1).strftime("%e").to_i
-
-# 日を表示
-(start_month..end_month).each do |x|
-  day = cal.get_day(x)
-  if day.strftime("%e").to_i == start_month then
-    print cal.first_day_search
-  elsif day.strftime("%e").to_i == end_month || day.saturday? then
-    puts day.strftime("%e").rjust(3)
-  elsif day.sunday?
-    print(day.strftime("%e"))
-  else
-    print(day.strftime("%e").rjust(3))
-  end
-end
+Calender.show_calender
