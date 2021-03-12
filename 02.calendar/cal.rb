0d966669cb77b4e0adb78956e5990baf0b0cde8b
rosh-1228 <sho.watanabe1228@gmail.com>
2021-03-12 23:41:59 +0900

    Option機能を実装（年月一緒にすると2つカレンダーが表示されるので、修正する）

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,43 +8,47 @@ class Calender
     Date.new(cal_year, cal_month, cal_day)
   end
 
-  def self.create_space(x)
-    get_day(1).strftime("%e").rjust(x)
+  def self.create_space(x, cal_year, cal_month)
+    get_day(1, cal_year, cal_month).strftime("%e").rjust(x)
   end
 
-  def self.first_day_search
-    if get_day(1).monday? then
-      print(create_space(5))
-    elsif get_day(1).tuesday? then
-      print(create_space(8))
-    elsif get_day(1).wednesday? then
-      print(create_space(11))
-    elsif get_day(1).thursday? then
-      print(create_space(14))
-    elsif get_day(1).friday? then
-      print(create_space(17))
+  def self.first_day_search(cal_year, cal_month)
+    if get_day(1, cal_year, cal_month).sunday? then
+      print(create_space(0, cal_year, cal_month))
+    elsif get_day(1, cal_year, cal_month).monday? then
+      print(create_space(5, cal_year, cal_month))
+    elsif get_day(1, cal_year, cal_month).tuesday? then
+      print(create_space(8, cal_year, cal_month))
+    elsif get_day(1, cal_year, cal_month).wednesday? then
+      print(create_space(11, cal_year, cal_month))
+    elsif get_day(1, cal_year, cal_month).thursday? then
+      print(create_space(14, cal_year, cal_month))
+    elsif get_day(1, cal_year, cal_month).friday? then
+      print(create_space(17, cal_year, cal_month))
+    elsif get_day(1, cal_year, cal_month).saturday? then
+      puts create_space(20, cal_year, cal_month)
     end
   end
 
-  CALENDER_START = get_day().strftime("%e").to_i
-  CALENDER_END = get_day(-1).strftime("%e").to_i
-
-  def self.show_calender
-    puts get_day().strftime("     %-m月 %Y")
+  def self.show_calender(cal_year, cal_month)
+    puts get_day(1, cal_year, cal_month).strftime("     %-m月 %Y")
     puts CALENDER_WEEK.join(" ")
-    (CALENDER_START..CALENDER_END).each do |x|
-      if get_day(x).strftime("%e").to_i == CALENDER_START then
-        print(first_day_search)
-      elsif get_day(x).strftime("%e").to_i == CALENDER_END || get_day(x).saturday? then
-        puts get_day(x).strftime("%e").rjust(3)
-      elsif get_day(x).sunday?
-        print(get_day(x).strftime("%e"))
+    (get_day(1, cal_year, cal_month).strftime("%e").to_i..get_day(-1, cal_year, cal_month).strftime("%e").to_i).each do |x|
+      if get_day(x, cal_year, cal_month).strftime("%e").to_i == get_day(1, cal_year, cal_month).strftime("%e").to_i then
+        print(first_day_search(cal_year, cal_month))
+      elsif get_day(x, cal_year, cal_month).strftime("%e").to_i == get_day(-1, cal_year, cal_month).strftime("%e").to_i || get_day(x, cal_year, cal_month).saturday? then
+        puts get_day(x, cal_year, cal_month).strftime("%e").rjust(3)
+      elsif get_day(x, cal_year, cal_month).sunday?
+        print(get_day(x, cal_year, cal_month).strftime("%e"))
       else
-        print(get_day(x).strftime("%e").rjust(3))
+        print(get_day(x, cal_year, cal_month).strftime("%e").rjust(3))
       end
     end
   end
 end
-
+# Calender.show_calender(Date.today.year, v)
 opt = OptionParser.new
-Calender.show_calender
+opt.on('-m [month]', Integer, 'show calender set month') {|v| Calender.show_calender(Date.today.year, v) }
+opt.on('-y [year]', Integer, 'show calender set month') {|v| Calender.show_calender(v, Date.today.month) }
+opt.parse!(ARGV)
+ARGV
