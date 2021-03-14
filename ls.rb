589cd7a5aae681a3537aba93777b284f4dcf8a2d
CHICALOG <66663821+CHICALOG@users.noreply.github.com>
2021-03-14 15:09:36 +0900

    lsコマンドを作る(Ruby)_途中経過(lオプションの実装)
    ```rubocop --auto-correct``` で可能なエラーは修正すみ

--- ls.rb
+++ ls.rb
@@ -1,25 +1,100 @@
 # !/usr/bin/env ruby
 # frozen_string_literal: true
 
-#ディレクトリの中身を取得(ls)
-p Dir.glob("*").sort
-#ディレクトリの中身を逆から取得(ls -r)
-p Dir.glob("*").sort.reverse
+require 'etc'
 
-#ディレクトリの隠しファイルを含めた中身を取得(ls -a)
-p Dir.glob("*", File::FNM_DOTMATCH).sort
-#ディレクトリの隠しファイルえお含めた中身を取得(ls -ar/ls -ra)
-p Dir.glob("*", File::FNM_DOTMATCH).sort.reverse
+def file_names
+  Dir.glob('*').sort
+end
 
-#ディレクトリの詳細を含めた中身を取得(考え中)
-#ディレクトリ内のファイルの詳細を出力するようにする
-fs = File::Stat.new($0) 
-p fs.rdev_major
-p fs.dev
-p fs.mode
-p fs.nlink
-p fs.uid
-p fs.gid
-p fs.size
-p fs.mtime
-#ディレクトリの名前を取得(考え中)
+def file_mode_permission(file)
+  eight_ary = File::Stat.new(file).mode.to_s(8)
+  alignment = format('%06d', eight_ary)
+end
+
+def file_type(file)
+  details_filename = File::Stat.new(file)
+  if details_filename.ftype == 'directory'
+    'd'
+  elsif details_filename.ftype == 'file'
+    '-'
+  else
+    details_filename.ftype == 'link'
+    'l'
+  end
+end
+
+def file_eachpermission_index_three(file)
+  file_eachpermission_number = file_mode_permission(file)[3]
+  case file_mode_permission(file)[3]
+  when '0' then '---'
+  when '1' then '--x'
+  when '3' then '-wx'
+  when '4' then 'r--'
+  when '5' then 'r-x'
+  when '6' then 'rw-'
+  when '7' then 'rwx'
+  end
+end
+
+def file_eachpermission_index_four(file)
+  file_eachpermission_number = file_mode_permission(file)[4]
+  case file_mode_permission(file)[4]
+  when '0' then '---'
+  when '1' then '--x'
+  when '3' then '-wx'
+  when '4' then 'r--'
+  when '5' then 'r-x'
+  when '6' then 'rw-'
+  when '7' then 'rwx'
+  end
+end
+
+def file_eachpermission_index_five(file)
+  file_eachpermission_number = file_mode_permission(file)[5]
+  case file_mode_permission(file)[5]
+  when '0' then '---'
+  when '1' then '--x'
+  when '3' then '-wx'
+  when '4' then 'r--'
+  when '5' then 'r-x'
+  when '6' then 'rw-'
+  when '7' then 'rwx'
+  end
+end
+
+def file_eachpermission(file)
+  modestring = [file_type(file)] + [file_eachpermission_index_three(file)] + [file_eachpermission_index_four(file)] + [file_eachpermission_index_five(file)]
+  modestring.join
+end
+
+total_blocks = File::Stat.new($0).blocks
+total = ['total'] + [' '] + [total_blocks]
+puts total.join
+
+file_names.each do |f|
+  details_filename = File::Stat.new(f) # 常に最大桁数(最大の桁を撮ってくる(今回の場合は4桁))に合わせて表示する
+  details = [file_eachpermission(f)] + [details_filename.nlink] + [Etc.getpwuid(details_filename.uid).name] + [Etc.getgrgid(details_filename.gid).name] + [details_filename.size] + [details_filename.mtime.strftime('%m %e %H:%M')] + [f]
+  puts details.join('    ')
+end
+
+# TAB_WIDTH = 8
+# COLUMN_COUNT = 3
+
+# file_names = Dir.glob("*").sort
+# file_names = Dir.glob("*").sort.reverse #-r
+# file_names = Dir.glob("*", File::FNM_DOTMATCH).sort #-a
+# file_names = Dir.glob("*", File::FNM_DOTMATCH).sort.reverse #-ar
+
+# longest_file_name = file_names.max_by(&:size).size
+# width = TAB_WIDTH * (longest_file_name / TAB_WIDTH.to_f).ceil
+
+# formatted_file_names = file_names.map {|file_name| file_name.ljust(width)}
+
+# row_count = (file_names.size.to_f / COLUMN_COUNT).ceil
+# nested_file_names = formatted_file_names.each_slice(row_count).to_a
+
+# nested_file_names = nested_file_names.map {|inner_file_names| inner_file_names.values_at(0...row_count)}
+# nested_file_names.transpose.each do |file|
+#   puts file.join(' ')
+# end
