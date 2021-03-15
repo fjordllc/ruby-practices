0ae4056e9eda9510e5992d7ff083a09b232b1220
rosh-1228 <sho.watanabe1228@gmail.com>
2021-03-15 23:52:19 +0900

    今日の日付の背景と文字色を反転させる仕様を実装

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,11 +1,11 @@
 require 'date'
 require 'optparse'
+require "colcolor"
 
 class Calender
   CALENDER_WEEK = ["日","月","火","水","木","金","土"]
   # カレンダーの日を取得
   def self.get_day(cal_day=1, cal_year, cal_month)
-    # Date.new(cal_year, cal_month, cal_day)
     @start_day = Date.new(cal_year, cal_month, 1)
     @start_day_num = @start_day.strftime("%e").to_i
     @end_day_num = Date.new(cal_year, cal_month, -1).strftime("%e").to_i
@@ -13,8 +13,11 @@ class Calender
   end
   # 日の間隔を調整
   def self.adjust_space(x)
-    #get_day(1, cal_year, cal_month).strftime("%e").rjust(x)
-    @start_day.strftime("%e").rjust(x)
+    if @cal_day == Date.today.strftime("%e").to_i then
+      @start_day.strftime("%e").rjust(x).colco(:black_white)
+    else
+      @start_day.strftime("%e").rjust(x)
+    end
   end
   # 月初日の表示位置を調整して表示
   def self.place_first_day
@@ -35,7 +38,7 @@ class Calender
     end
   end
   # カレンダーを表示
-  def self.show_calender(cal_year=Date.today.year, cal_month=Date.today.month)
+  def self.show_calender(cal_year, cal_month)
     get_day(1, cal_year, cal_month)
     # 月と年(西暦)を表示
     puts @start_day.strftime("     %-m月 %Y")
@@ -47,27 +50,49 @@ class Calender
       if x == @start_day_num then
         print(place_first_day)
       elsif x == @end_day_num && get_day(x, cal_year, cal_month).sunday?
-        puts @cal_day.strftime("%e")
+        if @cal_day == Date.today.strftime("%e").to_i then
+          puts @cal_day.strftime("%e").colco(:black_white)
+        else
+          puts @cal_day.strftime("%e")
+        end
       elsif x == @end_day_num || @cal_day.saturday? then
-        puts @cal_day.strftime("%e").rjust(3)
+        if @cal_day == Date.today.strftime("%e").to_i then
+          puts @cal_day.strftime("%e").rjust(3).colco(:black_white)
+        else
+          puts @cal_day.strftime("%e").rjust(3)
+        end
       elsif get_day(x, cal_year, cal_month).sunday?
-        print(@cal_day.strftime("%e"))
+        if @cal_day == Date.today.strftime("%e").to_i then
+          print(@cal_day.strftime("%e").colco(:black_white))
+        else
+          print(@cal_day.strftime("%e"))
+        end
       else
-        print(@cal_day.strftime("%e").rjust(3))
+        if @cal_day == Date.today then
+          print(@cal_day.strftime("%e").rjust(3).colco(:black_white))
+        else
+          print(@cal_day.strftime("%e").rjust(3))
+        end
       end
     end
   end
 end
 
-
-opt = OptionParser.new
+# オプション
 options = {}
-opt.on('-m month', Integer, 'Display the specified month.') {|v| options[:m] = v }
-opt.on('-y year', Integer, 'Display a calendar for the specified year.') {|v| options[:y] = v }
-opt.parse!(ARGV)
+OptionParser.new do |opt|
+  opt.on('-m [month]', Integer, 'Display the specified month.') {|v| options[:m] = v }
+  opt.on('-y [year]', Integer, 'Display a calendar for the specified year.') {|v| options[:y] = v }
+  opt.parse!(ARGV)
+end
 
+# オプション引数
 if options.empty? then
-  Calender.show_calender()
-else
+  Calender.show_calender(Date.today.year, Date.today.month)
+elsif options.has_key?(:y) && options.has_key?(:m) then
   Calender.show_calender(options[:y], options[:m])
+elsif options.has_key?(:y) then
+  Calender.show_calender(options[:y], Date.today.month)
+elsif options.has_key?(:m) then
+  Calender.show_calender(Date.today.year, options[:m])
 end
