cfe9c151de73147c3885d539c4bf34b4c8fa1b0d
ikukotky <ikuko_tky@yahoo.co.jp>
2021-03-27 21:00:30 +0900

    カレンダープログラム課題追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,27 +1,43 @@
-
 #!/usr/bin/env ruby
   
 require "date"
+require "optparse"
 
-head = Date.today.strftime("%m月  %Y")
-year = Date.today.year
-month = Date.today.month
-week = %w(日 月 火 水 木 金 土)
-firstday = Date.new(year,month,1).wday
-lastday = Date.new(year,month,-1).day
+params = ARGV.getopts("","y:#{Date.today.year}","m:#{Date.today.month}","d:#{Date.today.day}")
 
-puts head.center(20)
-puts week.join(" ")
-print " " * firstday
 
-day = firstday
-(1..lastday).each do |x|
-  print x.to_s.rjust(2) + " "
-  day = 1 + day
-  if day%7 == 0
+year = params.values[0].to_i
+month = params.values[1].to_i
+day = params.values[2].to_i
+
+first_day = Date.new(year,month,1)
+first_date = first_day.day
+last_day = Date.new(year,month,-1)
+last_date = last_day.day
+firstday_wday = first_day.wday
+
+hash = {}
+while first_date <= last_date
+  hash[:"#{first_date}"] = first_day.wday
+  first_date += 1
+  first_day += 1
+end
+
+puts ("#{month}"+ "月" + " " +  "#{year}").center(20)
+puts ["日","月","火","水","木","金","土"].join(" ")
+
+print "  " * firstday_wday + " "
+
+hash.each do |day, wday|
+  if wday %7 == 0
     print "\n"
   end
+  if day.length == 1
+    print " " + day.to_s + " "
+  elsif
+    print day.to_s + " "
+  end
 end
-if day %7 != 0
+if last_date != 0
   print "\n"
 end
