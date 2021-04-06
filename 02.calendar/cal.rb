dbdf8d68e70dbbce3532ae2ac65b5ced36173e7d
ikukotky <ikuko_tky@yahoo.co.jp>
2021-04-06 22:10:02 +0900

    カレンダープログラム修正(2回目)

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,14 +8,14 @@ params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
 year = params.values[0].to_i
 month = params.values[1].to_i
 
-first_date = Date.new(year,month,1)
-last_date = Date.new(year,month,-1)
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
 last_day = last_date.day
 firstday_wday = first_date.wday
 
 hash = {}
 1.upto(last_day) do |n|
-	hash[n] = (first_date + n - 1).wday
+  hash[n] = (first_date + n - 1).wday
 end
 
 puts ("#{month}月 #{year}").center(20)
@@ -23,11 +23,11 @@ puts "日 月 火 水 木 金 土"
 print "   " * firstday_wday
 
 hash.each do |day, wday|
-	if wday == 0 && day != 1
-		print "\n"
-	end
-	print day.to_s.rjust(2) + " "
+  if wday == 0 && day != 1
+    print "\n"
+  end
+  print day.to_s.rjust(2) + " "
 end
 if last_date != 0
-	print "\n"
+  print "\n"
 end
