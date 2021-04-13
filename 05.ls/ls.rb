e48f6c4003e0ba17a0f15d8d6b736ff36d02aec2
wind-up-bird <nagi.ooooo.1@gmail.com>
2021-04-13 12:04:42 +0900

    Convert file info from array to hash

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -38,7 +38,7 @@ def main
     puts "\n" if path_list.size > 1
 
     if options['l']
-      file_info_table = file_names.to_h { |file| [file, get_file_info(path, file)] }
+      file_info_table = file_names.to_h { |file_name| [file_name, get_file_info(path, file_name)] }
       output_with_l_option(file_info_table)
     else
       output(file_names)
@@ -62,20 +62,19 @@ def get_file_info(path, file_name)
 
   type = FILE_TYPES[fs.ftype]
   mode = convert_permission(fs.mode.to_s(8).to_i % 1000)
-  size = fs.size
-  nlink = fs.nlink
-
   date_time = fs.mtime.localtime
-  month = date_time.strftime('%b')
-  day = date_time.day
-  time = date_time.strftime('%H:%M')
 
-  uid = Etc.getpwuid(fs.uid).name
-  gid = Etc.getgrgid(fs.gid).name
-
-  block = fs.blocks
-
-  [type + mode, nlink, uid, gid, size, month, day, time, block]
+  {
+    type_mode: type + mode,
+    nlink: fs.nlink,
+    block: fs.blocks,
+    size: fs.size,
+    uid: Etc.getpwuid(fs.uid).name,
+    gid: Etc.getgrgid(fs.gid).name,
+    month: date_time.strftime('%b'),
+    day: date_time.day,
+    time: date_time.strftime('%H:%M')
+  }
 end
 
 def convert_permission(number)
@@ -101,28 +100,29 @@ def output(file_names)
 end
 
 def output_with_l_option(file_info_table)
-  file_attribute_count = 0
+  file_attr_count = 0
   file_info_table.each_value do |file_info|
-    file_attribute_count = file_info.size
+    file_attr_count = file_info.size
   end
 
-  max_length_list = Array.new(file_attribute_count) { 0 }
+  max_length_table = Hash.new(0)
 
   file_info_table.each_value do |file_info|
-    file_info.each_with_index do |item, index|
-      item_size = item.to_s.size
-      max_length_list[index] = item_size if max_length_list[index] < item_size
+    file_info.each do |attr_name, attr_value|
+      attr_size = attr_value.to_s.size
+      max_length_table[attr_name] = attr_size if max_length_table[attr_name] < attr_size
     end
   end
 
-  block_index = 8
-  total = file_info_table.each_value.sum { |file_info| file_info[block_index] }
-  puts "total #{total}"
+  block_total = file_info_table.each_value.sum { |file_info| file_info[:block] }
+  puts "total #{block_total}"
 
-  range_type_to_time = 0..7
-  format_str = "%#{max_length_list.slice(range_type_to_time).join('s %')}s %s"
+  display_items = %i[type_mode nlink uid gid size month day time]
+  format_str = display_items.map { |item| "%#{max_length_table[item]}s" }.join(' ')
+
   file_info_table.each do |file_name, file_info|
-    puts format(format_str, *file_info.slice(range_type_to_time), file_name)
+    attr_values = file_info.fetch_values(*display_items)
+    puts format("#{format_str} %s", *attr_values, file_name)
   end
 end
 
