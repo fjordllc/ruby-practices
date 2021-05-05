486d10c63c42d8fe363180cbded174a512520ce0
neutral2010 <kasiwamoti5@gmail.com>
2021-05-06 04:18:40 +0900

    自己レビュー後の全体修正、特に変数名を考え直しました。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,52 +1,15 @@
-# #!/usr/bin/env ruby
-# require 'date'
-#
-# today_day = Date.today
-# today_month = today_day.month.to_i
-# today_year = today_day.year.to_i
-# # p today_month
-# # p today_year
-#
-# first_day = Date.new(today_year, today_month, 1)
-# last_day = Date.new(today_year, today_month, -1)
-#
-# day_of_week = %w[日 月 火 水 木 金 土]
-# day_of_week.each do |x|
-#   print x
-#   print ' '
-# end
-# puts "\n"
-#
-# print "\s" * 3 if first_day.monday?
-# print "\s" * 6 if first_day.tuesday?
-# print "\s" * 9 if first_day.wednesday?
-# print "\s" * 12 if first_day.thursday?
-# print "\s" * 15 if first_day.friday?
-# print "\s" * 18 if first_day.saturday?
-#
-# (first_day..last_day).each do |everyday|
-#   print everyday.strftime('%e')
-#   print ' '
-#   if everyday.saturday?
-#     print "\n"
-#     format('%s', everyday)
-#   end
-# end
-# puts "\n"
-
 #!/usr/bin/env ruby
 require 'optparse'
 require 'date'
 
-options = ARGV.getopts("", "m:#{Date.today.month}", "y:#{Date.today.year}")
+options = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
+y_of_today = options['y'].to_i
+m_of_today = options['m'].to_i
 
-today_m = options['m'].to_i
-today_y = options['y'].to_i
+puts ("#{m_of_today}月"+ " " + "#{y_of_today}").center(20)
 
-puts ("#{today_m}月"+ " " + "#{today_y}").center(20)
-
-first_day = Date.new(today_y, today_m, 1)
-last_day = Date.new(today_y, today_m, -1)
+first_day = Date.new(y_of_today, m_of_today, 1)
+last_day = Date.new(y_of_today, m_of_today, -1)
 
 day_of_week = %w[日 月 火 水 木 金 土]
 day_of_week.each do |d|
@@ -67,7 +30,6 @@ print "\s" * 18 if first_day.saturday?
   print ' '
   if everyday.saturday?
     print "\n"
-    format('%s', everyday)
   end
 end
 puts "\n"
