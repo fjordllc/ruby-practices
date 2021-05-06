0e2dd8dfba32fde85c5df6a3b23de7b18900540b
muzina0 <2uta4sky6hima8kara@ezweb.ne.jp>
2021-05-06 10:27:51 +0900

    改行になっていないことの修正。15がFizz Buzzになっていないことの修正。インデントの修正。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 (1..20).each{|n|
-  if n % 3 == 0
-    print "Fizz "
+  if n % 15 == 0
+    print "FizzBuzz\n"
+  elsif n % 3 == 0
+    print "Fizz\n"
   elsif n % 5 == 0
-    print "Buzz "
-  elsif n % 15 == 0
-  print "FizzBuzz "
+    print "Buzz\n"
   else
-    printf("%d ", n)
+    printf("%d\n", n)
   end
 }
