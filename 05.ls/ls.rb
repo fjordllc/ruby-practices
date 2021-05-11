ae65746f06a86d541caa839cc0994f9363619339
Aseiide <eiseiide@gmail.com>
2021-05-11 16:11:39 +0900

    .と..を除外したファイルを出力

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,8 +4,11 @@ p dir = ARGV[0]
 p Dir.pwd
 p Dir.glob("*")
 
-
 # - ディレクトリ名を受け取る
 # - ディレクトリに含まれるファイル、ディレクトリを解析する
 # - ↑ で解析した内容をコンソールに出力する
 
+Dir.foreach('.') do |item|
+  next if item == '.' or item == '..'
+  puts item
+end
