903282f5218645a8d8ee99633ede6ffbf1425dae
Aseiide <eiseiide@gmail.com>
2021-05-12 18:58:44 +0900

    wip

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,10 +1,23 @@
-require "optparse"
+# frozen_string_literal: true
+
+require 'optparse'
 
 options = ARGV.getopts('a')
 if options['a']
-  print Dir.glob("*", File::FNM_DOTMATCH).sort
+  print Dir.glob('*', File::FNM_DOTMATCH).sort
   puts "\n"
 else
-  print Dir.glob("*").sort
-  puts "\n" 
+  print Dir.glob('*').sort
+  puts "\n"
 end
+
+permissions = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => 'r--',
+  '3' => '-w-',
+  '4' => '-wx',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}
