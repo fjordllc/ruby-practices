fbfdaa4aaa5ec709bd81261e8e5c7ad1d0ae6263
Aseiide <eiseiide@gmail.com>
2021-05-13 19:24:34 +0900

    wip

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,14 +3,16 @@
 require 'optparse'
 options = ARGV.getopts('a', 'l', 'r')
 
-contents = Dir.glob('*')
+# カレントディレクトリに含まれるファイルを配列で取得
+lists = Dir.glob('*')
+p lists.each_slice(3).to_a.transpose
 
 if options['a']
-  puts Dir.glob('*', File::FNM_DOTMATCH).sort.join(" ").rjust(15)
+  puts Dir.glob('*', File::FNM_DOTMATCH).sort.join(" ")
 elsif options['r']
-  puts contents.reverse.join(" ").rjust(15)
+  puts lists.reverse.join(" ")
 else
-  puts contents.join(" ").rjust(15)
+  puts lists.join(" ")
 end
 
 permissions = {
@@ -23,3 +25,15 @@ permissions = {
   '6' => 'rw-',
   '7' => 'rwx'
 }
+
+file_type = {
+  "file" => '-',
+  "directory" => 'd',
+  "symblic-link" => 'l'
+}
+
+lists.each do |file|
+  p file
+end
+
+filestat = File.stat("#{path}#{file}")
