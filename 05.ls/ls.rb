c0afa2a6374b8a639be106cc3e2bbae4cafddc1b
Aseiide <eiseiide@gmail.com>
2021-05-14 21:39:15 +0900

    wip

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -7,7 +7,7 @@ options = ARGV.getopts('a', 'l', 'r')
 
 # カレントディレクトリに含まれるファイルを配列で取得
 p lists = Dir.glob('*')
-p lists.each_slice(3).to_a.transpose
+# p lists.each_slice(3).to_a.transpose
 
 if options['a']
   puts Dir.glob('*', File::FNM_DOTMATCH).sort.join(" ")
@@ -26,30 +26,31 @@ file_type = {
 # file_status = File.stat("#{path}#{file}")
 
 def file_mode(mode)
-  s = []
-  mode.each do |i|
-    permissions = {
-      '0' => '---',
-      '1' => '--x',
-      '2' => 'r--',
-      '3' => '-w-',
-      '4' => '-wx',
-      '5' => 'r-x',
-      '6' => 'rw-',
-      '7' => 'rwx'
-    }
-    s << permissions[i] 
-  end
-  s.join
+  permissions = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => 'r--',
+    '3' => '-w-',
+    '4' => '-wx',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }
+  mode.to_s(8)[3, 5].chars.map {|i| permissions[i]}.join
 end
 
-lists.each do |list|
-  file = File.stat(list) # File::Statオブジェクトを作成
-  file_type = file.ftype
-  file_uid = Etc.getpwuid(file.uid).name
-  file_gid = Etc.getgrgid(file.gid).name
-  file_size = file.size
-  time_stamp = file.mtime.strftime("%m %d %H:%M")
-  file_name = list
-  puts "#{file_uid} #{file_gid} #{file_size} #{time_stamp} #{file_name}" 
+
+attribute = nil
+def attribute(attribute)
+  lists.each do |list|
+    file = File.stat(list) # File::Statオブジェクトを作成
+    file_mode = filemode(file.mode)
+    file_type = file.ftype
+    file_uid = Etc.getpwuid(file.uid).name
+    file_gid = Etc.getgrgid(file.gid).name
+    file_size = file.size
+    time_stamp = file.mtime.strftime("%m %d %H:%M")
+    file_name = list
+    puts "#{file_uid} #{file_gid} #{file_size} #{time_stamp} #{file_name}" 
+  end
 end
