d8b3dbc39dbb2b42621ced77eb765bb91aeafc3e
Gen Tamura <gen.tamura84@gmail.com>
2021-05-16 13:49:15 +0900

    Update review points

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,50 +10,34 @@ params = {}
 opt.on('-y YEAR') {|v| params[:year] = v }
 opt.on('-m MONTH') {|v| params[:month] = v }
 
-begin
-  opt.parse!(ARGV)
-rescue OptionParser::ParseError
-end
+opt.parse!(ARGV)
 
 today = Date.today
 
 year = params[:year] ? params[:year].to_i : today.year
 month = params[:month] ? params[:month].to_i : today.month
-last_mday = Date.new(year, month, -1).mday
-SAT = 6
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
 
 def inverse_color(str)
   "\e[30m\e[47m#{str}\e[0m"
 end
 
-
 puts "      #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
 
-(1..last_mday).each do |v|
-  date = Date.new(year, month, v)
+if first_date.cwday < 7
+  print "   " * first_date.cwday
+end
 
-  mday = if v.to_s.length == 2
-    "#{v}"
-  else
-    " #{v}"
-  end
-
+(first_date..last_date).each do |date|
+  mday = date.mday.to_s.rjust(2)
   mday = inverse_color(mday) if date == today
 
-  formatted_mday = if (date.cwday == SAT || last_mday == v)
+  formatted_mday = if (date.saturday? || last_date.mday == date.mday)
     mday << "\n"
   else
     mday << " "
-  end
-
-  if v === 1
-    first_week_padding_count = 7 - (7 - date.cwday)
-
-    if first_week_padding_count < 7
-      first_week_padding = first_week_padding_count.times.map{|n| " " * 3 }.join('')
-      formatted_mday = first_week_padding << formatted_mday
-    end
   end
 
   print formatted_mday
