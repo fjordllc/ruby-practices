5961392ce3262d8c2f190bca6d5d4306d152acc8
Ishimoto Koji <koji.ishimoto@gmail.com>
2021-05-16 20:43:01 +0900

    scoreをscoresに、pinsをscoreに名前変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -3,26 +3,26 @@
 require 'optparse'
 
 opt = OptionParser.new
-score = opt.parse(ARGV)[0].split(',')
+scores = opt.parse(ARGV)[0].split(',')
 
 total = 0
 frame = 1
 second = false
 
-score.map! { |i| i == 'X' ? 10 : i }
+scores.map! { |i| i == 'X' ? 10 : i }
 
-score.each_with_index do |pins, index|
-  if pins == 10
+scores.each_with_index do |score, index|
+  if score == 10
     total += 10
-    total += score[index + 1].to_i + score[index + 2].to_i unless frame >= 10
+    total += scores[index + 1].to_i + scores[index + 2].to_i unless frame >= 10
     second = false
     frame += 1
   else
-    total += pins.to_i
+    total += score.to_i
     if second == false
       second = true
     else
-      total += score[index + 1].to_i if score[index - 1].to_i + pins.to_i == 10 && frame < 10
+      total += scores[index + 1].to_i if scores[index - 1].to_i + score.to_i == 10 && frame < 10
       second = false
       frame += 1
     end
