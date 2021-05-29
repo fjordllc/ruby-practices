8ecd9cb0775ef6c2bdc90d100cdced40d3a8332f
neutral2010 <kasiwamoti5@gmail.com>
2021-05-30 07:16:29 +0900

    スコア時の加算実装

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -25,6 +25,11 @@ frames.each_with_index do |frame, i|
   if frames[i][0] == 10
     p frames[i]
     point += 10 + frames[i + 1].sum
+  elsif frame.sum == 10
+    puts "#{frame}です。"
+    #次の配列(frame)の一投目を加算
+    point += 10 + frames[i + 1][0]
+    p frames[i + 1]
   else
     point += frames[i].sum
     p frames[i]
