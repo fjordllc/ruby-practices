760a741d77e5d147a88a8b24632553f8a5b31cc9
neutral2010 <kasiwamoti5@gmail.com>
2021-06-03 21:23:14 +0900

    第一段階完成（計算は合っている）

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -3,6 +3,7 @@
 score = ARGV[0]
 scores = score.split(',')
 shots = []
+
 scores.each do |s|
   if s == 'X'
     shots << 10
@@ -13,50 +14,39 @@ scores.each do |s|
     shots << s.to_i
   end
 end
-# p shots
+each_shot_score_sum = shots.sum
 
 frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
 
-point = 0
-frames.each_with_index do |frame, i|
-  if frames[i][0] == 10
-    # p frames[i]
-    point += 10 + frames[i + 1].sum
-  elsif frame.sum == 10
-    # puts "#{frame}です。"
-    #次の配列(frame)の一投目を加算
-    point += 10 + frames[i + 1][0]
-    # p frames[i + 1]
+#加算について
+add_point = 0
+
+frames[0..7].each_with_index do |frame, n|
+  if frames[n].sum == 10 && frames[n][0] != 10
+    add_point += frames[n + 1][0]
+  elsif frames[n][0] == 10 && frames[n + 1][0] == 10
+    add_point += frames[n + 1][0] + frames[n + 2][0]
+  elsif frames[n][0] == 10
+    add_point += frames[n + 1].sum
   else
-    point += frames[i].sum
+    add_point += 0
   end
 end
-puts point
 
-# ストライクの次がまたストライク対応
-# 加算用の配列
-shots_for_add_x = []
-scores.each do |s|
-  if s == 'X'
-    shots_for_add_x << 10
-  elsif s == 'S'
-    shots_for_add_x << 10
-  else
-    shots_for_add_x << s.to_i
-  end
+add_point_9 = 0
+
+if frames[8].sum == 10 && frames[8][0] != 10
+  add_point_9 += frames[9][0]
+elsif frames[8][0] == 10 && frames[9][0] == 10
+  add_point_9 += frames[9][0] + frames[10][0]
+elsif frames[8][0] == 10 && frames[9][0] != 10
+  add_point_9 += frames[9].sum
+else
+  add_point_9 += 0
 end
-p shots_for_add_x
 
-# 加算分の計算
-add_x_point = 0
-shots_for_add_x.each_with_index do |x, i| # xはshots_for_add_x配列の要素
-  if shots_for_add_x[i] == 10 && shots_for_add_x[i + 1] == 10
-    add_x_point += shots_for_add_x[i + 2]
-  end
-end
-puts add_x_point
+puts each_shot_score_sum + add_point + add_point_9
 
-puts point + add_x_point
