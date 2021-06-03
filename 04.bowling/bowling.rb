a6c860086ea135632bf6c4188c13874da2a391ff
neutral2010 <kasiwamoti5@gmail.com>
2021-06-04 00:13:26 +0900

    robocop指摘分修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -24,7 +24,7 @@ end
 #加算について
 add_point = 0
 
-frames[0..8].each_with_index do |frame, n|
+frames[0..8].each_with_index do |_frame, n|
   if frames[n].sum == 10 && frames[n][0] != 10
     add_point += frames[n + 1][0]
   elsif frames[n][0] == 10 && frames[n + 1][0] == 10
@@ -35,18 +35,6 @@ frames[0..8].each_with_index do |frame, n|
     add_point += 0
   end
 end
-
-# add_point_9 = 0
-#
-# if frames[8].sum == 10 && frames[8][0] != 10
-#   add_point_9 += frames[9][0]
-# elsif frames[8][0] == 10 && frames[9][0] == 10
-#   add_point_9 += frames[9][0] + frames[10][0]
-# elsif frames[8][0] == 10 && frames[9][0] != 10
-#   add_point_9 += frames[9].sum
-# else
-#   add_point_9 += 0
-# end
 
 puts each_shot_score_sum + add_point
 
