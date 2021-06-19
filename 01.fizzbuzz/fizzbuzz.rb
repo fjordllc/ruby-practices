67d99822f7656f112e26c7be14b7694ce5f2d2ba
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2021-06-19 20:39:37 +0900

    fizzbuzz実行するファイルを追加

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+20.times do |i|
+  i =+ 1 + i
+
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
