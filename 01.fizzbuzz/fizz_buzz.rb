ed184de68e6262e84db08e93d31ed32cc57d34b2
ShotaTsuru <syota.crane@gmail.com>
2021-06-29 06:24:47 +0900

    eachを使用した実装

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -13,3 +13,18 @@ while x < 20
     puts x
   end
 end
+
+# eachをつかった実装
+
+array = (1..20).to_a
+array.each do |x|
+  if x % 15 == 0
+    puts "FizzBuzz"
+  elsif x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  else
+    puts x
+  end
+end
