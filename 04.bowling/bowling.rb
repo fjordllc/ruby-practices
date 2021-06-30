2cc604d5be0f401484fae9c8744c953e38335839
neutral2010 <kasiwamoti5@gmail.com>
2021-06-30 10:39:40 +0900

    プログラムの構成的に大幅修正
    インスタンス変数を使うのではなく、
    ローカル変数を使ってデータを受け取るために
    メソッドの引数と戻り値を利用しました。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,44 +1,46 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def main(score)
-  scores = score.split(',')
+def main(score_text)
+  scores = score_text.split(',')
   shots = []
-  scores.each do |s|
-    case s
-    when 'X'
+  scores.each do |score|
+    if score == 'X'
       shots << 10
       shots << 0
-    when 'S'
-      shots << 10
     else
-      shots << s.to_i
+      shots << score.to_i
     end
   end
   each_shot_score_sum = shots.sum
 
-  @frames = []
+  frames = []
   shots.each_slice(2) do |s|
-    @frames << s
+    frames << s
   end
-  add_point
-  each_shot_score_sum + @add_point
+  bonus_point = calc_bonus_point(frames)
+  each_shot_score_sum + bonus_point
 end
 
-def add_point
-  @add_point = 0
-
-  @frames[0..8].each_with_index do |_frame, n|
-    @add_point += if (@frames[n].sum == 10) && (@frames[n][0] != 10)
-                    @frames[n + 1][0]
-                  elsif (@frames[n][0] == 10) && (@frames[n + 1][0] == 10)
-                    @frames[n + 1][0] + @frames[n + 2][0]
-                  elsif @frames[n][0] == 10
-                    @frames[n + 1].sum
-                  else
-                    0
-                  end
+def calc_bonus_point(frames)
+  bonus_point = 0
+  frames[0..8].each_with_index do |_frame, n|
+    spare = frames[n].sum == 10 && frames[n][0] != 10
+    strike = frames[n][0] == 10
+    strike_after_strike = frames[n + 1][0] == 10
+    bonus_point += if spare
+                     frames[n + 1][0]
+                   elsif strike
+                     if strike_after_strike
+                       frames[n + 1][0] + frames[n + 2][0]
+                     else
+                       frames[n + 1].sum
+                     end
+                   else
+                     0
+                   end
   end
+  bonus_point
 end
 
 puts main(ARGV[0]) if __FILE__ == $PROGRAM_NAME
