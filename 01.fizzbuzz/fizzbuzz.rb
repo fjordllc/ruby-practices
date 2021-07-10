8675088f915055e70ba8b6464cd24bd4026e0ea8
mami-inuzuka <inuzuka.mami@gmail.com>
2021-07-10 19:40:57 +0900

    条件分岐の順序を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,12 +2,12 @@ num = 0
 while num < 20
   num = num + 1
   case
+  when num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
   when num % 3 == 0
     puts "Fizz"
   when num % 5 == 0
     puts "Buzz"
-  when num % 3 == 0 || num%5 == 0
-    puts "FizzBuzz"
   else
     puts num
   end
