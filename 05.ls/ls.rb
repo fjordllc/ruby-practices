0e4692b41ccde0bebb8808e715d29f15da07b9ca
neutral2010 <kasiwamoti5@gmail.com>
2021-07-14 04:15:38 +0900

    オプションなし・-aオプション、とりあえず完成
    それぞれ単独でなら表示に成功。リファクタリングの余地は大。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,43 +1,66 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# オプションなし
-dir_and_file_names = Dir.glob('*')
-p dir_and_file_names
+puts "オプションなし"
 
-# dir_and_file_namesは配列
+dir_and_file_names = Dir.glob('*')
+
 # 列数を定数に
 NumberOfColumn = 3
 # 配列の要素数取得、列数で割って行数を出す。
-NumberOfLines = dir_and_file_names.length / NumberOfColumn
-p NumberOfLines
-# dir_and_file_nameの先頭からNumberOfLinesずつ取得したデータで配列を作る。
-# それはNumberOfColumn繰り返される。
-# できた配列を、要素数 == NumberOfColumnの配列に入れこみ、「配列の配列」を作る
-# dir_and_file_nameがNumberOfLinesで割り切れる時とそうでない時で条件分岐。
-# NumberOfLinesで割り切れる時はNumberOfLinesで、それ以外はNumberOfLinesで割る
-# result = []
-a = dir_and_file_names.each_slice(NumberOfLines + 1).to_a
+number_of_lines = dir_and_file_names.length / NumberOfColumn
 
-FORMAT = "%20s"
-# a[0].zip(a[1], a[2]) do |array|
-#   # printf FORMAT, array
-#   # result << array #戻り値として利用できる
-# end
+# dir_and_file_nameがnumber_of_linesで割り切れる時とそうでない時で条件分岐。
+# number_of_linesで割り切れる時はnumber_of_linesで、それ以外はnumber_of_linesで割る
 
-#ブロックは渡さないので、戻り値を利用できる
-# results = a[0].zip(a[1], a[2]).flatten
-#indexが3でわってあまりがだったら、改行を加える それ以外は加えない。
-# フォーマットを適応させる
-
-results = a[0].zip(a[1], a[2])
+if dir_and_file_names.length % number_of_lines == 0
+  a = dir_and_file_names.each_slice(number_of_lines).to_a
+  FORMAT = "%-20s"
+  results = a[0].zip(a[1], a[2])
   results.each do |result|
     result.each do |fed|
       printf FORMAT, fed
     end
     puts "\n"
   end
+else
+  a = dir_and_file_names.each_slice(number_of_lines + 1).to_a
+  FORMAT = "%-20s"
+  results = a[0].zip(a[1], a[2])
+  results.each do |result|
+    result.each do |fed|
+      printf FORMAT, fed
+    end
+    puts
+  end
+end
 
-# -a
-# all_dir_and_file_name = Dir.glob("*", File::FNM_OUTMATCH)
-# p all_dir_and_file_name
+puts "\n"
+puts "-aオプション"
+
+all_dir_and_file_names = Dir.glob("*", File::FNM_DOTMATCH)
+
+# NumberOfColumn = 3
+number_of_lines = all_dir_and_file_names.length / NumberOfColumn
+
+if all_dir_and_file_names.length % number_of_lines == 0
+  a = all_dir_and_file_names.each_slice(number_of_lines).to_a
+  # FORMAT = "%-20s"
+  results = a[0].zip(a[1], a[2])
+  results.each do |result|
+    result.each do |fed|
+      printf FORMAT, fed
+    end
+    puts "\n"
+  end
+else
+  a = all_dir_and_file_names.each_slice(number_of_lines + 1).to_a
+  # FORMAT = "%-20s"
+  results = a[0].zip(a[1], a[2])
+  results.each do |result|
+    result.each do |fed|
+      printf FORMAT, fed
+    end
+    puts
+  end
+end
