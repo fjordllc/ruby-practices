cf57eca8687b3bd839e8895e7c12d17c7f26e3e9
uchihiro <tohchi4122@gmail.com>
2021-07-14 22:08:51 +0900

    オプションの指定に関するコード・日付の繰り返しに関するコード等を修正
    5行目にあったopt = OptionParser.newを削除しました。7行目以降のオプションの指定に関するコードを簡略化しました。24行目以降の日付の繰り返しに関するコードの重複していた部分を削除しました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,19 +3,16 @@
 require "date"
 require 'optparse'
 
-opt = OptionParser.new
 params = ARGV.getopts("y:", "m:")
-if params["y"] == nil && params["m"] == nil
+if params["y"] == nil
   year = Date.today.year
-  month = Date.today.month
-elsif params["y"] == nil
-  year = Date.today.year
-  month = params["m"].to_i
-elsif params["m"] == nil
-  year = params["y"].to_i
-  month = Date.today.month
 else
   year = params["y"].to_i
+end
+
+if params["m"] == nil
+  month = Date.today.month
+else
   month = params["m"].to_i
 end
 
@@ -25,26 +22,16 @@ puts "#{first_day.month}月 #{first_day.year}".center(20)
 puts ["日", "月", "火", "水", "木", "金", "土"].join(" ")
 
 (first_day..last_day).each do |range_day|
-  if range_day.day == 1 && range_day.wday == 6
+  if range_day.day == 1
     (3 * range_day.wday).times do
       print " "
     end
-    print " #{range_day.day}\n"
-  elsif range_day.day == 1
-    (3 * range_day.wday).times do
-      print " "
-    end
-    print " #{range_day.day} "
-  elsif range_day.day.to_s.length == 1 && range_day.wday == 6 
-    print " #{range_day.day}\n"
-  elsif range_day.day.to_s.length == 1
-    print " #{range_day.day} "
-  elsif range_day.wday == 6
-    print "#{range_day.day}\n"
-  elsif range_day.day == last_day.day
-    print "#{range_day.day}\n"
-  else
-    print range_day.day
-    print " "
+  end
+  print "#{range_day.day.to_s.rjust(2)} "
+  if range_day.wday == 6
+    print "\n"
+  end
+  if range_day.day == last_day.day
+    print "\n"
   end
 end
