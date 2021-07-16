ceb22e55ab6f593be3c43605e9ef7f67e61ba063
mami-inuzuka <inuzuka.mami@gmail.com>
2021-07-16 21:53:35 +0900

    記述をシンプルにするため、冗長な記述を書き換える

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -8,46 +8,28 @@ opt.on('-m [VAL]') {|v| params[:m] = v }
 opt.parse!(ARGV,into: params)
 
 #yの引数が設定されていなかったときにyearには本日の西暦を入れる
-if params[:y] != nil
-  year = params[:y].to_i
-else
-  year = Date.today.year.to_i
-end
+year = params[:y].nil? ? Date.today.year : params[:y].to_i
 
 #mの引数が設定されていなかったときにmonthには本日の月を入れる
-if params[:m] != nil
-  month = params[:m].to_i
-else
-  month = Date.today.month.to_i
-end
+month = params[:m].nil? ? Date.today.month : params[:m].to_i
 
-print "      #{month}月 #{year}\n"
+print "#{month}月 #{year}".center(20)
+print "\n"
 print("日 ", "月 ", "火 ", "水 ", "木 ", "金 ", "土","\n")
 
-count = 1
-last = Date.new(year, month, -1).day
+last_day = Date.new(year, month, -1).day
 first_wday = Date.new(year, month, 1).wday
 
-while count <= last
-  date = Date.new(year, month, count)
+(1..last_day).each do |day|
+  date = Date.new(year, month, day)
   day = date.day
 
   #1日の曜日を確認して冒頭の位置を合わせる
-  if count == 1
+  if day == 1
     start_position = 3 * first_wday
-    first_space = ''
-    start_position.times do
-      first_space = first_space + ' '
-    end
+    first_space = ' ' * start_position
   else
     first_space = ''
-  end
-  
-  #一桁の場合は数字の前に余白をつける
-  if day.to_s.length == 1
-    space = ' '
-  else
-    space = ''
   end
 
   #今日の日付に色をつける
@@ -59,12 +41,10 @@ while count <= last
     color_end=""
   end 
 
+  print("#{color_start}#{first_space}#{day.to_s.rjust(2)}#{color_end} ")
+
   #土曜日の場合は改行を入れる
   if date.saturday?
-    print("#{color_start}#{first_space}#{space}#{day}#{color_end} \n")
-  else
-    print("#{color_start}#{first_space}#{space}#{day}#{color_end} ")
+    print(" \n")
   end
-
-  count = count + 1
 end
