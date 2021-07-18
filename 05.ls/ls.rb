a12719fe30f3d630166c65d7154760ca253e1e73
kekosh <kenta0540@gmail.com>
2021-07-18 23:20:54 +0900

    オプションなし時のファイルリスト取得処理まで実装

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,10 +6,33 @@ require 'optparse'
 # オプション設定
 opt = OptionParser.new
 options = {}
-opt.on('-a [ALL]'){|v| options['-a'] = v}
-opt.on('-l [LIST_VERTICAL]'){|v| options['-l'] = v}
-opt.on('-r [REVERSE]'){|v| options['-r'] = v}
+opt.on('-a'){|v| options[:all] = v}
+opt.on('-l'){|v| options[:list] = v}
+opt.on('-r'){|v| options[:reverse] = v}
 
 # 引数リストARGVをパースして各オプションブロックを実行
 opt.parse!{ARGV}
-p options
+
+# オプションなし
+def show_file_list_of_default
+  items = Dir.glob("*").sort.map{|item| "#{item}".ljust(16)}
+
+  
+end
+
+# -aの場合(すべてのファイル)
+def show_file_list_of_all
+  p "show_file_list_of_all"
+end
+
+# -lの場合(ファイル詳細情報付き)
+def show_file_list_with_detail_info
+  p "show_file_list_with_detail_info"
+end
+
+# -rの場合(ソート逆順)
+def change_sort_reverse
+  p "change_sort_reverse"
+end
+
+show_file_list_of_default
