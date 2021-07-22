e3aa5bed28f96fc19fa8b2a8e8e91e94576a3715
takashima <takashima@catallaxy.me>
2021-07-22 15:29:44 +0900

    Fizzbuzz課題の提出

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,17 @@
+#! /usr/bin/env ruby
+
+#first commitで違うアイコンになってしまったのでもう一度commit
+(1..20).each do |x|
+	unless x%3==0||x%5==0
+		puts x
+	end
+	if x%3==0&&x%5==0
+		puts "FizzBuzz"
+	end
+	if x%3==0
+		puts "Fizz" unless x%5==0
+	end
+	if x%5==0
+			puts "Buzz" unless x%3==0
+	end
+end
