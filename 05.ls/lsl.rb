d6ed5375e6d283077a5c82e7c1175135424a491e
neutral2010 <kasiwamoti5@gmail.com>
2021-07-25 07:23:46 +0900

    ファイルタイプとパーミッションの変換完成。
    シンボリックリンクファイルがある時の表記がまだ。

--- 05.ls/lsl.rb
+++ 05.ls/lsl.rb
@@ -2,17 +2,31 @@
 # frozen_string_literal: true
 require 'etc'
 
-dir_and_file_name = Dir.glob('*')
+dir_and_file_names = Dir.glob('*')
 # ファイルの情報を取得するためのFile::Statクラスのインスタンス生成
-file_number = dir_and_file_name.length
+file_number = dir_and_file_names.length
 puts "total #{file_number}"
 
-FORMAT = "%10s%-6s %4s %8s %8s %7s %7s %-16s \n"
+FORMAT = "%2s%-6s %2s %8s %8s %7s %7s %-16s \n"
 a = Time.now
 file_number.times do |n|
-  ls_l_stat = File::Stat.new(dir_and_file_name[n])
+  ls_l_stat = File::Stat.new(dir_and_file_names[n])
   ft = ls_l_stat.ftype
+    case ft
+      when 'file'
+        ft.replace '-'
+      when 'directory'
+        ft.replace 'd'
+      when 'link'
+        ft.replace 'l'
+    end
+
   m = ls_l_stat.mode.to_s(8) # 8進数に変換
+  convert_pamissions = {'0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx','4' => 'r--',
+                        '5' => '---', '6' => 'rw-', '7' => 'rwx'}
+  converted_pamission = convert_pamissions[m[-3]] + convert_pamissions[m[-2]]
+                              + convert_pamissions[m[-1]]
+
   nl = ls_l_stat.nlink
   u_name = Etc.getpwuid(ls_l_stat.uid).name
   g_name = Etc.getgrgid(ls_l_stat.gid).name
@@ -22,11 +36,10 @@ file_number.times do |n|
        else
          ls_l_stat.mtime.strftime('%_m %_d %_5Y')
        end
-  file_name = dir_and_file_name[n]
+  file_name = dir_and_file_names[n]
   f = File.basename(file_name)
-  printf FORMAT, ft, m, nl, u_name, g_name, s, mt, f
+  printf FORMAT, ft, converted_pamission, nl, u_name, g_name, s, mt, f
 end
-
 
 # ファイルタイプを表すメソッド
 
