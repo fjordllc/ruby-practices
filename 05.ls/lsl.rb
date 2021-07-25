54ab9cf6d3c39608b16abf6c03c0c779d3aa608d
neutral2010 <kasiwamoti5@gmail.com>
2021-07-25 19:56:57 +0900

    total完成

--- 05.ls/lsl.rb
+++ 05.ls/lsl.rb
@@ -3,11 +3,22 @@
 require 'etc'
 
 dir_and_file_names = Dir.glob('*')
-# ファイルの情報を取得するためのFile::Statクラスのインスタンス生成
-file_number = dir_and_file_names.length
-puts "total #{file_number}"
+# dir_and_file_names = Dir.glob('*', File::FNM_DOTMATCH)
+# ファイルの情報を取得するためのFile::Statクラスのインスタンス生成cd
 
-FORMAT = "%2s%-6s %2s %8s %8s %7s %7s %-16s \n"
+FORMAT = "%2s%-6s %3s %8s %8s %7s %7s %-16s \n"
+
+file_number = dir_and_file_names.length
+
+array_for_blocks = []
+file_number.times do |n|
+  ls_l_stat = File::Stat.new(dir_and_file_names[n])
+  b = ls_l_stat.blocks
+  array_for_blocks << b
+end
+total_number = array_for_blocks.sum
+puts "total #{total_number}"
+
 a = Time.now
 file_number.times do |n|
   ls_l_stat = File::Stat.new(dir_and_file_names[n])
@@ -23,9 +34,8 @@ file_number.times do |n|
 
   m = ls_l_stat.mode.to_s(8) # 8進数に変換
   convert_pamissions = {'0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx','4' => 'r--',
-                        '5' => '---', '6' => 'rw-', '7' => 'rwx'}
-  converted_pamission = convert_pamissions[m[-3]] + convert_pamissions[m[-2]]
-                              + convert_pamissions[m[-1]]
+                        '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}
+  converted_pamission = convert_pamissions[m[-3]] + convert_pamissions[m[-2]] + convert_pamissions[m[-1]]
 
   nl = ls_l_stat.nlink
   u_name = Etc.getpwuid(ls_l_stat.uid).name
@@ -40,7 +50,3 @@ file_number.times do |n|
   f = File.basename(file_name)
   printf FORMAT, ft, converted_pamission, nl, u_name, g_name, s, mt, f
 end
-
-# ファイルタイプを表すメソッド
-
-# パーミションを表すメソッド
