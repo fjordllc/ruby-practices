1ed571f7c2c36725a4a5aff3a490334c8528c7ae
neutral2010 <kasiwamoti5@gmail.com>
2021-08-01 21:53:45 +0900

    完成

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-require 'optparse'
-require 'etc'
 
+require 'etc'
+require 'optparse'
 options = ARGV.getopts('a', 'r', 'l')
 
+# プログラム内で使用する定数の定義
 FORMAT = '%-20s'
-LFORMAT = "%s%-6s %3s %-8s %-8s %6s %7s %-16s \n"
-NumberOfColumn = 3
+L_FORMAT = "%s%-6s %3s %-8s %-8s %6s %7s %-16s \n"
+NUMBER_COLUMNS = 3
 
+# 出力したいデータの取得
 dir_and_file_names =
   if options['a'] && options['r']
     Dir.glob('*', File::FNM_DOTMATCH).reverse
@@ -20,9 +22,11 @@ dir_and_file_names =
     Dir.glob('*')
   end
 
-if options['l']
-  file_number = dir_and_file_names.length
+# 出力に関するプログラム
+present_time = Time.now # time_stampに関わる
 
+if options['l'] # -lオプション時の出力
+  file_number = dir_and_file_names.length
   array_for_blocks = []
   file_number.times do |n|
     ls_l_stat = File::Stat.new(dir_and_file_names[n])
@@ -32,56 +36,45 @@ if options['l']
   total_number = array_for_blocks.sum
   puts "total #{total_number}"
 
-  a = Time.now
   file_number.times do |n|
     ls_l_stat = File.lstat(dir_and_file_names[n])
-    ft = ls_l_stat.ftype
-    case ft
-    when 'link'
-      ft.replace 'l'
-    when 'file'
-      ft.replace '-'
-    when 'directory'
-      ft.replace 'd'
-    end
-
-    m = ls_l_stat.mode.to_s(8) # 8進数に変換
-    convert_pamissions = { '0' => '---', '1' => '--x', '2' => '-w-',
-                           '3' => '-wx', '4' => 'r--', '5' => 'r-x',
-                           '6' => 'rw-', '7' => 'rwx' }
-    converted_pamission =
-      convert_pamissions[m[-3]] + convert_pamissions[m[-2]] + convert_pamissions[m[-1]]
-
-    nl = ls_l_stat.nlink
-    u_name = Etc.getpwuid(ls_l_stat.uid).name
-    g_name = Etc.getgrgid(ls_l_stat.gid).name
-    s = ls_l_stat.size
-    mt = if (a.month + 6) > ls_l_stat.mtime.month && (a.month - 6) < ls_l_stat.mtime.month
-           ls_l_stat.mtime.strftime('%_m %_d %H:%M')
-         else
-           ls_l_stat.mtime.strftime('%_m %_d %_5Y')
-         end
+    convert_filetypes = { 'link' => 'l', 'file' => '-', 'directory' => 'd' }
+    converted_filetype = convert_filetypes[ls_l_stat.ftype]
+    m = ls_l_stat.mode.to_s(8)
+    convert_permissions = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' =>
+      'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
+    converted_permission = convert_permissions[m[-3]] + convert_permissions[m[-2]] + convert_permissions[m[-1]]
+    symbolic_link = ls_l_stat.nlink
+    user_name = Etc.getpwuid(ls_l_stat.uid).name
+    group_name = Etc.getgrgid(ls_l_stat.gid).name
+    file_size = ls_l_stat.size
+    time_stamp = if (present_time.month + 6) > ls_l_stat.mtime.month && (present_time.month - 6) < ls_l_stat.mtime.month
+                   ls_l_stat.mtime.strftime('%_m %_d %H:%M')
+                 else
+                   ls_l_stat.mtime.strftime('%_m %_d %_5Y')
+                 end
     file_name = dir_and_file_names[n]
-    f = File.basename(file_name)
-    printf LFORMAT, ft, converted_pamission, nl, u_name, g_name, s, mt, f
+    filename_for_format = File.basename(file_name)
+    printf L_FORMAT, converted_filetype, converted_permission, symbolic_link, user_name, group_name,
+           file_size, time_stamp, filename_for_format
   end
 
 else
-  number_of_lines = dir_and_file_names.length / NumberOfColumn
-  if dir_and_file_names.length % number_of_lines == 0
-    a = dir_and_file_names.each_slice(number_of_lines).to_a
-    results = a[0].zip(a[1], a[2])
-    results.each do |result|
-      result.each do |fed|
+  # -lオプションがない時の出力
+  number_of_lines = dir_and_file_names.length / NUMBER_COLUMNS
+  if (dir_and_file_names.length % number_of_lines).zero?
+    array_for_output = dir_and_file_names.each_slice(number_of_lines).to_a
+    array_for_output[0].zip(array_for_output[1], array_for_output[2]).each do |row|
+      row.each do |fed|
         printf FORMAT, fed
       end
-      puts "\n"
+      puts
     end
   else
-    a = dir_and_file_names.each_slice(number_of_lines + 1).to_a
-    results = a[0].zip(a[1], a[2])
-    results.each do |result|
-      result.each do |fed|
+    array_for_output = dir_and_file_names.each_slice(number_of_lines + 1).to_a
+    results = array_for_output[0].zip(array_for_output[1], array_for_output[2])
+    results.each do |row|
+      row.each do |fed|
         printf FORMAT, fed
       end
       puts
