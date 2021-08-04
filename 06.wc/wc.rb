b31c28e46f4735cc3ce0791c44ee61b59c1b1341
neutral2010 <kasiwamoti5@gmail.com>
2021-08-04 21:41:37 +0900

    データ取得と出力まで実装済み

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,1 +1,15 @@
-Hello wc!
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+FORMAT = "%7s %7s %7s %-10s"
+file_name = 'wc.rb'
+# file_name = 'sample.txt'
+
+ft = File::Stat.new(file_name)
+count_lines = IO.readlines(file_name).length #行数
+read_for_count_words = File.read(file_name) # 単語数
+count_words = read_for_count_words.split(/\s+/).size
+count_bit = ft.size# バイト数
+file_basename = File.basename(file_name) # ファイルネーム
+
+printf FORMAT, count_lines, count_words, count_bit, file_basename
