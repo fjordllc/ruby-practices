5af6268016994e47ee30d6ef976ac3f24abc849a
neutral2010 <kasiwamoti5@gmail.com>
2021-08-06 23:27:46 +0900

    メソッド作成

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,15 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-FORMAT = "%7s %7s %7s %-10s"
-file_name = 'wc.rb'
-# file_name = 'sample.txt'
+def output_by_option(file_name)
+  format = '%7s %7s %7s %-10s'
+  ft = File::Stat.new(file_name)
+  count_lines = IO.readlines(file_name).length # 行数
+  read_for_count_words = File.read(file_name) # 単語数
+  count_words = read_for_count_words.split(/\s+/).size
+  count_bit = ft.size # バイト数
+  file_basename = File.basename(file_name) # ファイルネーム
+  printf format, count_lines, count_words, count_bit, file_basename
+end
 
-ft = File::Stat.new(file_name)
-count_lines = IO.readlines(file_name).length #行数
-read_for_count_words = File.read(file_name) # 単語数
-count_words = read_for_count_words.split(/\s+/).size
-count_bit = ft.size# バイト数
-file_basename = File.basename(file_name) # ファイルネーム
+def output_without_option(file_name)
+  format = '%7s %-10s'
+  count_lines = IO.readlines(file_name).length # 行数
+  file_basename = File.basename(file_name) # ファイルネーム
+  printf format, count_lines, file_basename
+end
 
-printf FORMAT, count_lines, count_words, count_bit, file_basename
+# オプションや引数による条件分岐
+# a = 1
+# file_name = 'wc.rb'
+
+if output_by_option(file_name) # オプションあり
+else
+  output_without_option(file_name)
+end
