2646a34198bdf324372ce18266ed871f8ea56007
neutral2010 <kasiwamoti5@gmail.com>
2021-08-07 11:15:27 +0900

    基本機能実装完了。rubocop前の区切り

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,29 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+options = ARGV.getopts('l')
+file_name = ARGV[0]
+
 def output_by_option(file_name)
   format = '%7s %7s %7s %-10s'
   ft = File::Stat.new(file_name)
-  count_lines = IO.readlines(file_name).length # 行数
-  read_for_count_words = File.read(file_name) # 単語数
+  count_lines = IO.readlines(file_name).length
+  read_for_count_words = File.read(file_name)
   count_words = read_for_count_words.split(/\s+/).size
-  count_bit = ft.size # バイト数
-  file_basename = File.basename(file_name) # ファイルネーム
-  printf format, count_lines, count_words, count_bit, file_basename
+  count_bites = ft.size
+  file_basename = File.basename(file_name)
+  printf format, count_lines, count_words, count_bites, file_basename
 end
 
 def output_without_option(file_name)
   format = '%7s %-10s'
-  count_lines = IO.readlines(file_name).length # 行数
-  file_basename = File.basename(file_name) # ファイルネーム
+  count_lines = IO.readlines(file_name).length
+  file_basename = File.basename(file_name)
   printf format, count_lines, file_basename
 end
 
-# オプションや引数による条件分岐
-# a = 1
-# file_name = 'wc.rb'
-
-if output_by_option(file_name) # オプションあり
+if options['l']
+  output_by_option(file_name)
 else
   output_without_option(file_name)
 end
