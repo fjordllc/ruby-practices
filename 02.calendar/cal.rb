dc24a5c0c8f962e8c5bdedba5e621d4f8735f6d5
Gimpei Misono <ginpeimisonou@gmail.com>
2021-08-08 19:22:20 +0900

    カレンダーのタイトルと曜日追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
-# frozen_string_literal: true
 
 require 'date'
 require 'optparse'
 
+DOW = '日 月 火 水 木 金 土'.freeze
 opt = OptionParser.new
 params = {}
 
@@ -14,3 +14,16 @@ opt.parse!(ARGV)
 # 引数が渡されなかった際に当年当月を格納する
 params[:month] = Date.today.month if params[:month].nil?
 params[:year] = Date.today.year if params[:year].nil?
+
+def title(month, year)
+  "      #{month}月 #{year}\n"
+end
+
+def render(month, year)
+  result = ''
+  result << title(month, year)
+  result << DOW
+  puts result
+end
+
+render(params[:month], params[:year])
