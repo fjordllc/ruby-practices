077d6c47d7a5b0ec270eda38f2f26c79b75a888c
Gimpei Misono <ginpeimisonou@gmail.com>
2021-08-08 18:49:20 +0900

    引数が渡されなかった際のデフォルト値追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'date'
 require 'optparse'
 
 opt = OptionParser.new
@@ -9,3 +10,7 @@ params = {}
 opt.on('-m VAL') { |v| params[:month] = v }
 opt.on('-y VAL') { |v| params[:year] = v }
 opt.parse!(ARGV)
+
+# 引数が渡されなかった際に当年当月を格納する
+params[:month] = Date.today.month if params[:month].nil?
+params[:year] = Date.today.year if params[:year].nil?
