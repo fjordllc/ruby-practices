4c2f7d7508f55e84521aa9676ecdc70c084dc00e
Gimpei Misono <ginpeimisonou@gmail.com>
2021-08-08 18:38:53 +0900

    コマンドライン引数の処理を追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+require 'optparse'
+
+opt = OptionParser.new
+params = {}
+
+opt.on('-m VAL') { |v| params[:month] = v }
+opt.on('-y VAL') { |v| params[:year] = v }
+opt.parse!(ARGV)
