82b26e868fe32f2e623826a9b361cd705aad8bee
misono <ginpeimisonou@gmail.com>
2021-08-09 12:35:18 +0900

    カレンダーのコンテンツ領域を追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,12 +4,14 @@
 require 'date'
 require 'optparse'
 
+DAY_WIDTH = 2
 DOW = "日 月 火 水 木 金 土\n"
+
 opt = OptionParser.new
 params = {}
 
-opt.on('-m VAL') { |v| params[:month] = v }
-opt.on('-y VAL') { |v| params[:year] = v }
+opt.on('-m VAL') { |v| params[:month] = v.to_i }
+opt.on('-y VAL') { |v| params[:year] = v.to_i }
 opt.parse!(ARGV)
 
 # 引数が渡されなかった際に当年当月を格納する
@@ -20,8 +22,22 @@ def title(month, year)
   "      #{month}月 #{year}\n"
 end
 
+# カレンダーのコンテンツ領域を作成する
+def cal_content(month, year)
+  first_date = Date.new(year, month, 1)
+  last_date = Date.new(year, month, -1)
+
+  res = ' ' * first_date.wday * (DAY_WIDTH + 1)
+  (first_date..last_date).each do |date|
+    res += date.day.to_s.rjust(DAY_WIDTH)
+    res += date.saturday? ? "\n" : ' '
+  end
+
+  res
+end
+
 def render(month, year)
-  result = title(month, year) + DOW
+  result = title(month, year) + DOW + cal_content(month, year)
   puts result
 end
 
