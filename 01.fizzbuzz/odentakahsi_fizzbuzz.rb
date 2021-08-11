e55b868ba60f6715cf2ae97ed1f08f7642631e77
OdenTakashi <odentakashi@icloud.com>
2021-08-11 17:32:38 +0900

    課題提出

---
+++ 01.fizzbuzz/odentakahsi_fizzbuzz.rb
@@ -0,0 +1,11 @@
+numbers = 1..20
+
+numbers.each do |number|
+    if number % 3 == 0
+        puts "Fizz"
+    elsif number % 5 == 0
+        puts "Buzz"
+    else
+        puts number
+    end
+end
