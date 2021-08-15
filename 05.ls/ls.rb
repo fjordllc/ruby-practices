f0f3a1cc8e5ee00d1f4bdffcd038a267e2ac458c
neutral2010 <kasiwamoti5@gmail.com>
2021-08-15 12:40:26 +0900

    メソッド化により大幅に修正しました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,49 +3,36 @@
 
 require 'etc'
 require 'optparse'
-options = ARGV.getopts('a', 'r', 'l')
 
 # プログラム内で使用する定数の定義
 FORMAT = '%-20s'
 L_FORMAT = "%s%-6s %3s %-8s %-8s %6s %7s %-16s \n"
 NUMBER_COLUMNS = 3
 
-# 表示するデータ取得に関するプログラム
+def main
+  options = ARGV.getopts('a', 'r', 'l')
+  dir_and_file_names = get_dir_and_file_name(options)
+
+  if options['l']
+    output_with_option_l(dir_and_file_names)
+  else
+    output_without_option_l(dir_and_file_names)
+  end
+end
+
+# 表示するデータ取得
 def get_dir_and_file_name(options)
   result = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
   options['r'] ? result.reverse : result
 end
 
-dir_and_file_names = get_dir_and_file_name(options)
-
-# -lオプション時の出力に関するプログラム
+# -lオプション時の出力
 def calc_total_number(dir_and_file_names)
   total_number = dir_and_file_names.sum do |dir_and_file_name|
     ls_l_stat = File::Stat.new(dir_and_file_name)
     ls_l_stat.blocks
   end
   puts "total #{total_number}"
-end
-
-def convert_filetypes(ls_l_stat)
-  { 'link' => 'l', 'file' => '-', 'directory' => 'd' }[ls_l_stat.ftype]
-end
-
-def convert_permissions(ls_l_stat)
-  hash = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' =>
-    'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
-  m = ls_l_stat.mode.to_s(8)
-  hash[m[-3]] + hash[m[-2]] + hash[m[-1]]
-end
-
-def cal_time_stamp(ls_l_stat)
-  present_time = Time.now
-
-  if (present_time - ls_l_stat.mtime) < 60 * 60 * 24 * 180 # 半年を30*6日としている
-    ls_l_stat.mtime.strftime('%_m %_d %H:%M')
-  else
-    ls_l_stat.mtime.strftime('%_m %_d %_5Y')
-  end
 end
 
 def output_with_option_l(dir_and_file_names)
@@ -68,11 +55,35 @@ def output_with_option_l(dir_and_file_names)
   end
 end
 
+def convert_filetypes(ls_l_stat)
+  { 'link' => 'l', 'file' => '-', 'directory' => 'd' }[ls_l_stat.ftype]
+end
+
+def convert_permissions(ls_l_stat)
+  permission_hash = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' =>
+    'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
+  m = ls_l_stat.mode.to_s(8)
+  permission_hash[m[-3]] + permission_hash[m[-2]] + permission_hash[m[-1]]
+end
+
+def cal_time_stamp(ls_l_stat)
+  present_time = Time.now
+  if (present_time - ls_l_stat.mtime) < 60 * 60 * 24 * 180 # 半年を30*6日としている
+    ls_l_stat.mtime.strftime('%_m %_d %H:%M')
+  else
+    ls_l_stat.mtime.strftime('%_m %_d %_5Y')
+  end
+end
+
 # -lオプションがない時の出力
-def output(dir_and_file_names)
+def output_without_option_l(dir_and_file_names)
   number_of_lines = dir_and_file_names.length / NUMBER_COLUMNS
   number_of_lines += 1 unless (dir_and_file_names.length % number_of_lines).zero?
   array_for_outputs = dir_and_file_names.each_slice(number_of_lines).to_a
+  convert_matrix(array_for_outputs)
+end
+
+def convert_matrix(array_for_outputs)
   array_for_outputs.map! do |array_for_output|
     array_for_output.values_at(0..(array_for_outputs.max.size - 1))
   end
@@ -82,8 +93,4 @@ def output(dir_and_file_names)
   end
 end
 
-if options['l']
-  output_with_option_l(dir_and_file_names)
-else
-  output(dir_and_file_names)
-end
+main
