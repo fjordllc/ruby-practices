4ca18500fe263ec89c96784967fbdf028b5d1ba0
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-29 12:34:17 +0900

    Frameクラスのテストを作成

---
+++ 07.bowling_object/frame_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require_relative 'frame'
+
+class FrameTest < Minitest::Unit::TestCase
+  def test_3と2が入ったフレームの合計スコアは5
+    frame = Frame.new("3","2")
+    assert_equal 5, frame.score
+  end
+
+  def test_Xが渡されたらストライク
+    frame = Frame.new("X")
+    assert frame.strike?
+  end
+
+  def test_2投していてフレームの合計が10だったらスペア
+    frame = Frame.new("5","5")
+    assert frame.spare?
+  end
+end
