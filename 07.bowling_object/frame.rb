0dd6e2b8b70f20444a7541ec99e5c4c42b44a248
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-29 12:33:56 +0900

    Frameクラスの責務を見直し

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
-require './shot'
+require_relative 'shot'
 
 class Frame
-  def initialize(marks)
-    @shots = Shot.new(marks).parse_marks
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def initialize(first_mark, second_mark = nil, third_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
   end
 
-  def divide_into_frames
-    frames = []
-    frame = []
-    @shots.each do |shot|
-      frame << shot
-      if frames.size < 10
-        if frame.size >= 2 || shot == 10
-          frames << frame.dup
-          frame.clear
-        end
-      else
-        frames.last << shot
-      end
-    end
-    frames
+  def score
+    [@first_shot.score, @second_shot.score].sum
+  end
+
+  def strike?
+    @first_shot.score == 10
+  end
+
+  def spare?
+    @first_shot.score + @second_shot.score == 10
   end
 end
