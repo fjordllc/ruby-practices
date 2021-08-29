db3418bb8e93ea650d52f801e7007b5abe33b09b
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-08-29 21:57:34 +0900

    caseのインデント調整

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -1,10 +1,11 @@
 (1..20).each do |num|
-  output = case
+  output =
+    case
     when (num % 15).zero? then "FizzBuzz"
     when (num % 3).zero? then "Fizz"
     when (num % 5).zero? then "Buzz"
     else num
-  end
+    end
 
   puts output
 end
