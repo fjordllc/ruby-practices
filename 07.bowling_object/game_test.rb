1b8b949cd9159bc0f09296f873ed71f51e68bc64
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-31 11:54:41 +0900

    Gameクラスのテストを作成

---
+++ 07.bowling_object/game_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require_relative 'game'
+require_relative 'shot'
+require_relative 'frame'
+
+class GameTest < Minitest::Unit::TestCase
+  def test_calc1
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+    assert_equal 139, game.score
+  end
+
+  def test_calc3
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 164, game.score
+  end
+
+  def test_calc3
+    game = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 107, game.score
+  end
+
+  def test_calc4
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 134, game.score
+  end
+
+  def test_calc3
+    game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 300, game.score
+  end
+end
