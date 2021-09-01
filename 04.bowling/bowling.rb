1fcccc3c0751442df16f8ba510437dbb74d7b565
uchihiro <tohchi4122@gmail.com>
2021-09-01 17:54:01 +0900

    フレームごとにスコアを分割する際の条件分岐・スコアの計算方法を修正
    22-23行目のフレームごとにスコアを分割する際の条件分岐を削除しました。また、29−44行目の変数pointへの代入・加算を簡略化しました。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -19,29 +19,27 @@ frames = []
 shots.each_slice(2) do |shot|
   if frames.size == 10
     frames[9] << shot[-1]
-  elsif shot[0] == 10 && frames.size != 9
-    frames << [shot.shift]
   else
     frames << shot
   end
 end
 
-point = 0
-frames.each_with_index do |frame, index|
-  point = if index == 9
-            point + frame.sum
-          elsif frame[0] == 10
-            point += if frames[index + 1][0] == 10 && index == 8
-                       frames[index + 1][0] + frames[index + 1][1] + 10
-                     elsif frames[index + 1][0] == 10
-                       frames[index + 1][0] + frames[index + 2][0] + 10
-                     else
-                       frames[index + 1][0] + frames[index + 1][1] + 10
-                     end
-          elsif frame.sum == 10
-            point + frames[index + 1][0] + 10
-          else
-            point + frame.sum
-          end
+point = frames.each_with_index.sum do |frame, index|
+  if index == 9
+    frame.sum
+  elsif frame[0] == 10
+    if frames[index + 1][0] == 10 && index == 8
+      frames[index + 1][0] + frames[index + 1][1] + 10
+    elsif frames[index + 1][0] == 10
+      frames[index + 1][0] + frames[index + 2][0] + 10
+    else
+      frames[index + 1][0] + frames[index + 1][1] + 10
+    end
+  elsif frame.sum == 10
+    frames[index + 1][0] + 10
+  else
+    frame.sum
+  end
 end
+
 puts point
