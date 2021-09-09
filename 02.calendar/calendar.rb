78cdff9c110fe1e13c3f0bd887dcc058b50c9551
monyatto <83024928+monyatto@users.noreply.github.com>
2021-09-09 09:27:55 +0900

    Update calendar.rb

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,59 +1,33 @@
-#!/usr/bin/director/env ruby
+#!/usr/ruby/ruby-practices/02.calendar/env ruby
 
 require 'date'
 require 'optparse'
 require 'paint'
 
-today = Date.today.day
+year = Date.today.year
+month = Date.today.month
 
 opt = OptionParser.new
-opt.on('-y')
-opt.on('-m')
+opt.on('-y [year]', Integer) { |val| year = val if val != nil }
+opt.on('-m [month]', Integer) { |val| month = val if val != nil}
 opt.parse(ARGV)
-
-if  ARGV[0] == "-y"
-  year = ARGV[1].to_i
-else
-  year = Date.today.year
-end
-
-if ARGV[0] == "-m"
-  month = ARGV[1].to_i
-elsif ARGV[2] == "-m"
-  month = ARGV[3].to_i
-else
-  month = Date.today.month
-end
 
 days_of_month = Date.new(year, month, -1).day
 days_number = Date.new(year, month, 1).day
 day_of_week = Date.new(year, month, 1).cwday
-firstday_switch = true
 
-print "      #{month}月 #{year}
-日 月 火 水 木 金 土\n"
+print "      " + "#{month}月 #{year}" + "\n日 月 火 水 木 金 土\n"
+day_of_week.times {print "   " if day_of_week != 7}
 
-days_of_month.times do
-  if firstday_switch == true && day_of_week != 7
-    day_of_week.times do
-      print "   "
-    end
-    firstday_switch = false
-  end
-  if days_number < 10
-    space = " "
+days_of_month.times do |i|
+  date = Date.new(year, month, i + 1)
+  i + 1 < 10 ? space = " " : space = ""
+  if Date.today == date
+    print Paint["#{space}#{date.day}", :inverse] + " "
   else
-    space = ""
+    print "#{space}#{date.day}" + " "
   end
-    if Date.today.day == Date.new(year, month, days_number).day
-      print Paint["#{space}#{Date.new(year, month, days_number).day}", :inverse]
-    else
-      print "#{space}#{Date.new(year, month, days_number).day}"
-    end
-  print " "
-  if Date.new(year, month, days_number).cwday == 6
-    print "\n"
-  end
-  days_number += 1
+  print "\n" if date.cwday == 6
 end
+
 puts "\n\n"
