e9624a432292b17a7b5684cb28531fbcc5096a3f
mami-inuzuka <inuzuka.mami@gmail.com>
2021-09-14 15:18:06 +0900

    同じ項目のテストを一つにまとめる

--- 07.bowling_object/shot_test.rb
+++ 07.bowling_object/shot_test.rb
@@ -10,13 +10,10 @@ class ShotTest < Minitest::Unit::TestCase
     assert_equal 10, shot.score
   end
 
-  def test_数字が渡されたら整数オブジェクトに1
-    shot = Shot.new('1')
-    assert_equal 1, shot.score
-  end
-
-  def test_数字が渡されたら整数オブジェクトに2
-    shot = Shot.new('5')
-    assert_equal 5, shot.score
+  def test_数字が渡されたら整数オブジェクトに
+    shot_1 = Shot.new('1')
+    shot_2 = Shot.new('5')
+    assert_equal 1, shot_1.score
+    assert_equal 5, shot_2.score
   end
 end
