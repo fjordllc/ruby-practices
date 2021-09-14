f99ef95aa97121b9504a68564f4cd3e246fcefaf
neutral2010 <kasiwamoti5@gmail.com>
2021-09-15 06:33:20 +0900

    1回目の修正、完了しました。
    ロジックが大きく変化しています。

--- 06.wc/sample.rb
+++ 06.wc/sample.rb
@@ -5,37 +5,39 @@ require 'optparse'
 
 def main
   options = ARGV.getopts('l')
-  arrays_of_base_dates = []
+  array_of_base_dates = []
   if ARGV.length.zero?
-    data_by_standard_input(arrays_of_base_dates)
-    output_count_up_total(arrays_of_base_dates, options)
+    data_by_standard_input(array_of_base_dates)
+    output_count_up_total(array_of_base_dates, options)
   else
-    ARGV.each do |files|
-      base_dates = []
-      File.open(files, 'r') do |file|
-        file.map { |lines| base_dates << lines }
-      end
-      arrays_of_base_dates << base_dates
-    end
-    output_result(arrays_of_base_dates, options)
+    data_by_file(array_of_base_dates)
+    output_result(array_of_base_dates, options)
     if ARGV.length >= 2
-      output_count_up_total(arrays_of_base_dates, options)
+      output_count_up_total(array_of_base_dates, options)
       puts ' total'
     end
   end
 end
 
-def data_by_standard_input(arrays_of_base_dates)
+def data_by_standard_input(array_of_base_dates)
   while (line = $stdin.gets)
-    arrays_of_base_dates << line
+    array_of_base_dates << line
   end
 end
 
-def output_result(arrays_of_base_dates, options)
-  arrays_of_base_dates.each_index do |i|
-    count_lines = arrays_of_base_dates[i].length
-    count_words = arrays_of_base_dates[i].join.split(/\s+/).length
-    count_bites = arrays_of_base_dates[i].join.bytesize
+def data_by_file(array_of_base_dates)
+  ARGV.each do |files|
+    base_dates = []
+    File.open(files, 'r') { |file| file.map { |lines| base_dates << lines } }
+    array_of_base_dates << base_dates
+  end
+end
+
+def output_result(array_of_base_dates, options)
+  array_of_base_dates.each_index do |i|
+    count_lines = array_of_base_dates[i].length
+    count_words = array_of_base_dates[i].join.split(/\s+/).length
+    count_bites = array_of_base_dates[i].join.bytesize
     file_name = ARGV[i]
     if options['l']
       format_for_l = '%7s %-10s'
@@ -48,10 +50,10 @@ def output_result(arrays_of_base_dates, options)
   end
 end
 
-def output_count_up_total(arrays_of_base_dates, options)
-  total_count_lines = arrays_of_base_dates.flatten.length
-  total_count_words = arrays_of_base_dates.flatten.join.split(/\s+/).length
-  total_count_bites = arrays_of_base_dates.flatten.join.bytesize
+def output_count_up_total(array_of_base_dates, options)
+  total_count_lines = array_of_base_dates.flatten.length
+  total_count_words = array_of_base_dates.flatten.join.split(/\s+/).length
+  total_count_bites = array_of_base_dates.flatten.join.bytesize
   if options['l']
     printf('%7s', total_count_lines)
   else
