fe29070cc2c26cf57ad6d409831c5207094723a9
neutral2010 <kasiwamoti5@gmail.com>
2021-09-18 16:14:26 +0900

    結果出力のコード、別のやり方を記録するためのコミット
    String#rjustやString#ljustを使う方法も実装してみて、記録しておきたかったので

--- 06.wc/sample.rb
+++ 06.wc/sample.rb
@@ -10,29 +10,32 @@ def main
     $stdin.each_line { |line| arrays_of_lines << line }
     output_count_up_total(arrays_of_lines, options)
   else
-    ARGV.each do |filenames|
-      arrays_of_lines << File.open(filenames, 'r', &:readlines)
+    ARGV.each do |filename|
+      arrays_of_lines << File.readlines(filename)
     end
     output_result(arrays_of_lines, options)
     if ARGV.length >= 2
       output_count_up_total(arrays_of_lines, options)
-      puts ' total'
+      puts '  total'
     end
   end
 end
 
 def output_result(arrays_of_lines, options)
-  arrays_of_lines.each_index do |i|
-    count_lines = arrays_of_lines[i].length
-    count_words = arrays_of_lines[i].join.split(/\s+/).length
-    count_bites = arrays_of_lines[i].join.bytesize
+  arrays_of_lines.each_with_index do |arrays_of_line, i|
+    count_lines = arrays_of_line.length
+    count_words = arrays_of_line.join.split(/\s+/).length
+    count_bites = arrays_of_line.join.bytesize
     file_name = ARGV[i]
     if options['l']
-      format_for_l = '%7s %-10s'
-      printf format_for_l, count_lines, file_name
+      # format_for_l = '%7s %-10s'
+      # printf format_for_l, count_lines, file_name
+      print count_lines.to_s.rjust(8), '  ', file_name.ljust(16)
     else
-      format = '%7s %7s %7s %-10s'
-      printf format, count_lines, count_words, count_bites, file_name
+      # format = '%7s %7s %7s %-10s'
+      # printf format, count_lines, count_words, count_bites, file_name
+      print count_lines.to_s.rjust(8), count_words.to_s.rjust(8),
+            count_bites.to_s.rjust(8), '  ', file_name.ljust(16)
     end
     puts "\n"
   end
@@ -43,10 +46,12 @@ def output_count_up_total(arrays_of_lines, options)
   total_count_words = arrays_of_lines.flatten.join.split(/\s+/).length
   total_count_bites = arrays_of_lines.flatten.join.bytesize
   if options['l']
-    printf('%7s', total_count_lines)
+    printf('%8s', total_count_lines)
   else
-    format_for_total = '%7s %7s %7s'
-    printf format_for_total, total_count_lines, total_count_words, total_count_bites
+    # format_for_total = '%7s %7s %7s'
+    # printf format_for_total, total_count_lines, total_count_words, total_count_bites
+    print total_count_lines.to_s.rjust(8), total_count_words.to_s.rjust(8),
+          total_count_bites.to_s.rjust(8)
   end
 end
 
