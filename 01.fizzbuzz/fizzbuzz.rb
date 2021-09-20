70925aea3695d92207704986ea52e0a51e5f2ff1
kurosutomohiro <kurosu.66@gmail.com>
2021-09-21 08:54:27 +0900

    fizzbuzzプログラム実装

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,16 @@
+numbers = 1..20
+numbers.each do |number|
+    if  number % 15 == 0
+        puts "FizzBuzz"
+        next
+    elsif
+        number % 3 == 0
+        puts "Fizz"
+        next
+    elsif
+        number % 5 == 0
+        puts "Buzz"
+        next
+    end
+    puts number
+end
