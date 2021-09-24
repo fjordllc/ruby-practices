851e3e3e393ab8dd31e78f24700f395e6cd07012
OdenTakashi <odentakashi@icloud.com>
2021-09-24 14:25:55 +0900

    再提出

--- 01.fizzbuzz/odentakahsi_fizzbuzz.rb
+++ 01.fizzbuzz/odentakahsi_fizzbuzz.rb
@@ -1,10 +1,12 @@
 numbers = 1..20
 
 numbers.each do |number|
-    if number % 3 == 0
-        puts "Fizz"
+    if number % 3 == 0 && number % 5 == 0
+        puts "FizzBuzz"
     elsif number % 5 == 0
         puts "Buzz"
+    elsif number % 3 == 0
+        puts "Fizz"
     else
         puts number
     end
