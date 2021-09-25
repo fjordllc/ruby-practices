8bdfee788c3e2a5b4447479ed85d06cbc49443f5
shu913 <sss91327@icloud.com>
2021-09-25 14:18:44 +0900

    fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,31 @@
+def fizzbuzz(n)
+    if n % 15 == 0
+        puts "FizzBuzz"
+    elsif n % 3 == 0
+        puts "Fizz"
+    elsif n % 5 == 0
+        puts "Buzz"
+    else
+        puts n
+    end
+end
+fizzbuzz(1)
+fizzbuzz(2)
+fizzbuzz(3)
+fizzbuzz(4)
+fizzbuzz(5)
+fizzbuzz(6)
+fizzbuzz(7)
+fizzbuzz(8)
+fizzbuzz(9)
+fizzbuzz(10)
+fizzbuzz(11)
+fizzbuzz(12)
+fizzbuzz(13)
+fizzbuzz(14)
+fizzbuzz(15)
+fizzbuzz(16)
+fizzbuzz(17)
+fizzbuzz(18)
+fizzbuzz(19)
+fizzbuzz(20)
