ef5474f7126508c1884661e2a32252d9a43caca5
kurosutomohiro <kurosu.66@gmail.com>
2021-09-30 08:58:56 +0900

    elsifの条件文を改行しないよう修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -3,12 +3,10 @@ numbers.each do |number|
     if  number % 15 == 0
         puts "FizzBuzz"
         next
-    elsif
-        number % 3 == 0
+    elsif number % 3 == 0
         puts "Fizz"
         next
-    elsif
-        number % 5 == 0
+    elsif number % 5 == 0
         puts "Buzz"
         next
     end
