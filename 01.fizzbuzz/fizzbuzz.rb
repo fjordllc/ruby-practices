d654e60ca37f36bc322b111381d8516f9c89a90c
shu913 <sss91327@icloud.com>
2021-09-30 06:31:07 +0900

    fizzbuzz fix

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,31 +1,14 @@
-def fizzbuzz(n)
+def fizzbuzz
+  (1..20).each do |n|
     if n % 15 == 0
-        puts "FizzBuzz"
+      puts "FizzBuzz"
     elsif n % 3 == 0
-        puts "Fizz"
+      puts "Fizz"
     elsif n % 5 == 0
-        puts "Buzz"
+      puts "Buzz"
     else
-        puts n
+      puts n
     end
+  end
 end
-fizzbuzz(1)
-fizzbuzz(2)
-fizzbuzz(3)
-fizzbuzz(4)
-fizzbuzz(5)
-fizzbuzz(6)
-fizzbuzz(7)
-fizzbuzz(8)
-fizzbuzz(9)
-fizzbuzz(10)
-fizzbuzz(11)
-fizzbuzz(12)
-fizzbuzz(13)
-fizzbuzz(14)
-fizzbuzz(15)
-fizzbuzz(16)
-fizzbuzz(17)
-fizzbuzz(18)
-fizzbuzz(19)
-fizzbuzz(20)
+fizzbuzz
