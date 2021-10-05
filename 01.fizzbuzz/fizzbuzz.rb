b8a2d054c34cacc29952f7a1daac7515352cd6ec
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-05 12:25:25 +0700

    fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz num
+  (1..num).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts 'FizzBuzz'
+    elsif i % 3 == 0
+      puts 'Fizz'
+    elsif i % 5 == 0
+      puts 'Buzz'
+    else
+      puts i
+    end
+  end
+end
+
+fizzbuzz(20)
