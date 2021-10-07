e406aa8bccb371d0e6b19388a2766976a17e77bc
shu913 <sss91327@icloud.com>
2021-10-07 21:53:15 +0900

    output calendar

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,34 +1,35 @@
+#!/usr/bin/env ruby
+
 require 'date'
 require 'optparse'
 
-def calendar(y: Date.today.year, m: Date.today.month, d: Date.today.day)
+def output_calendar(year: Date.today.year, month: Date.today.month)
 
-  firstday = Date.new(y, m, 1)
-  lastday = Date.new(y, m, -1)
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1)
 
-  head = firstday.strftime("%B %Y").center(20)
-  wday = %w(Su Mo Tu We Th Fr Sa).join(' ')
-  puts head
-  puts wday
+  puts first_day.strftime("%B %Y").center(20)
+  puts %w(Su Mo Tu We Th Fr Sa).join(' ')
 
-  first_wday = firstday.wday
+  first_wday = first_day.wday
 
-  lastday_date = lastday.day
+  last_day_date = last_day.day
 
-  days = (1..lastday_date).map { |n| n.to_s.rjust(2) }
+  days = (1..last_day_date).map { |n| n.to_s.rjust(2) }
   days = Array.new(first_wday, '  ').push(days).flatten.each_slice(7)
 
   days.each { |week| puts week.join(' ') }
+
 end
 
-def optionparse
+def parsed_option
 
   options = {}
 
   opt = OptionParser.new
 
-  opt.on('-y YEAR') { |v| options[:y] = v.to_i }
-  opt.on('-m MONTH') { |v| options[:m] = v.to_i }
+  opt.on('-y YEAR') { |v| options[:year] = v.to_i }
+  opt.on('-m MONTH') { |v| options[:month] = v.to_i }
 
   opt.parse!
 
@@ -36,4 +37,4 @@ def optionparse
 
 end
 
-calendar(**optionparse)
+output_calendar(**parsed_option)
