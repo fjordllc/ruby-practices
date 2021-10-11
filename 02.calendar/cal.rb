f59e737c3cf0158092aa6ffee6a5653db93fee76
pofkuma <pof.kuma@gmail.com>
2021-10-11 22:09:50 +0900

    Use constant variables
    変更しない値は定数に変更
    (フォーマットの乱れも一緒に修正)

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,31 +5,29 @@ require 'date'
 options = ARGV.getopts("m:y:")
 
 # 月と年(引数を省略した場合は今日の年月)を取得する
-today = Date.today
-mon = options["m"] ? options["m"].to_i : today.mon
-year = options["y"] ? options["y"].to_i : today.year
+DATE_TODAY = Date.today
+mon = options["m"] ? options["m"].to_i : DATE_TODAY.mon
+year = options["y"] ? options["y"].to_i : DATE_TODAY.year
 
 # 月と年を中央揃えで表示する
-cal_width = 20
-puts "#{mon}月 #{year}".center(cal_width)
+CAL_WIDTH = 20
+puts "#{mon}月 #{year}".center(CAL_WIDTH)
 
 # 曜日を表示する
-line_cwday = "日 月 火 水 木 金 土"
-puts line_cwday
+LINE_CWDAY = "日 月 火 水 木 金 土"
+puts LINE_CWDAY
 
 # 当該年月の初日から末日までの配列を作成する
-last_day_number = Date.new(year, mon, -1).day
-days = (1..last_day_number).to_a
+last_day = Date.new(year, mon, -1).day
+days = (1..last_day).to_a
 
 # 初日の曜日の数(1-7、月曜は1)分の空欄を先頭に追加する
-first_cwday_number = Date.new(year, mon).cwday
-first_cwday_number.times { days.unshift(" ") }
+first_cwday = Date.new(year, mon).cwday
+first_cwday.times { days.unshift(" ") }
 
 # 配列の各要素を2桁に揃える
-days = days.map {|num| num.to_s.rjust(2)}
+days = days.map { |num| num.to_s.rjust(2) }
 
-# 日付を週毎に区切って出力
-length_a_week = 7
-days.each_slice(length_a_week) do |day|
-  puts day.join(" ")
-end
+# 日付を週毎に区切って出力する
+LENGTH_A_WEEK = 7
+days.each_slice(LENGTH_A_WEEK) { |day| puts day.join(" ") }
