9b5e8abe1ba532b01f8fbbb55dcd4048bf4eb818
OdenTakashi <odentakashi@icloud.com>
2021-10-11 14:51:29 +0900

    each式内での値取得法変更
    each式内で値取得をeach_with_indexを利用するようにした

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -3,12 +3,12 @@ scores = score.split(',')
 
 shots = [ ]
 scores.each do |s|
-    if s == 'x' #xはストライク
-        shots << 10
-        shots << 0
-    else
-        shots << s.to_i
-    end
+if s == 'x' #xはストライク
+    shots << 10
+    shots << 0
+else
+    shots << s.to_i
+end
 end
 
 frames = [ ]
@@ -22,22 +22,24 @@ shots.each_slice(2) do |s|
 end
 
 unless frames.count == 10 || frames.count == 11
-    p "フレーム数が不正だよ"
+    p "フレーム数が不正です"
     exit
 end
 
 point = 0 
-
-frames[0..8].each do |frame|
-    if frame[0] == 10 && frames[+1][0] == 10
-        point = point + 10 + frames[+1].sum + frames[+2][0]
-    elsif frame.sum == 10
-        point = point + 10 + frames[+1][0]
+points = []
+frames[0..8].each_with_index do |frame, idx|
+    if frame[0] == 10 && frames[idx +1][0] == 10
+        point = point + 10 + frames[idx +1].sum + frames[idx +2][0]
     elsif frame[0] == 10
-        point = point + 10 + frames[+1].sum
+        point = point + 10 + frames[idx +1].sum
+    elsif frame.sum == 10
+        point = point + 10 + frames[idx +1][0]   
     else
         point = point + frame.sum
     end
+
+    points << point
 end
 
 
@@ -49,3 +51,5 @@ else
 end
 
 p point
+
+
