2a9aae8d73e6e752a2c00a378adb17baec717b90
naganoshinya <cknumber5@gmail.com>
2021-10-18 16:32:36 +0900

    マジックナンバーの修正、each文の修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -22,25 +22,20 @@ end
 # コマンドライン(ARGV)の解析
 opt.parse(ARGV)
 
-# 最初の日と最後の日を変数に代入
-beginning_of_month = Date.new(@year, @month, 1)
-end_of_month = Date.new(@year, @month, -1)
-
-# 最初の日から最後の日までを配列にする
-month_days = []
-(beginning_of_month..end_of_month).each do |date|
-  month_days << date
-end
-
 # 何月のカレンダーか表示する
-puts "#{@month}月 #{@year}".center(20)
+calendar_width = 20
+puts "#{@month}月 #{@year}".center(calendar_width)
 
 # 曜日を表示する
 print "日 月 火 水 木 金 土\n"
 
+# 最初の日と最後の日を変数に代入
+beginning_of_month = Date.new(@year, @month, 1)
+end_of_month = Date.new(@year, @month, -1)
+
 # 日付を表示する
 print ' ' * (Date.new(@year, @month, 1).wday * 3)
-month_days.each do |month_day|
+(beginning_of_month..end_of_month).each do |month_day|
   if month_day.saturday?
     print "#{month_day.day}\n".rjust(3)
   else
