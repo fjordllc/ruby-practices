a4dc2ec05f601615b7f987b0fb3204855eaabba7
masanarih0ri <masanarihoridevelop@gmail.com>
2021-10-18 21:37:28 +0900

    add calendar program

---
+++ 02.calendar/calendar.rb
@@ -0,0 +1,55 @@
+require 'optparse'
+require 'date'
+
+params = ARGV.getopts("y:", "m:")
+p params
+
+year = params["y"]&.to_i || Date.today.year
+month = params["m"]&.to_i || Date.today.mon
+
+START_DAY = 1
+END_DAY = Date.new(year, month, -1).day
+
+before_blank = "   " * Date.new(year, month, START_DAY).wday
+after_blank =  "\n"
+
+day_of_week_list = ["日", "月", "火", "水", "木", "金", "土"]
+
+print "      #{month}月 #{year}      \n"
+
+day_of_week_list.each_with_index do |day_of_week, index|
+  if index == day_of_week_list.size - 1
+    print day_of_week + "\n"
+  else
+    print day_of_week + " "
+  end
+end
+
+def is_saturday?(year, month, day)
+  Date.new(year, month, day).wday == 6
+end
+
+def is_today?(year, month, day)
+  Date.today == Date.new(year, month, day)
+end
+
+print before_blank
+
+(START_DAY..END_DAY).each do |day|
+  display_day = is_today?(year, month, day) ? "\e[30;43m#{day.to_s}\e[0m" : day.to_s
+  # 土曜日かつ1桁の場合（2021年の10月なら2が該当）
+  if day < 10 && is_saturday?(year, month, day)
+    print " " + display_day + "\n"
+  # 2桁で土曜日で改行したい場合
+  elsif is_saturday?(year, month, day)
+    print display_day + "\n"
+  # 1桁で土曜以外の場合
+  elsif day < 10 && !is_saturday?(year, month, day)
+    print " " + display_day + " "
+  # 2桁で土曜以外の場合
+  else
+    print display_day + " "
+  end
+end
+
+print after_blank
