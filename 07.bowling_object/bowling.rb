249f5a1c62e1a9fe26efd5016c106d48d4b46bed
OdenTakashi <odentakashi@icloud.com>
2021-10-20 17:28:13 +0900

    each_with_index式内修正
    sumにブロックを渡すことにより、pointの重複を改善

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -28,17 +28,16 @@ unless frames.count == 10 || frames.count == 11
   exit
 end
 
-point = 0
-frames[0..8].each_with_index do |frame, idx|
-  point += if frame[0] == 10 && frames[idx + 1][0] == 10
-             10 + frames[idx + 1].sum + frames[idx + 2][0]
-           elsif frame[0] == 10
-             10 + frames[idx + 1].sum
-           elsif frame.sum == 10
-             10 + frames[idx + 1][0]
-           else
-             frame.sum
-           end
+point = frames[0..8].each_with_index.sum do |frame, idx|
+  if frame[0] == 10 && frames[idx + 1][0] == 10
+    10 + frames[idx + 1].sum + frames[idx + 2][0]
+  elsif frame[0] == 10
+    10 + frames[idx + 1].sum
+  elsif frame.sum == 10
+    10 + frames[idx + 1][0]
+  else
+    frame.sum
+  end
 end
 
 if frames[10] && frames[10].count == 1
