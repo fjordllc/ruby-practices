1110fbef3b1c67317d8473d322d8652214709ddf
umlautone uenishi <uenishi@umlautone.jp>
2021-10-23 23:23:25 +0900

    コードを整理

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,52 +3,43 @@
 require 'optparse'
 require 'date'
 
+target_year = ''
+target_month = ''
 current_year = Date.today.year
 current_month = Date.today.month
 
-opt = OptionParser.new
-
-target_year = ''
-target_month = ''
 days = ['日','月','火','水','木','金','土']
 last_date = ''
-dates = ''
-
 cal_title = ''
 day_header = ''
 
+# オプションの値を取得
+opt = OptionParser.new
 opt.on('-y')
 opt.on('-m')
-
 opt.parse!(ARGV)
 
-if ARGV.empty?
-  argument = [current_year.to_s,current_month.to_s]
-else
-  argument = ARGV
-end
-target_year = argument[0]
-target_month = argument[1]
-
+ARGV = [current_year.to_s,current_month.to_s] if ARGV.empty?
+target_year = ARGV[0]
+target_month = ARGV[1]
 
 last_date = Date.new(target_year.to_i, target_month.to_i, -1).strftime('%d')
 
+# カレンダータイトル
 cal_title.concat('      ' + target_month + '月 ' + target_year)
 
-
+# 曜日の表示
 days.each do |day|
   day_header.concat(day + ' ')
 end
 
-
-first_day_date = Date.new(target_year.to_i,target_month.to_i,1).wday
-
 puts cal_title
 puts day_header
 
-# 日付部分
-# 初日の曜日を確認し、必要な曜日数x3スペース開ける
-print dates.concat(' '* first_day_date * 3)
+# 日付部分の出力
+# 初日の曜日の数字を確認し、数字x3スペース開ける
+first_day_date = Date.new(target_year.to_i,target_month.to_i,1).wday
+print ' ' * first_day_date * 3
 
 (1..last_date.to_i).each do |date|
   date < 10 ? pre_space = ' ' : pre_space = ''
