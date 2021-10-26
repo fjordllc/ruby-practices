aec504c21ed601cf57a90e1bb5e675ce862bcd7c
masanarih0ri <masanarihoridevelop@gmail.com>
2021-10-26 09:40:36 +0900

    modify day of week display

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -13,7 +13,7 @@ day_of_week_list = ["日", "月", "火", "水", "木", "金", "土"]
 
 print "      #{month}月 #{year}      \n"
 
-print day_of_week_list.join(" ") + "\n"
+print "#{day_of_week_list.join(' ')}\n"
 
 def is_saturday?(year, month, day)
   Date.new(year, month, day).wday == 6
