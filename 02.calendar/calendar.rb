9f4be41f0983f5a782ceae5bde96ad92a8f3fee0
kurosutomohiro <kurosu.66@gmail.com>
2021-10-27 09:15:03 +0900

    余計なDate.newを省略、初日のスペース出力処理を繰り返しから除外

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,37 +3,35 @@
 require 'date'
 require 'optparse'
 
-#引数受け取り
+# 引数受け取り
 params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
 year = params['y'].to_i
 month = params['m'].to_i
 
-#月の初日と最終日を取得
-first_day = Date.new(year, month, 1).day
-last_day = Date.new(year, month, -1).day
-
-#月と年を出力
+# 月と年を出力
 date = Date.new(year, month)
-puts ("#{month}月 #{date.year}").center(20)
+puts ("#{date.month}月 #{date.year}").center(20)
 week = ['日','月','火','水','木','金','土']
 puts week.join(' ')
 
-#カレンダー出力
-days = first_day..last_day
-days.each do |day|
-    i = Date.new(year, month, day).day
-    wdays = Date.new(year, month, day).wday
-	is_saturday = Date.new(year, month, day).saturday?
+# 初日左部分のスペースを出力
+space = 3 * date.wday
+print ('').rjust(space)
 
-    # 初日左部分のスペース埋め処理
-    if day == 1
-        space = 3 * wdays
-        print ('').rjust(space)
-    end
-    print ("#{i} ").rjust(3)
-    if is_saturday 
-        print "\n"
-    end
+# 月の最終日を取得
+last_day = Date.new(year, month, -1)
+
+# カレンダー出力
+days = date.day..last_day.day
+days.each do
+  print ("#{date.day} ").rjust(3)
+
+  if date.saturday?
+    print "\n"
+  end
+
+  date += 1
+
 end
 
 print "\n"
