48a91c87a2d5df16c429642c3f04af5606519327
kurosutomohiro <kurosu.66@gmail.com>
2021-10-28 09:04:21 +0900

    繰り返し処理にブロック引数を渡すよう修正、後置ifに修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,29 +9,23 @@ year = params['y'].to_i
 month = params['m'].to_i
 
 # 月と年を出力
-date = Date.new(year, month)
-puts ("#{date.month}月 #{date.year}").center(20)
+date_obj = Date.new(year, month)
+puts ("#{date_obj.month}月 #{date_obj.year}").center(20)
 week = ['日','月','火','水','木','金','土']
 puts week.join(' ')
 
 # 初日左部分のスペースを出力
-space = 3 * date.wday
+space = 3 * date_obj.wday
 print ('').rjust(space)
 
 # 月の最終日を取得
 last_day = Date.new(year, month, -1)
 
 # カレンダー出力
-days = date.day..last_day.day
-days.each do
+days = date_obj..last_day
+days.each do |date|
   print ("#{date.day} ").rjust(3)
-
-  if date.saturday?
-    print "\n"
-  end
-
-  date += 1
-
+  print "\n" if date.saturday?
 end
 
 print "\n"
