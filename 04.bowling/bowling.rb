713582b2cade5ded2bfa1c66485268eb18e97419
masanarih0ri <masanarihoridevelop@gmail.com>
2021-10-29 09:07:45 +0900

    remove unnecessary code

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -23,20 +23,13 @@ while frame_count <= MAX_FRAME_COUNT
   end
 end
 
-p frames
-
-# フレームの計算
-# ストライクでもスペアでもない：ただの足し算
-# スペア：スペアのフレームは10と次のフレームの1投目を足した数
-# ストライク：10と次の次の投球までを足した数
 total_points = 0
 frames.each_with_index do |frame, index|
-  # 最終フレームは単なる足し算
+  # 最終フレームの場合
   if index == frames.size - 1
     total_points += frame.sum
-    p total_points
     break
-  # ストライクの場合は2つ先の投球まで足す
+  # ストライクの場合
   elsif frame[0] == 10
     # 最初の投球の次の投球もストライクの場合
     if frames[index + 1][0] == 10
@@ -48,17 +41,12 @@ frames.each_with_index do |frame, index|
       total_points += frames[index + 1][0]
       total_points += frames[index + 1][1]
     end
-    p total_points
-  # スペアの場合は次の投球を足す
+  # スペアの場合
   elsif frame[0] != 10 && frame.sum == 10
     total_points += frame.sum
     total_points += frames[index + 1][0]
-    p "next_frame#{frames[index + 1][0]}"
-    p total_points
-  # それ以外の場合は単に足すだけ
   else
     total_points += frame.sum
-    p total_points
   end
 end
 
