8881b274608f5d2b950e897808f5ac3465abe5d6
kurosutomohiro <kurosu.66@gmail.com>
2021-11-01 22:59:18 +0900

    変数名の変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,20 +9,20 @@ year = params['y'].to_i
 month = params['m'].to_i
 
 # 月と年を出力
-date_obj = Date.new(year, month)
-puts ("#{date_obj.month}月 #{date_obj.year}").center(20)
+normal_day = Date.new(year, month)
+puts ("#{normal_day.month}月 #{normal_day.year}").center(20)
 week = ['日','月','火','水','木','金','土']
 puts week.join(' ')
 
 # 初日左部分のスペースを出力
-space = 3 * date_obj.wday
+space = 3 * normal_day.wday
 print ('').rjust(space)
 
 # 月の最終日を取得
 last_day = Date.new(year, month, -1)
 
 # カレンダー出力
-days = date_obj..last_day
+days = normal_day..last_day
 days.each do |date|
   print ("#{date.day} ").rjust(3)
   print "\n" if date.saturday?
