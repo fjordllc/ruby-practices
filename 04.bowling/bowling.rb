9838825ac8aaf5431a556a7a043260350e49d7d4
lan <lanlehoang10@gmail.com>
2021-11-03 16:41:56 +0700

    remove unnecessary class definition, fix bug when it ignoring non X and non 0 case

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,67 +1,56 @@
 # frozen_string_literal: true
 
-class Bowling
-  def calculate(scores)
-    scores = scores.split(',')
-    frames = divide_by_frame(convert(scores))
-    points = 0
-    frames.each_with_index do |score, index|
-      if index == 9
-        points += calculate_for_last_turn(frames, score, index)
-      elsif index < 9
-        points += calculate_for_normal_turns(frames, score, index)
-      end
+def calculate(scores)
+  scores = scores.split(',')
+  frames = convert(scores).each_slice(2).to_a
+  points = 0
+  frames.each_with_index do |score, index|
+    if index == 9
+      points += calculate_for_last_turn(frames, score, index)
+    elsif index < 9
+      points += calculate_for_normal_turns(frames, score, index)
     end
-    p points
   end
-
-  def convert(scores)
-    convert_scores = []
-    scores.each do |score|
-      if score == 'X'
-        convert_scores << 10
-        convert_scores << 0
-      else
-        convert_scores << score.to_i
-      end
-    end
-    convert_scores
-  end
-
-  def divide_by_frame(array_of_scores)
-    scores_by_frame = []
-    array_of_scores.each_slice(2) do |score|
-      scores_by_frame << score
-    end
-    scores_by_frame
-  end
-
-  def calculate_for_normal_turns(frames, score, index)
-    points = 0
-    points += score.sum
-    if score[0] == 10
-      if frames[index + 1][0] == 10
-        points += frames[index + 1][0]
-        points += frames[index + 2][0]
-      elsif frames[index + 1][0] < 10
-        points += frames[index + 1].sum
-      end
-    elsif score[0] < 10 && score.sum == 10
-      points += frames[index + 1][0]
-    end
-    points
-  end
-
-  def calculate_for_last_turn(frames, score, index)
-    points = 0
-    points += score.sum
-    return points unless score.sum == 10
-
-    points += frames[index + 1] ? frames[index + 1][0] : 0
-    points += frames[index + 2] ? frames[index + 2][0] : 0
-    points
-  end
+  p points
 end
 
-bowling = Bowling.new
-bowling.calculate(ARGV[0])
+def convert(scores)
+  convert_scores = []
+  scores.each do |score|
+    if score == 'X'
+      convert_scores << 10
+      convert_scores << 0
+    else
+      convert_scores << score.to_i
+    end
+  end
+  convert_scores
+end
+
+def calculate_for_normal_turns(frames, score, index)
+  points = score.sum
+  if score[0] == 10
+    if frames[index + 1][0] == 10
+      points += frames[index + 1][0]
+      points += frames[index + 2][0]
+    else
+      points += frames[index + 1].sum
+    end
+  elsif score[0] < 10 && score.sum == 10
+    points += frames[index + 1][0]
+  end
+  points
+end
+
+def calculate_for_last_turn(frames, score, index)
+  points = 0
+  points += score.sum
+  return points unless score.sum == 10
+
+  points += frames[index + 1]&.first ? frames[index + 1][0] : 0
+  points += frames[index + 1]&.at(1) ? frames[index + 1][1] : 0
+  points += frames[index + 2]&.first ? frames[index + 2][0] : 0
+  points
+end
+
+calculate(ARGV[0])
