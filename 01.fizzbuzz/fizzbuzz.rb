e81432f8fc70f2449713f6541f8499fd18967983
lan <lanlehoang10@gmail.com>
2021-11-03 15:45:48 +0700

    change while loop to each loop

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
-class FizzBuzz
-  def print_results
-    i = 1
-    while i <= 20
-      if (i % 3).zero? && (i % 5).zero?
-        p 'FizzBuzz'
-      elsif (i % 3).zero?
-        p 'Fizz'
-      elsif (i % 5).zero?
-        p 'Buzz'
-      else
-        p i
-      end
-      i += 1
+def print_results
+  (1..20).each do |i|
+    if (i % 3).zero? && (i % 5).zero?
+      p 'FizzBuzz'
+    elsif (i % 3).zero?
+      p 'Fizz'
+    elsif (i % 5).zero?
+      p 'Buzz'
+    else
+      p i
     end
+    i += 1
   end
 end
 
-fizz_buzz = FizzBuzz.new
-fizz_buzz.print_results
+print_results
