357ec549d6d5f782692cd0e70970b17b94bba642
masanarih0ri <masanarihoridevelop@gmail.com>
2021-11-05 00:22:41 +0900

    fix index count

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -16,30 +16,31 @@ MAX_FRAME_COUNT = 10
 
 while frames.count < (MAX_FRAME_COUNT - 1)
   # 9フレーム目までの処理
-  frames << score.slice((frames.count) * 2, 2)
+  frames << score.slice(frames.count * 2, 2)
 end
 # 10フレーム目までの処理
-frames << score.slice((frames.count) * 2..-1)
+frames << score.slice(frames.count * 2..-1)
 
 total_points = 0
-frames.each_with_index do |frame, index|
+frames.each.with_index(1) do |frame, index|
   total_points += frame.sum
-  # 最終フレームの場合
-  break if index == frames.size - 1
 
-  # ストライクの場合
+  break if index == frames.size
+
   if frame[0] == 10
-    total_points += frames[index + 1][0]
+    total_points += frames[index][0]
     total_points +=
       # 最初の投球の次の投球もストライクの場合
-      if frames[index + 1][0] == 10
-        index + 1 == frames.size - 1 ? frames[index + 1][2] : frames[index + 2][0]
+      if frames[index][0] == 10
+        index == frames.size - 1 ? frames[index][2] : frames[index + 1][0]
       else
-        frames[index + 1][1]
+        frames[index][1]
       end
+
   # スペアの場合
   elsif frame.sum == 10
-    total_points += frames[index + 1][0]
+    total_points += frames[index][0]
+
   end
 end
 
