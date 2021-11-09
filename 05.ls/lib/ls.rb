e083c159d28ff768fc519e095731c13e652ad33b
OdenTakashi <odentakashi@icloud.com>
2021-11-09 20:24:57 +0900

    3列にならない場合の修正
    ファイル数が３の倍数＋１の際に３列に出力されない部分の修正

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -1,5 +1,21 @@
-puts(@final_sort_order)
-		#引数が2個以上与えられた時、一つ目の出力と二つ目の出力の間に空行
+# frozen_string_literal: true
+
+argument = ARGV
+argument.push(Dir.pwd) if argument.size <= 0
+
+def gets_argument(argument)
+  argument.each_with_index do |p, idx|
+    # ファイルの情報を受け取って、変数に代入
+    files_name = Dir.glob('*', base: p)
+    # ファイルの中で一番長い文字の値を取得
+    argument_of_size = files_name.map(&:size)
+    @maximum_size = argument_of_size.max
+    # 引数が2個以上与えられた時、ディレクトリ名：のように出力
+    puts "#{p}:" if argument.size > 1
+    make_jam(files_name)
+    outputs(@final_sort_order)
+    outputs2(@final_sort_order)
+    # 引数が2個以上与えられた時、一つ目の出力と二つ目の出力の間に空行
     if idx >= 0 && argument.size >= 2
       puts '  '
       puts '  '
@@ -8,11 +24,11 @@ puts(@final_sort_order)
 end
 
 def make_jam(files_name)
-	#3つに分割する際に、３で割り切れない時はnilで帳尻合わせ
-  (3 - files_name.size % 3).times do 
-	files_name.push(nil)
+  # 3つに分割する際に、３で割り切れない時はnilで帳尻合わせ
+  (3 - files_name.size % 3).times do
+    files_name.push(nil)
   end
-	#3行に出力するためにファイル数を３つに均等に分割
+  # 3行に出力するためにファイル数を３つに均等に分割
   @one_third_mom = files_name.size / 3
   @one_third_file = files_name.each_slice(@one_third_mom).to_a
   @final_sort_order = @one_third_file.transpose
@@ -20,15 +36,29 @@ end
 
 def outputs(_final_sort_order)
   count = 0
-  @final_sort_order.each do |e|
+  @final_sort_order[0...-2].each do |e|
     e.each do |f|
       count += 1
-			#3つごと出力
+      # 3つごと出力
       if (count % 3).zero? && !f.nil?
         puts f
-			#一番文字数の多いファイル+5に幅を合わせる
+      # 一番文字数の多いファイル+5に幅を合わせる
       elsif count % 3 != 0 && !f.nil?
         print f.ljust(@maximum_size + 5)
+      end
+    end
+  end
+end
+
+def outputs2(_final_sort_order2)
+  counters = 0
+  @final_sort_order[-2..].each do |r|
+    r.each do |x|
+      counters += 1
+      if (counters % 4).zero?
+        puts x
+      elsif !x.nil?
+        print x.ljust(@maximum_size + 5)
       end
     end
   end
