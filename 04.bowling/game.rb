c54cee7978e15c7e65621c112e98fd2e98e771aa
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-11-18 15:49:53 +0900

    frameインスタンスを作って、1game分のframeを配列に格納する

--- 04.bowling/game.rb
+++ 04.bowling/game.rb
@@ -4,4 +4,28 @@ class Game
   def initialize(pin)
     @game = pin
   end
+
+  def frame_ary
+    frame = []
+    frames = []
+    score_ary = @game.split(",")
+    score_ary.each do |sa|
+      frame.push(sa)
+      if frames.length == 10
+        frames.last.push(sa)
+      elsif frame.size >= 2 || sa == 'X'
+        frames.push(frame.dup)
+        frame.clear
+      end
+    end
+    frames
+  end
+
+  def frame_instance_ary
+    frames = []
+    frame_ary.each do |frame|
+      frames.push(Frame.new(*frame))
+    end
+    frames
+  end
 end
