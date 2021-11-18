90311c7f205f46b53c498d354b7429dc470028a6
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-11-18 16:04:54 +0900

    条件つけ忘れていた

--- 04.bowling/game.rb
+++ 04.bowling/game.rb
@@ -34,7 +34,7 @@ class Game
     frame_instance_ary.each_with_index do |frame, i|
       if frame.first_shot.score == 10 && !last_frame?(i)
         score.push(frame.score + add_strike_points(i))
-      elsif frame.score == 10
+      elsif frame.score == 10 && !last_frame?(i)
         score.push(frame.score + add_spare_points(i))
       else
         score.push(frame.score)
