3d92a477fbae7036a1276644f837b95e35a63c9e
OdenTakashi <odentakashi@icloud.com>
2021-11-25 14:46:30 +0900

    同じ処理を複数回させない
    ローカル変数にデータを保持することにより複数回の同じ処理を避ける

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -3,23 +3,17 @@
 argument = ARGV
 argument.push(Dir.pwd) if argument.size <= 0
 
-# 指定ディレクトリの中のファイル情報を取得
-def get_argument(directory)
-  Dir.glob('*', base: directory)
-end
-
 # ファイルの最大の文字数の値を取得
-def get_maximum_size(directory)
-  argument_of_size = get_argument(directory).map(&:size)
+def get_maximum_size(filesname)
+  argument_of_size = filesname.map(&:size)
   argument_of_size.max
 end
 
 # 3の倍数にファイルの要素数を調整
-def complete_array(directory)
-  numbers_of_files = get_argument(directory).size
-  add_nil_to_array_of_files = get_argument(directory)
-  if numbers_of_files % 3 != 0
-    (3 - numbers_of_files % 3).times do
+def complete_array(filesname)
+  add_nil_to_array_of_files = filesname
+  if add_nil_to_array_of_files.size % 3 != 0
+    (3 - add_nil_to_array_of_files.size % 3).times do
       add_nil_to_array_of_files.push(nil)
     end
   end
@@ -27,32 +21,32 @@ def complete_array(directory)
 end
 
 # 実際の出力に合わせて転置
-def one_third_file(directory)
-  one_third_mom = complete_array(directory).size / 3
-  one_third_file = complete_array(directory).each_slice(one_third_mom).to_a
+def one_third_file(filesname)
+  one_third_mom = complete_array(filesname).size / 3
+  one_third_file = complete_array(filesname).each_slice(one_third_mom).to_a
   one_third_file.transpose
 end
 
 # ３列になるとように出力
-def output(directory)
+def output(filesname,maxsize_filesname)
   counters = 0
-  one_third_file(directory).each do |files|
+  one_third_file(filesname).each do |files|
     files.each do |file|
       counters += 1
       if (counters % 3).zero? && !file.nil?
         puts file
       elsif counters % 3 != 0 && !file.nil?
-        print file.ljust(get_maximum_size(directory) + 5)
+        print file.ljust(maxsize_filesname + 5) #+5は定数化する
       end
     end
   end
 end
 
-# 3倍＋１のファイル数の時に合わせて出力（ファイル数が、9個以上の時）
-def output_files_size_is_special_big(directory)
+# 3倍＋１のファイル数の時に合わせて出力（転置後の配列の個数が3個以上の時）
+def output_files_size_is_special_big(filesname,maxsize_filesname)
   count = 0
   counters = 0
-  one_third_file(directory)[0...-2].each do |files|
+  one_third_file(filesname)[0...-2].each do |files|
     files.each do |file|
       counters += 1
       # 3つごと出力
@@ -60,75 +54,58 @@ def output_files_size_is_special_big(directory)
         puts file
       # 一番文字数の多いファイル+5に幅を合わせる
       elsif counters % 3 != 0 && !file.nil?
-        print file.ljust(get_maximum_size(directory) + 5)
+        print file.ljust(maxsize_filesname + 5)
       end
     end
   end
-  one_third_file(directory)[-2..].each do |files|
+  one_third_file(filesname)[-2..].each do |files|
     files.each do |file|
       count += 1
       if (count % 4).zero?
         puts file
       elsif !file.nil?
-        print file.ljust(get_maximum_size(directory) + 5)
+        print file.ljust(maxsize_filesname + 5)
       end
     end
   end
 end
 
-# 3倍＋１のファイル数の時に合わせて出力（ファイル数が、9個未満の時）
-def output_when_files_size_is_special(directory)
+# 3倍＋１のファイル数の時に合わせて出力（転置後の配列の個数が2つの時）
+def output_when_files_size_is_special(filesname,maxsize_filesname)
+    binding.irb
   count = 0
-  one_third_file(directory)[-2..].each do |files|
+  one_third_file(filesname)[-2..].each do |files|
     files.each do |file|
       count += 1
       if (count % 4).zero?
         puts file
       elsif !file.nil?
-        print file.ljust(get_maximum_size(directory) + 5)
+        print file.ljust(maxsize_filesname + 5)
       end
     end
   end
 end
 
-# 引数を一つまたは渡さない時の出力
-def branch_output_type_when_argument_is_single(argument)
-  argument.each do |directory|
-    numbers_of_files = get_argument(directory).size
-    if (numbers_of_files % 3) == 1 && one_third_file(directory).size >= 3
-      output_files_size_is_special_big(directory)
-    elsif (numbers_of_files % 3) == 1
-      output_when_files_size_is_special(directory)
-    else
-      output(directory)
-    end
-  end
+def print_directory_name(directory)
+  puts "#{directory}"
 end
 
 # 引数を2個以上渡した時の出力(改行を入れるため)
-def branch_output_type_when_argument_is_multi(argument)
-  argument.each_with_index do |directory, idx|
-    last_numbers_of_files = get_argument(argument[idx - 1]).size
-    numbers_of_files = get_argument(directory).size
-    if (last_numbers_of_files % 3).zero? && argument.size >= 2 && idx >= 1
-      puts ' '
-    elsif idx >= 1 && last_numbers_of_files % 3 != 0 && argument.size >= 2
-      puts ' '
-      puts ' '
-    end
-    puts "#{directory}:"
-    if (numbers_of_files % 3) == 1 && one_third_file(directory).size >= 3
-      output_files_size_is_special_big(directory)
-    elsif (numbers_of_files % 3) == 1
-      output_when_files_size_is_special(directory)
-    else
-      output(directory)
-    end
+def branch_output_type_when_argument_is_multi(directory, multiple_argument,filesname)
+  print_directory_name(directory)if multiple_argument
+  numbers_of_files = filesname.size
+  maxsize_filesname = get_maximum_size(filesname)
+  if (numbers_of_files % 3) == 1 && one_third_file(filesname).size >= 3
+    output_files_size_is_special_big(filesname,maxsize_filesname)
+  elsif (numbers_of_files % 3) == 1
+    output_when_files_size_is_special(filesname,maxsize_filesname)
+  else
+    output(filesname,maxsize_filesname)
   end
 end
 
-if argument.size > 1
-  branch_output_type_when_argument_is_multi(argument)
-else
-  branch_output_type_when_argument_is_single(argument)
+directories = argument
+directories.each do |directory|
+  filesname = Dir.glob('*', base: directory)
+	branch_output_type_when_argument_is_multi(directory, directories.size > 1,filesname)
 end
