abc14db3955d51d4aa34205d7864461e55457948
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-11-26 12:05:52 +0900

    shots変数を追加しsumメソッドをでscoreを返すようにした

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -9,12 +9,11 @@ class Frame
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
     @third_shot = Shot.new(third_mark)
+    @shots = [@first_shot, @second_shot, @third_shot]
   end
 
   def score
-    return 10 if first_shot.score == 'X'
-
-    [first_shot.score + second_shot.score + @third_shot.score].sum
+    @shots.sum(&:score)
   end
 
   def strike?
