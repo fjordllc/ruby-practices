7dffb6ad8be73c3a513127700ad945244e629058
OdenTakashi <odentakashi@icloud.com>
2021-12-01 15:55:54 +0900

    分岐を少なくした
    分岐が多かったので分岐を減らした

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -1,84 +1,71 @@
-# frozen_string_literal: true
-
+#argument = ['../']
 argument = ARGV
-argument.push(Dir.pwd) if argument.size <= 0
+argument.push(Dir.pwd) if argument.size < 1
 
+#step1
+  #1filesname = [file1, file2, file3, file4]
+  #'+ 5'はlsコマンドと幅が一緒なため付与
 
-# ファイルの最大の文字数の値を取得
-def get_maximum_size(filesname)
-  argument_of_size = filesname.map(&:size)
-  argument_of_size.max
+#step2
+  #filesname = [file1, file2, file3, nil, file4, nil]
+def get_number_of_count_files_mod_by_three(filesname)
+  max_column_length = 3
+  count_files = filesname.count
+  count_files_mod_by_three = count_files % max_column_length
+  get_max_line_length(count_files_mod_by_three, count_files, max_column_length, filesname)
 end
 
-# 3の倍数にファイルの要素数を調整
-def complete_array(filesname)
-  add_nil_to_array_of_files = filesname
-  if add_nil_to_array_of_files.size % 3 != 0
-    (3 - add_nil_to_array_of_files.size % 3).times do
-      add_nil_to_array_of_files.push(nil)
-    end
-  end
-  add_nil_to_array_of_files
-end
-
-# 実際の出力に合わせて転置
-def one_third_file(filesname)
-  one_third_mom = complete_array(filesname).size / 3
-  one_third_file = complete_array(filesname).each_slice(one_third_mom).to_a
-  one_third_file.transpose
-end
-
-def base_of_output(filesinformation,padding)
-	filesinformation.flatten.each.with_index(1) do |file, index|
-		if (index % 3).zero? && !file.nil?
-  	  puts file
-  	elsif index % 3 != 0 && !file.nil?
-  	  print file.ljust(padding) 
-		end
-	end
-end
-
-# 3倍＋１のファイル数の時に合わせて出力（転置後の配列の個数が2つの時）
-def output_when_files_size_is_special(filesinformation,padding)
-  filesinformation[-2..].flatten.each.with_index(1) do |file, index|
-    if (index % 4).zero?
-      puts file
-    elsif !file.nil?
-      print file.ljust(padding)
-    end
-  end
-end
-
-# 3倍＋１のファイル数の時に合わせて出力（転置後の配列の個数が3個以上の時）
-def output_files_size_is_special_big(filesinformation,padding)
-  base_of_output(filesinformation[0...-2],padding)
-	output_files_size_is_special(filesinformation, padding)
-end
-
-def print_directory_name(directory)
-  puts "#{directory}"
-end
-
-# 引数を2個以上渡した時の出力(改行を入れるため)
-def branch_output_type_when_argument_is_multi(directory, multiple_argument,filesname)
-  print_directory_name(directory)if multiple_argument
-
-  numbers_of_files = filesname.size
-  padding = get_maximum_size(filesname) + 5
-	filesinformation = one_third_file(filesname)
-
-  if (numbers_of_files % 3) == 1 && filesinformation.size >= 3
-    output_files_size_is_special_big(filesinformation,padding)
-  elsif (numbers_of_files % 3) == 1
-    output_when_files_size_is_special(filesinformation,padding)
+def get_max_line_length(count_files_mod_by_three, count_files, max_column_length, filesname)
+  max_line_length = if count_files_mod_by_three == 0
+    count_files / max_column_length
   else
-    base_of_output(filesinformation,padding)
+    (count_files / max_column_length).next
   end
-
+  decide_position_of_nil(max_line_length, filesname, count_files_mod_by_three)
 end
 
-directories = argument
-directories.each do |directory|
+def decide_position_of_nil(max_line_length, filesname, count_files_mod_by_three)
+  the_end_of_second_column_index = (max_line_length * 2).pred
+  the_end_of_third_column_index = (max_line_length * 3).pred
+  completement_with_nil(the_end_of_second_column_index, the_end_of_third_column_index, filesname, max_line_length, count_files_mod_by_three)
+end
+
+def completement_with_nil(the_end_of_second_column_index, the_end_of_third_column_index, filesname, max_line_length, count_files_mod_by_three)
+  padding = filesname.map(&:size).max + 5
+  case count_files_mod_by_three
+  when 1
+    filesname.insert(the_end_of_second_column_index, nil)
+    filesname.insert(the_end_of_third_column_index, nil)
+  when 2
+    filesname.insert(the_end_of_third_column_index, nil)
+  else
+    filesname
+  end
+  devide_files_by_columns(max_line_length, padding, filesname)
+end
+#step3
+  #filesname = [[file1, file2], [file3, nil], [file4, nil]]
+def devide_files_by_columns(max_line_length, padding, filesname)
+  devide_files_by_column = filesname.each_slice(max_line_length).to_a
+  transpose_files_devided(devide_files_by_column, padding)
+end
+
+#step4
+  #filesname = [[file1, file3, file4], [file2, nil, nil]]
+def transpose_files_devided(devide_files_by_column, padding)
+  create_file_matrix = devide_files_by_column.transpose
+  output(create_file_matrix, padding)
+end
+
+#step5
+  #標準出力に出力
+def output(create_file_matrix, padding) 
+  create_file_matrix.each do |file_line|
+    puts file_line.compact.join(' ')
+  end
+end
+
+argument.each do |directory|
   filesname = Dir.glob('*', base: directory)
-	branch_output_type_when_argument_is_multi(directory, directories.size > 1,filesname)
+  get_number_of_count_files_mod_by_three(filesname)
 end
