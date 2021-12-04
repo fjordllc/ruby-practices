1c9b907024d968db236b24e9ec8b603af4084a73
masanarih0ri <masanarihoridevelop@gmail.com>
2021-12-04 23:30:43 +0900

    modify output files logic and method name

--- 05.ls/ls_with_l.rb
+++ 05.ls/ls_with_l.rb
@@ -46,80 +46,6 @@ end
 # 以下、-lオプションの対応
 DISPLAY_DETAILS_COUNT = 7
 
-def build_divided_file_details
-  current_directory_files = Dir.glob('*')
-  divided_file_details = Array.new(DISPLAY_DETAILS_COUNT).map { [] }
-  current_directory_files.each do |file|
-    stat = File.lstat(file)
-    file_mode = stat.mode.to_s(8)
-    p file_mode
-    file_mode = file_mode[0] == '1' ? file_mode : "#{sprintf("%06d", file_mode)}"
-    divided_file_details[0] << translate_mode(file_mode)
-    divided_file_details[1] << stat.nlink.to_s
-    divided_file_details[2] << translate_uid(stat.uid)
-    divided_file_details[3] << translate_gid(stat.gid)
-    divided_file_details[4] << stat.size.to_s
-    divided_file_details[5] << translate_date(stat.mtime)
-    divided_file_details[6] << file
-  end
-  divided_file_details
-end
-
-def build_display_files
-  adjusted_file_details = []
-  build_divided_file_details.each.with_index do |data_list, index|
-    max_str_count = data_list.max_by(&:size).size
-    # ファイルサイズや日付などを右詰にしグループ名などを左詰するための処理
-    adjusted_file_details <<
-      if [1, 4, 5].include?(index)
-        data_list.map { |v| v.rjust(max_str_count) }
-      else
-        data_list.map { |v| v.ljust(max_str_count) }
-      end
-  end
-  adjusted_file_details.transpose
-end
-
-def total_blocks
-  current_directory_files = Dir.glob('*')
-  block_count = 0
-  current_directory_files.each do |file|
-    stat = File.lstat(file)
-    block_count += stat.blocks
-  end
-  print "total #{block_count}\n"
-end
-
-def display_l_option_files
-  total_blocks
-  build_display_files.each do |list|
-    list.each do |value|
-      suffix = "\n"
-      if value == list.last
-        print "#{value}#{suffix}"
-      else
-        print "#{value}  "
-      end
-    end
-  end
-end
-
-def translate_uid(uid)
-  Etc.getpwuid(uid).name
-end
-
-def translate_gid(gid)
-  Etc.getgrgid(gid).name
-end
-
-def translate_date(date)
-  if date.year == Time.now.year
-    "#{date.day} #{date.month} #{date.hour.to_s.rjust(2, '0')}:#{date.min.to_s.rjust(2, '0')}"
-  else
-    "#{date.day} #{date.month} #{date.year}"
-  end
-end
-
 FILE_TYPE_HASH = {
   '01' => 'p',
   '02' => 'c',
@@ -149,8 +75,81 @@ def translate_mode(mode)
   "#{file_type}#{file_access_right_user}#{file_access_right_group}#{file_access_right_other}"
 end
 
+def translate_uid(uid)
+  Etc.getpwuid(uid).name
+end
+
+def translate_gid(gid)
+  Etc.getgrgid(gid).name
+end
+
+def translate_date(date)
+  if date.year == Time.now.year
+    "#{date.day} #{date.month} #{date.hour.to_s.rjust(2, '0')}:#{date.min.to_s.rjust(2, '0')}"
+  else
+    "#{date.day} #{date.month} #{date.year}"
+  end
+end
+
+def format_input
+  current_directory_files = Dir.glob('*')
+  all_file_info = []
+  current_directory_files.each do |file|
+    stat = File.lstat(file)
+    file_mode = stat.mode.to_s(8)
+    file_mode = file_mode[0] == '1' ? file_mode : format('%06d', file_mode).to_s
+    all_file_info << translate_mode(file_mode)
+    all_file_info << stat.nlink.to_s
+    all_file_info << translate_uid(stat.uid)
+    all_file_info << translate_gid(stat.gid)
+    all_file_info << stat.size.to_s
+    all_file_info << translate_date(stat.mtime)
+    all_file_info << file
+  end
+  all_file_info.each_slice(DISPLAY_DETAILS_COUNT).to_a
+end
+
+def adjusted_files
+  adjusted_file_info = []
+  format_input.transpose.each.with_index do |data_list, index|
+    max_str_count = data_list.max_by(&:size).size
+    # ファイルサイズや日付などを右詰にしグループ名などを左詰するための処理
+    adjusted_file_info <<
+      if [1, 4, 5].include?(index)
+        data_list.map { |v| v.rjust(max_str_count) }
+      else
+        data_list.map { |v| v.ljust(max_str_count) }
+      end
+  end
+  adjusted_file_info.transpose
+end
+
+def total_blocks
+  current_directory_files = Dir.glob('*')
+  block_count = 0
+  current_directory_files.each do |file|
+    stat = File.lstat(file)
+    block_count += stat.blocks
+  end
+  print "total #{block_count}\n"
+end
+
+def output_files
+  total_blocks
+  adjusted_files.each do |list|
+    list.each do |value|
+      suffix = "\n"
+      if value == list.last
+        print "#{value}#{suffix}"
+      else
+        print "#{value}  "
+      end
+    end
+  end
+end
+
 if l_option?
-  display_l_option_files
+  output_files
 else
   display_files
 end
