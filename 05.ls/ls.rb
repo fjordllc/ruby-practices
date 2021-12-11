5aa03df85f99da080568f6bfc4d67d1e5f3097f6
lan <lanlehoang10@gmail.com>
2021-12-11 19:30:48 +0700

    fix to 3 columns maximum

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,21 +1,14 @@
 # frozen_string_literal: true
 
 def print_files(path)
-  files = path ? Dir.children(path).sort : Dir.children('.').sort
-  number = calculate_number(files.length)
+  files = path ? Dir.entries(path).sort : Dir.entries('.').sort
+  files = files.drop(2)
+  number = (files.length / 3).to_i + 2
   tab_files = files.each_slice(number).to_a
-  index = number - 1
-  (0..index).each do |i|
+  (0..(number-1)).each do |i|
     lines = tab_files.map { |file| file[i]&.slice(0, 15)&.ljust(20) unless file[i].nil? }.compact
     puts lines.join('') if lines.any?
   end
-end
-
-def calculate_number(length)
-  return 4 if length < 40
-
-  multiplier = length % 40
-  multiplier.zero? ? 4 : multiplier * 2
 end
 
 print_files(ARGV[0])
