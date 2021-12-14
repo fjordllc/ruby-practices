423d33c504543712ea4a7345fbb318d799f18676
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-12-14 19:34:05 +0900

    decoupled PrettyPrint from Wc class

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,56 +7,34 @@ require 'prettyprint'
 
 def main
   wc = Wc.new
-  wc.show
+  printer = Printer.new
+  wc.results.each { |result| printer.format(result) }
+  printer.format(wc.total, is_total: true) if wc.results.length > 1
+  printer.output
 end
 
 class Wc
+  attr_accessor :results, :total
+
   def initialize
     @results = []
-    @p2 = PrettyPrint.new
     @l_option, args = parse_options
     @results = args.map { |arg| count_line_num_bytes_words(arg) }
-
-    return unless @results.empty?
-
-    @results.push count_line_num_bytes_words
-  end
-
-  def show
-    total = @results.inject(
+    @results.push count_line_num_bytes_words if @results.empty?
+    @total = @results.inject(
       line_num: 0,
       words: 0,
       bytes: 0
     ) do |sum, result|
-      format(result)
       {
         line_num: sum[:line_num] + result[:line_num],
         words: sum[:words] + result[:words],
         bytes: sum[:bytes] + result[:bytes]
       }
     end
-    format(total, is_total: true) if @results.length > 1
-    @p2.flush
-    puts @p2.output
   end
 
   private
-
-  def format(result, is_total: false)
-    @p2.group do
-      @p2.text(result[:line_num].to_s.rjust(8))
-      unless @l_option
-        @p2.text(result[:words].to_s.rjust(8))
-        @p2.text(result[:bytes].to_s.rjust(8))
-      end
-      if is_total
-        @p2.text('total'.ljust(20).dup.prepend(' '))
-      elsif !result[:name].empty?
-        @p2.text(result[:name].to_s.ljust(60).dup.prepend(' '))
-      end
-      @p2.breakable
-    end
-  end
 
   def parse_options
     opts = OptionParser.new
@@ -88,6 +66,33 @@ class Wc
       bytes: lines.sum(&:bytesize),
       name: path
     }
+  end
+end
+
+class Printer
+  def initialize
+    @p2 = PrettyPrint.new
+  end
+
+  def format(result, is_total: false)
+    @p2.group do
+      @p2.text(result[:line_num].to_s.rjust(8))
+      unless @l_option
+        @p2.text(result[:words].to_s.rjust(8))
+        @p2.text(result[:bytes].to_s.rjust(8))
+      end
+      if is_total
+        @p2.text('total'.ljust(20).dup.prepend(' '))
+      elsif !result[:name].empty?
+        @p2.text(result[:name].to_s.ljust(60).dup.prepend(' '))
+      end
+      @p2.breakable
+    end
+  end
+
+  def output
+    @p2.flush
+    puts @p2.output
   end
 end
 
