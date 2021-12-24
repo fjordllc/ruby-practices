aaaba34f07a989f81f6508b0f84e54b29dbce1ee
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-12-24 19:04:47 +0900

    letを用いてインスタンスを生成する

--- 07.bowling_object/spec/shot_spec.rb
+++ 07.bowling_object/spec/shot_spec.rb
@@ -5,23 +5,23 @@ require './shot'
 RSpec.describe Shot do
   describe "Shot#score" do
     context "0のとき" do
+      let(:shot){ Shot.new('0') }
       it "scoreが0になること" do
-        @shot = Shot.new('0')
-        expect(@shot.score).to eq(0)
+        expect(shot.score).to eq(0)
       end
     end
 
     context "ストライクのとき" do
+      let(:shot){ Shot.new('X') }
       it "scoreが10になること" do
-        @shot = Shot.new('X')
-        expect(@shot.score).to eq(10)
+        expect(shot.score).to eq(10)
       end
     end
 
     context "5のとき" do
+      let(:shot){ Shot.new('5') }
       it "scoreが5になること" do
-        @shot = Shot.new('5')
-        expect(@shot.score).to eq(5)
+        expect(shot.score).to eq(5)
       end
     end
   end
