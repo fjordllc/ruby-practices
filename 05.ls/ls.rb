de3768e63740623fa537f4619a54746ce93d734f
shu913 <sss91327@icloud.com>
2021-12-26 10:36:41 +0900

    modify no_option_ls_command

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,24 +6,25 @@ def ls
 
   total_file = files.size
 
-  max_row = 3.0
-  columns = (total_file / max_row).ceil
+  maximum_width = 3.0
+  columns = (total_file / maximum_width).ceil
 
-  ary = []
+  lists = []
   files.each_slice(columns) do |list_of_file|
-    ary << list_of_file
+    lists << list_of_file
 
     # 最大要素数を取得して、その要素数に合わせる
-    max_size = ary.map(&:size).max
-    ary.map! { |it| it.values_at(0...max_size) }
+    max_size = lists.map(&:size).max
+    lists.map! { |it| it.values_at(0...max_size) }
   end
 
   # rowとcolumnの入れ替え
-  sort_of_lists = ary.transpose
+  sort_of_lists = lists.transpose
 
+  spacing_between_elements = 15
   sort_of_lists.each do |sort_of_file|
     sort_of_file.each do |s|
-      print s.to_s.ljust(15)
+      print s.to_s.ljust(spacing_between_elements)
     end
     print "\n"
   end
