16bcb421df0e657e9ca5735eccfb6eb1019f1b4d
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-03 06:50:10 +0900

    Fix rubocop warnings

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,13 @@
-(1..20).each { |i|
-  if i % 15 == 0
-    puts "FizzzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0 
-    puts "Buzz"
+# frozen_string_literal: true
+
+(1..20).each do |i|
+  if (i % 15).zero?
+    puts 'FizzzBuzz'
+  elsif (i % 3).zero?
+    puts 'Fizz'
+  elsif (i % 5).zero?
+    puts 'Buzz'
   else
     puts i
   end
-}
+end
