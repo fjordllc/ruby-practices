accca96009be79484a145bda8af22e3f37ecc426
karlley <karlley.jp@gmail.com>
2022-01-03 05:10:29 +0900

    Add: メソッド化している部分を削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,23 +2,19 @@
 require 'date'
 require 'optparse'
 
-def output_calender
-  today = Date.today
-  options = ARGV.getopts('y:m:')
-  year =  options['y']&.to_i || today.year
-  month = options['m']&.to_i || today.month
-  end_date = Date.new(year, month, -1).day
-  dates = (1..end_date).map do |d|
-    Date.new(year, month, d)
-  end
-
-  puts "#{month}月 #{year}".center(20)
-  puts ['日', '月', '火', '水', '木', '金', '土'].join(' ')
-  print '   ' * dates.first.wday
-  dates.each do |d|
-    print "#{d.day.to_s.rjust(2)} "
-    print "\n" if d.saturday?
-  end
+today = Date.today
+options = ARGV.getopts('y:m:')
+year =  options['y']&.to_i || today.year
+month = options['m']&.to_i || today.month
+end_date = Date.new(year, month, -1).day
+dates = (1..end_date).map do |d|
+  Date.new(year, month, d)
 end
 
-output_calender
+puts "#{month}月 #{year}".center(20)
+puts ['日', '月', '火', '水', '木', '金', '土'].join(' ')
+print '   ' * dates.first.wday
+dates.each do |d|
+  print "#{d.day.to_s.rjust(2)} "
+  print "\n" if d.saturday?
+end
