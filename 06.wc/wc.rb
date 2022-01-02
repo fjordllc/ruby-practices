48cee846096d8030be18e8872639825945f60c6b
masanarih0ri <masanarihoridevelop@gmail.com>
2022-01-02 15:17:44 +0900

    modify calc file summaries

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,26 +7,18 @@ def wc_command
   command_options = ARGV.getopts('l')
   if has_input_from_pipe
     standard_input_string = $stdin.read
-    file_summary = calc_file_summary(standard_input_string)
+    file_summary = calc_file_summaries(has_input_from_pipe, standard_input_string)
     output_file_summary(command_options, file_summary)
   else
     file_names = ARGV
     if file_names.length.positive?
-      file_summaries = calc_file_summaries(file_names)
+      file_summaries = calc_file_summaries(has_input_from_pipe, file_names)
       output_file_summaries(command_options, file_summaries)
     else
       standard_input_string = $stdin.read
       output_text_summary(command_options, standard_input_string)
     end
   end
-end
-
-def calc_file_summary(input)
-  file_summary = {}
-  file_summary[:line_count] = input.count("\n")
-  file_summary[:word_count] = input.tr("\n", ' ').split(' ').size
-  file_summary[:bytesize_count] = input.bytesize
-  file_summary
 end
 
 def output_file_summary(command_options, file_summary)
@@ -43,20 +35,24 @@ def output_file_detail(value)
   value.to_s.rjust(8)
 end
 
-def calc_file_summaries(file_names)
-  file_summaries = []
-  file_names.each do |file_name|
+def calc_file_summaries(has_input_from_pipe, file_inputs)
+  if has_input_from_pipe
     file_summary = {}
-    File.open(file_name) do |file|
-      file_contents = file.read
+    file_summary[:line_count] = file_inputs.count("\n")
+    file_summary[:word_count] = file_inputs.tr("\n", ' ').split(' ').size
+    file_summary[:bytesize_count] = file_inputs.bytesize
+    file_summary
+  else
+    file_inputs.map do |file_name|
+      file_summary = {}
+      file_contents = File.read(file_name)
       file_summary[:line_count] = file_contents.count("\n")
       file_summary[:word_count] = file_contents.split(' ').size
       file_summary[:bytesize_count] = file_contents.bytesize
       file_summary[:file_name] = file_name
+      file_summary
     end
-    file_summaries << file_summary
   end
-  file_summaries
 end
 
 def calc_total_file_summary(file_summaries)
