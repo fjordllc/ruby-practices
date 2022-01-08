f4c0e009a10ce9fe0de1cfff904c24e0ec2fb7d4
shu913 <sss91327@icloud.com>
2022-01-08 11:22:10 +0900

    modify ls -a

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,22 +3,22 @@
 
 require 'optparse'
 
-def list_file
+def list_of_elements
   options = ARGV.getopts('alr')
-  files = if options['a']
+  elements = if options['a']
             Dir.glob('*', File::FNM_DOTMATCH).sort
           else
             Dir.glob('*').sort
           end
 
-  total_file = files.size
+  total_element = elements.size
 
   maximum_width = 3.0
-  columns = (total_file / maximum_width).ceil
+  columns = (total_element / maximum_width).ceil
 
   lists = []
-  files.each_slice(columns) do |list_of_file|
-    lists << list_of_file
+  elements.each_slice(columns) do |list|
+    lists << list
 
     # 最大要素数を取得して、その要素数に合わせる
     max_size = lists.map(&:size).max
@@ -27,19 +27,19 @@ def list_file
   lists
 end
 
-def sort_list_file
+def main
   # rowとcolumnの入れ替え
-  sort_of_lists = list_file.transpose
+  sort_of_lists = list_of_elements.transpose
 
   # 配列の最大文字数を取得し、その文字数+余白分で等間隔表示する
   max_word_count = sort_of_lists.flatten.max_by { |x| x.to_s.length }
   spacing_between_elements = max_word_count.length + 15
 
-  sort_of_lists.each do |sort_of_file|
-    sort_of_file.each do |s|
+  sort_of_lists.each do |sort_of_list|
+    sort_of_list.each do |s|
       print s.to_s.ljust(spacing_between_elements)
     end
     print "\n"
   end
 end
-sort_list_file
+main
