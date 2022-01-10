a49b1bbcd072aa0ec02f5397302179341485036a
naganosinya <cknumber5@gmail.com>
2022-01-10 13:04:39 +0900

    -a オプションを追加

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,18 +1,28 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def main
-  display_column.each do |column|
-    column.each do |file|
-      print file.ljust(column_margin)
-    end
-    puts ''
+require 'pry'
+require 'optparse'
+
+def parse_option
+  opt = OptionParser.new
+  option = {}
+  opt.on('-a') { |boolean| p option[:a] = boolean }
+  opt.parse(ARGV)
+
+  if option_a?(option)
+    File::FNM_DOTMATCH
+  else
+    0
   end
 end
 
+def option_a?(option)
+  option.key?(:a)
+end
+
 def define_directory
-  file_path = ARGV
-  Dir.glob('*', base: file_path[0])
+  Dir.glob('*', parse_option)
 end
 
 def display_column
@@ -28,8 +38,16 @@ end
 def column_margin
   length = define_directory.map(&:length).max
   margin = 3
-
   length + margin
+end
+
+def main
+  display_column.each do |column|
+    column.each do |file|
+      print file.ljust(column_margin)
+    end
+    puts ''
+  end
 end
 
 main
