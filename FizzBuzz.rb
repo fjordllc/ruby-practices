a82e98221c5823d869ecfc6242d0dd3fc6b8e6bc
rurukanon <cwd.maho@gmail.com>
2022-01-12 18:07:33 +0900

    FizzBuzz問題の提出

---
+++ FizzBuzz.rb
@@ -0,0 +1,18 @@
+num = [*(1..20)]
+num.each do |x|
+
+  if x % 15 == 0
+   puts "FizzBuzz"
+ 
+  elsif x % 5 == 0
+    puts "Buzz"
+  
+  elsif x % 3 == 0
+    puts "Fizz"
+ 
+  else
+    puts x
+
+  end
+
+end
