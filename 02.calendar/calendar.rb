954645c1886afc6de6e04868e6a1c7f04b5961c7
ah <bechaleto@gmail.com>
2022-01-17 23:46:31 +0900

    余分な空白の修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -11,11 +11,11 @@ first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
 puts "       #{month}月 #{year}"
-puts ' 日 月 火 水 木 金 土'
+puts '日 月 火 水 木 金 土'
 
 print "   " * first_date.wday
 
 (first_date..last_date).each do |date|
-  print date.day.to_s.rjust(3)
+  print date.strftime('%e') + " "
   puts if date.saturday?
 end
