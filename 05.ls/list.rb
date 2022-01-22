6564d6b540012c63ad82115b97d5a9f902f620a5
karlley <karlley.jp@gmail.com>
2022-01-23 05:09:24 +0900

    処理をメソッドに分割

--- 05.ls/list.rb
+++ 05.ls/list.rb
@@ -1,8 +1,28 @@
 #!/usr/bin/env ruby
 
-entries = Dir.glob("*", base: Dir.getwd)
-max_line = (entries.count / 3.to_f).ceil
-
-entries.each_with_index do |e,i|
-    puts "#{entries[i].ljust(20)} #{entries[i + max_line].ljust(20)} #{entries[i + (max_line * 2)]}" if i < max_line
+def main
+  directory = ARGV[0] || Dir.getwd
+  max_row = 3
+  segments = Dir.glob("*", base: directory)
+  max_column = (segments.count / max_row.to_f).ceil
+  print_segments(max_row, segments, max_column)
 end
+
+def print_segments(max_row, segments, max_column)
+  columns = []
+  segments.each_with_index do |s,i|
+    if i < max_column
+      column = []
+      column << s.ljust(15)
+      (1..max_row - 1).each do |r|
+        column <<  segments[i + max_column * r].ljust(15) if i + max_column * r < segments.size
+      end
+      columns << column
+    end
+  end
+  columns.each do |c|
+    puts c.join("")
+  end
+end
+
+main
