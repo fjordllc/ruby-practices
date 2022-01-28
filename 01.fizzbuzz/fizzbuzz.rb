494214cbe27573543a849d83539667aed5bc9098
kosaku <bouru02@gmail.com>
2022-01-28 19:15:40 +0900

    githubコメントでの指摘を修正 & rubocopの警告を解消

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,18 +1,18 @@
-def fizzbuzz(number1=3, number2=5, max_count=20)
-    max_count.times do |i|
-        i += 1
-        common_multiple = number1*number2
-        case
-        when i%common_multiple == 0
-            puts 'FizzBuzz'
-        when i%number1 == 0
-            puts 'Fizz'
-        when i%number2 == 0
-            puts 'Buzz'
-        else
-            puts i
-        end
+# frozen_string_literal: true
+
+def fizzbuzz(number1 = 3, number2 = 5, max_count = 20)
+  1.upto(max_count) do |i|
+    common_multiple = number1 * number2
+    if (i % common_multiple).zero?
+      puts 'FizzBuzz'
+    elsif (i % number1).zero?
+      puts 'Fizz'
+    elsif (i % number2).zero?
+      puts 'Buzz'
+    else
+      puts i
     end
+  end
 end
 
 fizzbuzz
