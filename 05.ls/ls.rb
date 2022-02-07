ea5c5e8f79ce99d4b2121d110d9ddb941f3dc27b
caminotak <takjiroy@yahoo.co.jp>
2022-02-07 09:32:16 +0800

    3rd commit <列数を変数に変更>

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,19 +2,20 @@
 # frozen_string_literal: true
 
 names_original = Dir.glob('*') # コマンドを実行したディレクトリのファイル名の配列
+display_columns = 3 # 表示列数
 
 # メインのメソッド
-def print_ls(names_original)
-  names_tabbed = tabbing_names(names_original)
+def print_ls(names_original, display_columns)
+  names_tabbed = tabbing_names(names_original) # タブ揃えされたされた文字列の配列
+  display_lines = count_lines(names_tabbed, display_columns) # 表示行数
   names_vertical = [] # 空配列の中に行数分の空配列を追加（入れ子構造）
-  lines(names_tabbed).times do
+  display_lines.times do
     names_vertical << []
   end
   names_tabbed.each_with_index do |file, i| # 空配列に順番に値を追加、を繰り返す。
-    names_vertical[i % lines(names_tabbed)] << file.to_s
+    names_vertical[i % display_lines] << file.to_s
   end
-
-  names_vertical.each { |w| puts w.join }
+  names_vertical.each { |w| puts w.join } # 出力
 end
 
 # 取得したファイル名にタブを追加して長さを揃える
@@ -26,8 +27,8 @@ def tabbing_names(names_original)
 end
 
 # 取得した配列の要素数から表示行数を算出
-def lines(names_tabbed)
-  (names_tabbed.size - 1) / 3 + 1
+def count_lines(names_tabbed, display_columns)
+  (names_tabbed.size - 1) / display_columns + 1
 end
 
-print_ls(names_original)
+print_ls(names_original, display_columns)
