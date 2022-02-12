8ba7fcad038b07436b58eec0ea972730af4678e9
shu913 <sss91327@icloud.com>
2022-02-12 19:46:06 +0900

    ls -l

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,54 +2,124 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
-def options_get
-  ARGV.getopts('alr')
-end
+MAXIMUM_WIDTH = 3.0
 
-def elements_get
-  options = options_get
-  if options['a'] && options['r']
-    Dir.glob('*', File::FNM_DOTMATCH).sort.reverse
-  elsif options['a']
-    Dir.glob('*', File::FNM_DOTMATCH).sort
-  elsif options['r']
-    Dir.glob('*').sort.reverse
-  else
-    Dir.glob('*').sort
-  end
-end
+FILE_TYPE_LABELS =
+  {
+    'directory' => 'd',
+    'file' => '-',
+    'link' => 'l'
+  }.freeze
 
-def list_of_elements
-  elements = elements_get
-  total_element = elements.size
+PERMISSION_LABELS =
+  {
+    0 => '---',
+    1 => '--x',
+    2 => '-w-',
+    3 => '-wx',
+    4 => 'r--',
+    5 => 'r-x',
+    6 => 'rw-',
+    7 => 'rwx'
+  }.freeze
 
-  maximum_width = 3.0
-  columns = (total_element / maximum_width).ceil
+class Ls
+  def main
+    get_options = ARGV.getopts('arl')
 
-  lists = []
+    if get_options['a']
+      @elements = Dir.glob('*', File::FNM_DOTMATCH).sort
 
-  elements.each_slice(columns) do |list|
-    lists << list
+      list_of_elements
+      show_ls
+    elsif get_options['r']
+      @elements = Dir.glob('*').sort.reverse
 
-    # 最大要素数を取得して、その要素数に合わせる
-    max_size = lists.map(&:size).max
-    lists.map! { |it| it.values_at(0...max_size) }
-  end
-  lists
-end
+      list_of_elements
+      show_ls
+    elsif get_options['l']
+      @elements = Dir.glob('*').sort
 
-def main
-  # rowとcolumnの入れ替え
-  sort_of_lists = list_of_elements.transpose
-  # 配列の最大文字数を取得し、その文字数+余白分で等間隔表示する
-  max_word_count = sort_of_lists.flatten.max_by { |x| x.to_s.length }
-  spacing_between_elements = max_word_count.length + 15
-  sort_of_lists.each do |sort_of_list|
-    sort_of_list.each do |s|
-      print s.to_s.ljust(spacing_between_elements)
+      show_total_blocks
+      show_file_details
+    else
+      @elements = Dir.glob('*').sort
+
+      list_of_elements
+      show_ls
     end
+  end
+
+  def list_of_elements
+    total_element = @elements.size
+
+    columns = (total_element / MAXIMUM_WIDTH).ceil
+
+    lists = []
+
+    @elements.each_slice(columns) do |element|
+      lists << element
+
+      # 最大要素数を取得して、その要素数に合わせる
+      max_size = lists.map(&:size).max
+      lists.map! { |it| it.values_at(0...max_size) }
+    end
+    lists
+  end
+
+  def show_ls
+    # rowとcolumnの入れ替え
+    sort_of_lists = list_of_elements.transpose
+    # 配列の最大文字数を取得し、その文字数+余白分で等間隔表示する
+    max_word_count = sort_of_lists.flatten.max_by { |x| x.to_s.length }
+    spacing_between_elements = max_word_count.length + 15
+    sort_of_lists.each do |sort_of_list|
+      sort_of_list.each do |s|
+        print s.to_s.ljust(spacing_between_elements)
+      end
+      print "\n"
+    end
+  end
+
+  def show_total_blocks
+    blocks = []
+    @elements.each do |element|
+      @stat = File.stat(element)
+      blocks << @stat.blocks
+    end
+    print 'total '
+    print blocks.sum
     print "\n"
   end
+
+  def show_file_details
+    @elements.each do |element|
+      @stat = File.stat(element)
+      @element_mode = @stat.mode.to_s(8)
+
+      print FILE_TYPE_LABELS[@stat.ftype]
+
+      permission_number = @element_mode.to_i.digits.take(3).reverse
+      element_permission =
+        PERMISSION_LABELS[permission_number[0]] +
+        PERMISSION_LABELS[permission_number[1]] +
+        PERMISSION_LABELS[permission_number[2]]
+      print "#{element_permission} "
+
+      print "#{@stat.nlink} "
+
+      print "#{Etc.getpwuid(@stat.uid).name}  "
+      print "#{Etc.getgrgid(@stat.gid).name}  "
+
+      print "#{@stat.size}  "
+      print "#{File.mtime(element).strftime('%m %e %H:%M')}  "
+      print element
+
+      print "\n"
+    end
+  end
 end
-main
+ls = Ls.new
+ls.main
