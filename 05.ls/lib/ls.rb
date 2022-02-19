dfd85aafc3e735ab428e99536f65093c932d1950
seizaemon <oden@venus.dti.ne.jp>
2022-02-19 23:07:13 +0900

    feat: コンソール幅に応じて出力列数の調整
    fix: 空ディレクトリの際の表示

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'io/console/size'
 require 'debug'
 
 OUTPUT_MAX_COLUMNS = 3
@@ -9,7 +10,7 @@ OUTPUT_MAX_COLUMNS = 3
 module LsFilters
   class << self
     # 隠しファイルフィルタ
-    def hidden_files
+    def hide_hidden_files
       ->(entry) { entry !~ /^\./ }
     end
     # フィルタを追加していく
@@ -17,20 +18,24 @@ module LsFilters
 end
 
 class Ls
-  def initialize(entry, filters = [LsFilters.hidden_files])
-    @entry_name = entry
+  attr_writer :entry_name, :filters
+
+  def initialize(filters = [LsFilters.hide_hidden_files])
     @filters = filters
   end
 
   def output
-    entries = Dir.entries(@entry_name)
+    raise "ファイルまたはディレクトリが見つかりません: #{@entry_name}" unless File.exist?(@entry_name)
 
+    entries = Dir.entries(@entry_name)
     # 出力フィルタの適用
     @filters.each do |filter|
       entries.filter! { |e| filter.call(e) }
     end
 
     formatted = create_list_data(format_entries(entries.sort))
+    return if formatted.size.zero?
+
     max_rows = formatted[0].size
 
     (0..max_rows - 1).each do |n|
@@ -39,7 +44,7 @@ class Ls
         row << column[n] unless column[n].nil?
       end
 
-      puts row.join(" ").rstrip if row.count.positive?
+      puts row.join(' ').rstrip if row.count.positive?
     end
     0 # return code(success)
   end
@@ -53,24 +58,48 @@ class Ls
 
     entries.map do |entry|
       # 半角スペースに換算した時の文字数
-      string_width = entry.chars.map { |c| c.ascii_only? ? 1 : 2 }.sum
-      real_width = entry.size
+      string_width = entry.space_size
+      num_of_chars = entry.size
       # 右に埋めるべき空白数
-      supplement_spaces = max_width - string_width
-      format("%-#{real_width + supplement_spaces}s", entry)
+      spaces_of_completement = max_width - string_width
+      format("%-#{num_of_chars + spaces_of_completement}s", entry)
     end
   end
 
+  def console_width
+    IO.console_size[1]
+  end
+
   def create_list_data(entries)
+    return entries if entries.size.zero?
+
+    string_width = entries[0].space_size
+    num_of_columns = OUTPUT_MAX_COLUMNS
+
     # コンソールウインドウの幅に応じて列幅を調節する
-    columns = (1..OUTPUT_MAX_COLUMNS).map { [] }
-    max_rows = entries.size / OUTPUT_MAX_COLUMNS
-    max_rows += (entries.size % OUTPUT_MAX_COLUMNS).positive? ? 1 : 0
+    (1..OUTPUT_MAX_COLUMNS).to_a.reverse_each do |n|
+      if n * string_width <= console_width
+        num_of_columns = n
+        break
+      end
+    end
+
+    columns = (1..num_of_columns).map { [] }
+    max_rows = entries.size / num_of_columns
+    max_rows += (entries.size % num_of_columns).positive? ? 1 : 0
+
     entries.each_with_index do |entry, index|
       column_index = index / max_rows
       columns[column_index].push entry
     end
     columns
+  end
+end
+
+class String
+  # 空白に換算したときの文字幅を計算する
+  def space_size
+    chars.map { |c| c.ascii_only? ? 1 : 2 }.sum
   end
 end
 
@@ -81,14 +110,13 @@ def main
   argv = opt.parse(ARGV)
 
   # オプションに応じた出力フィルタ追加（予定）
-  filters = []
-  filters << LsFilters.hidden_files
+  filters = [LsFilters.hide_hidden_files]
+
+  ls = Ls.new(filters)
 
   argv = ['./'] if argv.count.zero?
   argv.each do |arg|
-    ls = Ls.new(arg, filters)
-    raise "ファイルまたはディレクトリが見つかりません: #{arg}" unless File.exist?(arg)
-
+    ls.entry_name = arg
     ls.output
   end
 end
--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -7,35 +7,51 @@ require_relative '../lib/ls'
 require 'tmpdir'
 require 'fileutils'
 require 'stringio'
+require 'io/console/size'
 
 class TestTools
   attr_reader :test_dir
 
   def initialize
-    # テスト用ディレクトリ生成
     @test_dir = Dir.mktmpdir
   end
 
-  # ファイル生成
-  def create_tmp_files(num = 1, is_ja: false)
-    prefix = is_ja ? '日本語のファイル' : 'test_file'
-    tmpfiles = []
-    (1..num).each do |n|
-      File.open("#{@test_dir}/#{prefix}#{n}", 'w+') { |file| file.write('test') }
-      tmpfiles << "#{prefix}#{n}"
+  def create_tmp_files(num_of_ascii = 1, num_of_ja = 0, auto_clean: true)
+    tmp_files = []
+    if num_of_ascii.positive?
+      (1..num_of_ascii).each do |n|
+        File.open("#{@test_dir}/test_file#{n}", 'w+') {}
+        tmp_files << "test_file#{n}"
+      end
     end
-    tmpfiles
+
+    if num_of_ja.positive?
+      (1..num_of_ja).each do |n|
+        File.open("#{@test_dir}/日本語のファイル#{n}", 'w+') {}
+        tmp_files << "日本語のファイル#{n}"
+      end
+    end
+    yield
+    remove_entries(tmp_files) if auto_clean
   end
 
-  # ディレクトリ生成
-  def create_tmp_dirs(num = 1, is_ja: false)
-    tmpdirs = []
-    prefix = is_ja ? '日本語のディレクトリ' : 'test_dir'
-    (1..num).each do |n|
-      Dir.mkdir("#{@test_dir}/#{prefix}#{n}")
-      tmpdirs << "#{prefix}#{n}"
+  def create_tmp_dirs(num_of_ascii = 1, num_of_ja = 0, auto_clean: true)
+    tmp_dirs = []
+    if num_of_ascii.positive?
+      (1..num_of_ascii).each do |n|
+        Dir.mkdir("#{@test_dir}/test_dir#{n}")
+        tmp_dirs << "test_dir#{n}"
+      end
     end
-    tmpdirs
+
+    if num_of_ja.positive?
+      (1..num_of_ja).each do |n|
+        Dir.mkdir("#{@test_dir}/日本語のディレクトリ#{n}")
+        tmp_dirs << "日本語のディレクトリ#{n}"
+      end
+    end
+    yield
+    remove_entries(tmp_dirs) if auto_clean
   end
 
   # テストディレクトリ削除（再帰的に全て削除）
@@ -44,14 +60,13 @@ class TestTools
   end
 
   # テストディレクトリ内の削除
-  def remove_all_entries(entries)
+  def remove_entries(entries)
     entries.each do |entry|
       FileUtils.remove_entry_secure "#{@test_dir}/#{entry}"
     end
   end
 
-  # 標準出力のキャプチャ
-  def capture_result(ls_instance)
+  def capture_stdout(ls_instance)
     out = StringIO.new
     $stdout = out
     ls_instance.output
@@ -64,75 +79,76 @@ end
 class LsTest < Minitest::Test
   def setup
     @test_tools = TestTools.new
-    @test_ls = Ls.new(@test_tools.test_dir)
+    # テストのために十分なコンソールの幅がなかった場合はエラーとする
+    raise "テストに必要なコンソールの表示幅がありません。#{22 * 3}以上の幅を確保してください" if IO.console_size[1] < 66
+
+    @test_ls = Ls.new
+    @test_ls.entry_name = @test_tools.test_dir
+  end
+
+  def test_vacant_dir
+    assert_equal '', @test_tools.capture_stdout(@test_ls)
   end
 
   def test_one_file_with_ascii_name
-    files = @test_tools.create_tmp_files(1)
-    assert_equal "test_file1\n", @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(files)
+    @test_tools.create_tmp_files(1) do
+      assert_equal "test_file1\n", @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_one_file_with_japanese_name
-    files = @test_tools.create_tmp_files(1, is_ja: true)
-    assert_equal "日本語のファイル1\n", @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(files)
+    @test_tools.create_tmp_files(0, 1) do
+      assert_equal "日本語のファイル1\n", @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_one_dir_with_ascii_name
-    dirs = @test_tools.create_tmp_dirs(1)
-    assert_equal "test_dir1\n", @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(dirs)
+    @test_tools.create_tmp_dirs(1) do
+      assert_equal "test_dir1\n", @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_one_dir_with_japanese_name
-    dirs = @test_tools.create_tmp_dirs(1, is_ja: true)
-    assert_equal "日本語のディレクトリ1\n", @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(dirs)
+    @test_tools.create_tmp_dirs(0, 1) do
+      assert_equal "日本語のディレクトリ1\n", @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_many_files_with_same_format
-    files = @test_tools.create_tmp_files(7)
     expected = <<~TEXT
       test_file1 test_file4 test_file7
       test_file2 test_file5
       test_file3 test_file6
     TEXT
-    assert_equal expected, @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(files)
+    @test_tools.create_tmp_files(7) do
+      assert_equal expected, @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_many_japanese_files_with_same_format
-    files = @test_tools.create_tmp_files(7, is_ja: true)
     expected = <<~TEXT
       日本語のファイル1 日本語のファイル4 日本語のファイル7
       日本語のファイル2 日本語のファイル5
       日本語のファイル3 日本語のファイル6
     TEXT
-    assert_equal expected, @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(files)
+    @test_tools.create_tmp_files(0, 7) do
+      assert_equal expected, @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_many_files_mixed_format
-    entries = @test_tools.create_tmp_files(4)
-    entries << @test_tools.create_tmp_files(3, is_ja: true)
-    entries.flatten!
     # test_file1 = 9スペース分 日本語のファイル1 = 17スペース分
     expected = <<~TEXT
       test_file1        test_file4        日本語のファイル3
       test_file2        日本語のファイル1
       test_file3        日本語のファイル2
     TEXT
-    assert_equal expected, @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(entries)
+    @test_tools.create_tmp_files(4, 3) do
+      assert_equal expected, @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def test_many_entries_mixed_format
-    entries = @test_tools.create_tmp_files(2)
-    entries << @test_tools.create_tmp_files(2, is_ja: true)
-    entries << @test_tools.create_tmp_dirs(2)
-    entries << @test_tools.create_tmp_dirs(2, is_ja: true)
-    entries.flatten!
     # test_file1 = 10スペース分
     # 日本語のファイル1 = 17スペース分
     # test_dir = 8スペース分
@@ -142,8 +158,11 @@ class LsTest < Minitest::Test
       test_dir2             日本語のディレクトリ1 日本語のファイル2
       test_file1            日本語のディレクトリ2
     TEXT
-    assert_equal expected, @test_tools.capture_result(@test_ls)
-    @test_tools.remove_all_entries(entries)
+
+    @test_tools.create_tmp_files(2, 2, auto_clean: false) {}
+    @test_tools.create_tmp_dirs(2, 2) do
+      assert_equal expected, @test_tools.capture_stdout(@test_ls)
+    end
   end
 
   def teardown
