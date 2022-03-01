5b6ea3ad3ef7fc886565864eace2e55b8305eec2
choco <superbeaver.180809@gmail.com>
2022-03-02 00:00:22 +0900

    [remove]不要なコメント削除

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
-# score = ARGV
-# score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-# score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-# score = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-# score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-# score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'
-# score = 'X,X,X,X,X,X,X,X,X,X,X,X '
+score = ARGV
 score_array = score.split(',')
 score_array_num = []
 score_array.each do |s|
@@ -23,7 +17,6 @@ score_array_num.each_slice(2) do |s|
 end
 total_score = 0
 frames.each_with_index do |f, index|
-  # ストライクの場合
   total_score = if index + 1 >= 10
                   total_score + f.sum
                 elsif f[0] == 10 && frames[index + 1][0] == 10
