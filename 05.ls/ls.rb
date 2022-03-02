89e6fe89ca77fc753801f621b1b5db4751ca1308
kurosutomohiro <kurosu.66@gmail.com>
2022-03-03 08:49:24 +0900

    lsコマンド作成

---
+++ 05.ls/ls.rb
@@ -0,0 +1,41 @@
+# #!/usr/bin/env ruby
+
+# ファイル取得
+files = Dir.glob('*').sort
+
+# 後にtransposeを使用するため、filesの中身の数が3で割り切れる数になるまで空白を配列に追加する
+until files.size % 3 == 0
+  files << " "
+end
+
+# 左揃えにするため、20を上限としてfilesの各要素に空白を追加した配列を作成
+space_added_files = files.map do |file|
+  file.ljust(20, " ")
+end
+
+# 取得したファイルを分割して3つの配列に格納
+array1 = []
+array2 = []
+array3 = []
+
+divide = (files.size / 3).ceil
+space_added_files.each.with_index(1) do |file, i|
+  if array1.size < divide
+    array1 << file
+  elsif array2.size < divide
+    array2 << file
+  else
+    array3 << file
+  end
+end
+
+# transposeで行列を逆にする
+transposed_array = [
+  array1,
+  array2,
+  array3].transpose
+
+# 出力
+transposed_array.each do |v|
+  puts v.join(" ")
+end
