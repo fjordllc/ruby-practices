15cd20cfa9f0bb69e6d0e1e8bab05390a865f267
daiki0381 <daiki03810218@gmail.com>
2022-03-09 00:16:38 +0900

    Fix ls1.rb

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -1,26 +1,21 @@
 # frozen_string_literal: true
 
 # !/usr/bin/env ruby
-dir = Dir.glob('*')
-dir.sort
-arrs = Array.new(3) { [] }
-division_number = 3
-element_number = Rational(dir.size, division_number).ceil
-indent = 0
-dir.each do |file|
-  arrs[indent] << file
-  indent += 1 if (arrs[indent].size % element_number).zero?
+NUMBEROFCOLUMNS = 3
+dirs = Dir.glob('*').sort
+arrs_containing_dirs = Array.new(NUMBEROFCOLUMNS) { [] }
+number_of_elements_per_column = Rational(dirs.size, NUMBEROFCOLUMNS).ceil
+index = 0
+dirs.each do |dir|
+  arrs_containing_dirs[index] << dir
+  index += 1 if (arrs_containing_dirs[index].size % number_of_elements_per_column).zero?
 end
-max_element_number = arrs.map(&:size).max
-arrs.map! { |arr| arr.values_at(0...max_element_number) }
-row_column_replacement = arrs.transpose
-row_column_replacement.each do |arr|
-  arr.map! { |x| x.nil? ? '' : x }
-end
-max_word_count = dir.map(&:length).max
-row_column_replacement.each do |arr|
-  print arr[0].ljust(max_word_count + 7)
-  print arr[1].ljust(max_word_count + 7)
-  print arr[2]
+arrs_containing_dirs.map! { |arr| arr.values_at(0...number_of_elements_per_column) }
+arrs_with_rows_and_columns_swapped = arrs_containing_dirs.transpose
+maximum_number_of_words = dirs.map(&:size).max
+arrs_with_rows_and_columns_swapped.each do |arr|
+  arr.each do |dir|
+    print dir.to_s.ljust(maximum_number_of_words + 7)
+  end
   print "\n"
 end
