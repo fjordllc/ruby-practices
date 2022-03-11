682e7aabd778561aca362e0fac262a11cbdaed58
daiki0381 <daiki03810218@gmail.com>
2022-03-11 15:38:02 +0900

    Fix ls1.rb

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 # !/usr/bin/env ruby
-NUMBEROFCOLUMNS = 3
+NUMBER_OF_COLUMNS = 3
+
 dirs = Dir.glob('*').sort
-arrs_containing_dirs = Array.new(NUMBEROFCOLUMNS) { [] }
-number_of_elements_per_column = Rational(dirs.size, NUMBEROFCOLUMNS).ceil
+arrs_containing_dirs = Array.new(NUMBER_OF_COLUMNS) { [] }
+number_of_elements_per_column = Rational(dirs.size, NUMBER_OF_COLUMNS).ceil
 index = 0
 dirs.each do |dir|
   arrs_containing_dirs[index] << dir
   index += 1 if (arrs_containing_dirs[index].size % number_of_elements_per_column).zero?
 end
-arrs_containing_dirs.map! { |arr| arr.values_at(0...number_of_elements_per_column) }
-arrs_with_rows_and_columns_swapped = arrs_containing_dirs.transpose
+unity_number_of_arr_elements = arrs_containing_dirs.map { |arr| arr.values_at(0...number_of_elements_per_column) }
+arrs_with_rows_and_columns_swapped = unity_number_of_arr_elements.transpose
 maximum_number_of_words = dirs.map(&:size).max
 arrs_with_rows_and_columns_swapped.each do |arr|
   arr.each do |dir|
