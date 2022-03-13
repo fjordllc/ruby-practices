84d2efda6d077eef3b58f6684f13745aa6a4bcf9
daiki0381 <daiki03810218@gmail.com>
2022-03-13 13:47:59 +0900

    Fix ls1.rb

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -3,20 +3,30 @@
 # !/usr/bin/env ruby
 NUMBER_OF_COLUMNS = 3
 
-dirs = Dir.glob('*').sort
-arrs_containing_dirs = Array.new(NUMBER_OF_COLUMNS) { [] }
-number_of_elements_per_column = Rational(dirs.size, NUMBER_OF_COLUMNS).ceil
-index = 0
-dirs.each do |dir|
-  arrs_containing_dirs[index] << dir
-  index += 1 if (arrs_containing_dirs[index].size % number_of_elements_per_column).zero?
+def get_dirs
+  Dir.glob('*').sort
 end
-unity_number_of_arr_elements = arrs_containing_dirs.map { |arr| arr.values_at(0...number_of_elements_per_column) }
-arrs_with_rows_and_columns_swapped = unity_number_of_arr_elements.transpose
-maximum_number_of_words = dirs.map(&:size).max
-arrs_with_rows_and_columns_swapped.each do |arr|
-  arr.each do |dir|
-    print dir.to_s.ljust(maximum_number_of_words + 7)
+
+def add_dirs_to_arrs
+  arrs_containing_dirs = Array.new(NUMBER_OF_COLUMNS) { [] }
+  number_of_elements_per_column = Rational(get_dirs.size, NUMBER_OF_COLUMNS).ceil
+  index = 0
+  get_dirs.each do |dir|
+    arrs_containing_dirs[index] << dir
+    index += 1 if (arrs_containing_dirs[index].size % number_of_elements_per_column).zero?
   end
-  print "\n"
+  unity_number_of_arr_elements = arrs_containing_dirs.map { |arr| arr.values_at(0...number_of_elements_per_column) }
+  unity_number_of_arr_elements.transpose
 end
+
+def output_dirs
+  maximum_number_of_words = get_dirs.map(&:size).max
+  add_dirs_to_arrs.each do |arr|
+    arr.each do |dir|
+      print dir.to_s.ljust(maximum_number_of_words + 7)
+    end
+    print "\n"
+  end
+end
+
+output_dirs
