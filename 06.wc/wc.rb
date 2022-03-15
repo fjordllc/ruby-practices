4189d5b9d07dbb2b81c70099a76af2a86c957eb4
uchihiro <tohchi4122@gmail.com>
2022-03-15 12:36:57 +0900

    rubocopの指摘を修正
    rubocopに指摘された点を修正しました。

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -11,30 +11,30 @@ def main
   output_string_data(params, string_data, total_files_data)
 end
 
-#ファイル名をwcの出力結果にくっつける
-
 def collect_string
-  collected_string = []
-  ARGV.empty? ? collected_string << $stdin.read : ARGV.map { |file| collected_string << File.read(file) }
-  collected_string
-end
-
-def collect_filename
-  files_name = []
-  ARGV.map do |file|
-    file_name = {filename: File.basename(file)}
-    files_name << file_name
+  strings_data = []
+  if ARGV.empty?
+    strings_data << { string: $stdin.read }
+  else
+    ARGV.map do |file|
+      string_data = {
+        string: File.read(file),
+        filename: File.basename(file)
+      }
+      strings_data << string_data
+    end
   end
-  files_name
+  strings_data
 end
 
 def collect_string_data(collected_string)
   strings_data = []
   collected_string.map do |string|
     string_data = {
-      lines: string.count("\n"),
-      words: string.split(/\s+/).size,
-      bytes: string.bytesize
+      lines: string[:string].count("\n"),
+      words: string[:string].split(/\s+/).size,
+      bytes: string[:string].bytesize,
+      filename: string[:filename]
     }
     strings_data << string_data
   end
@@ -49,21 +49,19 @@ def sum_file_data(string_data)
   }
 end
 
-#ここも lines words bytes filename option を渡すと決められたフォーマットで出力するメソッドを定義しておくと、
-#出力フォーマットを1箇所にまとめられそうですね。
-
 def output_string_data(params, string_data, total_files_data)
   string_data.each do |string|
-    print "#{string[:lines].to_s.rjust(8)}"
-    print "#{string[:words].to_s.rjust(8)} #{string[:bytes].to_s.rjust(7)} " unless params['l']
-    puts "\n"
+    print string[:lines].to_s.rjust(8)
+    print ' '
+    print "#{string[:words].to_s.rjust(7)}#{string[:bytes].to_s.rjust(8)} " unless params['l']
+    puts string[:filename]
   end
 
-  if string_data.size > 1
-    print "#{total_files_data[:total_of_lines].to_s.rjust(8)}" 
-    print "#{total_files_data[:total_of_words].to_s.rjust(8)} #{total_files_data[:total_of_bytes].to_s.rjust(7)}" unless params['l']
-    puts " total"
-  end
+  return unless string_data.size > 1
+
+  print total_files_data[:total_of_lines].to_s.rjust(8)
+  print "#{total_files_data[:total_of_words].to_s.rjust(8)}#{total_files_data[:total_of_bytes].to_s.rjust(8)}" unless params['l']
+  puts ' total'
 end
 
 main
