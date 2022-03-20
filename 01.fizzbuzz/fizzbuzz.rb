82d2fa758cc267f4296a9f26eb57098a89e9eacc
takuya3769 <dasgquhf11@gmail.com>
2022-03-20 22:11:07 +0900

    FizzBuzzプログラム作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+range = (1..20)
+
+range.each do |x|
+  if x % 15 == 0
+    puts "FizzBuzz"
+  elsif x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  else
+    puts x
+  end
+end
