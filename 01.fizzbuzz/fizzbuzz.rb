27f89721cbf8d275e4f8308b4379486ef9745055
shinshinshin123 <lingmushennaijie@gmail.com>
2022-03-21 22:57:39 +0900

    BazzをBuzzに変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,11 +2,11 @@ numbers = (1..20)
 
 numbers.each do |number|
   if number % 3 == 0 && number % 5 == 0
-    puts "FizzBazz"
+    puts "FizzBuzz"
   elsif number % 3 == 0
     puts "Fizz"
   elsif number % 5 == 0
-    puts "Bazz"
+    puts "Buzz"
   else
     puts number
   end
