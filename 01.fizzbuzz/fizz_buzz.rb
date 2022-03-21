d3935aa492ceb7d804d9948f8f24de0f746334f9
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-03-21 22:56:22 +0900

    refactor
    クラスを作成し、メソッドとして呼び出せるように修正

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -1,16 +1,34 @@
-# @param number
-# @return Fizz Buzz FizzBuzzのいずれかの文字列
-# FizzBuzzのチェクメソッドです
-def check_fizz_buzz(number)
-  if number % 3 == 0 && number % 5 != 0
-    puts "Fizz"
-  elsif number % 3 != 0 && number % 5 == 0
-    puts "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
-  else
-    puts number
+
+class CheckFizzBuzz
+  FIZZ_NUM = 3
+  BUZZ_NUM = 5
+
+  TEXT_MULTIPLE_3 = 'FIZZ'
+  TEXT_MULTIPLE_5 = 'BUZZ'
+  TEXT_MULTIPLE_3_AND_5 = 'FIZZ_BUZZ'
+
+  def initialize(min, max)
+    @max = max
+    @min = min
+  end
+
+  def call_check_fizz_buzz
+    @min.upto(@max) do |number|
+      puts check_fizz_buzz(number)
+    end
+  end
+
+  def check_fizz_buzz(number)
+    if (number % FIZZ_NUM).zero? && number % BUZZ_NUM != 0
+      puts TEXT_MULTIPLE_3
+    elsif number % FIZZ_NUM != 0 && (number % BUZZ_NUM).zero?
+      puts TEXT_MULTIPLE_5
+    elsif (number % FIZZ_NUM).zero? && (number % BUZZ_NUM).zero?
+      puts TEXT_MULTIPLE_3_AND_5
+    else
+      puts number
+    end
   end
 end
-# RubyではForはほぼ使用されない。また、イテレータがインクリメントするような処理にはuptoメソッドの使用が推奨されている
-1.upto(20){|n| puts check_fizz_buzz(n)}
+
+CheckFizzBuzz.new(1,20).call_check_fizz_buzz
