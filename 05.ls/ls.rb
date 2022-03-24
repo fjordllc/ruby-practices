669838bec5b4baa110e3b444ea5b748df1e4d830
kurosutomohiro <kurosu.66@gmail.com>
2022-03-24 23:25:04 +0900

    変数名修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,32 +3,32 @@
 
 def main
   # ファイル取得
-  files = Dir.glob('*')
+  current_dir_items = Dir.glob('*')
 
   columun_count = 3
-  files << ' ' until (files.size % columun_count).zero?
+  current_dir_items << ' ' until (current_dir_items.size % columun_count).zero?
 
   # 1列に格納するファイル数
-  row = (files.size / columun_count).ceil
+  row = (current_dir_items.size / columun_count).ceil
 
-  # filesの中で、最も大きいファイルサイズ＋固定で追加するスペース数を格納
+  # current_dir_itemsの中で、最も大きいファイルサイズ＋固定で追加するスペース数を格納
   fixed_space_size = 7
-  max_space_size = files.max_by(&:length).length + fixed_space_size
+  max_space_size = current_dir_items.max_by(&:length).length + fixed_space_size
 
   # 左揃えにするため、各要素に空白を追加
-  space_added_files = files.map do |f|
-    each_space_size = max_space_size - f.size
-    f + ' ' * each_space_size
+  space_added_current_dir_items = current_dir_items.map do |items|
+    each_space_size = max_space_size - items.size
+    items + ' ' * each_space_size
   end
 
-  transposed_arrays = space_added_files.each_slice(row).to_a.transpose
-  display(transposed_arrays)
+  transposed_items = space_added_current_dir_items.each_slice(row).to_a.transpose
+  display(transposed_items)
 end
 
 # 出力
-def display(transposed_arrays)
-  transposed_arrays.each do |file|
-    puts file.join('')
+def display(transposed_items)
+  transposed_items.each do |items|
+    puts items.join('')
   end
 end
 
