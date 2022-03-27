1b7d84203fa069702cb6eb927eaa5764aaa72532
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-03-27 21:19:27 +0900

    feat
    10フレーム目が２投の場合のスコア計算ができるように実装

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # score = ARGV[0]
-score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,３'
+score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,3'
 scores = score.split(',')
 
 shots = []
@@ -14,13 +14,37 @@ scores.each do |s|
 end
 
 frames = []
-if shots.size == 20
-  shots.each_slice(2) do |s|
-    frames << s
+shots.each_slice(2) do |s|
+  frames << s
+end
+frames.each do |frame|
+  if frame[0] == 10
+    frame.pop #ストライクの場合は1フレーム一ム1投とする
   end
 end
+
+
 score = 0
-frames.each do |frame|
-  score += frame.sum
+frames.each_with_index do |frame, i|
+  if i < 9
+    next_first_throw = frames[i + 1][0]
+    if next_first_throw == 10
+      next_second_throw = frames[i +2][0]
+    else
+      next_second_throw = frames[i + 1][1]
+    end
+    #strikeの場合
+    if frame[0] == 10
+      score += 10 + next_first_throw + next_second_throw
+      #spareの場合
+    elsif frame.sum == 10
+      score += 10 + next_first_throw
+    else
+      score += frame.sum
+    end
+  else
+    #10投目
+    score += frame.sum
+  end
 end
 puts score
