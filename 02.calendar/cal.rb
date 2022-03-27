d27b9d4bf530eddde39713665c318d095779c900
pachikuriii <stakahashi0728@gmail.com>
2022-03-27 14:10:32 +0900

    文字列の連携を式展開にし、日付のRangeをfirstdateを使って簡単に

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,29 +14,22 @@ rescue ArgumentError
   dd = Date.new(y = Date.today.year, m = Date.today.month)
 end
 
-firstdate = Date.new(y,m,1).wday
+firstdate = Date.new(y,m,1).mday
+firstdate_week = Date.new(y,m,1).wday
 lastdate = Date.new(y,m,-1)   
 caltitle = "#{dd.month}月 #{dd.year}"
 calwday = "日 月 火 水 木 金 土"
 
 puts caltitle.center(20)
 puts calwday
-print "   " * firstdate
-(1..lastdate.day).each do |x|
-  print x.to_s.rjust(2) + " "
-  firstdate += 1
-  if firstdate % 7 == 0
-    print "\n"
+print  "#{"   " * firstdate_week}"
+(firstdate..lastdate.day).each do |x|
+  print "#{x.to_s.rjust(2) + " "}"
+  firstdate_week += 1
+
+  if firstdate_week % 7 == 0
+    puts
   end 
 
-#今日の日付の部分の色を反転させようとコードを書いてみたのですが、
-#該当の日付の隣に、色が反転した日付が横付けされる表示になってしまいます。
-#歓迎要件とのことなので、一旦コメントアウトしてコードを載せておきます。
-=begin
-  if x == Date.today.day && y == Date.today.year && m == Date.today.month
-    print "\e[7m#{x}\e[0m" + " "
-  end
-=end
-
 end
-print "\n"
+puts
