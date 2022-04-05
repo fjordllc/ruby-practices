937491a979ea16e4fcead011ab36cc4a11e48011
kosaku <bouru02@gmail.com>
2022-04-05 15:17:23 +0900

    ターミナル幅の変更に対応

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -1,63 +1,51 @@
 # frozen_string_literal: true
 
-MAX_COLUMN = 3  # 出力時の最大列数
-ITEM_MARGIN = 4 # 出力時の文字間の余白（半角スペースの数）
+MAX_COLUMN_COUNT = 3 # 出力時の最大列数
+MAX_ITEM_MARGIN = 2 # 出力時の文字間の最大余白（半角スペースの数）
+TERMINAL_RIGHT_PADDING = 3 # ターミナル右端の余白
 
 # 引数で渡したパスに存在するファイルとディレクトリをターミナルに出力
 def ls(dir: Dir.pwd)
   # 対象のディレクトリを取得
-  items = Dir.entries(dir)
-  items.delete_if { |item| /^\./ =~ item }
+  original_array = Dir.entries(dir)
+  original_array.delete_if { |item| /^\./ =~ item }
 
-  # 要素を3列で縦並びにするため2次元配列に変換
-  format_items = group_to_columns(items)
+  # 3列に成型
+  formatted_array = format_array_to_output(original_array)
+  # ターミナル幅に合わせたカラム数を計算
+  column_count = calc_column_count(formatted_array)
 
-  # ターミナル幅におさまる列数を取得
-  temp_column_count = MAX_COLUMN
-  minimum_column_count = MAX_COLUMN
-  p '---------------------'
-  p format_items
-  p '---------------------'
-  format_items.each do |column_group|
-    fit_to_character_longest!(column_group)
-    next unless over_tarminal_width?(column_group.join)
+  formatted_array = format_array_to_output(original_array, column_count) if column_count < MAX_COLUMN_COUNT
 
-    temp_column_count = calc_columns_count(column_group)
-    minimum_column_count = temp_column_count if temp_column_count < minimum_column_count
+  # 各行を出力
+  formatted_array.each { |row| puts row.join }
+end
+
+# 配列の成型全般
+def format_array_to_output(original_array, column_count = MAX_COLUMN_COUNT)
+  # 行を列に転置するために列ごとに要素をまとめた2次元配列を作成
+  columns = group_to_columns(original_array, column_count)
+
+  # 列毎に要素の長さを合わせる
+  fit_columns = columns.map do |column|
+    fit_to_longest_column_item(column)
   end
 
-  # 3列では大きすぎる場合はターミナル幅におさまる列数の2次元配列に再度変換
-  format_items = group_to_columns(items, minimum_column_count) if minimum_column_count < MAX_COLUMN
-
-  format_items.each do |format_item|
-    fit_to_character_longest!(format_item)
-  end
-  format_items = format_items.transpose
-
-  # 最終列の要素から余白を削除
-  format_items.each do |format_item|
-    last_item = format_item[-1]
-    format_item[-1] = last_item.gsub(/#*$/, '')
-    format_item[-2] = format_item[-2].gsub(/#*$/, '') if last_item == ''
+  # 行と列を転置した配列を作成
+  rows = fit_columns.transpose.map do |row|
+    # テキストが存在する最後列から余白を削除
+    row.delete_if { |item| item.match(/^\s*$/) }
+    row[-1].gsub!(/\s*$/, '')
+    row
   end
 
-  # 列間に余白を追加
-  format_items.each do |column|
-    item_margin = calc_margin(column) < ITEM_MARGIN ? calc_margin(column) : ITEM_MARGIN
-    # item_margin = ITEM_MARGIN
-    dist = column.map.with_index do |e, i|
-      if i == column.size - 1
-        e
-      else
-        +e << '@' * item_margin
-      end
-    end
-    puts dist.join
-  end
+  # 各列間に余白を付与
+  margin = calc_margin(rows)
+  add_column_margin(rows, margin)
 end
 
 # 渡された配列の並び順を列数に合わせて変換
-def group_to_columns(original_array, column_count = MAX_COLUMN)
+def group_to_columns(original_array, column_count = MAX_COLUMN_COUNT)
   # 出力時の最大列数をMAX_COUNTとした場合に行数が何行になるのかを計算
   row_count = original_array.size / column_count
   row_count += 1 unless (original_array.size % column_count).zero?
@@ -81,20 +69,32 @@ def group_to_columns(original_array, column_count = MAX_COLUMN)
 end
 
 # 出力時のファイル間の余白をターミナル幅から計算
-def calc_margin(row)
-  return 0 if (row.count - 1).zero?
+def calc_margin(rows)
+  max_length_row = rows.max_by { |row| row.join.length }
+  # 1列しかないorターミナル幅を行の長さが超えていたら余白0を返す
+  return 1 if max_length_row.size == 1 || !within_tarminal_width?(max_length_row.join)
 
-  terminal_width = terminal_size[1]
-  row_width = row.join.length
-  (terminal_width - row_width) / ((row.count - 1)).floor
+  terminal_width = terminal_size[1] - TERMINAL_RIGHT_PADDING
+  margin = (terminal_width - max_length_row.join.length) / ((max_length_row.size - 1)).floor
+  margin < MAX_ITEM_MARGIN ? margin : MAX_ITEM_MARGIN
 end
 
 # 配列の各要素の長さを最大の要素の幅に合わせる（短い要素の末尾に半角スペースを追加する）
-def fit_to_character_longest!(items)
-  longest_length = items.max_by(&:length).length
-  items.each do |item|
+# 引数：列にあたる配列
+# 戻り値：最長の要素に合わせて各要素に半角スペースを追加した配列
+def fit_to_longest_column_item(column)
+  longest_length = column.max_by(&:length).length
+  column.map do |item|
     space_count = longest_length - item.length
-    +item << '#' * space_count if space_count.positive?
+    space_count.positive? ? item + "\s" * space_count : item
+  end
+end
+
+def add_column_margin(columns, margin)
+  columns.map do |column|
+    column.map.with_index do |item, i|
+      i == column.size - 1 ? item : item + "\s" * margin
+    end
   end
 end
 
@@ -106,32 +106,36 @@ def terminal_size
   cols = 80
   buf = [0, 0, 0, 0].pack('SSSS')
   rows, cols, row_pixels, col_pixels = buf.unpack('SSSS') [0..1] if $stdout.ioctl(TIOCGWINSZ, buf) >= 0
-  [rows, cols]
+  [rows, cols, row_pixels, col_pixels]
 end
 
 # ターミナル幅に合わせた出力列数を返す
-def calc_columns_count(target_array)
-  terminal_width = terminal_size[1]
-  array_width = 0
-  columns_count = 1
-  target_array.each_with_index do |e, i|
-    array_width += e.length
-    break if terminal_width < array_width
+# 引数：配列
+# 戻り値：列数（数値）
+def calc_column_count(rows)
+  terminal_length = terminal_size[1] - TERMINAL_RIGHT_PADDING
+  max_length_row = rows.max_by { |row| row.join.length }
+  return MAX_COLUMN_COUNT if within_tarminal_width?(max_length_row.join)
 
-    columns_count += i
+  row_length = 0
+  column_count = 1
+  max_length_row.each_with_index do |item, i|
+    row_length += item.length
+    break if terminal_length < row_length
+
+    column_count += i
   end
-
-  columns_count
+  column_count < MAX_COLUMN_COUNT ? column_count : MAX_COLUMN_COUNT
 end
 
 # 出力するテキストの横幅がターミナル幅を超えているか
-def over_tarminal_width?(object)
-  terminal_width = terminal_size[1]
+def within_tarminal_width?(object)
+  terminal_width = terminal_size[1] - TERMINAL_RIGHT_PADDING
   object_item_width = object.to_s.length
-
-  terminal_width < object_item_width
+  terminal_width > object_item_width
 end
 
+# ターミナルから引数を取得しlsを実行
 param = {}
 param[:dir] = ARGV[0] if ARGV[0]
 ls(param)
