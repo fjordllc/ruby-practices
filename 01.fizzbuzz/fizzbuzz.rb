26777327d7e725c7cfbb39420029ce137234f27f
yuji <foxylady0315@icloud.com>
2022-04-09 11:53:45 +0900

    add fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+n = 0
+while n <= 19
+  n += 1
+  if n % 3 == 0 && n % 5 == 0 
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0 
+    puts "Buzz"
+  else
+    puts n
+  end
+end
