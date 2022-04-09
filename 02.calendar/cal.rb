5e720a65356acce6ab4caf2cc1d57da2f58fbab4
pachikuriii <stakahashi0728@gmail.com>
2022-04-09 16:57:09 +0900

    firstdate_weekを削除、eachメソッドのDateのRangeを変更、後置ifへ変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,35 +1,31 @@
+# frozen_string_literal: true
+
 require 'optparse'
 require 'date'
 
 opt = OptionParser.new
-opt.on('-y') {|v| v }
-opt.on('-m') {|v| v }
-opt.parse!(ARGV) 
-y =  ARGV[0].to_i
+opt.on('-y') { |v| v }
+opt.on('-m') { |v| v }
+opt.parse!(ARGV)
+y = ARGV[0].to_i
 m = ARGV[1].to_i
 
 begin
-  dd = Date.new(y,m)
+  dd = Date.new(y, m)
 rescue ArgumentError
   dd = Date.new(y = Date.today.year, m = Date.today.month)
 end
 
-firstdate = Date.new(y,m,1).mday
-firstdate_week = Date.new(y,m,1).wday
-lastdate = Date.new(y,m,-1)   
+firstdate = Date.new(y, m, 1)
+lastdate = Date.new(y, m, -1)
 caltitle = "#{dd.month}月 #{dd.year}"
-calwday = "日 月 火 水 木 金 土"
+calwday = '日 月 火 水 木 金 土'
 
 puts caltitle.center(20)
 puts calwday
-print  "#{"   " * firstdate_week}"
-(firstdate..lastdate.day).each do |x|
-  print "#{x.to_s.rjust(2) + " "}"
-  firstdate_week += 1
-
-  if firstdate_week % 7 == 0
-    puts
-  end 
-
+print ('   ' * firstdate.wday).to_s
+(firstdate..lastdate).each do |calday|
+  print "#{calday.mday.to_s.rjust(2)} ".to_s
+  puts if calday.wday == 6
 end
 puts
