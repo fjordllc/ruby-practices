9b587426145d920601a01d26da7507c4c00e1efb
pachikuriii <stakahashi0728@gmail.com>
2022-04-10 19:21:56 +0900

    レビュー指摘を修正後、動く状態に

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -22,16 +22,14 @@ frames.first(10).each_with_index do |frame, i|
 
   if frame[0] == 10 # strike
     point += afternext_frame[0] if next_frame[0] == 10
-
-    point += 10
-    point += next_frame.sum
+    point += 10 + next_frame.sum
 
   elsif frame.sum == 10 # spare
-    point += 10
-    point += next_frame[0]
+    point += 10 + next_frame[0]
 
   else
     point += frame.sum
   end
+
 end
 puts point
