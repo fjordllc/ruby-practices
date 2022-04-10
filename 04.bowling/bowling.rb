de185b70eafe65779ec16a48dd9c73a16b259963
pachikuriii <stakahashi0728@gmail.com>
2022-04-10 19:53:44 +0900

    レビュー頂いた後、sumを使ってリファクタリング

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,22 +14,21 @@ end
 
 frames = shots.each_slice(2).to_a
 
-point = 0
-
-frames.first(10).each_with_index do |frame, i|
+point = frames.first(10).each_with_index.sum do |frame, i|
   next_frame = frames[i + 1]
   afternext_frame = frames[i + 2]
 
   if frame[0] == 10 # strike
-    point += afternext_frame[0] if next_frame[0] == 10
-    point += 10 + next_frame.sum
+    score = 0
+    score += afternext_frame[0] if next_frame[0] == 10
+    score + 10 + next_frame.sum
 
   elsif frame.sum == 10 # spare
-    point += 10 + next_frame[0]
+    10 + next_frame[0]
 
   else
-    point += frame.sum
+    frame.sum
   end
-
 end
+
 puts point
