2eaf7f98abb096e576e4c24b8a1f52b66a21da5c
pachikuriii <stakahashi0728@gmail.com>
2022-04-14 23:48:32 +0900

    メソッドの引数名を変更

--- 05.ls/ls01.rb
+++ 05.ls/ls01.rb
@@ -1,13 +1,12 @@
 current_files = Dir.glob('*').sort
 row_num = 3
 
-def add_blank(receive_blank_file)
-  receive_blank_file << ' '
+def add_blank(receive_blank)
+  receive_blank << ' '
 end
 
 add_blank(current_files) while current_files.length % row_num != 0
 column_num = current_files.length / row_num
-
 
 transposed_files = current_files.each_slice(column_num).to_a.transpose
 
@@ -18,9 +17,8 @@ transposed_files.first(column_num).each do |each_column|
 end
 
 transposed_files.each do |displayed_files|
-  def join(x)
-    puts x.join
+  def join(each_transposed_file)
+    puts each_transposed_file.join
   end
-
   join(displayed_files)
 end
