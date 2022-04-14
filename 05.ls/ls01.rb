723ac3b3af2f09405e4c9367ada0dc0649ee9ff6
pachikuriii <stakahashi0728@gmail.com>
2022-04-14 23:39:31 +0900

    lsコマンド1を作成

---
+++ 05.ls/ls01.rb
@@ -0,0 +1,21 @@
+current_files = Dir.glob('*').sort
+row_num = 3
+current_files << ' ' while current_files.length % row_num != 0
+column_num = current_files.length / row_num
+
+
+transposed_files = current_files.each_slice(column_num).to_a.transpose
+
+transposed_files.first(column_num).each do |each_column|
+  row_num.times do |index|
+    each_column[index] << ' ' * (24 - each_column[index].length)
+  end
+end
+
+transposed_files.each do |displayed_files|
+  def join(x)
+    puts x.join
+  end
+
+  join(displayed_files)
+end
