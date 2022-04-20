771712c41dd817b92100a55aac5ab26bb77428b4
asahifuke <asahi.fuke@medpeer.co.jp>
2022-04-20 16:20:10 +0900

    fix fizzbuzz

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-for num in 1..20 do
-  if num % 3 == 0 and num % 5 == 0
+for number in 1..20 do
+  if number % 3 == 0 and number % 5 == 0
     puts "FizzBuzz"
-  elsif num % 3 == 0
+  elsif number % 3 == 0
     puts "Fizz"
-  elsif num % 5 == 0
+  elsif number % 5 == 0
     puts "Buzz"
   else
-    puts num
+    puts number
   end
 end
