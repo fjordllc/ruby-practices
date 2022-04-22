77605d088a86a84565ea156b95dbb5a998455400
asahifuke <asahi.fuke@medpeer.co.jp>
2022-04-22 12:30:01 +0900

    [create] rubocop

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-for number in 1..20 do
-  if number % 3 == 0 and number % 5 == 0
-    puts "FizzBuzz"
-  elsif number % 3 == 0
-    puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  else
-    puts number
+class FizzBuzz
+  def show
+    (1..20).each do |number|
+      if (number % 15).zero?
+        puts 'FizzBuzz'
+      elsif (number % 3).zero?
+        puts 'Fizz'
+      elsif (number % 5).zero?
+        puts 'Buzz'
+      else
+        puts number
+      end
+    end
   end
 end
+
+fizz_buzz = FizzBuzz.new
+fizz_buzz.show
