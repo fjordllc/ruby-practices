b849cae42e92625180d1e6aaf001e2f093586491
pachikuriii <stakahashi0728@gmail.com>
2022-04-22 20:18:17 +0900

    メソッド定義を盛り込んだ

--- 05.ls/ls01.rb
+++ 05.ls/ls01.rb
@@ -1,35 +1,32 @@
 # frozen_string_literal: true
 
 def main
-  current_files = Dir.glob('*').sort
   row_num = 3
-  current_column_num = get_current_columns(current_files,row_num)
-  # transposed_to_display = transposed_files(current_column_num)
-  p current_files
-  p current_column_num
+  all_files = get_all_files(row_num)
+  column_num = all_files.length / row_num
+  transposed_all_files = get_transposed_all_files(row_num, all_files, column_num)
+  files_to_display(transposed_all_files)
 end
 
-def get_current_columns(current_files,row_num)
-  current_files << " " while current_files.length % row_num != 0
-  current_files.length / row_num
+def get_all_files(row_num)
+  files = Dir.glob('*').sort
+  new_files = files.push(' ') while files.length % row_num != 0
+  new_files
 end
 
-# def transposed_files(colu)
-#   transposed_files = current_files.each_slice(column_num).to_a.transpose
+def get_transposed_all_files(row_num, all_files, column_num)
+  transposed_files = all_files.each_slice(column_num).to_a.transpose
+  transposed_files.first(column_num).each do |each_column|
+    row_num.times do |index|
+      each_column[index] += ' ' * (24 - each_column[index].length)
+    end
+  end
+end
 
-#   transposed_files.first(column_num).each do |each_column|
-#     row_num.times do |index|
-#     each_column[index] += ' ' * (24 - each_column[index].length)
-#     end
-#   end
-
-# end
+def files_to_display(transposed_all_files)
+  transposed_all_files.each do |transposed_each_files|
+    puts transposed_each_files.join
+  end
+end
 
 main
-# def join_file()
-#   transposed_files.each do |displayed_files|
-#     puts displayed_files.join
-#   end
-# end
-
-#   main
