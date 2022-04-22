e0e41b7a859428edea5ac48e0c654c98e1fb26dc
asahifuke <asahi.fuke@medpeer.co.jp>
2022-04-22 15:42:40 +0900

    rubocopが通るように修正しました

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,34 +2,41 @@
 # frozen_string_literal: true
 
 class Bowling
+  def initialize
+    @first_throws  = []
+    @second_throws = []
+    @bonuses       = []
+    @throw_index   = 0
+    @throws        = ARGV[0].split(',')
+  end
+
+  def score(first_throw:, second_throw:, bonus:)
+    @first_throws.push(first_throw)
+    @second_throws.push(second_throw)
+    @bonuses.push(bonus)
+  end
+
+  def judge_strike(next_throw_index)
+    @throws[@throw_index + next_throw_index] == 'X' ? @first_strike_score : @throws[@throw_index + next_throw_index].to_i
+  end
+
   def play
-    first_throws  = []
-    second_throws = []
-    bonuses       = []
-    throw_index   = 0
-    throws        = ARGV[0].split(',')
+    @first_strike_score  = 10
+    @second_strike_score = 0
+    @spare_score         = 10
+    @no_bonus            = 0
 
     10.times do
-      if throws[throw_index] == 'X'
-        first_throws.push(10)
-        second_throws.push(0)
-        bonus1 = throws[throw_index + 1] == 'X' ? 10 : throws[throw_index + 1].to_i
-        bonus2 = throws[throw_index + 2] == 'X' ? 10 : throws[throw_index + 2].to_i
-        bonuses.push(bonus1 + bonus2)
-        throw_index += 1
+      if @throws[@throw_index] == 'X'
+        score(first_throw: @first_strike_score, second_throw: @second_strike_score, bonus: judge_strike(1) + judge_strike(2))
+        @throw_index += 1
       else
-        first_throws.push(throws[throw_index].to_i)
-        second_throws.push(throws[throw_index + 1].to_i)
-        if throws[throw_index].to_i + throws[throw_index + 1].to_i == 10
-          bonus = throws[throw_index + 2] == 'X' ? 10 : throws[throw_index + 2].to_i
-          bonuses.push(bonus)
-        else
-          bonuses.push(0)
-        end
-        throw_index += 2
+        bonus = @throws[@throw_index].to_i + @throws[@throw_index + 1].to_i == @spare_score ? judge_strike(2) : @no_bonus
+        score(first_throw: @throws[@throw_index].to_i, second_throw: @throws[@throw_index + 1].to_i, bonus: bonus)
+        @throw_index += 2
       end
     end
-    puts first_throws.sum + second_throws.sum + bonuses.sum
+    puts @first_throws.sum + @second_throws.sum + @bonuses.sum
   end
 end
 
