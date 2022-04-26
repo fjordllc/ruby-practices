60414ce6d53e5e97afd2206c188f09cb942806fb
pachikuriii <stakahashi0728@gmail.com>
2022-04-27 06:13:50 +0900

    rubocopを実行

--- 05.ls/ls01.rb
+++ 05.ls/ls01.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
+
 ROW_NUM = 3
-ROW_MAX_WIDTH = 24 
+ROW_MAX_WIDTH = 24
 
 def main
   all_files = Dir.glob('*').sort
   column_num = all_files.length / ROW_NUM
-  files_in_columns = get_transposed_all_files(all_files,column_num)
+  files_in_columns = get_transposed_all_files(all_files, column_num)
   display(files_in_columns)
 end
 
-def get_transposed_all_files(all_files,column_num)
+def get_transposed_all_files(all_files, column_num)
   all_files.push(' ') while all_files.length % ROW_NUM != 0
   transposed_files = all_files.each_slice(column_num).to_a.transpose
   transposed_files.first(column_num).each do |column|
