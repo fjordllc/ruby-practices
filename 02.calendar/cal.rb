5809f3afb88925d026fe6e6ec41cbaf563345d48
kgoa <y-keigoe@ruby-dev.jp>
2022-04-27 10:34:45 +0900

    🛠Fixed  calendar val.2

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,22 +1,13 @@
 #!/usr/bin/env ruby
 
-#こんな感じで作ろうか
-# *******00月*0000年*******
-# *日_*月_*火_*水_*木_*金_*土
-
-
-
 require 'date'
 require 'optparse'
 
 opt = OptionParser.new
 
-year = 2022   #年の初期値
-month = 4   #月の初期値
-
-# ショートオプションのみならopt.on('-m')で平気！
-#opt.on('-m', '--add ITEM', Integer, 'add an year') {|v| month = v }
-#opt.on('-y', '--add ITEM', Integer, 'add an year') {|v| year = v }
+today = Date.today
+year = today.year   #年の初期値
+month = today.month   #月の初期値
 
 opt.on('-m month'){|v| month = v }
 opt.on('-y year'){|v| year = v }
@@ -24,94 +15,37 @@ opt.on('-y year'){|v| year = v }
 
 opt.parse!(ARGV) #これがないと引数が渡されないみたい
 
-
-
-#binding.irb
-#p month
-#p year
-
-=begin
-
-puts a = Date.new(year, month, 1)
-puts a.strftime('%a')
-
-# %u: 月曜日を1とした、曜日の数値表現 (1..7)
-puts start_day = a.strftime('%u').to_i 
-# カレンダーのスタートまでstart_spaceの数だけスペースを入れる
-
-
-puts Date.new(2022, 4, 7).strftime('%U')
-
-
-puts "-----------------------------------------------"
-puts "カレンダー"
-
-=end
-
 #カレンダーのタイトル
 def title (month, year)
-  puts "#{month}月".rjust(9) + "#{year}".rjust(6)
+  month_string =  "#{month}月".rjust(9)
+  year_string = year.to_s.rjust(6)
+  puts "#{month_string}#{year_string}"
 end
-
-#1週間分の曜日
-=begin
-def days
-  week = [" 日", "月", "火", "水", "木", "金", "土"]
-  week.each do |day|
-    if day == week.last
-      print day
-    else
-      print day + " "
-    end
-  end
-  puts " "
-end
-=end
 
 #1ヶ月分の日付
 def date(month, year)
-  space = "   "
+  space = " ".rjust(3)
   month = month.to_i
   year = year.to_i
   start_date = Date.new(year, month, 1)
-  start_space = start_date.wday #1日が何曜日からスタートか？
+  end_date = Date.new(year, month, -1) #何日まであるか
 
-  end_date = Date.new(year, month, -1).day #何日まであるか
-
-  date = 1
-  start_space.times{  #曜日分スペースを入れる
-    print space
-  }
-  x = start_space +1  #スペース数+1からスタート
-  y = start_space.to_i + end_date.to_i  #スペース数+最終日まで
+  print space * start_date.wday #曜日分スペースを入れる
 
   #ここから日付
-  while x <= y
-    if x % 7 ==0  #7の倍数で改行する
-      if date >9
-        print " " + "#{date}\n"
-      else
-        print " " + " " + "#{date}\n"
-      end
-      
-    else
-      if date >9
-        print " " + "#{date}"
-      else
-        print " " + " " + "#{date}"
-      end
-    end
-    date += 1
-    x += 1
+  (start_date..end_date).each do |date|
+    date_string = date.day
+    date_day = date.wday 
+
+    print date_string.to_s.rjust(3)
+    puts if date_day == 6  #土曜(0)になったら改行
   end
 end
-
 
 ####出力部####
 #タイトル
 title(month, year)
 #曜日
-#days
-puts " 日 月 火 水 木 金 土 " # これで十分
+puts " 日 月 火 水 木 金 土 "
 #日付
 date(month, year)
