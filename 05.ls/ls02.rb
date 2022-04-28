d3b81b01c5440b8789b3fcf38a966ec5089cf005
pachikuriii <stakahashi0728@gmail.com>
2022-04-28 21:20:57 +0900

    rubocopを実行

--- 05.ls/ls02.rb
+++ 05.ls/ls02.rb
@@ -5,31 +5,39 @@ require 'optparse'
 ROW_NUM = 3
 ROW_MAX_WIDTH = 24
 
-def main
-  all_files = Dir.glob('*', File::FNM_DOTMATCH).sort
-  column_num = all_files.length / ROW_NUM
-  files_in_columns = get_transposed_all_files(all_files, column_num)
-  display(files_in_columns)
-end
+class Files
+  def main
+    all_files = Dir.glob('*', File::FNM_DOTMATCH).sort
+    column_num = all_files.length / ROW_NUM
+    files_in_columns = get_transposed_all_files(all_files, column_num)
+    display(files_in_columns)
+  end
 
-def get_transposed_all_files(all_files, column_num)
-  all_files.push(' ') while all_files.length % ROW_NUM != 0
-  transposed_files = all_files.each_slice(column_num).to_a.transpose
-  transposed_files.first(column_num).each do |column|
-    ROW_NUM.times do |index|
-      column[index] += ' ' * (ROW_MAX_WIDTH - column[index].length)
+  def get_transposed_all_files(all_files, column_num)
+    all_files.push(' ') while all_files.length % ROW_NUM != 0
+    transposed_files = all_files.each_slice(column_num).to_a.transpose
+    transposed_files.first(column_num).each do |column|
+      ROW_NUM.times do |index|
+        column[index] += ' ' * (ROW_MAX_WIDTH - column[index].length)
+      end
+    end
+  end
+
+  def display(files_in_columns)
+    files_in_columns.each do |column|
+      puts column.join
     end
   end
 end
 
-def display(files_in_columns)
-  files_in_columns.each do |column|
-    puts column.join
+files = Files.new
+
+def define_option(files)
+  opt = OptionParser.new
+  opt.on('-a') do
+    files.main
   end
+  opt.parse(ARGV)
 end
 
-opt = OptionParser.new
-opt.on('-a') do
-  main
-end
-opt.parse(ARGV)
+define_option(files)
