12edaa873ae0e0943ebcc656941249042ac2b8cd
pachikuriii <stakahashi0728@gmail.com>
2022-05-05 13:25:17 +0900

    コマンドなしでもプログラムが動くように

--- 05.ls/ls02.rb
+++ 05.ls/ls02.rb
@@ -5,36 +5,39 @@ require 'optparse'
 ROW_NUM = 3
 ROW_MAX_WIDTH = 24
 
-  def main
-    all_files = Dir.glob('*', File::FNM_DOTMATCH).sort
-    column_num = all_files.length / ROW_NUM
-    files_in_columns = get_transposed_all_files(all_files, column_num)
-    display(files_in_columns)
-  end
-
-  def get_transposed_all_files(all_files, column_num)
-    all_files.push(' ') while all_files.length % ROW_NUM != 0
-    transposed_files = all_files.each_slice(column_num).to_a.transpose
-    transposed_files.first(column_num).each do |column|
-      ROW_NUM.times do |index|
-        column[index] += ' ' * (ROW_MAX_WIDTH - column[index].length)
-      end
-    end
-  end
-
-  def display(files_in_columns)
-    files_in_columns.each do |column|
-      puts column.join
-    end
-  end
-
-
-def define_option(main)
-  opt = OptionParser.new
-  opt.on('-a') do
-    main
-  end
-  opt.parse(ARGV)
+def no_command_main
+  all_files = Dir.glob('*').sort
+  column_num = all_files.length / ROW_NUM
+  files_in_columns = get_transposed_all_files(all_files, column_num)
+  display(files_in_columns)
 end
 
-define_option(main)
+def a_command_main
+  all_files = Dir.glob('*', File::FNM_DOTMATCH).sort
+  column_num = all_files.length / ROW_NUM
+  files_in_columns = get_transposed_all_files(all_files, column_num)
+  display(files_in_columns)
+end
+
+
+def get_transposed_all_files(all_files, column_num)
+  all_files.push(' ') while all_files.length % ROW_NUM != 0
+  transposed_files = all_files.each_slice(column_num).to_a.transpose
+  transposed_files.first(column_num).each do |column|
+    ROW_NUM.times do |index|
+      column[index] += ' ' * (ROW_MAX_WIDTH - column[index].length)
+    end
+  end
+end
+
+def display(files_in_columns)
+  files_in_columns.each do |column|
+    puts column.join
+  end
+end
+
+opt = OptionParser.new
+opt.on('-a') { a_command_main }
+opt.parse(ARGV)
+
+no_command_main if ARGV == []
