e8096e748e898628ecc3ed593df61e87cf4973be
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-05-09 16:29:38 +0900

    FizzBuzz問題(ruby)に関する回答を作成
    フィヨルドブートキャンプのプラクティスであるFizzBuzz問題(ruby)に関する回答を作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+(1..20).each do |number|
+  case
+  when number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  when number % 3 == 0
+    puts "Fizz"
+  when number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
