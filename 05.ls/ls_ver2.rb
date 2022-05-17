cfc781362612482788d545b14e0ef30a21095ae3
github ayk4 <fuzi_asahi_6699i@yahoo.co.jp>
2022-05-17 22:48:27 +0200

    条件を満たしていない点を修正

--- 05.ls/ls_ver2.rb
+++ 05.ls/ls_ver2.rb
@@ -1,36 +1,33 @@
 require 'optparse'
 require 'debug'
 
-#カレントディレクトリの情報を取得
 def main
   current_dir = Dir.glob("*").sort
   make_array(current_dir)
 end
 
-#配列を最大３列に並べる
 def make_array(current_dir)
-  #縦横の指定をする
   max_row = 3.0
   total_file_size = current_dir.size
   columns = (total_file_size / max_row).ceil
-
-  #配列の用意
-  arrays =[]
-  current_dir.each_slice(columns) do |list_of_file|
-    arrays << list_of_file
-    #flattenを使用するため最大要素数までnilを配列に詰める
+  if total_file_size >= 0
+    arrays = []
+    current_dir.each_slice(columns) do |list_of_file|
+      arrays << list_of_file
+    end
     max_size = arrays.map(&:size).max
     arrays.map! { |element| element.values_at(0...max_size) }
+  else
+    arrays = []
   end
   show_files(arrays)
 end
 
-#表示する
 def show_files(arrays)
   transposed_array = arrays.transpose
   transposed_array.each do |two_dimensional_array|
     two_dimensional_array.each do |file|
-     print "#{file}".ljust(25)
+      print "#{file}".ljust(25)
     end
     print  "\n"
   end
