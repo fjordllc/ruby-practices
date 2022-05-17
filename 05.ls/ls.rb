c8e278ca35b3eb9f4a7dc8365133567f0a4d25b6
kgoa <y-keigoe@ruby-dev.jp>
2022-05-17 11:05:06 +0900

    🛠Fixed as reviewed.vol.2

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,20 +4,19 @@
 require 'optparse'
 
 # オプション
-dir_name = ARGV[0] ? "#{ARGV[0]}/*" : '*'
+dir_name = ARGV[0] || '.'
 
 # ディレクトリ情報取得
 files = []
 
 # ls デフォルト
 def defo_ls(dir_name, files)
-  dir_exist = Dir.exist?(ARGV[0] || '.')
-  if dir_exist
-    Dir.glob(dir_name) do |f|
-      files << f.gsub("#{ARGV[0]}/", '')
+  if Dir.exist?(dir_name)
+    Dir.glob('*', base: dir_name).each do |f|
+      files << f
     end
   else
-    puts "#{ARGV[0]}: No such file or directory"
+    puts "#{dir_name}: No such file or directory"
   end
 end
 
@@ -29,8 +28,9 @@ defo_ls(dir_name, files)
 count = (files.size / 3.0).ceil(0)
 files_sort = files.sort
 count.times do |i|
-  print files_sort[i].ljust(12).to_s
-  print files_sort[i + count].ljust(12).to_s if files_sort[i + count]
-  print files_sort[i + count * 2].ljust(12).to_s if files_sort[i + count * 2]
+  3.times do |j|
+    result = files_sort[i + j * count]
+    print result.ljust(12).to_s if result
+  end
   puts ''
 end
