951d029159b505b218a3139b0b88a00d82be522f
kgoa <y-keigoe@ruby-dev.jp>
2022-05-18 08:58:57 +0900

    🛠Fixed as reviewed. vol.3

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -12,8 +12,15 @@ files = []
 # ls デフォルト
 def defo_ls(dir_name, files)
   if Dir.exist?(dir_name)
-    Dir.glob('*', base: dir_name).each do |f|
-      files << f
+    files = Dir.glob('*', base: dir_name)
+    count = (files.size / 3.0).ceil(0)
+    files_sort = files.sort
+    count.times do |i|
+      3.times do |j|
+        result = files_sort[i + j * count]
+        print result.ljust(12).to_s if result
+      end
+      puts ''
     end
   else
     puts "#{dir_name}: No such file or directory"
@@ -25,12 +32,3 @@ end
 
 # 出力
 defo_ls(dir_name, files)
-count = (files.size / 3.0).ceil(0)
-files_sort = files.sort
-count.times do |i|
-  3.times do |j|
-    result = files_sort[i + j * count]
-    print result.ljust(12).to_s if result
-  end
-  puts ''
-end
