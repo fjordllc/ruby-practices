5afeaa7c5cd7a7b5f881546ce521d8722c58fa28
github Kassy <github@shinbokutaro0220@gmail.com>
2022-05-19 07:26:49 +0900

    問題箇所の修正(述語メソッドに変更、メソッド名変更、コメント削除)を行いました

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,11 @@
-# コマンドライン上で与えられた引数が2つ以外の時、処理を終了する
-def check_arguments(stdin)
+def validate_number_of_arguments(stdin)
   unless stdin.length == 2
     puts "数値は2つ入力してください"
     exit
   end
 end
 
-# 整数以外を入力した場合、処理を終了する
-def check_if_integer(stdin)
-  # 整数に変換
+def convert_to_integer(stdin)
   stdin.map! {|n| Integer(n)}
 rescue => e
   puts "整数を入力してください - [#{e.message}]"
@@ -16,7 +13,7 @@ rescue => e
 end
 
 def check_fizzbuzz(number)
-  return number if number == 0
+  return number if number.zero?
 
   if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
@@ -30,9 +27,8 @@ def check_fizzbuzz(number)
 end
 
 def output_fizzbuzz(stdin)
-  check_arguments(stdin)
-  check_if_integer(stdin)
-  # sort!で昇順にしてから取り出す
+  validate_number_of_arguments(stdin)
+  convert_to_integer(stdin)
   first = stdin.sort!.shift
   last = stdin.shift
 
