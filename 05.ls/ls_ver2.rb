1c41ad6c3292a01b5c42e965116915279af0db3d
github ayk4 <fuzi_asahi_6699i@yahoo.co.jp>
2022-05-20 13:28:30 +0200

    修正を反映

--- 05.ls/ls_ver2.rb
+++ 05.ls/ls_ver2.rb
@@ -2,29 +2,32 @@ require 'optparse'
 require 'debug'
 
 def main
-  current_dir = Dir.glob("*").sort
-  make_array(current_dir)
+  get_deta
+  make_array
+  show_files
 end
 
-def make_array(current_dir)
-  max_row = 3.0
-  total_file_size = current_dir.size
-  columns = (total_file_size / max_row).ceil
+def get_deta
+  @current_dir = Dir.glob("*").sort
+end
+
+MAX_ROW = 3.0
+def make_array
+  total_file_size = @current_dir.size
+  columns = (total_file_size / MAX_ROW).ceil
+  arrays = []
   if total_file_size >= 0
-    arrays = []
-    current_dir.each_slice(columns) do |list_of_file|
+    @current_dir.each_slice(columns) do |list_of_file|
       arrays << list_of_file
     end
     max_size = arrays.map(&:size).max
     arrays.map! { |element| element.values_at(0...max_size) }
-  else
-    arrays = []
   end
-  show_files(arrays)
+  @arrays=arrays
 end
 
-def show_files(arrays)
-  transposed_array = arrays.transpose
+def show_files
+  transposed_array = @arrays.transpose
   transposed_array.each do |two_dimensional_array|
     two_dimensional_array.each do |file|
       print "#{file}".ljust(25)
