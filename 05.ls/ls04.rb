7d26bf6d3b98253769a7fe4c253e6e92a4dbe91e
pachikuriii <stakahashi0728@gmail.com>
2022-05-24 21:35:02 +0900

    rubocopを実行

--- 05.ls/ls04.rb
+++ 05.ls/ls04.rb
@@ -6,28 +6,25 @@ SIX_MONTH = 15_552_000
 
 def main
   paths = acquire_absolute_paths
- 
 
   total_block = acquire_blocks(paths)
-
   file_modes = acquire_file_modes
-  links = get_link(paths)
-  names = get_name(paths)
-  groups = get_group(paths)
-  sizes = get_size(paths)
-  months = get_month(paths)
-  days = get_day(paths)
-  times = get_time(paths)
-  files = get_file
+  links = acquire_links(paths)
+  names = acquire_names(paths)
+  groups = acquire_groups(paths)
+  sizes = acquire_sizes(paths)
+  months = acquire_months(paths)
+  days = acquire_days(paths)
+  times = acquire_times(paths)
+  files = acquire_files
 
   puts "total #{total_block}"
-  file_modes.zip(links, names, groups, sizes, months, days, times, files).each do |display|
-    puts display.join
+  file_modes.zip(links, names, groups, sizes, months, days, times, files).each do |row|
+    puts row.join
   end
 end
 
-
-#絶対パスの取得
+# 絶対パスの取得
 def acquire_absolute_paths
   files = Dir.glob('*').sort
   current_path = Dir.getwd
@@ -36,7 +33,7 @@ def acquire_absolute_paths
   end
 end
 
-#総ブロック数の取得
+# 総ブロック数の取得
 def acquire_blocks(paths)
   blocks = paths.map do |path|
     File.stat(path).blocks
@@ -44,143 +41,134 @@ def acquire_blocks(paths)
   blocks.sum
 end
 
-#ファイルモードの取得
+# ファイルモードの取得
 def acquire_file_modes
   paths = acquire_absolute_paths
-  numbers = acquire_numbers(paths)
-  letters = convert_to_letter(numbers)
+  nums = acquire_file_mode_nums(paths)
+  adjusted_nums = adjust_file_mode_nums_length(nums)
+  convert_to_letter(adjusted_nums)
 end
 
-def acquire_numbers(paths)
-  new_file_mode = []
-  file_mode = paths.map do |path|
-    '0%o' % File.stat(path).mode
+def acquire_file_mode_nums(paths)
+  paths.map do |path|
+    format('0%o', File.stat(path).mode)
   end
-  file_mode.map do |number|
-    number.insert(0, '0') if number.length < 7
-  end
-  new_file_mode << file_mode
-  new_file_mode
 end
 
-def convert_to_letter(permission)
-  file_types = { '001' => 'p', '002' => 'c', '004' => 'd', '006' => 'b', '010' => '-', '012' => 'l', '014' => 's' }
-  file_permissions = { '00' => '---', '01' => '--x', '02' => '-w-', '03' => '-wx', '04' => 'r--', '05' => 'r-x', '06' => 'rw-', '07' => 'rwx' }
-
-  parts1 = []
-  parts2 = []
-  parts3 = []
-  parts4 = []
-
-  permission.each do |numbers|
-    numbers.each do |num|
-      a = file_types.select { |key, _value| key == num[0, 3] }
-      parts1 << a.values
-
-      b = file_permissions.select { |key, _value| key == num[3, 2] }
-      parts2 << b.values
-
-      c = file_permissions.select { |key, _value| key[1, 1] == num[5, 1] }
-      parts3 << c.values
-
-      d = file_permissions.select { |key, _value| key[1, 1] == num[6, 1] }
-      parts4 << d.values
-    end
-  end
-
-  parts1.zip(parts2, parts3, parts4).each(&:join)
-end
-
-#出力用調整
-
-def into_arrays(total, blank_size)
-  final = []
-  adjust_blank(total).each do |blank|
-    final << "#{' ' * blank_size} " + blank.to_s
-  end
-  final
-end
-
-def adjust_blank(sizes)
-  max_length = sizes.max.to_s.length
-  sizes.map do |size|
-    gap = max_length - size.to_s.length
-    if gap != 0
-      size.to_s.insert(0, (' ' * gap).to_s)
+def adjust_file_mode_nums_length(nums)
+  nums.map do |num|
+    if num.length < 7
+      num.insert(0, '0')
     else
-      size.to_s
+      num
     end
   end
 end
 
+def convert_to_letter(adjusted_nums)
+  file_types_nums = { '001' => 'p', '002' => 'c', '004' => 'd', '006' => 'b', '010' => '-', '012' => 'l', '014' => 's' }
+  file_permissions_nums = { '00' => '---', '01' => '--x', '02' => '-w-', '03' => '-wx', '04' => 'r--', '05' => 'r-x', '06' => 'rw-', '07' => 'rwx' }
 
-def get_link(paths)
-  links_sizes = paths.map do |path|
+  file_types = []
+  owner_permissions = []
+  group_permissions = []
+  other_permissions = []
+
+  adjust_file_mode_nums_length(adjusted_nums).each do |num|
+    file_type = file_types_nums.select { |key, _value| key == num[0, 3] }
+    file_types << file_type.values
+
+    owner_permission = file_permissions_nums.select { |key, _value| key == num[3, 2] }
+    owner_permissions << owner_permission.values
+
+    group_permission = file_permissions_nums.select { |key, _value| key[1, 1] == num[5, 1] }
+    group_permissions << group_permission.values
+
+    other_permission = file_permissions_nums.select { |key, _value| key[1, 1] == num[6, 1] }
+    other_permissions << other_permission.values
+  end
+  file_types.zip(owner_permissions, group_permissions, other_permissions).each(&:join)
+end
+
+# リンク数、ユーザー名、グループ名、ファイルサイズの取得
+# 空白調整のためのメソッド
+def adjust_blank(contents, blank_size_num)
+  max_length = contents.max.to_s.length
+
+  contents_with_blank = contents.map do |content|
+    gap = max_length - content.to_s.length
+    if gap != 0
+      content.to_s.insert(0, (' ' * gap).to_s)
+    else
+      content.to_s
+    end
+  end
+
+  contents_with_blank.map do |content|
+    "#{' ' * blank_size_num} " + content.to_s
+  end
+end
+
+# 以下各種情報の取得
+def acquire_links(paths)
+  links = paths.map do |path|
     File.stat(path).nlink
   end
-  into_arrays(links_sizes, 0)
+  adjust_blank(links, 0)
 end
 
-def get_name(paths)
-  users_total = paths.map do |path|
+def acquire_names(paths)
+  users = paths.map do |path|
     Etc.getpwuid(File.stat(path).uid).name
   end
-  into_arrays(users_total, 0)
+  adjust_blank(users, 0)
 end
 
-def get_group(paths)
-  groups_total = paths.map do |path|
+def acquire_groups(paths)
+  groups = paths.map do |path|
     Etc.getgrgid(File.stat(path).gid).name
   end
-  into_arrays(groups_total, 1)
+  adjust_blank(groups, 1)
 end
 
-def get_size(paths)
-  sizes_total = paths.map do |path|
+def acquire_sizes(paths)
+  sizes = paths.map do |path|
     File.stat(path).size
   end
-  into_arrays(sizes_total, 1)
+  adjust_blank(sizes, 1)
 end
 
-
-
-def get_month(paths)
-  new_month = []
+def acquire_months(paths)
   paths.map do |path|
-    months = File.stat(path).mtime.mon
-    new_month << months.to_s.rjust(3)
+    month = File.stat(path).mtime.mon
+    month.to_s.rjust(3)
   end
-  new_month
 end
 
-def get_day(paths)
-  new_days = []
+def acquire_days(paths)
   paths.map do |path|
-    days = File.stat(path).mtime.mday
-    new_days << days.to_s.rjust(3)
+    day = File.stat(path).mtime.mday
+    day.to_s.rjust(3)
   end
-  new_days
 end
 
-def get_time(paths)
-  time_details = paths.map do |path|
+def acquire_times(paths)
+  detailed_times = paths.map do |path|
     File.stat(path).mtime
   end
 
-  new_times = []
-  time_details.each do |detail|
+  detailed_times.map do |detailed_time|
     today = Time.new
-    display = if today - detail > SIX_MONTH
-                "  #{detail.year}"
-              else
-                " #{detail.strftime('%H:%M')}"
-              end
-    new_times << display.to_s.rjust(6)
+    time = if today - detailed_time > SIX_MONTH
+             "  #{detailed_time.year}"
+           else
+             " #{detailed_time.strftime('%H:%M')}"
+           end
+    time.to_s.rjust(6)
   end
-  new_times
 end
 
-def get_file
+def acquire_files
   Dir.glob('*').sort.map do |file|
     " #{file}"
   end
