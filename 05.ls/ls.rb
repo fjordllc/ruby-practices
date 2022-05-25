85aedb35c7e843ac3e04c45436bc9319a6c7c424
kgoa <y-keigoe@ruby-dev.jp>
2022-05-25 14:27:36 +0900

    🔡Add -r option.

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,27 +3,37 @@
 
 require 'optparse'
 
+# ネットで見つけた崩れないljust
+class String
+  def mb_ljust(width, padding = ' ')
+    output_width = each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.reduce(0, &:+)
+    padding_size = [0, width - output_width].max
+    self + padding * padding_size
+  end
+end
+
 # オプション
-option = ''
+option = []
 opt = OptionParser.new
-opt.on('-a') { option = '-a' }
+opt.on('-a') { option << 'a' }
+opt.on('-r') { option << 'r' }
 opt.parse!(ARGV)
 dir_name = ARGV[0] || '.'
 
 # ディレクトリ情報取得
 files = []
 
-# ls デフォルト
+# lsコマンド
 def ls_cmd(dir_name, files, option)
-  dotmatch = option == '-a' ? File::FNM_DOTMATCH : 0
+  dotmatch = option.include?('a') ? File::FNM_DOTMATCH : 0
   if Dir.exist?(dir_name)
-    files = Dir.glob('*', dotmatch, base: dir_name)
+    files = Dir.glob('*', dotmatch, base: dir_name, sort: true)
     count = (files.size / 3.0).ceil(0)
-    files_sort = files.sort
+    files_sort = option.include?('r') ? files.sort.reverse : files
     count.times do |i|
       3.times do |j|
         result = files_sort[i + j * count]
-        print result.ljust(12).to_s if result
+        print result.mb_ljust(12).to_s if result
       end
       puts ''
     end
