15d815969e3175005acdf0852cda505eeaa844ef
kosaku <bouru02@gmail.com>
2022-05-27 13:13:57 +0900

    wcコマンド完成

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -15,15 +15,17 @@ def main
   end
 
   wc = WC.new(**param)
-  wc.format_output
+  wc.output_wc_results
 end
 
 class WC
   def initialize(param)
-    @strings = obtain_target_strings(param[:files])
+    @options = param[:options]
+    @target_files = param[:files]
+    @strings = obtain_target_strings(@target_files)
     @lines = []
     @word_count = []
-    @bytesizes = obtain_bytesizes(param[:files])
+    @bytesizes = obtain_bytesizes(@target_files)
 
     @strings.each do |string|
       @lines << string.length
@@ -31,18 +33,13 @@ class WC
         line.split(/[\s　]+/).length
       end.sum
     end
-  end
 
-  def total_lines
-    @lines.each(&:sum)
-  end
+    return if @target_files.size <= 1
 
-  def total_word_count
-    @word_count.each(&:sum)
-  end
-
-  def total_filesize
-    @filesizes.each(&:sum)
+    @lines << @lines.sum
+    @word_count << @word_count.sum
+    @bytesizes << @bytesizes.sum
+    @target_files << 'total'
   end
 
   def calc_total_filesizes(files)
@@ -52,13 +49,24 @@ class WC
     @filesizes.sum
   end
 
-  def format_output
-    @strings.map do |i|
-      
+  def output_wc_results
+    outputs = []
+    outputs << @lines
+    unless @options.grep(/l/).length >= 1
+      outputs << @word_count
+      outputs << @bytesizes
     end
-    p @lines
-    p @word_count
-    p @bytesizes
+    outputs << @target_files
+
+    if @target_files.empty?
+      puts outputs.join(' ')
+    else
+      formatted_outputs = outputs.map.with_index do |string, i|
+        # 最後の列（ファイル名）以外は右揃えにする
+        i == outputs.size - 1 ? string : fit_to_longest_item(string, right_aligned: true)
+      end.transpose
+      formatted_outputs.each { |item| puts item.join(' ') }
+    end
   end
 
   private
@@ -87,6 +95,25 @@ class WC
       end
     end
     bytesizes
+  end
+
+  # 配列の各要素の長さを最大の要素の幅に合わせる（短い要素の末尾に半角スペースを追加する）
+  # 引数：配列
+  # 戻り値：最長の要素に合わせて各要素に半角スペースを追加した配列
+  def fit_to_longest_item(items, right_aligned: false)
+    longest_length = items.max_by { |item| item.to_s.length }.to_s.length
+    items.map do |item|
+      space_count = longest_length - item.to_s.length
+      if space_count.positive?
+        if right_aligned
+          item.to_s.rjust(longest_length)
+        else
+          item.to_s.ljust(longest_length)
+        end
+      else
+        item
+      end
+    end
   end
 end
 
