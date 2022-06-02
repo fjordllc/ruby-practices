ebf819fcbba64c0bdd2a0cf1da8c870953ce79c4
pachikuriii <stakahashi0728@gmail.com>
2022-06-02 12:18:34 +0900

    メソッドの順番を変更

--- 05.ls/ls04.rb
+++ 05.ls/ls04.rb
@@ -45,23 +45,7 @@ def exec_no_option(all_files)
   exit
 end
 
-def get_transposed_all_files(all_files)
-  all_files.push(' ') while all_files.length % ROW_NUM != 0
-  column_num = all_files.length / ROW_NUM
-  transposed_files = all_files.each_slice(column_num).to_a.transpose
-  transposed_files.first(column_num).each do |column|
-    ROW_NUM.times do |index|
-      column[index] += ' ' * (ROW_MAX_WIDTH - column[index].length)
-    end
-  end
-end
-
-def display(files_in_columns)
-  files_in_columns.each do |column|
-    puts column.join
-  end
-end
-
+# lオプションありの場合の細かいメソッド
 def acquire_file_info(all_files)
   all_files.map do |file|
     File.stat(file)
@@ -183,6 +167,24 @@ def acquire_times(files_info)
              " #{detailed_time.strftime('%H:%M')}"
            end
     time.to_s.rjust(6)
+  end
+end
+
+# lオプションなしの場合の細かいメソッド
+def get_transposed_all_files(all_files)
+  all_files.push(' ') while all_files.length % ROW_NUM != 0
+  column_num = all_files.length / ROW_NUM
+  transposed_files = all_files.each_slice(column_num).to_a.transpose
+  transposed_files.first(column_num).each do |column|
+    ROW_NUM.times do |index|
+      column[index] += ' ' * (ROW_MAX_WIDTH - column[index].length)
+    end
+  end
+end
+
+def display(files_in_columns)
+  files_in_columns.each do |column|
+    puts column.join
   end
 end
 
