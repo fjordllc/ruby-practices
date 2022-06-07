5865e277263829264e7126cfc0d32eaa76bdd2ab
Yatsu0720 <witdesigns@yatsu-world.com>
2022-06-07 20:03:41 +0900

    ボウリング計算プログラムの修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,42 +14,25 @@ scores.each do |s|
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
-point = 0
-
-(0..7).each do |frame| # 1~8フレーム目までの計算
+point = (0..8).sum do |frame| # 1~8フレーム目までの計算
   next_frame = frame + 1
   next_to_next_frame = next_frame + 1
 
-  point += if frames[frame][0] == 10 # strikeの場合
-             if frames[next_frame][0] == 10 # 次フレームもstrikeの場合
-               frames[next_to_next_frame][0] + 20
-             else
-               frames[next_frame].sum + 10
-             end
-           elsif frames[frame].sum == 10 # spare
-             # spare特殊処理
-             frames[next_frame][0] + 10
-           else
-             frames[frame].sum
-           end
+  if frames[frame][0] == 10 # strikeの場合
+    if frames[next_frame][0] == 10 # 次フレームもstrikeの場合
+      frames[next_to_next_frame][0] + 20
+    else
+      frames[next_frame].sum + 10
+    end
+  elsif frames[frame].sum == 10 # spare
+    # spare特殊処理
+    frames[next_frame][0] + 10
+  else
+    frames[frame].sum
+  end
 end
-
-point += if frames[8][0] == 10 # 9フレーム目の計算,strike
-           if frames[9][0] == 10 # 10フレーム目の1投目がstrike
-             frames[10][0] + 20
-           else
-             frames[9].sum + 10
-           end
-         elsif frames[8].sum == 10 # 9フレーム目の計算,spare
-           frames[9][0] + 10
-         else
-           frames[8].sum
-         end
 
 last_frame = frames[9].to_a + frames[10].to_a + frames[11].to_a
 point += last_frame.sum # 最終フレームは便宜上、frames[9]~franes[11]と扱う
