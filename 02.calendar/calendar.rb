9160d93073abc361e02db4ff596661e32c745248
taea <igiari@gmail.com>
2022-06-13 12:31:52 +0900

    Remove unnecessary variable

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -12,8 +12,7 @@ def calendar(year, month)
   month = month.to_i
   puts "      #{month}月 #{year}"
   puts "日 月 火 水 木 金 土"
-  last_day_of_the_month = Date.new(year, month, -1).day
-  (1..last_day_of_the_month).each do |day|
+  (1..Date.new(year, month, -1).day).each do |day|
     date = Date.new(year, month, day)
     date.cwday.times { print "\s" * 3 } if day == 1
     print "\s" if day / 10 == 0
