8ea3f9c5f51317e408a92373a10dbef69fb5d905
taea <igiari@gmail.com>
2022-06-18 20:12:23 +0900

    Add case when the first day is Sunday

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,7 +6,7 @@ def calendar(year, month)
   puts "日 月 火 水 木 金 土"
   (Date.new(year, month, 1)..Date.new(year, month, -1)).each do |date|
     day = date.day
-    date.cwday.times { print "\s" * 3 } if day == 1
+    date.cwday.times { print "\s" * 3 } if day == 1 && date.cwday < 7
     print "\s" if day / 10 == 0
     print "#{ date == Date.today ? "\e[30;47m#{day}\e[0m" : day }"
     print "\s"
