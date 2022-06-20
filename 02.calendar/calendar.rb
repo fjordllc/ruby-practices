93b786f1164cd6d0eef3ba3b9f19594350ff9c0d
taea <igiari@gmail.com>
2022-06-20 17:41:01 +0900

    Set rjust to variable in advance

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,9 +5,9 @@ def calendar(year, month)
   puts "      #{month}月 #{year}"
   puts "日 月 火 水 木 金 土"
   (Date.new(year, month, 1)..Date.new(year, month, -1)).each do |date|
-    day = date.day
+    day = date.day.to_s.rjust(2)
     date.cwday.times { print "\s" * 3 } if day == 1 && !date.sunday?
-    print date == Date.today ? "\e[30;47m#{day.to_s.rjust(2)}\e[0m" : day.to_s.rjust(2)
+    print date == Date.today ? "\e[30;47m#{day}\e[0m" : day
     print "\s"
     print "\n" if date.saturday?
   end
