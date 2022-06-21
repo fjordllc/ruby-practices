1364d93b3b27ed6e2808196d1b1d144f81fc344b
taea <igiari@gmail.com>
2022-06-21 14:25:33 +0900

    Fix error of day count

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,7 +6,7 @@ def calendar(year, month)
   puts "日 月 火 水 木 金 土"
   (Date.new(year, month, 1)..Date.new(year, month, -1)).each do |date|
     day = date.day.to_s.rjust(2)
-    date.cwday.times { print "\s" * 3 } if day == 1 && !date.sunday?
+    date.cwday.times { print "\s" * 3 } if date.day == 1 && !date.sunday?
     print date == Date.today ? "\e[30;47m#{day}\e[0m" : day
     print "\s" unless date.saturday?
     print "\n" if date.saturday?
