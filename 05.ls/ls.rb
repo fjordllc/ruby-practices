2ef6d332bf7aa1d547815ad623dcb4f1428f7b40
lan <le.hoang.lan@actindi.net>
2022-06-21 14:42:13 +0900

    fix logic

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
-def get_files(path)
-  files = path ? Dir.entries(path) - %w[. ..] : Dir.entries('.') - %w[. ..]
+def get_files
+  files = Dir.glob("*")
   files = files.sort
-  files.filter { |file| file&.chr != '.' }
 end
 
-def print_files(file_name)
-  files = get_files(file_name)
+def print_files
+  files = get_files
   # カラムを変更する変数
   columns = 3
-  number_row = files.length % columns ? files.length / columns + 1 : files.length
-  tab_files = files.each_slice(number_row).to_a
-  (0..(number_row - 1)).each do |i|
+  number_of_rows = (files.length % columns).zero? ? files.length / columns : files.length / columns + 1 
+  tab_files = files.each_slice(number_of_rows).to_a
+  number_of_rows.times do |i|
     lines = tab_files.map { |file| file[i]&.slice(0, 15)&.ljust(20) unless file[i].nil? }.compact
     puts lines.join('')
   end
 end
-print_files(ARGV[0])
+print_files
