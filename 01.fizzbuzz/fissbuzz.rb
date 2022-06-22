0042c5b19dd53313e89d0806f572c69974fafa83
yamabe009 <yamabe.kinoko@gmail.com>
2022-06-22 21:56:45 +0900

    add fizzbuzz

---
+++ 01.fizzbuzz/fissbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuss(num=0)
+  return num unless fizz?(num) || buzz?(num)
+  result = ""
+  result += "FIZZ" if fizz?(num)
+  result += "BUZZ" if buzz?(num)
+  result
+end
+
+def fizz?(num)
+  num % 3 == 0
+end
+
+def buzz?(num)
+  num % 5 == 0
+end
+
+(1..20).to_a.each {|x|
+  puts fizzbuss(x)
+}
