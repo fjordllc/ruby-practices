9a0965838eb994ba7805c64f6d238494a937a107
pachikuriii <stakahashi0728@gmail.com>
2022-06-25 19:47:37 +0900

    メソッドに切り出した

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,12 +3,44 @@ require 'optparse'
 def main
   files = ARGV
   params = ARGV.getopts('clw')
-  file = files.map { |file| File.open(file).read }
-  puts file
+  display(files)
 end
 
-def acquire_number_of_lines(content)
-  content.count("\n")
+def acquire_num_of_lines(files)
+  files.map do |file|
+    content = File.open(file).read
+    content.count("\n").to_s.rjust(8)
+  end
+end
+
+def acquire_num_of_words(files)
+  files.map do |file|
+    content = File.open(file).read
+    content.split(/\s+/).size.to_s.rjust(8)
+  end
+end
+
+def acquire_size_of_file(files)
+  files.map do |file|
+    content = File.open(file).read
+    File::Stat.new(file).size.to_s.rjust(8)
+  end
+end
+
+def acquire_file_name(files)
+  files.map do |file|
+    " #{file}"
+  end
+end
+
+def display(files)
+  num_of_lines = acquire_num_of_lines(files)
+  num_of_words = acquire_num_of_words(files)
+  size_of_file = acquire_size_of_file(files)
+  file_name = acquire_file_name(files)
+  num_of_lines.zip(num_of_words, size_of_file, file_name).each do |row|
+    puts row.join
+  end
 end
 
 main
