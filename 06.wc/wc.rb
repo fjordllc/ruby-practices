f337efb0616b8a9837c377b2a7fedd7a5bd1f2e4
pachikuriii <stakahashi0728@gmail.com>
2022-06-26 20:44:21 +0900

    どの順番でオプションが入力されても対応できるように

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,38 +5,48 @@ MAX_WIDTH = 8
 def main
   params = ARGV.getopts('clw')
   files = ARGV
-  info = acquire_info(files, params)
-  display(params, info, files)
-  total(files) if files.length > 1
+  display(params, files)
 end
 
-def acquire_info(files, params)
-  files = files.map { |file| }
-
-  files = File.open(file).read.count("\n") if params['l']
-  files = File.open(file).read.split(/\s+/).size if params['w']
-  files = File::Stat.new(file).size if params['c']
-
-  files
-end
-
-def display(params, _info, files)
-  if params['l']
-    exec_l_option(files)
-  else
+def display(params, files)
+  if params.values.any? == false
     exec_no_option(files)
+  else
+    exec_options(params, files)
   end
 end
 
-def exec_l_option(files)
+def exec_no_option(files)
+  num_of_lines = acquire_num_of_lines(files)
+  num_of_words = acquire_num_of_words(files)
+  size_of_file = acquire_size_of_file(files)
+  file_name = acquire_file_name(files)
+  num_of_lines.zip(num_of_words, size_of_file, file_name).each do |row|
+    puts row.join
+  end
+  no_option_total(files) if files.length > 1
+end
+
+def exec_options(params, files)
+  stats = []
+  stats = stats << acquire_num_of_lines(files) if params['l']
+  stats = stats << acquire_num_of_words(files) if params['w']
+  stats = stats << acquire_size_of_file(files) if params['c']
+  stats = stats << acquire_file_name(files)
+  stats.transpose.each do |stat|
+    puts stat.join
+  end
+end
+
+def acquire_num_of_lines(files)
   files.map { |file| File.open(file).read.count("\n").to_s.rjust(MAX_WIDTH) }
 end
 
-def exec_w_option(files)
+def acquire_num_of_words(files)
   files.map { |file| File.open(file).read.split(/\s+/).size.to_s.rjust(MAX_WIDTH) }
 end
 
-def exec_c_option(files)
+def acquire_size_of_file(files)
   files.map { |file| File::Stat.new(file).size.to_s.rjust(MAX_WIDTH) }
 end
 
@@ -44,27 +54,7 @@ def acquire_file_name(files)
   files.map { |file| " #{file}" }
 end
 
-def exec_other_options(files)
-  num_of_lines = exec_l_option(files)
-  num_of_words = exec_w_option(files)
-  size_of_file = exec_c_option(files)
-  file_name = acquire_file_name(files)
-  num_of_lines.zip(num_of_words, size_of_file, file_name).each do |row|
-    puts row.join
-  end
-end
-
-def exec_no_option(files)
-  num_of_lines = exec_l_option(files)
-  num_of_words = exec_w_option(files)
-  size_of_file = exec_c_option(files)
-  file_name = acquire_file_name(files)
-  num_of_lines.zip(num_of_words, size_of_file, file_name).each do |row|
-    puts row.join
-  end
-end
-
-def total(files)
+def no_option_total(files)
   num_of_lines = files.map { |file| File.open(file).read.count("\n") }
   num_of_words = files.map { |file| File.open(file).read.split(/\s+/).size }
   num_of_sizes = files.map { |file| File::Stat.new(file).size }
