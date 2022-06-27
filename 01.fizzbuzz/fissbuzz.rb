7779b2fb3a4e1c7d0bfa790e5349f5bc633cd700
yamabe009 <yamabe.kinoko@gmail.com>
2022-06-27 22:36:41 +0900

    フォーマッターでフォーマット

--- 01.fizzbuzz/fissbuzz.rb
+++ 01.fizzbuzz/fissbuzz.rb
@@ -1,8 +1,9 @@
-def fizzbuss(num=0)
+def fizzbuss(num = 0)
   return num unless fizz?(num) || buzz?(num)
-  result = ""
-  result += "FIZZ" if fizz?(num)
-  result += "BUZZ" if buzz?(num)
+
+  result = ''
+  result += 'FIZZ' if fizz?(num)
+  result += 'BUZZ' if buzz?(num)
   result
 end
 
@@ -14,6 +15,6 @@ def buzz?(num)
   num % 5 == 0
 end
 
-(1..20).to_a.each {|x|
+(1..20).to_a.each do |x|
   puts fizzbuss(x)
-}
+end
