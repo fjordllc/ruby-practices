8ae6447b282c16a60afbd5256bad0d2eede5085e
mame0124 <mame0124.rkn@gmail.com>
2022-06-27 11:03:27 +0900

    メソッド名変更、rubocopでコード修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,61 +1,69 @@
+# frozen_string_literal: true
+
 require 'optparse'
 
 class LSCommand
-	def initialize
-		opt = OptionParser.new
-		params = {}
-		opt.on('-a') {|v| params[:a] = v }
-		opt.on('-r') {|v| params[:r] = v }
-		opt.on('-l') {|v| params[:l] = v }
-		opt.parse!(ARGV)
-		#puts ARGV.class
-		#puts ARGV[0].class
-		#puts ARGV[1].class
+  def initialize
+    opt = OptionParser.new
+    params = {}
+    opt.on('-a') { |v| params[:a] = v }
+    opt.on('-r') { |v| params[:r] = v }
+    opt.on('-l') { |v| params[:l] = v }
+    opt.parse!(ARGV)
 
-		@path = ARGV[0] ? ARGV[0] : Dir.pwd
-		#puts @path
-	end
+    @path = Dir.pwd if ARGV #== []
+    @path = ARGV if ARGV[1]
+    @path = ARGV[0] if ARGV[0] && ARGV[1].nil?
+  end
 
-	def datein 
-		@name = {}
-		@max_size = 0
+  def output
+    case @path
+    when String
+      file_date_in(@path)
+      output_without_options
+    when Array
+      @path.each do |path|
+        puts "#{path}:"
+        file_date_in(path)
+        output_without_options
+      end
+    end
+  end
 
-		Dir.chdir(@path) do
-			Dir.glob(@path + "/*" ).each do |n|
-				filename = n.gsub(/.+\/([^\/]+$)/){ $1 }
-				@max_size = filename.size if @max_size < filename.size
-				@name[:"#{filename}"] = File.stat(filename)	
-			end
-		end
-	end
+  def file_date_in(path)
+    @file_date = {}
+    @max_name_size = 0
+    Dir.chdir(path) do
+      Dir.glob('*').each do |n|
+        filename = n.gsub(%r{.+/([^/]+$)}) { '\1' }
+        @max_name_size = filename.size if @max_name_size < filename.size
+        @file_date[:"#{filename}"] = File.stat(filename)
+      end
+    end
+  end
 
-	def row_line
-		case @max_size
-		when (35..)
-			row = 1
-		when (24..34)
-			row = 2
-		else
-			row = 3
-		end
+  def output_without_options
+    case @max_name_size
+    when (35..)
+      row = 1
+    when (24..34)
+      row = 2
+      width = 36
+    else
+      row = 3
+      width = 23
+    end
 
-		@line = @name.size / row 
-		@line += 1 if @name.size % row > 0
-	end
-
-	def output
-		@line.times do |time|
-			@name.select.with_index{|(k, v), i| i % @line == time }.keys.each{|k| print "%-23s" % k}
-			print "\n"
-			time += 1
-		end
-	end
-
+    line = @file_date.size / row
+    line += 1 if (@file_date.size % row).positive?
+    line.times do |time|
+      @file_date.select.with_index { |(_k, _v), i| i % line == time }.each_key { |k| print format("%-#{width}s", k) }
+      print "\n"
+      time += 1
+    end
+    print "\n"
+  end
 end
 
 ls = LSCommand.new
-#puts ls.class
-ls.datein
-#puts ls.class
-ls.row_line
 ls.output
