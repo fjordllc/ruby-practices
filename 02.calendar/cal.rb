fbd3ccc6ed4e683f25c3f06e963a6f7f01058df5
yamabe009 <yamabe.kinoko@gmail.com>
2022-06-29 08:53:35 +0900

    リファクタのため、Dateオブジェクトの作成タイミングを変更
    ループの中でDateオブジェクトを作るのではなく、
    Dateで範囲オブジェクトを作ってループを回すように修正しました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,32 +9,30 @@ def cal
   params = ARGV.getopts('y:m:')
 
   if params['y'].nil? && params['m'].nil?
-    date = Date.today
+    first_date = Date.new(Date.today.year, Date.today.month, 1)
   elsif params['y'].present? && params['m'].present?
-    date = Date.new(params['y'].to_i, params['m'].to_i, 1)
+    first_date = Date.new(params['y'].to_i, params['m'].to_i, 1)
   else
     puts '引数が不正です。'
     puts '引数を設定する場合は、-yに年数、-mに月数の両方を渡してください'
     return
   end
 
-  print_cal(date)
+  print_cal(first_date)
 end
 
-def print_cal(date)
+def print_cal(first_date)
   result_lines = []
   result_lines.push "#{date.month}月 #{date.year}".center(LINE_LENGTH)
-
   result_lines.push '日 月 火 水 木 金 土'
 
-  first_day = 1
-  last_day = Date.new(date.year, date.month, -1).day
+  last_date = Date.new(date.year, date.month, -1)
   week = []
 
-  (first_day..last_day).to_a.each do |day|
-    week.push(day.to_s.rjust(2))
+  (first_date..last_date).each do |date|
+    week.push(date.day.to_s.rjust(2))
 
-    if Date.new(date.year, date.month, day).saturday?
+    if date.saturday?
       result_lines.push week.join(' ').rjust(LINE_LENGTH)
       week.clear
     end
