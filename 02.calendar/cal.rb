0d15319f1abe323b3f828bce1e6595a19a9169ed
yamabe009 <yamabe.kinoko@gmail.com>
2022-06-29 08:45:55 +0900

    過剰なコメントを削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -23,33 +23,25 @@ end
 
 def print_cal(date)
   result_lines = []
-  # 6月 2022
   result_lines.push "#{date.month}月 #{date.year}".center(LINE_LENGTH)
 
-  # 日 月 火 水 木 金 土
   result_lines.push '日 月 火 水 木 金 土'
 
-  # 月の初日と末日
   first_day = 1
   last_day = Date.new(date.year, date.month, -1).day
   week = []
 
-  # 初日から末日まで回して、週ごとに整形処理
   (first_day..last_day).to_a.each do |day|
     week.push(day.to_s.rjust(2))
 
-    # 土曜日になったら
     if Date.new(date.year, date.month, day).saturday?
       result_lines.push week.join(' ').rjust(LINE_LENGTH)
       week.clear
     end
   end
-  # 最終週
   result_lines.push week.join(' ') unless week.empty?
 
-  # 改行入れて描画！
   print result_lines.join('\n')
 end
 
-# 実行
 cal
