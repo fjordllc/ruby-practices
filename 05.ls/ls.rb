d3f50c6bc024339cd7630b7c14c4760d3f1032a8
taea <igiari@gmail.com>
2022-07-01 17:38:21 +0900

    Rubocop

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,23 +1,25 @@
-require "find"
+# frozen_string_literal: true
+
+require 'find'
 
 def ls(dir, column)
   files = []
   Find.find(dir) do |file|
     file_name = File.basename(file)
-    unless /^\./ =~ file_name || File.basename(dir) == file_name
-      case File.ftype(file)
-      when "directory"
-        files << file_name + '/'
-      when "link"
-        files << file_name + '@'
-      else
-        files << file_name
-      end
-    end
+    next if /^\./ =~ file_name || File.basename(dir) == file_name
+
+    files << case File.ftype(file)
+             when 'directory'
+               "#{file_name}/"
+             when 'link'
+               "#{file_name}@"
+             else
+               file_name
+             end
   end
   files_per_column = files.length / column + 1
   0.upto(files_per_column).each_with_index do |line_number, index|
-    files.each_slice( files_per_column ) do |file|
+    files.each_slice(files_per_column) do |file|
       columns = file.to_a
       print columns[line_number].ljust(17) if columns[line_number]
     end
