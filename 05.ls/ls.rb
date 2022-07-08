c3a13c39838451f1714e01f107ad7d3881eb9ffc
lan <le.hoang.lan@actindi.net>
2022-07-08 19:57:38 +0900

    プリントできた

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
-require "etc"
+
+require 'etc'
 require 'optparse'
 
+TYPES_OF_FILE = {
+  '10' => 'p',
+  '20' => 'c',
+  '40' => 'd',
+  '60' => 'b',
+  '100' => '-',
+  '120' => 'l',
+  '140' => 's'
+}.freeze
+PERMISSION_CODE = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'].freeze
 class LS
   def initialize
     opt = OptionParser.new
@@ -16,65 +27,43 @@ class LS
   def exec
     files = Dir.glob('*', @option[:a] ? File::FNM_DOTMATCH : 0)
     files.reverse! if @option[:r]
-    files.map{ |file| print_file_detail(file) } if @option[:l]
+    files.map { |file| print_file_detail(file) } if @option[:l]
     files
   end
 
   def print_files
     files = exec
-    # # カラムを変更する変数
-    # columns = 3
-    # number_of_rows = (files.length % columns).zero? ? files.length / columns : files.length / columns + 1
-    # tab_files = files.each_slice(number_of_rows).to_a
-    # number_of_rows.times do |i|
-    #   lines = tab_files.map { |file| file[i]&.slice(0, 15)&.ljust(20) unless file[i].nil? }.compact
-    #   puts lines.join('')
-    # end
-  end
+    return if @option[:l]
 
-  def get_permission_code permission
-    permission_code= ['---','--x','-w-','-wx','r--','r-x','rw-','rwx']
-    return permission_code[permission]
-  end
-
-  def handling_type_of_file file
-    case file
-    when '10'
-      permission_str = 'p' 
-    when '20'
-      permission_str = 'c'
-    when '40'
-      permission_str = 'd'
-    when '60'
-      permission_str = 'b'
-    when '100'
-      permission_str = '-'
-    when '120'
-      permission_str = 'l'
-    when '140'
-      permission_str = 's'
+    # カラムを変更する変数
+    columns = 3
+    number_of_rows = (files.length % columns).zero? ? files.length / columns : files.length / columns + 1
+    tab_files = files.each_slice(number_of_rows).to_a
+    number_of_rows.times do |i|
+      lines = tab_files.map { |file| file[i]&.slice(0, 15)&.ljust(20) unless file[i].nil? }.compact
+      puts lines.join('')
     end
   end
 
-  def print_file_permissions file
+  def print_file_permissions(file)
     file_stat = File.lstat(file).mode.to_s(8)
     permission_number = file_stat[-3..-1]
-    type_of_file = handling_type_of_file(file_stat.tr(permission_number, ''))
-    permission_code = permission_number.chars.map{ |c|
-      get_permission_code(c.to_i)
-    }.join('')
-    full_permission = type_of_file + permission_code
+    type_of_file = TYPES_OF_FILE[(file_stat[0..-4])]
+    permission_code = permission_number.chars.map do |c|
+      PERMISSION_CODE[c.to_i]
+    end.join('')
+    type_of_file + permission_code
   end
 
-  def print_file_detail file
-    str = "%s  %s  %s  %s  %s  %s  %s"
+  def print_file_detail(file)
+    str = '%s  %s  %s  %s  %s  %s  %s'
     file_detail = [print_file_permissions(file),
-    File.lstat(file).nlink.to_s, user_name(File.lstat(file).uid),
-    group_name(File.lstat(file).gid),
-    File.lstat(file).size.to_s,
-    format_created_time(File.lstat(file).mtime),
-    file]
-    p str % file_detail
+                   File.lstat(file).nlink.to_s, user_name(File.lstat(file).uid),
+                   group_name(File.lstat(file).gid),
+                   File.lstat(file).size.to_s,
+                   format_created_time(File.lstat(file).mtime),
+                   file]
+    puts(str % file_detail).inspect
   end
 
   def user_name(uid)
@@ -85,7 +74,7 @@ class LS
     Etc.getgrgid(gid).name
   end
 
-  def format_created_time created_time
+  def format_created_time(created_time)
     created_time.strftime('%m月 %d %H:%M').to_s
   end
 end
