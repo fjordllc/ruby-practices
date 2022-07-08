049b94c933831090c06f31bc7efa4f3ea748e2c4
pachikuriii <stakahashi0728@gmail.com>
2022-07-08 10:44:45 +0900

    レビューでアドバイスいただいた箇所を変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,82 +13,60 @@ def main
 end
 
 def exec(params, files, files_content)
-  params = { 'c' => true, 'l' => true, 'w' => true } if params.values.any? == false
+  params = { 'c' => true, 'l' => true, 'w' => true } unless params.values.any?
 
   num_of_lines = num_of_lines(files_content)
   num_of_words = num_of_words(files_content)
   num_of_size_of_file = num_of_size_of_file(files_content)
 
   stats = []
-  stats = stats << convert_to_string(num_of_lines, files_content) if params['l']
-  stats = stats << convert_to_string(num_of_words, files_content) if params['w']
-  stats = stats << convert_to_string(num_of_size_of_file, files_content) if params['c']
-  stats << file_name(files)
+  stats = stats << convert_to_string(num_of_lines) if params['l']
+  stats = stats << convert_to_string(num_of_words) if params['w']
+  stats = stats << convert_to_string(num_of_size_of_file) if params['c']
+  stats << file_name(files) unless files.empty?
+  stats
 end
 
 def display(stats, files, params, files_content)
-  if ARGV.empty?
-    puts stats.join
-  else
-    stats.transpose.each do |stat|
-      puts stat.join
-    end
+  stats.transpose.each do |stat|
+    puts stat.join
   end
   options_total(params, files_content) if files.length > 1
 end
 
 def read_file(files)
   if ARGV.empty?
-    $stdin.read
+    [$stdin.read]
   else
     files.map { |file| File.open(file).read }
   end
 end
 
 def num_of_lines(files_content)
-  if files_content.instance_of?(String)
-    files_content.count("\n")
-  else
-    files_content.map { |content| content.count("\n") }
-  end
+  files_content.map { |content| content.count("\n") }
 end
 
 def num_of_words(files_content)
-  if files_content.instance_of?(String)
-    files_content.split(/\s+/).size
-  else
-    files_content.map { |content| content.split(/\s+/).size }
-  end
+  files_content.map { |content| content.split(/\s+/).size }
 end
 
 def num_of_size_of_file(files_content)
-  if files_content.instance_of?(String)
-    files_content.bytesize
-  else
-    files_content.map(&:bytesize)
-  end
+  files_content.map(&:bytesize)
 end
 
 def file_name(files)
   files.map { |file| " #{file}" }
 end
 
-def convert_to_string(stat, files_content)
-  if files_content.instance_of?(String)
-    stat.to_s.rjust(MAX_WIDTH)
-  else
-    stat.map { |num| num.to_s.rjust(MAX_WIDTH) }
-  end
+def convert_to_string(stat_nums)
+  stat_nums.map { |stat_num| stat_num.to_s.rjust(MAX_WIDTH) }
 end
 
 def options_total(params, files_content)
-  params = { 'c' => true, 'l' => true, 'w' => true } if params.values.any? == false
-  totals = []
-  totals = totals << num_of_lines(files_content).sum.to_s.rjust(MAX_WIDTH) if params['l']
-  totals = totals << num_of_words(files_content).sum.to_s.rjust(MAX_WIDTH) if params['w']
-  totals = totals << num_of_size_of_file(files_content).sum.to_s.rjust(MAX_WIDTH) if params['c']
-
+  params = { 'c' => true, 'l' => true, 'w' => true } unless params.values.any?
+  totals = [%w[lines l], %w[words w], %w[size_of_file c]].map do |name, opt|
+    send("num_of_#{name}", files_content).sum.to_s.rjust(MAX_WIDTH) if params[opt]
+  end
   puts "#{totals.join} total"
 end
-
 main
