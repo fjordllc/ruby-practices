1c128039866286a986f072c170f4d52a176a8665
hibikiosawa <105911942+hibikiosawa@users.noreply.github.com>
2022-07-11 12:11:48 +0900

    each doに変更など 修正

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
+# リストのクラス
 class List
-  @@arr = []
+  @arr = []
 
   def self.input
     Dir.glob('*').each do |d|
-      @@arr.push(d)
+      @arr.push(d)
     end
   end
 
   def self.output
-    arrsize = @@arr.size / 3
-    a = 0
-    for i in 1..arrsize
-      puts "#{@@arr[i + a]} ".ljust(25) + "#{@@arr[i + arrsize + a]} ".ljust(25) + "#{@@arr[i + arrsize + arrsize + a]} ".ljust(25)
-      a += 1
+    arrsize = @arr.size / 3
+    (1..arrsize).each do |i|
+      puts "#{@arr[i]} ".ljust(25) + "#{@arr[i + arrsize]} ".ljust(25) + "#{@arr[i + arrsize + arrsize]} ".ljust(25)
     end
   end
 end
