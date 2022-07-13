fdba6d605a6f9b1201a25677501e4e7d072076a5
Yatsu0720 <witdesigns@yatsu-world.com>
2022-07-13 18:43:01 +0900

    lsコマンドの-rオプションを作成

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -7,10 +7,11 @@ MAX_COLUMN = 3
 
 def print_file_list
   file_list = fetch_file_list
+
   max_word_count = file_list.max_by(&:length).length
   max_row = file_list.size / MAX_COLUMN + 1
 
-  separated_list = file_list.sort.each_slice(max_row).to_a
+  separated_list = file_list.each_slice(max_row).to_a
 
   formatted_list = separated_list.map do |file_names|
     file_names.values_at(0..max_row - 1).map do |file_name|
@@ -22,15 +23,15 @@ def print_file_list
 end
 
 def fetch_file_list
-  options = ARGV.getopts('a')
+  options = ARGV.getopts('r')
   ls_dir = ARGV[0] || '.'
 
-  file_list = Dir.foreach(ls_dir).to_a
+  file_list = Dir.foreach(ls_dir).to_a.reject { |file_name| file_name.start_with?('.') }
 
-  if options['a']
-    file_list
+  if options['r']
+    file_list.sort.reverse
   else
-    file_list.reject { |file_name| file_name.start_with?('.') }
+    file_list.sort
   end
 end
 
