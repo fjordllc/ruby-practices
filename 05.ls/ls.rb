d44a84c72d6f119ff1bfc76e47260a922dd9ed6c
Yatsu0720 <witdesigns@yatsu-world.com>
2022-07-16 12:36:56 +0900

    -lコマンドを表示の崩れ以外実装

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 MAX_COLUMN = 3
 
@@ -23,13 +24,41 @@ def print_file_list
 end
 
 def fetch_file_list
-  options = ARGV.getopts('r')
+  options = ARGV.getopts('l')
   ls_dir = ARGV[0] || '.'
 
   file_list = Dir.foreach(ls_dir).to_a.reject { |file_name| file_name.start_with?('.') }
+  file_list.each do |file|
+    file_state = File.stat(file)
+    file_state_array = file_state.mode.to_s(8).rjust(6, '0').split('')
+    
+    file_type = if file_state_array[0] + file_state_array[1] == '10'
+                  '-'
+                elsif file_state_array[0] + file_state_array[1] == '04'
+                  'd'
+                else
+                  'l'
+                end
+                  
+                
+        
+    correspondence_table = {'0'=> '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}
+
+    number_of_hard_link = file_state.nlink
+    user_name = Etc.getpwuid(file_state.uid).name
+    group_name = Etc.getgrgid(file_state.gid).name
+    file_size = file_state.size
+    timestamp = file_state.mtime.strftime('%b %d %R')
+
+    puts(file_type + correspondence_table[file_state_array[3]] + correspondence_table[file_state_array[4]]+ correspondence_table[file_state_array[5]] + '  ' + number_of_hard_link.to_s + ' ' + user_name + '  ' + group_name + '  ' + file_size.to_s + ' ' + timestamp.to_s + ' ' +file)
+
+
+  end
 
   if options['r']
-    file_list.sort.reverse
+    file_list.sort do |a,b|
+      
+    end.reverse
   else
     file_list.sort
   end
