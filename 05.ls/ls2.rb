196fd448ef3cc925aa1294d9ab8876294027fffb
osawa <hibiki20000627@gmail.com>
2022-07-25 14:03:42 +0900

    ls2の提出

---
+++ 05.ls/ls2.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+require 'optparse'
+opt = OptionParser.new
+
+args = {}
+  opt.on('-a') {|v| args[:a] = v}
+  opt.parse!(ARGV)
+
+def input(args)
+  if args[:a].nil?
+    Dir.glob('*')
+  else
+    Dir.glob('.*') + Dir.glob('*')
+  end
+end
+
+def output(arr)
+  files = ((arr.size + 1).to_f / 3).ceil
+  files.times do |row|
+    col = 0
+    3.times do
+      print "#{arr[row+col]} ".ljust(25)
+      col += files
+    end
+  puts "\n"
+  end
+end
+
+arr = input(args)
+output(arr)
