ad2152943fc8aaa4f774b81524fecaccf84e1a3e
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-07-27 16:03:00 +0900

    refactoring

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,58 +6,35 @@ opt.on('-m')
 opt.on('-y')
 opt.parse!(ARGV)
 
-
-
-# p ARGV
-# puts "today is #{ARGV[1]}/#{ARGV[0]}"
-# ARGV  引数個数判定　処理
-## 引数-yがnull  -> 今年 
-## 引数-yyがnull  -> 今年 
-
-
 year = ARGV[1].to_i
-if year == 0
-    year = Date.today.year
-end
+year = Date.today.year if year == 0
 
 month = ARGV[0].to_i
-if month == 0
-    month = Date.today.month
-end
-
-
+month = Date.today.month if month == 0
 
 date = Date.new(year, month, -1)
 month_last = date.to_s.slice(8..10).to_i
-
-
 
 # カレンダーのヘッダ
 puts "      #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
 
 # 第一週の右詰
-sample = Date.new(year, month, 1).wday
-for i in 0..(sample-1) do
+first_date = Date.new(year, month, 1).wday
+for i in 0..(first_date-1) do
     print "   "
 end
 
 # 日付描画
 for i in 1..month_last do
-    
     # write day
     if i/10 == 0
-        print " #{i} "
+        print format("%02d ", i)
     elsif
         print "#{i} "
     end
 
     # 土曜日だったなら改行
     weekday = Date.new(year, month, i).wday
-    if weekday == 6
-        puts ""
-    end
-    
-
-
+    puts "" if weekday == 6
 end
