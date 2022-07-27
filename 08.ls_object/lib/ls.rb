c9c72a023a8f94027fb9b6bfb8a6a14b98fa3407
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-07-27 22:10:24 +0900

    create Ls class in Ls module

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -6,39 +6,55 @@ require_relative 'ls/stat'
 
 module Ls
   def self.run
-    path, options = Ls::Parser.parse(ARGV)
-    file_paths = get_file_paths(path, options[:dot_match], options[:reverse])
-    width = IO.console.winsize[1]
-    options[:long_format] ? ls_long(file_paths) : ls_short(file_paths, width)
+    path, options = Parser.parse(ARGV)
+    ls = Ls.new(path, options)
+    ls.run
   end
 
-  def self.get_file_paths(pathname, dot_match, reverse)
-    pattern = pathname.join('*')
-    file_paths = dot_match ? Dir.glob(pattern, File::FNM_DOTMATCH) : Dir.glob(pattern)
-    reverse ? file_paths.reverse : file_paths
-  end
+  class Ls
+    attr_reader :options
 
-  def self.ls_long(paths)
-    stats = paths.map { Stat.new(_1) }
-    total_block = stats.map(&:blocks).sum
-    total = "total #{total_block}\n"
-    body = stats.map(&:body)
-    [total, body].join
-  end
-
-  def self.ls_short(paths, width)
-    max_length = paths.map { File.basename(_1).size }.max
-    col_num = width / (max_length + 2)
-    row_num = (paths.size.to_f / col_num).ceil
-    sliced = paths.each_slice(row_num).to_a
-    transposed = sliced[0].zip(*sliced[1..])
-    output = ''
-    transposed.map do |row_paths|
-      output += row_paths.map do |path|
-        path&.ljust(max_length + 2)
-      end.join
-      output += "\n"
+    def initialize(path, options, width = IO.console.winsize[1])
+      @path = path
+      @options = options
+      @file_paths = get_file_paths(path, options[:dot_match], options[:reverse])
+      @width = width
     end
-    output
+
+    def run
+      @options[:long_format] ? long : short
+    end
+
+    def long
+      stats = @file_paths.map { Stat.new(_1) }
+      total_block = stats.map(&:blocks).sum
+      total = "total #{total_block}\n"
+      body = stats.map(&:body)
+      [total, body].join.chomp
+    end
+
+    def short
+      max_length = @file_paths.map { File.basename(_1).size }.max
+      col_num = @width / (max_length + 2)
+      row_num = (@file_paths.size.to_f / col_num).ceil
+      sliced = @file_paths.each_slice(row_num).to_a
+      transposed = sliced[0].zip(*sliced[1..])
+      output = ''
+      transposed.map do |row_paths|
+        output += row_paths.map do |path|
+          File.basename(path).ljust(max_length) unless path.nil?
+        end.join('  ').rstrip
+        output += "\n"
+      end
+      output.chomp
+    end
+
+    private
+
+    def get_file_paths(pathname, dot_match, reverse)
+      pattern = pathname.join('*')
+      file_paths = dot_match ? Dir.glob(pattern, File::FNM_DOTMATCH) : Dir.glob(pattern)
+      reverse ? file_paths.reverse : file_paths
+    end
   end
 end
