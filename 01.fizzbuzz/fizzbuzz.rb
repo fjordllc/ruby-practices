db3dfac1d512e0fb054e586d4f779e5d6448d4fc
y-oji <yo-00331pc@outlook.com>
2022-08-05 18:33:19 +0900

    fizz buzz できた

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,1 +1,13 @@
-
+x = 1
+while x <= 20
+    if x % 15 == 0
+        puts "fizz buzz"
+    elsif x % 3 == 0
+        puts "fizz"
+    elsif x % 5 == 0
+        puts "buzz"
+    else
+        puts x
+    end
+    x += 1
+end
