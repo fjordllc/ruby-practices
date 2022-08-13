f14bff7f69641556fd330246c6a3ebc8582be55f
uchihiro <tohchi4122@gmail.com>
2022-08-13 14:30:26 +0900

    Gameクラスのインスタンスを作成するメソッドを追加
    Frameクラスがフレーム分配列として入ったものを初期値とする、Gameクラスのインスタンスを作成するメソッドを追加しました。

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -7,4 +7,21 @@ class Game
     @scores = scores
   end
 
+  def split_score
+    @scores.split(',')
+  end
+
+  def frames
+    scores = []
+    frames = []
+
+    split_score.each do |score|
+      scores << score
+      scores << '0' if scores.size < 18 && score == 'X'
+    end
+
+    scores.each_slice(2) { |score| frames.size == 10 ? frames[9] << score[-1] : frames << score }
+    frames = frames.map { |frame| Frame.new(frame[0], frame[1], frame[2]) }
+    Game.new(frames)
+  end
 end
