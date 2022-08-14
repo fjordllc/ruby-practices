80967a208c74966d9793824729eba70de6f032fa
uchihiro <tohchi4122@gmail.com>
2022-08-14 22:08:09 +0900

    メソッドの追加・メソッド名の変更
    ボウリングのスコアを計算するメソッドを追加しました。また、Gameクラスのインスタンスを作成するメソッドの名前を変更しました。

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -1,4 +1,5 @@
 require_relative 'frame'
+require 'debug'
 
 class Game
   attr_reader :scores
@@ -11,7 +12,7 @@ class Game
     @scores.split(',')
   end
 
-  def frames
+  def game_scores
     scores = []
     frames = []
 
@@ -24,4 +25,24 @@ class Game
     frames = frames.map { |frame| Frame.new(frame[0], frame[1], frame[2]) }
     Game.new(frames)
   end
+
+  def calc_scores
+    frames = game_scores.scores
+    frames.each_with_index.sum do |frame, index|
+      if frame.first_shot.score == 10
+        if index < 8 && frames[index + 1].first_shot.score == 10
+          frame.sum_shots + frames[index + 1].first_shot.score + frames[index + 2].first_shot.score
+        elsif index == 9
+          frame.sum_shots
+        else
+          frame.sum_shots + frames[index + 1].first_shot.score + frames[index + 1].second_shot.score
+        end
+      elsif frame.sum_shots == 10 && index < 9
+        frame.sum_shots + frames[index + 1].first_shot.score
+      else
+        frame.sum_shots
+      end
+    end
+  end
+
 end
