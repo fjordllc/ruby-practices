46bbdc2fc12cd81f11b42ea592233e80ae02d22d
uchihiro <tohchi4122@gmail.com>
2022-08-17 22:27:32 +0900

    Gameクラスのインスタンスの初期値@framesに、Frameクラスのインスタンスを代入するメソッドを作成
    Gameクラスのインスタンスの初期値である@framesに、Frameクラスのインスタンスの配列を代入するメソッドを作成しました。また、メソッドの作成に伴いcalc_scoresメソッドを修正しました。

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -1,9 +1,8 @@
 require_relative 'frame'
 
 class Game
-  attr_reader :scores
-
   def initialize(scores)
+    @frames = []
     @scores = scores
   end
 
@@ -17,28 +16,24 @@ class Game
     scores
   end
 
-  def game_scores
-    frames = []
-    split_scores.each_slice(2) { |score| frames.size == 10 ? frames[9] << score[-1] : frames << score }
-    frames = frames.map { |frame| Frame.new(frame[0], frame[1], frame[2]) }
-    Game.new(frames)
+  def frames
+    split_scores.each_slice(2) { |score| @frames.size == 10 ? @frames[9] << score[-1] : @frames << score }
+    @frames = @frames.map { |frame| Frame.new(frame[0], frame[1], frame[2]) }
   end
 
   def calc_scores
-    frames = game_scores.scores
     frames.each_with_index.sum do |frame, index|
       if frame.strike? && index <= 8
-        if frames[index + 1].strike? && index < 8
-          frame.sum_shots + frames[index + 1].first_shot.score + frames[index + 2].first_shot.score
+        if @frames[index + 1].strike? && index < 8
+          frame.sum_shots + @frames[index + 1].first_shot.score + @frames[index + 2].first_shot.score
         else
-          frame.sum_shots + frames[index + 1].first_shot.score + frames[index + 1].second_shot.score
+          frame.sum_shots + @frames[index + 1].first_shot.score + @frames[index + 1].second_shot.score
         end
       elsif frame.spare? && index < 9
-        frame.sum_shots + frames[index + 1].first_shot.score
+        frame.sum_shots + @frames[index + 1].first_shot.score
       else
         frame.sum_shots
       end
     end
   end
-
 end
