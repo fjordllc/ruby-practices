16adfad76f1ec43b31b7293ac1a905a85358dbbb
nicole2525 <nicole2525nomail@gmail.com>
2022-08-24 21:57:27 +0900

    delete task content

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,9 +1,4 @@
 #!/usr/bin/env ruby
-
-# 1から20までの数をプリントするプログラムを書け。
-# ただし3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、
-# 3と5両方の倍数の場合には｢FizzBuzz｣とプリントすること。
-
 numbers = (1..20)
 numbers.each do |number|
   case 
