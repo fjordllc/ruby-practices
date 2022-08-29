b750aaca5d1d9d00f04999538df3e9681e2bf699
nicole2525 <nicole2525nomail@gmail.com>
2022-08-29 12:38:48 +0900

    Change Fizz and Buzz from variables to constants

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
-fizz = 3
-buzz = 5
+Fizz = 3
+Buzz = 5
 numbers = (1..20)
 numbers.each do |number|
   case 
-  when number%fizz == 0 && number%buzz == 0
+  when number%Fizz == 0 && number%Buzz == 0
     puts "FizzBuzz"
-  when number%fizz == 0
+  when number%Fizz == 0
     puts "Fizz"
-  when number%buzz == 0
+  when number%Buzz == 0
     puts "Buzz" 
   else
   puts number
