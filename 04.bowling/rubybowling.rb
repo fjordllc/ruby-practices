b0f159113d7d86a99ecdbdac5af9ab85c86fa27f
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-09-05 10:59:38 +0900

    ボーリングプログラム作成

--- 04.bowling/rubybowling.rb
+++ 04.bowling/rubybowling.rb
@@ -15,18 +15,17 @@ shots.each_slice(2) do |s|
   frames << s
 end
 
-
 point = 0
-frames.each_with_index do |frame,count|
-  if frame[0] == 10 && count < 9             #strike
-    point = point + 10 + frames[count + 1].sum
-    if frames[count + 1][0] == 10
-      point = point + frames[count + 2][0]
+frames.each_with_index do |item,i|      #item=frame配列の値 i=要素の番号
+  if item[0] == 10 && i < 9             #strike
+    point = point + 10 + frames[i + 1][0] + frames[i + 1][1]
+    if frames[i + 1][0] == 10           #strike2連続
+      point = point + frames[i + 2][0]
     end
-  elsif frame[0] + frame[1] == 10 && count < 9  #spare
-    point = point + 10 + frames[count + 1][0]
-  else
-    point = point + frame.sum
+  elsif item[0] + item[1] == 10 && i < 9  #spare
+    point = point + 10 + frames[i + 1][0]
+  elsif
+    point = point + item[0] + item[1]
   end
 end
 
