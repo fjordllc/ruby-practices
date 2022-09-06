4961c66674ec88fa3672abdd290bfcdef5db7592
showtime <s.n.0704.g@gmail.com>
2022-09-06 22:16:17 +0900

    レビュー後修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -15,29 +15,26 @@ flame_list = []
 shots.each_slice(2) do |shot|
   flame_list << shot
 end
+flame_list_modified = []
+if flame_list.count > 10
+  flame_sep_after_list = flame_list.slice(9..)
+  flame_sep_after_list.flatten!.delete(0)
+  flame_list_modified = flame_list.slice(0..8)
+  flame_list_modified << flame_sep_after_list
+else
+  flame_list_modified = flame_list
+end
 flame_count = 0
 strike_cont = 0 # 0 (ストライクなし) or 1 (単独ストライク) or 2 (連続ストライク)
 spare_cont = 0 # 0 (スペアなし) or 1 (スペアあり)
 score = 0
-flame_list.each do |flame| 
+flame_list_modified.each do |flame| 
   flame_count += 1
   if flame_count == 10 # Last Frame
     if strike_cont == 0
         score += flame.sum + flame[0] * spare_cont
     else
         score += flame.sum + flame[0] * strike_cont + flame[1]
-        if flame[0] == 10
-            strike_cont = 1
-        else
-            strike_cont = 0
-        end
-    end
-  elsif flame_count > 10 # Last Frame
-    if strike_cont == 1
-        score += flame.sum + flame.sum
-        strike_cont = 0
-    elsif strike_cont == 0
-        score += flame.sum
     end
   elsif flame[0] == 10 # Strike
     score += 10 + 10 * strike_cont + 10 * spare_cont
@@ -47,22 +44,17 @@ flame_list.each do |flame|
         strike_cont = 2
     end
     spare_cont = 0
-  elsif flame.sum == 10 # Spare
-    if strike_cont == 0
-        score += flame.sum + flame[0] * spare_cont
-        spare_cont = 1
-    else
-        score += flame.sum + flame[0] * strike_cont + flame[1]
-        strike_cont = 0
-        spare_cont = 1
-    end
   else
     if strike_cont == 0
         score += flame.sum + flame[0] * spare_cont
-        spare_cont = 0
     else
         score += flame.sum + flame[0] * strike_cont + flame[1] 
-        strike_cont = 0
+    end
+    strike_cont = 0
+    if flame.sum == 10 # spare
+      spare_cont = 1
+    else
+      spare_cont = 0
     end
   end
 end
