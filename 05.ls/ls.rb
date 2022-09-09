522deb8abe5245d9fdbb95ac31a47fd5593042ac
showtime <s.n.0704.g@gmail.com>
2022-09-09 09:52:54 +0900

    format code

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,8 +4,8 @@
 
 # TODO: オプション処理時追加 opt = OptionParser.new
 ls_target_path = ARGV[-1].nil? ? '.' : ARGV[-1]
-dir_item_list = []
-sorted_dir_item_list = []
+dir_items_list = []
+sorted_dir_items_list = []
 output_item_width = 1
 
 HORIZONTAL_ITEMS_COUNT = 3
@@ -18,21 +18,22 @@ begin
       # TODO: -aオプションによる切り替えを行う
       next unless /^\..*/.match(item_in_dir).nil?
 
-      dir_item_list << item_in_dir
+      dir_items_list << item_in_dir
       output_item_width = item_in_dir.size + ITEMS_INTERVAL > output_item_width ? item_in_dir.size + ITEMS_INTERVAL : output_item_width
-      # print dir_item_list
+      # print dir_items_list
     end
     # TODO: -rオプションの際にASC処理ではなくDESC処理をする
-    dir_item_list = dir_item_list.sort
-    max_vertical_items_count = (dir_item_list.size.to_f / HORIZONTAL_ITEMS_COUNT).ceil
+    dir_items_list = dir_items_list.sort
+    max_vertical_items_count = (dir_items_list.size.to_f / HORIZONTAL_ITEMS_COUNT).ceil
     max_vertical_items_count.times do |count|
-      dir_item_list.each_with_index do |v, i|
+      dir_items_list.each_with_index do |v, i|
         next if i % max_vertical_items_count != count
 
-        sorted_dir_item_list << v
+        sorted_dir_items_list << v
       end
     end
-    sorted_dir_item_list.each_slice(HORIZONTAL_ITEMS_COUNT) do |line_items|
+
+    sorted_dir_items_list.each_slice(HORIZONTAL_ITEMS_COUNT) do |line_items|
       line_items.each do |line_item|
         print format("%-#{output_item_width}.*s", output_item_width, line_item)
       end
@@ -46,6 +47,7 @@ begin
   else
     raise StandardError, "ls: #{ls_target_path}: No such file or directory"
   end
+  
 rescue StandardError => e
   print e.message
   puts
