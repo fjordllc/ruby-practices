479618f75a4366155ae64b9231999c488d418754
Cephas604 <cephas.healthy604@gmail.com>
2022-09-12 21:49:12 +0900

    define file_length_max, group_size, and file_groups

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -7,15 +7,28 @@ options = ARGV.getopts('a',
 'all(-a)    list all files including hidden files.')
 
 COLUMN = 3
+
 if options['a'] # option a
   file_names = Dir.glob('.*') + Dir.glob('*')
 else
   file_names = Dir.glob('*')
 end
-file_length_max = file_names.map(&:size).max
-group_size = file_names.size / COLUMN + 1
 
-file_groups = file_names.sort.map { |fname| fname.ljust(file_length_max) }.each_slice(group_size).to_a
+def file_length_max
+  file_names.map(&:size).max
+end
+
+def group_size
+  file_names.size / COLUMN + 1
+end
+
+def file_groups
+  file_names
+  .sort
+  .map { |fname| fname.ljust(file_length_max) }
+  .each_slice(group_size)
+  .to_a
+end
 
 (0..group_size).each do |gs|
   (0..file_groups.size - 1).each do |row|
