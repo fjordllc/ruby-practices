76b2229f2d1c3d1ce6c2ecca31f7b64be471b478
nicole2525 <nicole2525nomail@gmail.com>
2022-09-12 12:58:06 +0900

    fixed arguments and constants, simplified code

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,43 +1,29 @@
-#!/usr/bin/env ruby
+!/usr/bin/env ruby
 
 require 'optparse'
 require 'date'
 
-opt = OptionParser.new
-opt.on('-m')
-opt.on('-y')
-
-opt.parse!(ARGV)
-month = ARGV[0] || Date.today.mon
-year = ARGV[1] || Date.today.year
-
-header = "#{month}月"+" "+year.to_s
-print header.center(20) + "\n"
+options = ARGV.getopts('m:', 'y:')
+month = options['m'] || Date.today.mon
+year = options['y'] || Date.today.year
+puts "#{month}月 #{year}".center(20)
 
 day_of_week = %w(日 月 火 水 木 金 土)
-  print " "+day_of_week.join(" ") + "\n"
+print " "+day_of_week.join(" ") + "\n"
 
-First_day = Date.new(year.to_i, month.to_i, 1)
+first_day = Date.new(year.to_i, month.to_i, 1)
 last_day = Date.new(year.to_i, month.to_i, -1)
 
-space = First_day.wday
-space.times do
-print "   "
-end
+print "   " * first_day.wday
 
-days_of_the_month = (First_day..last_day)
+days_of_the_month = (first_day..last_day)
 
 (days_of_the_month).each do |day|
-  number = day.wday
   date_display = day.day.to_s.rjust(2)
-  case
-  when day == Date.today && number == 6
-    print " " + "\e[30;47m#{date_display}\e[0m" + "\n"
-  when day == Date.today
+  if day == Date.today
     print " " + "\e[30;47m#{date_display}\e[0m"
-  when number == 6
-    print " " + date_display + "\n"
   else
     print " " + date_display
   end
+  print "\n" if day.saturday?
 end
