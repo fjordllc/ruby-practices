2cf95f627b7060333596b06ce264903785c415b3
Cephas604 <cephas.healthy604@gmail.com>
2022-09-14 20:40:44 +0900

    rename and add arguments

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -21,8 +21,8 @@ def length_max(array)
   array.map(&:size).max
 end
 
-def count_group_size(array)
-  array.size / COLUMN_MAX + 1
+def count_group_size(array, groups)
+  array.size / groups + 1
 end
 
 def divide_into_groups(array)
@@ -33,10 +33,10 @@ def divide_into_groups(array)
     .to_a
 end
 
-def list_files(file_names)
+def list_files(file_names, lines = COLUMN_MAX)
   group_in_groups = divide_into_groups(file_names)
   column = group_in_groups.size - 1
-  row = count_group_size(file_names)
+  row = count_group_size(file_names, lines)
   (0..row).each do |r|
     (0..column).each do |c|
       print "#{group_in_groups[c][r]} "
