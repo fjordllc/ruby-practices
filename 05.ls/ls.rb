5c16acf11cafb54098fd4831c71612a05b7dd039
Cephas604 <cephas.healthy604@gmail.com>
2022-09-14 21:19:31 +0900

    remove arguments

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -9,22 +9,22 @@ options = ARGV.getopts('a',
 # columns to display on the screen
 COLUMN_MAX = 3
 
-def count_group_size(array, groups)
-  array.size / groups + 1
+def count_group_size(array)
+  array.size / COLUMN_MAX + 1
 end
 
 def divide_into_groups(array)
   array
     .sort
     .map { |a| a.ljust(array.map(&:size).max) }
-    .each_slice(count_group_size(a))
+    .each_slice(count_group_size(array))
     .to_a
 end
 
-def list_files(file_names, lines = COLUMN_MAX)
+def list_files(file_names)
   group_in_groups = divide_into_groups(file_names)
   column = group_in_groups.size - 1
-  row = count_group_size(file_names, lines)
+  row = count_group_size(file_names)
   (0..row).each do |r|
     (0..column).each do |c|
       print "#{group_in_groups[c][r]} "
