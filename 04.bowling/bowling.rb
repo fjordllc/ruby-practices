76a1c3005c089fcc4a0eca10b5ce423d1cfd6eb1
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-09-14 18:23:42 +0900

    構造が分かりにくかったので、全体的な改善といただいたコメントの修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,70 +1,53 @@
 # frozen_string_literal: true
-
 SPARE = 1
 STRIKE = 2
 
-# xto10 method
-def xto10(pin)
-  pin == 'X' ? 10 : pin.to_i
-end
-
 # add_bonus method
-def add_bonus(bonus, pins, index)
-  score = 0
-  # add spare-strike bonus
-  score += xto10(pins[index]) if [SPARE, STRIKE].include?(bonus)
-  # add strike bonus
-  score += xto10(pins[index + 1]) if bonus == 2
-  score
+def add_bonus(pins, i, frame = -1)
+  # what kind of bonus?
+  bonus_flag = 0
+  if pins[i] == 10
+    bonus_flag = STRIKE
+  elsif pins[i..i+1].sum == 10
+    bonus_flag = SPARE
+  end
+
+  bonus = 0
+  # spare bonus
+  bonus += pins[i + 2] if bonus_flag == SPARE
+  # strike bonus
+  bonus += pins[(i + 1)..(i + 2)].sum if bonus_flag == STRIKE
+  bonus
 end
 
-def update_bonus(pins, index)
-  return 1 if pins[index].to_i + pins[index + 1].to_i == 10
-end
-
-# MAIN
+# get input
 pins = ARGV[0].split(',')
-score = 0
-bonus = 0
-i = 0
+# convert X to 10
+pins.size.times {|n| pins[n] = 10 if pins[n] == "X"} 
+pins.map!{|x| x.to_i}
 
+score = 0
+i = 0
 10.times do |frame|
-  # 10th frame process
+  # 10th frame
   if frame == 9
-    # nomal point
+    score += pins[i..i+2].sum
+  # 1-9th frame
+  else
+    # get frame-th frame  
     frame_pins = []
-    frame_pins[0] = xto10(pins[i])
-    frame_pins[1] = xto10(pins[i + 1])
-    frame_pins[2] = xto10(pins[i + 2])
+    frame_pins[0] = pins[i]
+    frame_pins[1] = pins[i + 1] if pins[i] != 10
+    
+    # nomal point
     score += frame_pins.sum
     # add bonus
-    score += add_bonus(bonus, pins, i)
-    break
-  # 0-8th frame process
-  else
-    # 数値なら加算
-    if pins[i] =~ /^[0-9]+$/
-      # nomal point
-      frame_pins = []
-      frame_pins[0] = xto10(pins[i])
-      frame_pins[1] = xto10(pins[i + 1])
-      score += frame_pins.sum
-      # add bonus
-      score += add_bonus(bonus, pins, i)
-      # UPDATE VARIABLE
-      bonus = 0
-      # spare bonus flag
-      bonus = update_bonus(pins, i)
-      i += 2
-      # NOT数値　→ 文字X ストライク処理
-    else
-      score += 10
-      # add bonus
-      score += add_bonus(bonus, pins, i)
-      # UPDATE VARIABLE
-      bonus = 2
-      i += 1
-    end
+    score += add_bonus(pins, i, frame)
+
+    # increment   
+    i += 1 if pins[i] != 10
+    i += 1
   end
 end
+
 puts score
