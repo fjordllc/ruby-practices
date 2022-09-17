4bab7b82a4503902b0f4d3b6a31fec91c5d7828c
中村翔平 <sho-he@nakamurashouheinoMacBook-Pro.local>
2022-09-17 11:22:45 +0900

    REFACTOR: ls_print関数の分岐を集約 output_item_width更新ロジックの説明追加

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,9 +5,9 @@
 # TODO: オプション処理時追加 opt = OptionParser.new
 ls_target_path = ARGV[-1].nil? ? '.' : ARGV[-1]
 dir_items_list = []
+horizontal_items_count = 3
 output_item_width = 10
 
-horizontal_items_count = 3
 ITEMS_INTERVAL = 5
 
 def ls_sort(target_list, horizontal_num, sort_reverse: false)
@@ -28,18 +28,10 @@ end
 
 def ls_print(target, item_width, horizontal_num)
   # TODO: -l オプション時に詳細を取得する処理を書く
-  # ls結果のターゲットが複数存在する場合
-  if target.is_a?(Array)
-    target.each_slice(horizontal_num) do |line_items|
-      line_items.each do |line_item|
-        print format("%-#{item_width}.*s", item_width, line_item)
-      end
-      puts
+  target.each_slice(horizontal_num) do |line_items|
+    line_items.each do |line_item|
+      print format("%-#{item_width}.*s", item_width, line_item)
     end
-  # ls結果のターゲットが一つの場合
-  # TODO: 複数ファイル指定処理する場合はこの辺りの処理を変更
-  else
-    print target
     puts
   end
 end
@@ -51,6 +43,8 @@ begin
       next unless /^\..*/.match(item_in_dir).nil?
 
       dir_items_list << item_in_dir
+      # NOTE: 表示アイテムの最大文字列とアイテム間のスペースがoutput_item_widthを超えていたらoutput_item_widthを更新する
+      # NOTE: （補足）output_item_widthとは一つのアイテムが横幅でとってよい幅のこと
       output_item_width = item_in_dir.size + ITEMS_INTERVAL > output_item_width ? item_in_dir.size + ITEMS_INTERVAL : output_item_width
     end
     sorted_dir_items_list = ls_sort(dir_items_list, horizontal_items_count)
@@ -58,7 +52,8 @@ begin
     ls_print(sorted_dir_items_list, output_item_width, horizontal_items_count)
 
   elsif FileTest.file?(ls_target_path)
-    ls_print(ls_target_path, output_item_width, 1)
+    dir_items_list << ls_target_path
+    ls_print(dir_items_list, output_item_width, 1)
   else
     raise StandardError, "ls: #{ls_target_path}: No such file or directory"
   end
