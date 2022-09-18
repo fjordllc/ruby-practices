c67dbc853cb36d6f0318765f0e65ad4910ae1dc5
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-09-18 18:30:09 +0900

    fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+number = 0
+while number <= 20
+    number = number + 1
+    if number % 15 == 0
+        puts "FizzBuzz"
+    elsif number % 3 == 0
+        puts "Buzz"
+    elsif number % 5 == 0
+        puts "Fizz"
+    else
+        puts number
+    end
+end
