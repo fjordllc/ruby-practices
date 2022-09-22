63ca297c8514f9254fbae8d5d97acf0d300e8922
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-09-22 17:38:34 +0900

    lsオプションなしコマンドの実装

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,4 +1,23 @@
-files = Dir.glob("*") 
+# 表示列の最大数をココで変更
+ROW = 3
 
+# scale変数を求める
+def calc_scale(num)
+  if (num % 3) == 0
+    num / 3
+  else
+    (num / 3) + 1
+  end
+end
+
+# ファイル一覧をlsのルールに従い表示
+def display(files, scale)
+  scale.times do |y|
+    ROW.times {|x| printf "#{files[x * scale + y]}     "}
+    printf "\n"
+  end
+end
+
+files = Dir.glob("*") 
 files.sort!
-puts files
+display(files, calc_scale(files.size))
