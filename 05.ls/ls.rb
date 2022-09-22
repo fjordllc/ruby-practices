7ffc96331410302c459f49419594e1742007fb79
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-09-22 18:42:26 +0900

    rubocop修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 # 表示列の最大数をココで変更
 ROW = 3
 
 # scale変数を求める
 def calc_scale(num)
-  (num % 3) == 0 ? num / 3 : (num / 3) + 1
+  (num % 3).zero? ? num / 3 : (num / 3) + 1
 end
 
 # ファイル一覧をlsのルールに従い表示
 def display(files, scale)
-  scale.times do |y| 
-    ROW.times { |x| printf("#{files[x * scale + y]}".ljust(files.map(&:size).max + 3)) }
-    
+  scale.times do |y|
+    ROW.times { |x| printf(files[x * scale + y].to_s.ljust(files.map(&:size).max + 3)) }
     printf "\n"
   end
 end
 
-files = Dir.glob("*") 
+files = Dir.glob('*')
 files.sort!
 display(files, calc_scale(files.size))
