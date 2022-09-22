9d07b09aa0f5e53be94fbd8c977c1710210a5e78
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-09-22 18:14:21 +0900

    リファクタリング

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,17 +3,13 @@ ROW = 3
 
 # scale変数を求める
 def calc_scale(num)
-  if (num % 3) == 0
-    num / 3
-  else
-    (num / 3) + 1
-  end
+  (num % 3) == 0 ? num / 3 : (num / 3) + 1
 end
 
 # ファイル一覧をlsのルールに従い表示
 def display(files, scale)
   scale.times do |y|
-    ROW.times {|x| printf "#{files[x * scale + y]}     "}
+    ROW.times {|x| printf "#{files[x * scale + y]}" + " "*10}
     printf "\n"
   end
 end
