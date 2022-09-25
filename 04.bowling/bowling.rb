f2e055b3384fb72951861fa2fc4bea4854cb5598
nicole2525 <nicole2525nomail@gmail.com>
2022-09-25 11:49:18 +0900

    合計スコアの算出をsumメソッドに変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -20,9 +20,8 @@ frames = scores.each_slice(2).to_a
 frame_with_number = {}
 frames.each.with_index(1) { |fp, i| frame_with_number.store(i, fp) }
 
-total = 0
-frame_with_number.each do |frame, point|
-  total +=
+total = 
+frame_with_number.each.sum do |frame, point|
     if frame <= 9 && point[0] == 10 && frame_with_number[frame + 1][0] == 10
       10 + frame_with_number[frame + 1][0] + frame_with_number[frame + 2][0]
     elsif frame <= 9 && point[0] == 10
