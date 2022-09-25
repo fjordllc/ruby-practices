6e4523b86729f7b1f03ecac3bea85252087fdc7a
nicole2525 <nicole2525nomail@gmail.com>
2022-09-25 12:14:40 +0900

    robocopの指摘事項を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -20,18 +20,18 @@ frames = scores.each_slice(2).to_a
 frame_with_number = {}
 frames.each.with_index(1) { |fp, i| frame_with_number.store(i, fp) }
 
-total = 
-frame_with_number.each.sum do |number_of_frame, points|
-  next_frame = frame_with_number[number_of_frame + 1]
-  after_next_frame = frame_with_number[number_of_frame + 2]
-  if number_of_frame <= 9 && points[0] == 10 && next_frame[0] == 10
-    10 + next_frame[0] + after_next_frame[0]
-  elsif number_of_frame <= 9 && points[0] == 10
-    10 + next_frame.sum
-  elsif number_of_frame <= 9 && points.sum == 10
-    10 + next_frame[0]
-  else
-    points.sum
+total =
+  frame_with_number.each.sum do |number_of_frame, points|
+    next_frame = frame_with_number[number_of_frame + 1]
+    after_next_frame = frame_with_number[number_of_frame + 2]
+    if number_of_frame <= 9 && points[0] == 10 && next_frame[0] == 10
+      10 + next_frame[0] + after_next_frame[0]
+    elsif number_of_frame <= 9 && points[0] == 10
+      10 + next_frame.sum
+    elsif number_of_frame <= 9 && points.sum == 10
+      10 + next_frame[0]
+    else
+      points.sum
+    end
   end
-end
 puts total
