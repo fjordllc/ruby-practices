27d0c276cc0f18d0290491cc80cc0985ce9c58db
nicole2525 <nicole2525nomail@gmail.com>
2022-09-27 21:19:13 +0900

    変数名を短縮

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -19,18 +19,18 @@ frames = scores.each_slice(2).to_a
 
 total =
   frames.each.with_index(1).sum do |points, frame_number|
-    next_frame_points = frames[frame_number]
-    after_next_frame_points = frames[frame_number + 1]
+    next_points = frames[frame_number]
+    after_next_points = frames[frame_number + 1]
     double = frame_number <= 9 && points[0] == 10 && frames[frame_number][0] == 10
     strike = frame_number <= 9 && points[0] == 10
     spare = frame_number <= 9 && points.sum == 10
 
     if double
-      10 + next_frame_points[0] + after_next_frame_points[0]
+      10 + next_points[0] + after_next_points[0]
     elsif strike
-      10 + next_frame_points.sum
+      10 + next_points.sum
     elsif spare
-      10 + next_frame_points[0]
+      10 + next_points[0]
     else
       points.sum
     end
