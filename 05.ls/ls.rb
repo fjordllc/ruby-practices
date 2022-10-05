d8a45a498e51def43b54cce789ce05fafd91ac3f
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-10-05 10:10:11 +0900

    頂いたレビューの修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 # 表示列の最大数をココで変更
-ROW = 3
+COLUMN = 3
 
-# scale変数を求める
-def calc_scale(num)
+# 表示時に必要な行数rowを求める
+def calc_row(num)
   (num % 3).zero? ? num / 3 : (num / 3) + 1
 end
 
 # ファイル一覧をlsのルールに従い表示
-def display(files, scale)
-  scale.times do |y|
-    ROW.times { |x| printf(files[x * scale + y].to_s.ljust(files.map(&:size).max + 3)) }
-    printf "\n"
+def display(files, row)
+  row.times do |y|
+    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
+    puts
   end
 end
 
 files = Dir.glob('*')
-files.sort!
-display(files, calc_scale(files.size))
+sorted_files = files.sort
+display(sorted_files, calc_row(files.size))
