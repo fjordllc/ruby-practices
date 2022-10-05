f7d9f0d2448d055f34421cc5856930fb755e0d0f
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-10-05 12:28:44 +0900

    Xの扱い方を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 # add_bonus method
-def add_bonus(pins, index, frame = -1)
+def add_bonus(pins, index)
   bonus = 0
-  # spare bonus
+  # spare,strike bonus
   bonus += pins[index + 2] if pins[index..index + 1].sum == 10
   # strike bonus
-  bonus += pins[(index + 1)..(index + 2)].sum if pins[index] == 10
+  if pins[index] == 10
+    bonus += pins[index + 3]
+    bonus += pins[index + 4] if pins[(index + 2)..(index + 3)] == [10, 0]
+  end
   bonus
 end
 
 # get  input
 pins = ARGV[0].split(',')
-# convert X to 10s
-pins.map!{|pin| pin == "X" ? 10 : pin} 
+# convert X to [10,0]
+pins.map! { |pin| pin == 'X' ? [10, 0] : pin }.flatten!
 pins.map!(&:to_i)
 
 index = 0
@@ -24,17 +27,14 @@ score = 10.times.sum do |frame|
     pins[index..].sum
   # 1-9th frame
   else
-    # get frame-th frame  
-    frame_pins = []
-    frame_pins[0] = pins[index]
-    frame_pins[1] = pins[index + 1] if pins[index] != 10
-    
-    # nomal point + bonus
-    frame_score = frame_pins.sum + add_bonus(pins, index, frame)
+    # get frame-th frame
+    frame_pins = pins[index..index + 1]
 
-    # increment   
-    index += 1 if pins[index] != 10
-    index += 1
+    # nomal point + bonus
+    frame_score = frame_pins.sum + add_bonus(pins, index)
+
+    # increment
+    index += 2
     frame_score
   end
 end
--- 04.bowling/test/bowling_test.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-require_relative '../bowling'
-
-class BowlingTest < Minitest::Test
-  def test_bowling
-  end
-end
