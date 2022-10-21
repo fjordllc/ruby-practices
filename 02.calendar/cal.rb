ec9ddbf7364d508f5917c8cb27d28a9d8d259e44
masyuko0222 <masyuko0222@gmail.com>
2022-10-22 02:00:31 +0900

    指摘箇所を一括で修正しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,89 +1,44 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
-#月と西暦を取得・表示するメソッド定義-始まり-
-class MonthAndYear
-  @params = ARGV.getopts("m:y:")
+@params = ARGV.getopts('m:y:')
 
-  def self.add_month
-    if @params["m"] != nil
-      @params["m"].to_i
+# 1行目に月を表示させるメソッド
+def show_month
+  @params['m']&.to_i || Date.today.month
+end
+
+# 1行目に年を表示させるメソッド
+def show_year
+  @params['y']&.to_i || Date.today.year
+end
+
+# 2行目に曜日を表示させるメソッド
+def show_wday
+  [' 日', '月', '火', '水', '木', '金', '土'].join(' ')
+end
+
+# 3行目以降に日付を表示させるメソッド
+def show_days
+  firstday = Date.new(show_year, show_month, 1).day
+  lastday = Date.new(show_year, show_month, -1).day
+
+  (firstday..lastday).each do |day|
+    wday = Date.new(show_year, show_month, day).wday
+    if day == 1
+      print '   ' * wday + "  #{day}"
+    elsif day < 10 && day != 1
+      print "  #{day}"
     else
-      Date.today.month
+      print " #{day}"
     end
-  end
-
-  def self.add_year
-    if @params["y"] != nil
-      @params["y"].to_i
-    else
-      Date.today.year
-    end
-  end
-
-  def self.load_month
-    @month = add_month
-  end
-
-  def self.load_year
-    @year = add_year
-  end
-
-  def self.show_month_and_year
-    load_month
-    load_year
-    puts "#{@month}月 #{@year}".center(27)
+    puts "\n" if wday == 6
   end
 end
-#月と西暦を取得・表示するメソッド定義-終わり-
 
-#日付を取得・表示するメソッド -始まり-
-class DatesOfMonth
-  def self.add_firstday
-    @firstday = Date.new(MonthAndYear.load_year.to_i, MonthAndYear.load_month.to_i, 1).day
-  end
-
-  def self.add_lastday
-    @lastday = Date.new(MonthAndYear.load_year.to_i, MonthAndYear.load_month.to_i, -1).day
-  end
-
-  def self.show_dates
-    add_firstday
-    add_lastday
-
-    (@firstday..@lastday).each do |day|
-      @wday = Date.new(MonthAndYear.load_year.to_i, MonthAndYear.load_month.to_i, day).wday
-
-      if @wday == 6
-        if day < 10 && day != 1
-          print "  #{day}"
-        elsif day == 1
-          print "   " * @wday
-          print "  #{day}"
-        else
-          print " #{day}"
-        end
-        print "\n"
-      else
-        if day <10 && day != 1
-          print "  #{day}"
-        elsif day == 1
-          print "   " * @wday
-          print "  #{day}"
-        else
-          print " #{day}"
-        end
-      end
-    end
- end
-end
-#日付を取得・表示するメソッド -終わり-
-
-
-
-#1行目に月と西暦を表示する
-MonthAndYear.show_month_and_year
-#2行目に曜日を表示する
-puts ["日", "月", "火", "水", "木", "金", "土"].join(" ")
-#3行目以降に日付を表示する
-DatesOfMonth.show_dates
+puts "#{show_month}月 #{show_year}".center(22)
+puts show_wday
+show_days
