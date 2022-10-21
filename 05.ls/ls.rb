f6805b906285e6287c93ccd4435ca93a0424639d
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-10-21 12:27:20 +0900

    前ブランチからのコピー

---
+++ 05.ls/ls.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# 表示列の最大数をココで変更
+COLUMN = 3
+
+# 表示時に必要な行数rowを求める
+def calc_row(num)
+  (num % 3).zero? ? num / 3 : (num / 3) + 1
+end
+
+# ファイル一覧をlsのルールに従い表示
+def display(files, row)
+  row.times do |y|
+    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
+    puts
+  end
+end
+
+files = Dir.glob('*')
+sorted_files = files.sort
+display(sorted_files, calc_row(files.size))
