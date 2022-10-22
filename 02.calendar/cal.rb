79a6ed57a19492b3598a644f2e05f6893cef2d75
masyuko0222 <masyuko0222@gmail.com>
2022-10-23 00:12:22 +0900

    表示がcalコマンドと同じになるように修正しました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,38 +7,43 @@ require 'optparse'
 @params = ARGV.getopts('m:y:')
 
 # 1行目に月を表示させるメソッド
-def show_month
+def month
   @params['m']&.to_i || Date.today.month
 end
 
 # 1行目に年を表示させるメソッド
-def show_year
+def year
   @params['y']&.to_i || Date.today.year
 end
 
-# 2行目に曜日を表示させるメソッド
-def show_wday
-  [' 日', '月', '火', '水', '木', '金', '土'].join(' ')
-end
-
 # 3行目以降に日付を表示させるメソッド
-def show_days
-  firstday = Date.new(show_year, show_month, 1).day
-  lastday = Date.new(show_year, show_month, -1).day
+def days
+  firstday = Date.new(year, month, 1).day
+  lastday = Date.new(year, month, -1).day
 
   (firstday..lastday).each do |day|
-    wday = Date.new(show_year, show_month, day).wday
+    wday = Date.new(year, month, day).wday
     if day == 1
-      print '   ' * wday + "  #{day}"
-    elsif day < 10 && day != 1
-      print "  #{day}"
+      if wday == 0
+        print " #{day}"
+      else
+        print '   ' * wday + " #{day}"
+      end
     else
-      print " #{day}"
+      if wday == 0 && day < 10
+        print " #{day}"
+      elsif wday == 0 && day >= 10
+        print "#{day}"
+      elsif wday != 0 && day < 10
+        print "  #{day}"
+      elsif wday != 0 && day >= 10
+        print " #{day}"
+      end
     end
     puts "\n" if wday == 6
   end
 end
 
-puts "#{show_month}月 #{show_year}".center(22)
-puts show_wday
-show_days
+puts "#{month}月 #{year}".center(20)
+puts ['日', '月', '火', '水', '木', '金', '土'].join(' ')
+days
