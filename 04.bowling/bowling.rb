e5001d9ab194656454ae19d205fdd7bbd9268615
masyuko0222 <masyuko0222@gmail.com>
2022-10-25 02:45:22 +0900

    ボウリングの課題を提出します

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -4,70 +4,47 @@
 score = ARGV[0]
 scores = score.split(',')
 
-def convert_strikes_to_ten(scores)
-  shots = []
-  scores.each do |s|
-    if s == 'X'
-      shots << 10
-      shots << 0
-    else
-      shots << s.to_i
-    end
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
   end
-  shots # 戻り値
 end
 
-shots = convert_strikes_to_ten(scores)
-
-# shotsを２投ずつか、ストライクが出ている場合は１投ずつで区切る
-def divide_pithces(shots)
-  # ２投ずつで区切る
-  divided_scores = []
-  shots.each_slice(2) do |s|
-    divided_scores << s
-  end
-  # ストライクが出たフレームは１投で区切るようにpopを使う
-  divided_scores.each do |array|
-    array.pop if array[0] == 10
-  end
-  divided_scores # 戻り値
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
 end
 
-divided_scores = divide_pithces(shots)
-
-# divided_scoresにindexをつけて、何フレーム目かを分かりやすくする
-def divided_scores_w_idx(divided_scores)
-  divided_scores_w_idx = []
-  divided_scores.each_with_index do |s, f|
-    divided_scores_w_idx << [s, f]
-  end
-  divided_scores_w_idx # 戻り値
+frames.each do |array|
+  array.pop if array[0] == 10
 end
 
-scores_w_idx = divided_scores_w_idx(divided_scores)
+index = []
+frames.each_with_index do |s, f|
+  index << [s, f]
+end
 
-# スコアの計算をする
-def calculate_scores(scores_w_idx)
-  sum_score = 0
-  (0..scores_w_idx.size - 1).each do |n|
-    current_score = scores_w_idx[n][0]
-    next_score = scores_w_idx[n + 1][0] if scores_w_idx[n][1] + 1 < 10
+sum_score = 0
 
-    if scores_w_idx[n][1] + 1 >= 10 # １０フレーム目の計算
-      sum_score += current_score.sum
-    elsif current_score[0] == 10 # ストライク
-      if next_score[0] == 10 # 連続ストライク
-        sum_score += 10 + next_score[0] + scores_w_idx[n + 2][0][0]
+(0..index.size - 1).each do |i|
+  sum_score +=
+    if index[i][1] + 1 >= 10
+      index[i][0].sum
+    elsif index[i][0][0] == 10
+      if index[i + 1][0][0] == 10
+        10 * 2 + index[i + 2][0][0]
       else
-        sum_score += 10 + next_score.sum
+        10 + index[i + 1][0].sum
       end
-    elsif current_score.sum == 10 # スペア
-      sum_score += 10 + next_score[0]
+    elsif index[i][0].sum == 10
+      10 + index[i + 1][0][0]
     else
-      sum_score += current_score.sum
+      index[i][0].sum
     end
-  end
-  sum_score # 戻り値
 end
 
-p calculate_scores(scores_w_idx)
+p sum_score
