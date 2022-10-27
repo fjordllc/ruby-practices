21afedacf6db121fe7bd2995e2f1a5e65bdcac73
そがたつや <sogatatsuya@Soga-MacBook-Pro.local>
2022-10-27 18:22:20 +0900

    add fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+# 1から20までの数をプリントするプログラムを書け。ただし3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、3と5両方の倍数の場合には｢FizzBuzz｣とプリントすること。
+
+1.upto(20) {|num|
+  if num % 3 == 0
+    puts("Fizz")
+  elsif num % 5 == 0
+    puts("Buzz")
+  else
+    puts(num)
+  end
+}
+
