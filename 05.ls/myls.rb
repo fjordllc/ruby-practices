fd7edde1f3034533897f32519a6ec029f1c9a009
nicole2525 <nicole2525nomail@gmail.com>
2022-10-31 21:42:17 +0900

    レビューでいただいた内容を修正

--- 05.ls/myls.rb
+++ 05.ls/myls.rb
@@ -1,48 +1,46 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def make_the_list
-  elements = Dir.glob('*').sort
-  @elements = elements
+def make_list
+  Dir.glob('*').sort
 end
 
-def computes_numbers(max_columns_number)
-  longest_name_size = @elements.max_by(&:size).size
+def display_width
+  `tput cols`
+end
 
-  display_width = `tput cols`
+def columns_number
+  max_columns = 3
 
-  minus_row = 0
-  while longest_name_size > display_width.to_i / (max_columns_number - minus_row)
-    minus_row += 1
-    break if minus_row == max_columns_number - 1
+  longest_name_size = make_list.max_by(&:size).size
+
+  minus_columns = 0
+  while longest_name_size > display_width.to_i / (max_columns - minus_columns)
+    minus_columns += 1
+    break if minus_columns == max_columns - 1
   end
-
-  decided_row_number = max_columns_number - minus_row
-
-  decided_row_width = display_width.to_i / decided_row_number
-  @decided_row_width = decided_row_width
-
-  n = @elements.size
-  vertical_elements =
-    if (n % decided_row_number).zero?
-      n / decided_row_number
-    else
-      n / decided_row_number + 1
-    end
-  @vertical_elements = vertical_elements
+  max_columns - minus_columns
 end
 
-def processing_for_display
-  elements_with_blank = @elements.map { |d| d.ljust(@decided_row_width) }
-
-  slice_elements = []
-  elements_with_blank.each_slice(@vertical_elements) { |slice_element| slice_elements << slice_element }
-
-  slice_elements_with_nil = slice_elements.map { |element| element.values_at(0...@vertical_elements) }
-
-  slice_elements_with_nil.transpose.each { |display| puts display.join('') }
+def columns_width
+  display_width.to_i / columns_number
 end
 
-make_the_list
-computes_numbers(3)
-processing_for_display
+def vertical
+  n = make_list.size
+  calc_vertical = n / columns_number
+  if (n % columns_number).zero?
+    calc_vertical
+  else
+    calc_vertical + 1
+  end
+end
+
+def display_result
+  left_alignment = make_list.map { |d| d.ljust(columns_width) }
+  slice = left_alignment.each_slice(vertical).to_a
+  add_nil = slice.map { |element| element.values_at(0...vertical) }
+  add_nil.transpose.each { |display| puts display.join('') }
+end
+
+display_result
