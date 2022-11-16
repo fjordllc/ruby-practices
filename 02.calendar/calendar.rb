64cdb9b81205654b69f7f1dfa1d758778a7715a8
rira <101010hayakawa@gmail.com>
2022-11-16 16:25:30 +0900

    コメントの削除
    コメントの量が多かったため、なくても意味が分かる場所は削除しました。
    再レビューの指摘点です。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,7 +9,6 @@ SUNDAY = 7
 LEFT_EDGE_YOUBI_INDEX = 7
 SATURDAY = 6
 
-# 一週目の1日までの間にスペースを置く
 def print_space_to_first(first_youbi)
   youbi_position = first_youbi == SUNDAY ? 0 : first_youbi * SPACE_NUM_FOR_DAY
   space = ' ' * youbi_position
@@ -63,11 +62,8 @@ def parse_option
 end
 
 def print_calendar(year, month)
-  # 対象の月の1日を取得
   first_day = Date.parse("#{year}-#{month}-1")
-  # 対象の月の1日の曜日を計算
   first_youbi = first_day.cwday
-  # 対象の月の末日を取得
   last_day = Date.new(year.to_i, month.to_i, -1).day
   puts "#{year}年#{month}月"
   puts %w[日 月 火 水 木 金 土].join(' ')
@@ -76,20 +72,16 @@ def print_calendar(year, month)
   (1..last_day).each do |day|
     print_day(year, month, day)
     print ' '
-    # 週の切り替わり計算
     print "\n" if saturday?(youbi_index)
     youbi_index += 1
   end
   puts '' # zshがつける不要な%を消す
 end
 
-# コマンドラインから引数を取得
 options = parse_option
 year    = options['y']
 month   = options['m']
-# 引数がなければ現在の年、月を取得
 year = Date.today.year if year.nil?
 month = Date.today.month if month.nil?
 
-# カレンダー表示
 print_calendar(year, month)
