f11460d931a545c4d76b88d47b5a2d8f3d5ae8d4
rira <101010hayakawa@gmail.com>
2022-11-22 10:28:45 +0900

    RuboCop対応
    RuboCopの指摘部分修正
    定数SPACE_FOR_COLUMNS追加

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -1,8 +1,33 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_file_list()
+require 'debug'
+COLUMNS = 3
+SPACE_FOR_COLUMNS = 2
+def make_file_list
   Dir.glob('*')
 end
 
-puts get_file_list
+def print_file_list
+  files = make_file_list.sort
+  rows = files.size / COLUMNS + 1
+  lines = []
+  max_file_names = []
+  files.each_with_index do |file_name, index|
+    now_row = index % rows
+    now_column = index / rows
+    lines[now_row] = [] if now_column.zero?
+    max_file_names[now_column] ||= 0
+
+    lines[now_row] << file_name
+    max_file_names[now_column] = file_name.size if max_file_names[now_column] < file_name.size
+  end
+  lines.each do |line|
+    line.each_with_index do |element, i|
+      print element.ljust(max_file_names[i] + SPACE_FOR_COLUMNS)
+    end
+    print "\n"
+  end
+end
+
+print_file_list
