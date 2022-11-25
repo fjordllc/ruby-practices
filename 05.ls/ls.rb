766cb97579742fb59837bfbcb061c42ccc244b7a
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-11-25 14:26:37 +0900

    基盤となる部分の実装（正しい情報が表示できていない）

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -18,13 +18,79 @@ def display(files, row)
   end
 end
 
+def display_l(files, row)
+  puts "l option"
+  row.times do |y|
+    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
+    puts
+  end
+end
+
 opt = OptionParser.new
 params = {}
-opt.on('-a') { |v| params[:a] = v }
+
+opt.on('-l') { |v| params[:l] = v }
 opt.parse!(ARGV)
 
-disp_files = params[:a] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
 
 # display files
-sorted_files = disp_files.sort
-display(sorted_files, calc_row(sorted_files.size))
+sorted_files = Dir.glob('*').sort
+
+cmod_table = { 
+  '01' => 'p',
+  '02' => 'c',
+  '04' => 'd',
+  '06' => 'b',
+  '10' => '-',
+  '12' => 'l',
+  '14' => 's',
+
+  '0' => '',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx',
+  }
+
+result_l_opt = []
+
+sorted_files.size.times.with_index do |i|
+  fs = File::Stat.new(sorted_files[i])
+  puts fs.mode.to_s(8)
+  # 0 4 0 7 5 5 
+  permittion_num = format("%06d", fs.mode.to_s(8)).split("")
+  # 04 0 7 5 5 
+  permittion_num = [permittion_num[0..1].join,permittion_num[2..5]].flatten
+  # d rwx r-x r-x
+  # display
+  permittion_num.size.times.with_index do |j|
+    print cmod_table[permittion_num[j]]
+  end
+
+  print " #{fs.nlink}"
+  print " #{fs.uid}"
+  print " #{fs.gid}"
+  print " #{fs.size}"
+  print " #{fs.mtime}"
+  print " #{sorted_files[i]}"
+
+
+
+
+  # drwxr-xr-x  4 taku.fujisaki  staff   128 Nov 18 09:35 01.fizzbuzz
+  puts
+
+  
+
+end
+
+
+
+
+
+
+
+# params[:l] ? display_l(sorted_files, calc_row(sorted_files.size)) : display(sorted_files, calc_row(sorted_files.size))
