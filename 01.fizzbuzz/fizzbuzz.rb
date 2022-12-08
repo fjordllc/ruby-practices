0972743f6f3c560821a12112e45efeaced631911
motohiro-mm <polarbear.dx@gmail.com>
2022-12-08 21:11:13 +0900

    空白と条件を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,15 @@
 number = 0
 while number < 20
-    number += 1
-    case 
-    when number %3 == 0 && number %5 == 0
-        puts "FizzBuzz"
-    when number %3 == 0 && number %5 != 0
-        puts "Fizz"
-    when number %3 != 0 && number %5 == 0
-        puts "Buzz"
-    else
-        puts number
-    end
+  number += 1
+  case 
+  when number %3 == 0 && number %5 == 0
+      puts "FizzBuzz"
+  when number %3 == 0
+      puts "Fizz"
+  when number %5 == 0
+      puts "Buzz"
+  else
+      puts number
+  end
 end
 
