123886f2986362db71ae90d405fd77982fea324b
motohiro-mm <polarbear.dx@gmail.com>
2022-12-09 14:36:54 +0900

    コメント,Range,the,繰り返し,rjustを修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,51 +3,34 @@ require "date"
 
 params = ARGV.getopts("y:", "m:")
 
-date = Date.today #dateは今の年月日
+today_date = Date.today
 
-if !params["m"] #-mオプションがなければ今月を指定
-    the_month = date.month
+if !params["m"]
+  target_month = today_date.month
 else
-    the_month = params["m"].to_i
+  target_month = params["m"].to_i
 end
 
-if !params["y"] #-yオプションがなければ今年を指定
-    the_year = date.year
+if !params["y"]
+  target_year = today_date.year
 else
-    the_year = params["y"].to_i
+  target_year = params["y"].to_i
 end
 
-last_date = Date.new(the_year,the_month,-1) #last_dateは指定年月最終日
-last_day = last_date.day #last_dayは指定年月の最終日
-days = [last_day] #days配列[最終日のみ]
-while last_day > 1
-    last_day = last_day - 1
-    days.unshift(last_day) #最終日から１ずつ引いた数を配列daysの先頭に追加
-end 
-#days配列に指定月の日付けが追加された
+puts "       #{target_month}月 #{target_year}"
 
-puts "      #{the_month}月 #{the_year}"  #指定年月を表示
+wdays = [" 日","月","火","水","木","金","土"]
+puts wdays.join(" ")
 
-wdays = ["日","月","火","水","木","金","土"]
-puts wdays.join(" ") #曜日を表示
+date1 = Date.new(target_year,target_month,1)
+blank_count = date1.wday * 3
+print " " * blank_count
 
-#1の前の空白を表示
-date1 = Date.new(the_year,the_month,1)
-blank = date1.wday * 3
-blank.to_i.times { print " "}
-
-#日付を表示
-days.each do |day|
-    cal_date = Date.new(the_year,the_month,day)
-    case 
-    when cal_date.saturday? && day < 10 #土曜日かつ10以下で改行と左スペース
-        puts " #{day}"
-    when cal_date.saturday? #土曜日で改行
-        puts day
-    when day < 10 #10以下で左右スペース
-        print " #{day} "
-    else #それ以外は右スペース
-        print "#{day} "
-    end
+(Date.new(target_year,target_month,1)..Date.new(target_year,target_month,-1)).each do |date|
+  if date.saturday? 
+    puts date.day.to_s.rjust(3)
+  else 
+    print date.day.to_s.rjust(3)
+  end
 end
 
