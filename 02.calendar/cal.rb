dc84f6a69fa583a6db738bb23d64077fe080ddde
motohiro-mm <polarbear.dx@gmail.com>
2022-12-12 11:48:44 +0900

    if文と変数名を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,25 +5,27 @@ params = ARGV.getopts("y:", "m:")
 
 today_date = Date.today
 
-if !params["m"]
-  target_month = today_date.month
-else
-  target_month = params["m"].to_i
-end
+target_month =
+  if params["m"]
+    params["m"].to_i
+  else
+    today_date.month
+  end
 
-if !params["y"]
-  target_year = today_date.year
-else
-  target_year = params["y"].to_i
-end
+target_year = 
+  if params["y"]
+    params["y"].to_i
+  else
+    today_date.year
+  end
 
 puts "       #{target_month}月 #{target_year}"
 
 wdays = [" 日","月","火","水","木","金","土"]
 puts wdays.join(" ")
 
-date1 = Date.new(target_year,target_month,1)
-blank_count = date1.wday * 3
+first_date_of_month = Date.new(target_year,target_month,1)
+blank_count = first_date_of_month.wday * 3
 print " " * blank_count
 
 (Date.new(target_year,target_month,1)..Date.new(target_year,target_month,-1)).each do |date|
