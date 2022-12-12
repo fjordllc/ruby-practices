8d93fb5dd25bde13d7351b943aa819515bd844bf
motohiro-mm <polarbear.dx@gmail.com>
2022-12-12 21:30:47 +0900

    変数を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -28,7 +28,7 @@ first_date_of_month = Date.new(target_year,target_month,1)
 blank_count = first_date_of_month.wday * 3
 print " " * blank_count
 
-(Date.new(target_year,target_month,1)..Date.new(target_year,target_month,-1)).each do |date|
+(first_date_of_month..Date.new(target_year,target_month,-1)).each do |date|
   if date.saturday? 
     puts date.day.to_s.rjust(3)
   else 
