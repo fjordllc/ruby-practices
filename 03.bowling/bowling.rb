e5b12495956fe104335a499581178f678b862235
motohiro-mm <polarbear.dx@gmail.com>
2022-12-13 21:28:00 +0900

    フレームへの条件分岐を変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,51 +2,44 @@
 
 score = ARGV[0]
 scores = score.split(',')
-shots = []
-last_frame = []
-# 9フレームまではshots,10フレームはlast_frameへ追加
-scores.each do |s|
-  if s == 'X' && shots.size < 18
-    shots << 10
-    shots << 0
-  elsif s == 'X' && shots.size >= 18
-    last_frame << 10
-  elsif shots.size >= 18
-    last_frame << s.to_i
-  else
-    shots << s.to_i
-  end
-end
 
-# framesへ１フレームずつ分割して追加
 frames = []
-shots.each_slice(2) do |s|
-  frames << s
+one_frame = []
+last_frame = []
+
+scores.each do |s|
+  if s == 'X' && frames.size >= 9
+    last_frame << 10
+  elsif frames.size >= 9
+    last_frame << s.to_i
+  elsif s == 'X'
+    one_frame << 10
+  else
+    one_frame << s.to_i
+  end
+  next if one_frame.size == 1 && one_frame != [10]
+
+  frames << one_frame
+  one_frame = []
 end
 
-# ストライクは[10]へ表示修正
-frames.each do |frame|
-  frame.pop if frame == [10, 0]
-end
-# 10フレーム追加
+frames.delete([])
 frames.push(last_frame)
 
-# 全てのフレームをナンバリング
 numbers = [*1..10]
 result = [numbers, frames].transpose.to_h
 
-# 点数計算
 point = 0
 result.each do |number, frame|
-  point += if frame[0] == 10 && number != 10 # 1~9フレームのストライク
+  point += if frame[0] == 10 && number != 10
              if result.fetch(number + 1)[0] == 10 && number != 9
                10 + result.fetch(number + 1)[0] + result.fetch(number + 2)[0]
              else
                10 + result.fetch(number + 1)[0] + result.fetch(number + 1)[1]
              end
-           elsif frame.sum == 10 && number != 10 # スペア
+           elsif frame.sum == 10 && number != 10
              10 + result.fetch(number + 1)[0]
-           else # それ以外
+           else
              frame.sum
            end
 end
