cc19b71709538ab4053a84520026de458823c036
motohiro-mm <polarbear.dx@gmail.com>
2022-12-14 15:59:09 +0900

    frameの変数名、frameの番号取得を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,7 +4,7 @@ score = ARGV[0]
 scores = score.split(',')
 
 frames = []
-one_frame = []
+current_frame = []
 last_frame = []
 
 scores.each do |s|
@@ -13,32 +13,29 @@ scores.each do |s|
   elsif frames.size >= 9
     last_frame << s.to_i
   elsif s == 'X'
-    one_frame << 10
+    current_frame << 10
   else
-    one_frame << s.to_i
+    current_frame << s.to_i
   end
-  next if one_frame.size == 1 && one_frame != [10]
+  next if current_frame.size == 1 && current_frame != [10]
 
-  frames << one_frame
-  one_frame = []
+  frames << current_frame
+  current_frame = []
 end
 
 frames.delete([])
 frames.push(last_frame)
 
-numbers = [*1..10]
-result = [numbers, frames].transpose.to_h
-
 point = 0
-result.each do |number, frame|
-  point += if frame[0] == 10 && number != 10
-             if result.fetch(number + 1)[0] == 10 && number != 9
-               10 + result.fetch(number + 1)[0] + result.fetch(number + 2)[0]
+frames.each_with_index do |frame, i|
+  point += if frame[0] == 10 && i != 9
+             if frames[i + 1][0] == 10 && i != 8
+               10 + frames[i + 1][0] + frames[i + 2][0]
              else
-               10 + result.fetch(number + 1)[0] + result.fetch(number + 1)[1]
+               10 + frames[i + 1][0] + frames[i + 1][1]
              end
-           elsif frame.sum == 10 && number != 10
-             10 + result.fetch(number + 1)[0]
+           elsif frame.sum == 10 && i != 9
+             10 + frames[i + 1][0]
            else
              frame.sum
            end
