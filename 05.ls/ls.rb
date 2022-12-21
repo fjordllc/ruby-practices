b0dbddd55e085f849b3e3890c230781f4650c20f
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-12-21 12:52:14 +0900

    実装し忘れていたオプション無効時の実装

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,34 +6,6 @@ require 'etc'
 # 表示列の最大数をココで変更
 COLUMN = 3
 
-# 表示時に必要な行数rowを求める
-def calc_row(num)
-  (num % 3).zero? ? num / 3 : (num / 3) + 1
-end
-
-# ファイル一覧をlsのルールに従い表示
-def display(files, row)
-  row.times do |y|
-    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
-    puts
-  end
-end
-
-def display_l(files, row)
-  row.times do |y|
-    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
-    puts
-  end
-end
-
-opt = OptionParser.new
-params = {}
-opt.on('-l') { |v| params[:l] = v }
-opt.parse!(ARGV)
-
-# display files
-sorted_files = Dir.glob('*').sort
-
 cmod_table = {
   '01' => 'p',
   '02' => 'c',
@@ -68,27 +40,63 @@ month_table = {
   '12' => 'Dec'
 }
 
-total = sorted_files.size.times.sum do |i|
-  File::Stat.new(sorted_files[i]).blocks.to_i
+# 表示時に必要な行数rowを求める
+def calc_row(num)
+  (num % 3).zero? ? num / 3 : (num / 3) + 1
 end
-puts "total #{total}"
 
-sorted_files.size.times do |i|
-  fs = File::Stat.new(sorted_files[i])
-  permittion_num = format('%06d', fs.mode.to_s(8)).split('')
-  permittion_num = [permittion_num[0..1].join, permittion_num[2..5]].flatten
-
-  permittion_num.size.times do |j|
-    print cmod_table[permittion_num[j]]
+# ファイル一覧をlsのルールに従い表示
+def display(files, row)
+  row.times do |y|
+    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
+    puts
   end
-
-  print "  #{fs.nlink}"
-  print " #{Etc.getpwuid(fs.uid).name}"
-  print "  #{Etc.getgrgid(fs.gid).name}"
-  print format(' %5d', fs.size)
-  print " #{month_table[fs.mtime.to_a.slice(4).to_s]}"
-  print format('% 3d', fs.mtime.to_a.slice(3))
-  print Time.now - fs.mtime < 15_552_000 ? " #{fs.mtime.to_s.slice(11, 5)}" : " #{format(' %2d', fs.mtime.to_a.slice(5))}"
-  print " #{sorted_files[i]}"
-  puts
 end
+
+def display_l(files, row)
+  row.times do |y|
+    COLUMN.times { |x| printf(files[x * row + y].to_s.ljust(files.map(&:size).max + 3)) }
+    puts
+  end
+end
+
+opt = OptionParser.new
+params = {}
+opt.on('-l') { |v| params[:l] = v }
+opt.parse!(ARGV)
+
+# display files
+sorted_files = Dir.glob('*').sort
+
+# l オプション機能
+if params[:l]
+  # ブロックの合計値
+  total = sorted_files.size.times.sum do |i|
+    File::Stat.new(sorted_files[i]).blocks.to_i
+  end
+  puts "total #{total}"
+
+  sorted_files.size.times do |i|
+    fs = File::Stat.new(sorted_files[i])
+    # 権限
+    permittion_num = format('%06d', fs.mode.to_s(8)).split('')
+    permittion_num = [permittion_num[0..1].join, permittion_num[2..5]].flatten
+    permittion_num.size.times do |j|
+      print cmod_table[permittion_num[j]]
+    end
+
+    print "  #{fs.nlink}"
+    print " #{Etc.getpwuid(fs.uid).name}"
+    print "  #{Etc.getgrgid(fs.gid).name}"
+    print format(' %5d', fs.size)
+    print " #{month_table[fs.mtime.to_a.slice(4).to_s]}"
+    print format('% 3d', fs.mtime.to_a.slice(3))
+    print Time.now - fs.mtime < 15_552_000 ? " #{fs.mtime.to_s.slice(11, 5)}" : " #{format(' %2d', fs.mtime.to_a.slice(5))}"
+    print " #{sorted_files[i]}"
+    puts
+  end
+else
+  display(sorted_files, calc_row(sorted_files.size))
+end
+
+
