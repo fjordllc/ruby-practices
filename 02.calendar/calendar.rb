37b3b32497627340bad8ce7af96a82a29455eedd
terumitt <terumitt@pepabo.com>
2023-01-05 17:44:41 +0900

    カレンダープログラム思考中

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,6 +6,10 @@ def calender
     puts today.strftime('%m月 %Y').center(20)
     puts "\ 日\ 月\ 火\ 水\ 木\ 金\ 土"
 
+    first_day = Date.new(today.year,today.month,1)
+    start_day = first_day.strftime('%d').to_i
+
+    p start_day
 end
 
 puts calender
