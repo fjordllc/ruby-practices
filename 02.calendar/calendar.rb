24f9596ef9222ec231e9924275575efbdcbfc09c
terumitt <terumitt@pepabo.com>
2023-01-05 16:13:59 +0900

    first_commit

---
+++ 02.calendar/calendar.rb
@@ -0,0 +1,15 @@
+require 'date'
+
+def calender
+    today = Date.today
+
+    puts today.strftime('%m月 %Y').center(20)
+    puts "\ 日\ 月\ 火\ 水\ 木\ 金\ 土"
+
+    
+    #first_day = Date.new(today.year,today.month,1)
+    #start_day = first_day - first_day.strftime('%w').to_i
+
+end
+
+puts calender
