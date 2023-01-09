23d27a54f3bbcdcab353e85f6543dd5893a4021a
motohiro-mm <polarbear.dx@gmail.com>
2023-01-09 15:30:54 +0900

    配列から新しい配列を作成する部分をmapで修正、定数を定義

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,28 +3,22 @@
 FILES = Dir.glob('*').sort
 
 def file_column
-  filename_sizes = []
-  FILES.each do |file|
-    filename_sizes << file.size
-  end
+  filename_sizes = FILES.map(&:size)
   (filename_sizes.max / 8 + 1) * 8
 end
 
+MAX_COLUMN = 3
 def current_column
-  max_column = 3
   terminal_column = `tput cols`.chomp.to_i
-  file_column * max_column > terminal_column ? terminal_column / file_column : max_column
+  file_column * MAX_COLUMN > terminal_column ? terminal_column / file_column : MAX_COLUMN
 end
 
 files_size = FILES.size
 d = (files_size % current_column).zero? ? files_size / current_column : files_size / current_column + 1
 
-arrays = []
-(1..d).each { |n| arrays << n.step(by: d, to: files_size).to_a }
+arrays = (1..d).map { |n| n.step(by: d, to: files_size).to_a }
 
 arrays.each do |array|
-  array.each do |i|
-    print FILES[i - 1].ljust(file_column, ' ')
-  end
+  array.each { |i| print FILES[i - 1].ljust(file_column, ' ') }
   puts
 end
