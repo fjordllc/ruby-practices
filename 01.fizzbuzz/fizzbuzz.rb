25ba43de94686b80bcd3060ada00885a4edc58a6
tomo <o_tomo_1990@yahoo.co.jp>
2023-01-11 12:11:43 +0900

    三項演算子を用いるリファクタ

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,3 +1,13 @@
 (1..20).each do |i|
-  puts i
+  multiple3 = i % 3 == 0 ? true : false
+  multiple5 = i % 5 == 0 ? true : false
+  if multiple3 && multiple5
+    puts 'FizzBuzz'
+  elsif multiple3
+    puts 'Fizz'
+  elsif multiple5
+    puts 'Buzz'
+  else
+    puts i
+  end
 end
