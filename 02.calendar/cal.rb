b50aed45b3d520472c6f19d957078a5b40591140
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-01-29 12:28:49 +0900

    コメントの修正 + カレンダーの最初の週の空白を変数にする

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,18 +2,19 @@ require 'date'
 require 'optparse'
 
 params = ARGV.getopts("y:","m:")
+
 year = (params["y"] || Date.today.year).to_i # str型だとDate.new出来ないので
 month = (params["m"] || Date.today.month).to_i
 
-start_day = Date.new(year,month,1)
-end_day = Date.new(year,month,-1)
+start_day = Date.new(year,month,1) # カレンダーで最初に表示する日
+end_day = Date.new(year,month,-1) # カレンダーで最後に表示する日
+
+# カレンダーのスタートは日曜日から始まり、最初に改行なしの空白を差し込めば、その曜日から出力を行える
+blank_first_calendar = "   " * start_day.wday
 
 puts "#{month}月 #{year}".center(20)
 puts " 日 月 火 水 木 金 土"
-# wdayの日曜日は0:int型になる
-# カレンダーのスタートは日曜日から始まる
-# なので、最初に改行なしの空白を差し込めば、その曜日から出力を行える
-print "   " * start_day.wday
+print blank_first_calendar
 # 月の最初の日から、最後の日までループを回す
 (start_day..end_day).each{ |day|
   # 土曜日でなければ、改行無しで出力
