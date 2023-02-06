5609fe0f6e69b124674459bffde12aa945719a41
nekonote <nekonote.civ@gmail.com>
2023-02-06 15:42:34 +0000

    ls コマンド処理作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+NUMBER_OF_COLUMNS = 3
+LENGTH_OFFSET = 2
+
+files = Dir.glob('*')
+
+max_row_length = (files.length % NUMBER_OF_COLUMNS).zero? ? files.length / NUMBER_OF_COLUMNS : files.length / NUMBER_OF_COLUMNS + 1
+file_names = files.each_slice(max_row_length).to_a
+
+lengths = file_names.map do |file_name|
+  file_name.max_by(&:length).length
+end
+
+max_row_length.times do |row|
+  file_names.length.times do |col|
+    print format("%-#{lengths[col] + LENGTH_OFFSET}s", file_names[col][row])
+  end
+  puts
+end
