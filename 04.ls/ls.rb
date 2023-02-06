3bfe99962b75837d21849762fc36b99eebb06c05
nekonote <nekonote.civ@gmail.com>
2023-02-06 15:53:00 +0000

    メソッドの切り出し

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,18 +3,30 @@
 NUMBER_OF_COLUMNS = 3
 LENGTH_OFFSET = 2
 
-files = Dir.glob('*')
-
-max_row_length = (files.length % NUMBER_OF_COLUMNS).zero? ? files.length / NUMBER_OF_COLUMNS : files.length / NUMBER_OF_COLUMNS + 1
-file_names = files.each_slice(max_row_length).to_a
-
-lengths = file_names.map do |file_name|
-  file_name.max_by(&:length).length
-end
-
-max_row_length.times do |row|
-  file_names.length.times do |col|
-    print format("%-#{lengths[col] + LENGTH_OFFSET}s", file_names[col][row])
+def file_name_lengths(file_names)
+  file_names.map do |file_name|
+    file_name.max_by(&:length).length
   end
-  puts
 end
+
+def print_format_file_name(text, length)
+  print format("%-#{length + LENGTH_OFFSET}s", text)
+end
+
+def main
+  files = Dir.glob('*')
+
+  max_row_length = (files.length % NUMBER_OF_COLUMNS).zero? ? files.length / NUMBER_OF_COLUMNS : files.length / NUMBER_OF_COLUMNS + 1
+  file_names = files.each_slice(max_row_length).to_a
+
+  lengths = file_name_lengths(file_names)
+
+  max_row_length.times do |row|
+    file_names.length.times do |col|
+      print_format_file_name(file_names[col][row], lengths[col])
+    end
+    puts
+  end
+end
+
+main
