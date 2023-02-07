47bd84343282db4638d4e0902f6abb5a93de3ff9
nekonote <nekonote.civ@gmail.com>
2023-02-07 15:31:43 +0000

    ディレクトリを指定出来るようにして rubocop を通した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,14 +3,14 @@
 NUMBER_OF_COLUMNS = 3
 SINGLE_BYTE_CHAR_DISPLAY_LENGTH = 1
 MULTI_BYTE_CHAR_DISPLAY_LENGTH = 2
-OFFSET_SPACES = "  "
+OFFSET_SPACES = '  '
 
 def translate_display_char_length(char)
   char.bytesize == 1 ? SINGLE_BYTE_CHAR_DISPLAY_LENGTH : MULTI_BYTE_CHAR_DISPLAY_LENGTH
 end
 
-def display_length(string)
-  string.each_char.map do |char|
+def display_length(text)
+  text.each_char.map do |char|
     translate_display_char_length(char)
   end.sum
 end
@@ -22,21 +22,23 @@ def max_file_name_length(file_names)
 end
 
 def print_format_file_name(file_names_hash)
-  spaces = " " * (file_names_hash[:max_length] - file_names_hash[:length])
+  spaces = ' ' * (file_names_hash[:max_length] - file_names_hash[:length])
   print "#{file_names_hash[:name]}#{spaces}#{OFFSET_SPACES}"
 end
 
 def main
-  files = Dir.glob('*')
+  argv = ARGV[0] || ''
+  files = Dir.glob('*', base: argv)
+  return if files.empty?
 
   max_row = (files.length % NUMBER_OF_COLUMNS).zero? ? files.length / NUMBER_OF_COLUMNS : files.length / NUMBER_OF_COLUMNS + 1
   file_names_list = files.each_slice(max_row).to_a
 
   file_names_hash = file_names_list.map do |file_names|
     max_length = max_file_name_length(file_names)
-    file_names.each.map do |file_name|
-      length = display_length(file_name)
-      { name: file_name, length: length, max_length: max_length }
+    file_names.each.map do |name|
+      length = display_length(name)
+      { name:, length:, max_length: }
     end
   end
 
