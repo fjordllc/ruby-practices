31e0f5239369421ccc46dcedb11071fef0734544
nekonote <nekonote.civ@gmail.com>
2023-02-07 13:17:36 +0000

    マルチバイト文字が含まれていても表示が崩れないようにした

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,11 +2,23 @@
 
 NUMBER_OF_COLUMNS = 3
 LENGTH_OFFSET = 2
+SINGLE_BYTE_CHAR_DISPLAY_LENGTH = 1
+MULTI_BYTE_CHAR_DISPLAY_LENGTH = 2
+
+def translate_display_char_length(char)
+  char.bytesize == 1 ? SINGLE_BYTE_CHAR_DISPLAY_LENGTH : MULTI_BYTE_CHAR_DISPLAY_LENGTH
+end
+
+def display_length(string)
+  string.each_char.map do |char|
+    translate_display_char_length(char)
+  end.sum
+end
 
 def file_name_lengths(file_names)
-  file_names.map do |file_name|
-    file_name.max_by(&:length).length
-  end
+  file_names.each.map do |file_name|
+    display_length(file_name)
+  end.max
 end
 
 def print_format_file_name(text, length)
@@ -16,14 +28,16 @@ end
 def main
   files = Dir.glob('*')
 
-  max_row_length = (files.length % NUMBER_OF_COLUMNS).zero? ? files.length / NUMBER_OF_COLUMNS : files.length / NUMBER_OF_COLUMNS + 1
-  file_names = files.each_slice(max_row_length).to_a
+  max_row = (files.length % NUMBER_OF_COLUMNS).zero? ? files.length / NUMBER_OF_COLUMNS : files.length / NUMBER_OF_COLUMNS + 1
+  file_names_list = files.each_slice(max_row).to_a
 
-  lengths = file_name_lengths(file_names)
+  lengths = file_names_list.map do |file_names|
+    file_name_lengths(file_names)
+  end
 
-  max_row_length.times do |row|
-    file_names.length.times do |col|
-      print_format_file_name(file_names[col][row], lengths[col])
+  max_row.times do |row|
+    file_names_list.length.times do |col|
+      print_format_file_name(file_names_list[col][row], lengths[col])
     end
     puts
   end
