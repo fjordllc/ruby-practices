03a92192a96fccca074e0d5b9c1c3f21c568991d
massanengineer <matsuda4645@gmail.com>
2023-03-09 15:38:01 +0900

    再提出しました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,30 +1,24 @@
 # frozen_string_literal: true
 
-def files
-  Dir.glob('*').map do |list|
-    list
-  end
-end
-
-def columns_list
-  columns = 3
-  lists = files
-  rest_row = lists.size % columns
-  if rest_row != 0
-    (columns - rest_row).times do
-      lists << nil
+def row_column
+  files = Dir.glob('*')
+  column = 3
+  rest_of_row_count = files.size % column
+  if rest_of_row_count != 0 && rest_of_row_count.positive?
+    (column - rest_of_row_count).times do
+      files << nil
     end
   end
-  rows = lists.size / columns
-  lists.each_slice(rows).to_a
+  row_count = files.size / column
+  files.each_slice(row_count).to_a
 end
 
-def displays(file_lists)
-  file_lists.each do |index|
-    index.each do |item|
-      print item.to_s.ljust(10)
+def display(column_row)
+  column_row.each do |files_line|
+    files_line.each do |line|
+      print line.to_s.ljust(10)
     end
     puts "\n"
   end
 end
-displays(columns_list.transpose)
+display(row_column.transpose)
