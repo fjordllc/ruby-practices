d28f488d734a330a875dc878da13d2f9791e3ecc
zamami <s1413115@gmail.com>
2023-04-04 11:02:46 +0900

    fizzbuzzプログラムを作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,14 @@
+numbers = (1..20).to_a
+numbers.each{|number| 
+
+    if number % 15.0 == 0
+        puts "FizzBuzz"
+    elsif number % 3.0 == 0
+        puts "Fizz"
+    elsif number % 5.0 == 0
+        puts "Buzz"
+    else 
+        puts number
+    end
+
+}
