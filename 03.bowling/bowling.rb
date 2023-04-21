c562fcc59dd7de3d5485c46f9e9871f62d094e70
s-miura7 <s-miura7@ruby-dev.jp>
2023-04-21 10:43:16 +0900

    いらない行を削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -6,7 +6,7 @@ require 'debug'
 user_input = ARGV[0].split(',')
 
 # 全て数字に変える
-user_input_convert_num = user_input.map { |score| score == 'X' ? 10 : score.to_i }
+converted_num = user_input.map { |score| score == 'X' ? 10 : score.to_i }
 
 # フレームごとに区切る
 split_user_input = 0
@@ -14,25 +14,21 @@ split_user_input = 0
 score = 0
 
 10.times do |frame|
-  score_of_two = user_input_convert_num[split_user_input] + user_input_convert_num[split_user_input + 1]
+  score_of_two = converted_num[split_user_input] + converted_num[split_user_input + 1]
   # ストライク
-  if user_input_convert_num[split_user_input] == 10
-    sum_of_frame = score_of_two + user_input_convert_num[split_user_input + 2]
-    sum_of_frame += user_input_convert_num[split_user_input + 3] if frame == 10
-    score += sum_of_frame
+  if converted_num[split_user_input] == 10
+    score += score_of_two + converted_num[split_user_input + 2]
     split_user_input += 1
   # スペア
   elsif score_of_two == 10
-    sum_of_frame = score_of_two + user_input_convert_num[split_user_input + 2]
-    sum_of_frame += user_input_convert_num[split_user_input + 3] if frame == 10
-    score += sum_of_frame
+    score += score_of_two + converted_num[split_user_input + 2]
     split_user_input += 2
   # 9本以下
   else
-    sum_of_frame = score_of_two
-    score += sum_of_frame
+    score += score_of_two
     split_user_input += 2
   end
+  p frame
 end
 
 p score
