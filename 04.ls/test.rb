9b4d38cfd4cd06bf6fc0b4ca3e5354cb8807fd75
kochi-echo <tnodtegrof@gmail.com>
2023-04-23 10:05:17 +0900

    テスト雛形追加

--- 04.ls/test.rb
+++ 04.ls/test.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 
+require 'minitest/autorun'
+require_relative 'my-ls'
+
+class TestFileNameReciever < Minitest::Test
+  def test_get_file_names_no_argument
+    get_file_names()
+  end
+end
