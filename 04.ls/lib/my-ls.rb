180d10c30ab98a45597cb2b85a4261664be851a3
kochi-echo <tnodtegrof@gmail.com>
2023-04-23 16:44:44 +0900

    漢字のテキスト作成まで完成

--- 04.ls/lib/my-ls.rb
+++ 04.ls/lib/my-ls.rb
@@ -19,6 +19,51 @@ def get_file_names(argument_name)
   end
 end
 
+class Array
+  def divide_equal(number)
+    split_num = (self.size / number.to_f).ceil
+    self.each_slice(split_num).to_a
+  end
+end
+
+class String
+  def size_jp
+    split_num = (self.size / number.to_f).ceil
+    self.each_slice(split_num).to_a
+  end
+end
+
+class String
+  def size_jp
+    count = 0
+    self.each_char do |char|
+      if char.match?(/\p{Han}|\p{Hiragana}|\p{Katakana}/)
+        count += 2
+      else
+        count += 1
+      end
+    end
+    count
+  end
+
+  def ljust_jp(max_size)
+    self + ' ' * (max_size - self.size_jp)
+  end
+end
+
+def generate_name_list_text(file_names, number)
+  separatiopn_names = file_names.divide_equal(number)
+  max_name_size = file_names.max {|a, b| a.size_jp <=> b.size_jp }.size_jp
+  max_names_array_size = separatiopn_names.map(&:size).max
+  while separatiopn_names.last.size < max_names_array_size
+    separatiopn_names.last << ''
+  end
+  pp separatiopn_names.transpose
+  separatiopn_names.transpose.inject('') do |text, names|
+    text += names.map { |n| n.ljust_jp(max_name_size) }.join(' ') + "\n"
+  end
+end
+
 # disp_column = file_names.each_slice(3).map{|n| n}
 
 # (0..disp_column.size - 1).each do
--- 04.ls/test/test.rb
+++ 04.ls/test/test.rb
@@ -9,7 +9,7 @@ require 'debug'
 # cd ~/Documents/Fjord/ruby-practices/04.ls/test/test_target
 # ruby ~/Documents/Fjord/ruby-practices/04.ls/test/test.rb
 
-class TestFileNameReciever < Minitest::Test
+class TestNameReciever < Minitest::Test
   def test_get_file_names_no_argument
     assert_equal ['a_test.txt', 'b_test.rb', 'sub.dir', 'テスト-ターゲット.md', '試験.txt'], get_file_names('')
     # lsだと 'a_test.txt', 'b_test.rb', 'sub.dir', '試験.txt', 'テスト-ターゲット.md'
@@ -27,5 +27,29 @@ class TestFileNameReciever < Minitest::Test
     assert_equal '試験.txt', get_file_names('試験.txt')
     assert_equal 'テスト-ターゲット.md', get_file_names('テスト-ターゲット.md')
     assert_equal '.test', get_file_names('.test')
+  end
+end
+
+class TestGenerationNameListText < Minitest::Test
+  def test_generate_name_list_text_only_alphabet
+      assert_equal "abc bc \n", generate_name_list_text(['abc','bc'], 3)
+      assert_equal "abc bc  c  \n", generate_name_list_text(['abc','bc','c'], 3)
+      assert_equal "abc c  \nbc  d  \n", generate_name_list_text(['abc','bc','c','d'], 3)
+  end
+
+  def test_generate_name_list_text_with_japanese
+    result_text = <<~TEXT
+    life.md    楽ありゃ   苦もあるさ
+    人生       happy.rb              
+    TEXT
+    assert_equal result_text, generate_name_list_text(['life.md', '人生', '楽ありゃ', 'happy.rb', '苦もあるさ'], 3)
+  end
+
+  def test_generate_name_list_text_with_files
+    result_text = <<~TEXT
+    a_test.txt           sub.dir           試験.txt            
+    b_test.rb            テスト-ターゲット.md                     
+    TEXT
+    assert_equal result_text, generate_name_list_text(['a_test.txt', 'b_test.rb', 'sub.dir', 'テスト-ターゲット.md', '試験.txt'], 3)
   end
 end
