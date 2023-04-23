918c9ed508b8a474dba59040e4a52b72438645aa
kochi-echo <tnodtegrof@gmail.com>
2023-04-23 19:03:42 +0900

    クラスのテスト追加、メインを追加

--- 04.ls/lib/my-ls.rb
+++ 04.ls/lib/my-ls.rb
@@ -1,21 +1,45 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+require 'optparse'
+
+LIST_ROW_NAME = 3
 
 def get_file_names(argument_name)
-  target_file = ''
+  argument_name ||= ''
   absolute_path = File.expand_path(argument_name)
+  target_directory, target_file = path_to_directory_and_file(absolute_path, argument_name)
+  select_file(target_directory, target_file)
+end
+
+def path_to_directory_and_file(absolute_path, argument_name)
+  target_file = ''
   if File.directory?(absolute_path)
     target_directory = absolute_path
   else
     target_directory =  '.'
     target_file = argument_name unless argument_name == ''
   end
-  
+  [target_directory, target_file]
+end
+
+
+def select_file(target_directory, target_file)
   file_names_all = Dir.entries(target_directory).sort
   if target_file.empty?
     file_names_all.reject { |file_name| file_name =~ /^\./ }
   else
     file_names_all.select{ |file_name| file_name == target_file}.first
+  end  
+end
+
+
+def generate_name_list_text(file_names, number)
+  separatiopn_names = file_names.divide_equal(number)
+  max_name_size = file_names.max {|a, b| a.size_jp <=> b.size_jp }.size_jp
+  separatiopn_names.transpose_lack.inject('') do |text, names|
+    text += names.map.with_index(1) do |name, index| 
+      index < names.size ? name.ljust_jp(max_name_size) : name
+    end.join(' ') + "\n"
   end
 end
 
@@ -24,12 +48,12 @@ class Array
     split_num = (self.size / number.to_f).ceil
     self.each_slice(split_num).to_a
   end
-end
 
-class String
-  def size_jp
-    split_num = (self.size / number.to_f).ceil
-    self.each_slice(split_num).to_a
+  def transpose_lack
+    max_size = self.map(&:size).max
+    (0...max_size).map do |selection_num|
+      self.map { |nest_array| nest_array[selection_num] }.compact
+    end
   end
 end
 
@@ -51,28 +75,6 @@ class String
   end
 end
 
-def generate_name_list_text(file_names, number)
-  separatiopn_names = file_names.divide_equal(number)
-  max_name_size = file_names.max {|a, b| a.size_jp <=> b.size_jp }.size_jp
-  max_names_array_size = separatiopn_names.map(&:size).max
-  while separatiopn_names.last.size < max_names_array_size
-    separatiopn_names.last << ''
-  end
-  pp separatiopn_names.transpose
-  separatiopn_names.transpose.inject('') do |text, names|
-    text += names.map { |n| n.ljust_jp(max_name_size) }.join(' ') + "\n"
-  end
-end
-
-# disp_column = file_names.each_slice(3).map{|n| n}
-
-# (0..disp_column.size - 1).each do
-#   print(disp_column[])
-# end
-
-
-
-# file_names = Dir.foreach('.') do |file_name|
-#   next if file_name == '.' || file_name == '..' || file_name =~ /^./
-# end
-# p file_names
+input = ARGV[0]
+file_names = get_file_names(input)
+print generate_name_list_text(file_names, LIST_ROW_NAME)
--- 04.ls/test/test.rb
+++ 04.ls/test/test.rb
@@ -30,25 +30,54 @@ class TestNameReciever < Minitest::Test
   end
 end
 
+class TestArrayMethod < Minitest::Test
+  def test_divide_equal
+    assert_equal [[1,2],[3,4],[5]], [1,2,3,4,5].divide_equal(3)
+    assert_equal [[1,2],[3,4],[5,6]], [1,2,3,4,5,6].divide_equal(3)
+  end
+
+  def transpose_lack
+    assert_equal [[1,3,5],[2,4]], [[1,2],[3,4],[5]].transpose_lack
+    assert_equal [[1,4,7],[2,5],[3,6]], [[1,2,3],[4,5,6],[7]].transpose_lack
+  end
+end
+
+class TestStringMethod < Minitest::Test
+  def test_size_jp
+    assert_equal 3, "abc".size_jp
+    assert_equal 6, "あいう".size_jp
+    assert_equal 4, "aあc".size_jp
+  end
+
+  def test_ljust_jp
+    assert_equal "abc   ", "abc".ljust_jp(6)
+    assert_equal "あいう", "あいう".ljust_jp(6)
+    assert_equal "あいう  ", "あいう".ljust_jp(8)
+  end
+end
+
 class TestGenerationNameListText < Minitest::Test
   def test_generate_name_list_text_only_alphabet
-      assert_equal "abc bc \n", generate_name_list_text(['abc','bc'], 3)
-      assert_equal "abc bc  c  \n", generate_name_list_text(['abc','bc','c'], 3)
-      assert_equal "abc c  \nbc  d  \n", generate_name_list_text(['abc','bc','c','d'], 3)
+      assert_equal "abc bc\n", generate_name_list_text(['abc','bc'], 3)
+      assert_equal "abc bc  c\n", generate_name_list_text(['abc','bc','c'], 3)
+      assert_equal "abc c\nbc  d\n", generate_name_list_text(['abc','bc','c','d'], 3)
   end
 
   def test_generate_name_list_text_with_japanese
+    assert_equal "人生       苦もあるさ\n", generate_name_list_text(['人生','苦もあるさ'], 3)
+    assert_equal "人生       happy.rb   苦もあるさ\n", generate_name_list_text(['人生','happy.rb','苦もあるさ'], 3)
+    assert_equal "人生       苦もあるさ happy.rb\n", generate_name_list_text(['人生','苦もあるさ','happy.rb'], 3)
     result_text = <<~TEXT
     life.md    楽ありゃ   苦もあるさ
-    人生       happy.rb             
+    人生       happy.rb
     TEXT
     assert_equal result_text, generate_name_list_text(['life.md', '人生', '楽ありゃ', 'happy.rb', '苦もあるさ'], 3)
   end
 
   def test_generate_name_list_text_with_files
     result_text = <<~TEXT
-    a_test.txt          sub.dir             試験.txt           
-    b_test.rb           テスト-ターゲット.md                     
+    a_test.txt          sub.dir             試験.txt
+    b_test.rb           テスト-ターゲット.md
     TEXT
     assert_equal result_text, generate_name_list_text(['a_test.txt', 'b_test.rb', 'sub.dir', 'テスト-ターゲット.md', '試験.txt'], 3)
   end
