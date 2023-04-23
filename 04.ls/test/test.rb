2b4e63b64cb2faafae82553d2bf25001a9fee814
kochi-echo <tnodtegrof@gmail.com>
2023-04-23 11:34:58 +0900

    テストターゲットファイル作成

--- 04.ls/test/test.rb
+++ 04.ls/test/test.rb
@@ -2,10 +2,10 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require_relative 'my-ls'
+# require_relative 'my-ls'
 
 class TestFileNameReciever < Minitest::Test
   def test_get_file_names_no_argument
-    get_file_names()
+    get_file_names('')
   end
 end
