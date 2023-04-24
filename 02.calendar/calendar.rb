f8adcd3e06916a5d7bd3aff4f9204685059f6f90
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-04-24 14:41:17 +0900

    変数名を修正、当日が一桁時の空行をrjustで対応

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,10 +9,8 @@ month = input_month.zero? ? today.month : input_month
 input_year = params["y"].to_i
 year = input_year.zero? ? today.year : input_year
 reference_date = Date.new(year, month, 1)
-
 last_date = Date.new(year, month, -1)
 first_date =  Date.new(year, month, 1)
-first_date_wday= first_date.wday
 
 puts "       #{month}月 #{year}"
 puts " 日 月 火 水 木 金 土"
@@ -22,7 +20,7 @@ printf "%#{first_empty_line}s",""
 (first_date..last_date).each do |date|
   display_day = date.mday.to_s
   if date == today
-    printf (" \e[7m#{display_day}\e[0m")
+    printf (" \e[7m#{display_day.rjust(2)}\e[0m")
   else  
     printf (display_day.rjust(3))
   end
