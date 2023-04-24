f3da815d83eb9bbbda6b26d3a2945693d9531f24
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-04-24 14:08:58 +0900

    y,mを独立させて考える、条件演算子、DateをRangeに使用

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,40 +3,30 @@
 require 'optparse'
 require 'date'
 params = ARGV.getopts("m:y:")
-select_month = params["m"].to_i
-select_year = params["y"].to_i
 today = Date.today
+input_month = params["m"].to_i
+month = input_month.zero? ? today.month : input_month
+input_year = params["y"].to_i
+year = input_year.zero? ? today.year : input_year
+reference_date = Date.new(year, month, 1)
 
-if select_month && select_year != 0
-  reference_date = Date.new(select_year,select_month,1)
-elsif (select_month != 0 ) && (select_year == 0)
-  reference_date = Date.new(today.year,select_month,1)
-else
-  reference_date = today
-end
+last_date = Date.new(year, month, -1)
+first_date =  Date.new(year, month, 1)
+first_date_wday= first_date.wday
+number_of_days =  last_date.mday
 
-display_year = reference_date.year
-display_mon = reference_date.mon
-reference_day = reference_date.mday
-reference_wday = reference_date.wday
-firstday = reference_date - reference_day + 1
-firstday_wday= firstday.wday
-lastday = Date.new(display_year,display_mon,-1)
-number_of_days =  lastday.mday
-
-puts "       #{display_mon}月 #{display_year}"
+puts "       #{month}月 #{year}"
 puts " 日 月 火 水 木 金 土"
 
-first_empty_line = firstday_wday * 3
+first_empty_line = first_date_wday * 3
 printf "%#{first_empty_line}s",""
-(1..number_of_days).each do |day|
-  days = Date.new(display_year, display_mon,day)
-  day_of_week = days.wday
-  day_empty_line = " " if day < 10
-  if days == today
-    printf("%s"," #{day_empty_line}\e[7m#{day}\e[0m")
+(first_date..last_date).each do |date|
+  day_of_week = date.wday
+  day_empty_line = " " if date.mday < 10
+  if date == today
+    printf("%s"," #{day_empty_line}\e[7m#{date.mday}\e[0m")
   else  
-    printf("%3d",day)
+    printf("%3d",date.mday)
   end
   puts "" if day_of_week == 6
 end
