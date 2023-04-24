304b5d6622168617bdc99af00cb48171f8e9b4ad
s-miura7 <s-miura7@ruby-dev.jp>
2023-04-24 14:55:45 +0900

    いらない空白や行を削除

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,17 +1,17 @@
 require 'optparse'
-require "date"
+require 'date'
 
-#コマンドラインのオプション
+# コマンドラインのオプション
 opt = OptionParser.new
-opt.on('-m') {|v| p v }
-opt.on('-y') {|v| p v }
+opt.on('-m') { |v| p v }
+opt.on('-y') { |v| p v }
 
-#デフォルトの年月
+# デフォルトの年月
 time = Time.new
 calendar_year = time.year
 calender_month = time.month
 
-#オプションがある時の年月
+# オプションがある時の年月
 unless ARGV[1].nil?
   calender_month = ARGV[1]
   unless ARGV[3].nil?
@@ -19,30 +19,28 @@ unless ARGV[1].nil?
   end
 end
 
-#月の初めの曜日
+# 月の初めの曜日
 start_of_wday = Date.new(calendar_year.to_i, calender_month.to_i, 1).wday
 
-#月の最後の日にち
+# 月の最後の日にち
 end_of_month = Date.new(calendar_year.to_i, calender_month.to_i, -1).day
 
-
-
-#レイアウト
-text = " "*start_of_wday*3
+# レイアウト
+text = ' ' * start_of_wday * 3
 1.upto(end_of_month) do |num|
-  today =  calendar_year == time.year && calender_month == time.month && num == time.day
-  text += "\n" if (num + start_of_wday -1) % 7 == 0
-  text += "\e[37m\e[40m"   if today
-  text += " " if num.to_s.length == 1
+  today = calendar_year == time.year && calender_month == time.month && num == time.day
+  text += "\n" if ((num + start_of_wday - 1) % 7).zero
+  text += "\e[37m\e[40m" if today
+  text += ' ' if num.to_s.length == 1
   text += num.to_s
-  text += "\e[0m"   if today
-  text += " "
+  text += "\e[0m" if today
+  text += ' '
 end
 
-#曜日
-wdays = ["日", "月", "火", "水", "木", "金", "土"]
+# 曜日
+wdays = ['日', '月', '火', '水', '木', '金', '土']
 
-#表示
+# 表示
 puts "      #{calender_month}月 #{calendar_year}"
 puts wdays.join(' ')
 printf text += "\n"
