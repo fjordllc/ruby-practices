b39d71eb2215157cdb85b4edae97e5b8093f23ac
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-04-25 13:35:17 +0900

    変数名変更display_day→display_date

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -19,11 +19,11 @@ puts " 日 月 火 水 木 金 土"
 padding_size_for_first_date = first_date.wday * 3
 print " "  * padding_size_for_first_date,""
 (first_date..last_date).each do |date|
-  display_day = date.mday.to_s
+  display_date = date.mday.to_s
   if date == today
-    print (" \e[7m#{display_day.rjust(2)}\e[0m")
+    print (" \e[7m#{display_date.rjust(2)}\e[0m")
   else  
-    print (display_day.rjust(3))
+    print (display_date.rjust(3))
   end
   puts "" if date.wday == 6
 end
