d6810487f49abc8ab6a5c9b0bb7c66655f12ddd7
s-miura7 <s-miura7@ruby-dev.jp>
2023-04-25 10:48:56 +0900

    uptoをeachに変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,7 +2,6 @@ require 'optparse'
 require 'date'
 
 # コマンドラインのオプション
-
 opts = OptionParser.new
 ProgramConfig = Hash.new
 opts.on('-m value') { |v| ProgramConfig[:m] = v }
@@ -19,18 +18,18 @@ calendar_month = ProgramConfig[:m].to_i unless ProgramConfig[:m].nil?
 calendar_year = ProgramConfig[:y].to_i unless ProgramConfig[:y].nil?
 
 # 月の初めの曜日
-start_of_wday = Date.new(calendar_year, calendar_month, 1).wday
+start_wday = Date.new(calendar_year, calendar_month, 1).wday
 
 # 月の最後の日にち
-end_of_month = Date.new(calendar_year, calendar_month, -1).day
+last_date = Date.new(calendar_year, calendar_month, -1).day
 
 # レイアウト
-text = ' ' * start_of_wday * 3
-1.upto(end_of_month) do |num|
-  today = calendar_year == current_time.year && calendar_month == current_time.month && num == current_time.day
-  text += "\n" if ((num + start_of_wday - 1) % 7).zero?
+text = ' ' * start_wday * 3
+(1..last_date).each do |date|
+  today = calendar_year == current_time.year && calendar_month == current_time.month && date == current_time.day
+  text += "\n" if ((date + start_wday - 1) % 7).zero?
   text += "\e[37m\e[40m" if today
-  text += num.to_s.length == 1 ? num.to_s.rjust(2) : num.to_s
+  text += date.to_s.length == 1 ? date.to_s.rjust(2) : date.to_s
   text += "\e[0m" if today
   text += ' '
 end
