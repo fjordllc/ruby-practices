f560622b00f299397649b3f82a958a66afb94c69
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-04-26 16:54:59 +0900

    ボウリングスコア計算プラクティスの提出について

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |shot|
+  if shot == 'X'
+    shots << 10
+    shots << 0 # ストライク時の２投目を仮登録
+  else
+    shots << shot.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2) do |frame|
+  frames << frame
+end
+
+frames.map do |frame|
+  frame.pop if frame[0] == 10 # 仮登録したストライク時の２投目を削除
+end
+
+point = 0
+frames.each_with_index do |frame, index|
+  point += if index <= 8 && frame.length == 2 && frame.sum == 10 # スペア
+             (10 + frames.dig(index + 1, 0))
+           elsif index <= 8 && frame[0] == 10 # ストライク
+             if frames[index + 1].length == 2 # 次投がストライク以外
+               (10 + frames[index + 1].sum)
+             else
+               (10 + 10 + frames.dig(index + 2, 0)) # 次投がストライク
+             end
+           else
+             frame.sum
+           end
+end
+
+puts point
