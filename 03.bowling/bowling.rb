0965d304c8822cdd67a8c5d51483222b33068056
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-04-26 14:27:46 +0900

    変更を追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,7 +3,7 @@
 user_inputs = ARGV[0].split(',')
 
 # 全て数字に変える
-converted_nums = user_input.map { |score| score == 'X' ? 10 : score.to_i }
+converted_nums = user_inputs.map { |score| score == 'X' ? 10 : score.to_i }
 
 # フレームごとに区切る
 current_throwing_location = 0
@@ -11,14 +11,14 @@ current_throwing_location = 0
 score = 0
 
 10.times do
-  score_of_two = converted_num[current_throwing_location] + converted_num[current_throwing_location + 1]
+  score_of_two = converted_nums[current_throwing_location] + converted_nums[current_throwing_location + 1]
   # ストライク
-  if converted_num[current_throwing_location] == 10
-    score += score_of_two + converted_num[current_throwing_location + 2]
+  if converted_nums[current_throwing_location] == 10
+    score += score_of_two + converted_nums[current_throwing_location + 2]
     current_throwing_location += 1
   # スペア
   elsif score_of_two == 10
-    score += score_of_two + converted_num[current_throwing_location + 2]
+    score += score_of_two + converted_nums[current_throwing_location + 2]
     current_throwing_location += 2
   # 9本以下
   else
