c85773d1d80bdce8c2d109f90a73debd225f6c45
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-04-27 09:21:27 +0900

    変数の名前を変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-user_inputs = ARGV[0].split(',')
+inputs = ARGV[0].split(',')
 
 # 全て数字に変える
-converted_nums = user_inputs.map { |score| score == 'X' ? 10 : score.to_i }
+to_i_inputs = inputs.map { |input| input == 'X' ? 10 : input.to_i }
 
 # フレームごとに区切る
-current_throwing_location = 0
+current_input_location = 0
 
 score = 0
 
 10.times do
-  score_of_two = converted_nums[current_throwing_location] + converted_nums[current_throwing_location + 1]
+  score_of_two = to_i_inputs[current_input_location] + to_i_inputs[current_input_location + 1]
   # ストライク
-  if converted_nums[current_throwing_location] == 10
-    score += score_of_two + converted_nums[current_throwing_location + 2]
-    current_throwing_location += 1
+  if to_i_inputs[current_input_location] == 10
+    score += score_of_two + to_i_inputs[current_input_location + 2]
+    current_input_location += 1
   # スペア
   elsif score_of_two == 10
-    score += score_of_two + converted_nums[current_throwing_location + 2]
-    current_throwing_location += 2
+    score += score_of_two + to_i_inputs[current_input_location + 2]
+    current_input_location += 2
   # 9本以下
   else
     score += score_of_two
-    current_throwing_location += 2
+    current_input_location += 2
   end
 end
 
