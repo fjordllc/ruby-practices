6839e937e4fa5e5368d89abfe0083ec8ad1eec0d
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-04-28 15:29:19 +0900

    ディレクトリを指定可能

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,14 +3,17 @@
 
 columns = 3
 
+# 仮の配列
 temporary_outputs = []
 
 # ファイル名の最大文字数
 max_file_length = 0
 
+file = ARGV[0] || '.'
+
 # ファイルの取得
-Dir.foreach('./outputs') do |item|
-  next if item.include?('.') || item.include?('..')
+Dir.foreach(file) do |item|
+  next if item.eql?('.') || item.eql?('..')
 
   temporary_outputs << item
   max_file_length = item.length if max_file_length < item.length
@@ -19,14 +22,14 @@ end
 # 一列に出力するファイルの数
 max_size = temporary_outputs.length / columns + 1
 
+# 出力する配列
 outputs = Array.new(columns) { [] }
 
-count = 0
-
 # ファイルの並び替えと二次元配列に変える
+array_num = 0
 temporary_outputs.sort.each do |item|
-  outputs[count].push(item)
-  count += 1 if (outputs[count].length % max_size).zero?
+  outputs[array_num].push(item)
+  array_num += 1 if (outputs[array_num].length % max_size).zero?
 end
 
 # 出力
