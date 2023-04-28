7c7a4dfb823a5f1e140cbdab7e1ebf579448e407
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-04-28 13:16:56 +0900

    整理

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,30 +1,39 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-outputs = [[], [], []]
-temporary_outputs = []
-
-Dir.foreach('./outputs') do |item|
-  next if item.include('.') || item.include('..')
-
-  temporary_outputs << item
-end
-
 columns = 3
 
-size = temporary_outputs.length / columns + 1
+temporary_outputs = []
+
+# ファイル名の最大文字数
+max_file_length = 0
+
+# ファイルの取得
+Dir.foreach('./outputs') do |item|
+  next if item.include?('.') || item.include?('..')
+
+  temporary_outputs << item
+  max_file_length = item.length if max_file_length < item.length
+end
+
+# 一列に出力するファイルの数
+max_size = temporary_outputs.length / columns + 1
+
+outputs = Array.new(columns) { [] }
 
 count = 0
 
+# ファイルの並び替えと二次元配列に変える
 temporary_outputs.sort.each do |item|
   outputs[count].push(item)
-  count += 1 if (outputs[count].length % size).zero?
+  count += 1 if (outputs[count].length % max_size).zero?
 end
 
-size.times do |time|
+# 出力
+max_size.times do |time|
   columns.times do |column|
     print outputs[column][time]
-    print outputs[column][time].to_s.length == 1 ? '  ' : ' '
+    print ' ' * (max_file_length - outputs[column][time].to_s.length + 1)
   end
-  puts '\n'
+  puts "\n"
 end
