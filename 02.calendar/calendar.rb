4f0a83576b3683a19f66a8df7b3262a0f67c1f4c
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-07 17:44:27 +0900

    カレンダーの色付け。今日の日付の色付け。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,6 +2,7 @@ require 'date'
 day = Date.today
 year = day.year
 month = day.month
+today = day.day
 require 'optparse'
 opt = OptionParser.new
 opt.on('-y VAL') {|v| year = v.to_i }
@@ -11,8 +12,8 @@ opt.parse!(ARGV)
 # まずはopt.parse!(ARGV)を読み込まないと3〜４行目は動かない。
 # p ARGV
 # ARGVはただ入れた情報を読み込んでそのままarrayとして入れているだけのもの。opt.parse!で動くようになる。
-print "#{month}月"
-puts " #{year}"
+print "\x1B[36;1m#{month}\x1B[37;m月"
+puts " \x1B[36;1m#{year}\x1B[37;m"
 puts "日 月 火 水 木 金 土"
 end_of_date = Date.new(year, month, -1).day
 #-1は一番最後の日にち取得
@@ -33,14 +34,16 @@ num = 0
 while num < end_of_date do
   num += 1
   case 
+  when num == today
+    print "\x1B[31;1m#{num}\x1B[37;m".rjust(15)
   when (youbi)%7 == 0 && num < 10
-      print "#{num}".rjust(2)
+    print "\x1B[36;1m#{num}\x1B[37;m".rjust(15)
   #曜日が０かつ、数字が１０未満だったら
   when (youbi)%7 == 0 
-      print num
+    print "\x1B[36;1m#{num}\x1B[37;m"
   #曜日が０だったら
   else 
-      print " " + "#{num}".rjust(2)
+    print " " + "\x1B[36;1m#{num}\x1B[37;m".rjust(15)
   end
   if ((youbi+=1)%7 == 0)
       #曜日は0〜６までなので、numにプラス１することで、７の倍数が作れる。
