003729ea466e0cf7858d26e130f1ba4bef178e72
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2023-05-11 14:53:56 +0900

    I fixed fizzbuzz practice

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,16 @@
 fizzbuzz = 0
 while fizzbuzz < 20
-    fizzbuzz = fizzbuzz + 1
-    if fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0
-        puts "Fizzbuzz"
+  fizzbuzz = fizzbuzz + 1
+  if fizzbuzz % 15 == 0
+    puts "Fizzbuzz"
+  end
+  if fizzbuzz % 3 == 0 && fizzbuzz % 5 != 0
+    puts "Fizz"
     end
-    if fizzbuzz % 3 == 0 && fizzbuzz % 5 != 0
-        puts "Fizz"
-    end
-    if fizzbuzz % 3 != 0 && fizzbuzz % 5 == 0
-        puts "Buzz"
-    end
-    if fizzbuzz % 3 != 0 && fizzbuzz % 5 != 5
-        puts fizzbuzz
-    end
+  if fizzbuzz % 3 != 0 && fizzbuzz % 5 == 0
+    puts "Buzz"
+  end
+  if fizzbuzz % 3 != 0 && fizzbuzz % 5 != 0
+    puts fizzbuzz
+  end
 end
