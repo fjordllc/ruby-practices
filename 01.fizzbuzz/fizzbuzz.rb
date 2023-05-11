ccf4156336dde24ac5accf313a8a3e3ed93f84d9
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-05-11 16:22:51 +0900

    15の倍数でFizzBuzzを表示させる機能を追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,13 @@
 range = 1..20  #表示される数字の範囲を設定
 
 #数字を順番に表示
+#3の倍数かつ5の倍数(即ち15の倍数)なら"FizzBuzz"を表示
 #3の倍数なら"Fizz"を表示
 #5の倍数なら"Buzz"を表示
 range.each do |n|
-  if n % 3 == 0
+  if n % 15 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
     puts "Fizz"
   elsif n % 5 == 0
     puts "Buzz"
