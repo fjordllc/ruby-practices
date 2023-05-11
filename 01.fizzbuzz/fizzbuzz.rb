0e3cc0821cc257033c2f737ae8d037333b0c8ecf
kuma-900 <abcyoc69@gmail.com>
2023-05-11 19:34:29 +0900

    Add fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+num = (1..20)
+
+num.each do |x|
+    if x%3 == 0 && x%5 == 0
+        puts "FizzBuzz"
+    elsif x%3 == 0
+        puts "Fizz"
+    elsif x%5 == 0
+        puts "Buzz"
+    else
+        puts x
+    end
+end
