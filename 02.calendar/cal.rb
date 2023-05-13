c1d40c95279663fce214618a1903204c9905dbb6
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-05-14 02:12:15 +0900

    cal.rbに今日の日付の色を変更する機能を追加
    cal.rbに以下の機能を追加しました。
    出力する年月が今日の年月と一致している場合は、今日の日付の数字とその背景を反転させる

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,10 +5,10 @@ require 'optparse'
 today = Date.today
 
 #出力する日付データを持たせるハッシュ変数「date」を作成 
-#初期値として今日の日付を代入
+#初期値として今日の年月日を代入
 date = {year:today.year, month:today.mon, day:today.mday} 
 
-#オプション引数で得た月と年を出力する日付dateに設定
+#オプション引数で得た月と年を出力する年月に設定
 opt = OptionParser.new
 opt.on('-m VAL',Integer) {|m| date[:month]= m}
 opt.on('-y VAL',Integer) {|y| date[:year] = y}
@@ -19,6 +19,16 @@ last_day = Date.new(date[:year],date[:month],-1).mday
 
 #出力する年月の初日の曜日の整数値を「first_yobi」に代入
 first_yobi = Date.new(date[:year],date[:month],1).wday
+
+#出力する年月が
+#今日の年月と一致していたら「color_on_date」に今日の日付(整数)を代入、
+#今日の年月と一致していなければ「color_on_date」にfalseを代入
+if date[:year] == today.year && date[:month] == today.mon
+  color_on_date = today.mday
+else
+  color_on_date = false
+end
+
 #月と年を出力
 puts "      #{date[:month]}月 #{date[:year]}"
 
@@ -31,8 +41,16 @@ print "   " * first_yobi
 #日付を出力
 count = first_yobi
 (1..last_day).each do |day| 
-  print "#{day}".rjust(2)+" "
-  count += 1
+  if color_on_date == day # color_on_dateがdayと一致していたら色反転
+    print "\e[30m\e[47m"
+    print "#{day}".rjust(2)
+    print "\e[0m"
+    print " "
+    count += 1
+  else
+    print "#{day}".rjust(2)+" "
+    count += 1
+  end
   if count >= 7
     print "\n"
     count = 0
