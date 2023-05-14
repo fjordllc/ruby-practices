0533535f04f319301959350d05c8dad54ae50bdd
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2023-05-14 21:22:51 +0900

    I used elsif instead of if

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -3,14 +3,11 @@ while fizzbuzz < 20
   fizzbuzz = fizzbuzz + 1
   if fizzbuzz % 15 == 0
     puts "Fizzbuzz"
-  end
-  if fizzbuzz % 3 == 0 && fizzbuzz % 5 != 0
+  elsif fizzbuzz % 3 == 0 && fizzbuzz % 5 != 0
     puts "Fizz"
-    end
-  if fizzbuzz % 3 != 0 && fizzbuzz % 5 == 0
+  elsif fizzbuzz % 3 != 0 && fizzbuzz % 5 == 0
     puts "Buzz"
-  end
-  if fizzbuzz % 3 != 0 && fizzbuzz % 5 != 0
+  else fizzbuzz % 3 != 0 && fizzbuzz % 5 != 0
     puts fizzbuzz
   end
 end
