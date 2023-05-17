a6b3a6ed4ec55adf35c8e7ff5bdfdda89f0675d6
ktgwShota <ktgw.shota.1998@gmail.com>
2023-05-17 15:07:19 +0900

    PRに対する修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -12,50 +12,30 @@ opt.parse!(ARGV)
 option_year = options[:year] || Date.today.year
 option_month = options[:month] || Date.today.month
 
-def date(year = Date.today.year, month = Date.today.month)
-    month_frist_day = Date.new(year, month)
-    month_last_day = Date.new(year, month, -1)
-    year = Date.new(year).year
-    month = Date.new(year, month).month
-    today =  Date.today
-    return month_frist_day, month_last_day, year.to_s, month.to_s, today.day.to_s
-end
+month_first_day = Date.new(option_year, option_month)
+month_last_day = Date.new(option_year, option_month, -1)
+year = Date.new(option_year).year
+month = Date.new(option_year, option_month).month
+today = Date.today
+  
+puts "      #{month}月 #{year}" 
+puts " 日 月 火 水 木 金 土"
 
-date = date(option_year,option_month) 
-month_frist_day = date[0]
-month_last_day = date[1]
-year = date[2]
-month = date[3]
-today = date[4]
-
-puts "      " + month + "月 " + year
-puts " 日" + " 月" + " 火" + " 水" + " 木" + " 金" + " 土"
-(month_frist_day..month_last_day).each do |date|
-    if date.day != 1 && date.sunday? 
-        puts "\n"
+(month_first_day..month_last_day).each do |date|
+  for i in 1..6
+    if date.day == 1 && date.wday == i
+      print ' ' * i * 3
     end
-
-    if date.day == 1 && date.monday?
-        print "   "
-    elsif date.day == 1 && date.tuesday?
-        print "      "
-    elsif date.day == 1 && date.wednesday?
-        print "         "
-    elsif date.day == 1 && date.thursday?
-        print "            "
-    elsif date.day == 1 && date.friday?
-        print "               "
-    elsif date.day == 1 && date.saturday?
-        print "                  "
-    end
-
-    if date.day == today.to_i
-        print " " + "\e[47m\e[30m" + today + "\e[0m"
-    elsif date.day.to_s.length == 1 
-        print "  " + date.day.to_s
-    elsif date.day.to_s.length == 2
-        print  " " + date.day.to_s
-    end
+  end 
+  if date.day == today.day
+    print " \e[47m\e[30m#{today.day.to_s.rjust(2)}\e[0m"
+  else
+    print " #{date.day.to_s.rjust(2)}"
+  end 
+  if date.saturday?
+    print "\n"
+  end
 end
 
 puts "\n"
+   
