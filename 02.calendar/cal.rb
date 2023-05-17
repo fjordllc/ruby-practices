6da0b82c6199f4a4f2d7400c00977b02e3efdd3c
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-05-17 10:25:59 +0900

    cal.rbの各処理をメソッド化
    cal.rbを以下のように変更しました
    オプションと今日の日付から出力する年月を決定する処理をidentify_tgt_dateメソッドとして定義
    カレンダー出力に必要な情報に加工する処理をprocessing_dateメソッドとして定義
    カレンダーを出力する処理をoutput_calメソッドとして定義

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,61 +1,78 @@
 require 'date'
 require 'optparse'
 
-#今日のDateオブジェクトを変数todayに代入
+#今日の日付とオプション入力から出力する年月を決定するメソッド
+def identify_tgt_date(today)
+  #出力する日付データを持たせるハッシュ変数「date」を作成 
+  #初期値として今日の年月日を代入
+  date = {year:today.year, month:today.mon, day:today.mday} 
+
+  #オプション引数が指定された場合、その値を月と年を出力する年月に設定
+  opt = OptionParser.new
+  opt.on('-m VAL',Integer) {|m| date[:month]= m}
+  opt.on('-y VAL',Integer) {|y| date[:year] = y}
+  opt.parse(ARGV)
+
+  return date
+end
+
+
+
+#日付ハッシュと今日のDateオブジェクトから、カレンダー出力に必要な情報へ加工するメソッド
+def processing_date (date:, today:)
+  info_for_cal = {}
+  info_for_cal[:month] = date[:month]
+  info_for_cal[:year] = date[:year]
+
+  #出力する年月の初日の曜日を整数値でfirst_yobiに設定
+  info_for_cal[:first_yobi] = Date.new(date[:year],date[:month],1).wday
+
+  #出力する年月の末日の数値をlast_dayに設定
+  info_for_cal[:last_day]= Date.new(date[:year],date[:month],-1).mday
+
+  #出力する年月が
+  #今日の年月と一致していたら「color_inv_date」に今日の日付(整数)を代入、
+  #今日の年月と一致していなければ「color_inv_date」にnilを代入
+  if date[:year] == today.year && date[:month] == today.mon
+    info_for_cal[:color_inv_date] = today.mday
+  else
+    info_for_cal[:color_inv_date] = nil
+  end
+  
+  return info_for_cal
+end
+
+#カレンダーを出力するメソッド
+def output_cal(info_for_cal)
+  #曜日の名称を出力
+  puts "      #{info_for_cal[:month]}月 #{info_for_cal[:year]}"
+  puts "日 月 火 水 木 金 土"
+  #初日の曜日に応じて日付の出力開始位置を調整
+  print "   " * info_for_cal[:first_yobi]
+  count = info_for_cal[:first_yobi] #改行位置を決めるための変数countを設定
+  (1..info_for_cal[:last_day]).each do |day| 
+    if info_for_cal[:color_inv_date] == day #color_inv_dateがdayと一致していたら色反転
+      print "\e[30m\e[47m"
+      print "#{day}".rjust(2)
+      print "\e[0m"
+      print " "
+      count += 1
+    else
+      print "#{day}".rjust(2)+" "
+      count += 1
+    end
+    if count >= 7
+      print "\n"
+      count = 0
+    end
+  end
+  puts "" #なぜか最後に%が表示されてしまう問題への暫定策
+end
+
+
+#mainプログラム
 today = Date.today
 
-#出力する日付データを持たせるハッシュ変数「date」を作成 
-#初期値として今日の年月日を代入
-date = {year:today.year, month:today.mon, day:today.mday} 
-
-#オプション引数で得た月と年を出力する年月に設定
-opt = OptionParser.new
-opt.on('-m VAL',Integer) {|m| date[:month]= m}
-opt.on('-y VAL',Integer) {|y| date[:year] = y}
-opt.parse(ARGV)
-
-#出力する年月の日数を「last_day」に代入
-last_day = Date.new(date[:year],date[:month],-1).mday
-
-#出力する年月の初日の曜日の整数値を「first_yobi」に代入
-first_yobi = Date.new(date[:year],date[:month],1).wday
-
-#出力する年月が
-#今日の年月と一致していたら「color_on_date」に今日の日付(整数)を代入、
-#今日の年月と一致していなければ「color_on_date」にfalseを代入
-if date[:year] == today.year && date[:month] == today.mon
-  color_on_date = today.mday
-else
-  color_on_date = false
-end
-
-#月と年を出力
-puts "      #{date[:month]}月 #{date[:year]}"
-
-#曜日の名称を出力
-yobi_name_list = ["日 ", "月 ", "火 ", "水 ", "木 ", "金 ", "土\n"]
-yobi_name_list.each {|day_of_the_week|print day_of_the_week}
-
-#初日の曜日に応じて日付の出力開始位置を調整
-print "   " * first_yobi
-#日付を出力
-count = first_yobi
-(1..last_day).each do |day| 
-  if color_on_date == day # color_on_dateがdayと一致していたら色反転
-    print "\e[30m\e[47m"
-    print "#{day}".rjust(2)
-    print "\e[0m"
-    print " "
-    count += 1
-  else
-    print "#{day}".rjust(2)+" "
-    count += 1
-  end
-  if count >= 7
-    print "\n"
-    count = 0
-  end
-end
-
-#なぜか最後に%が表示されてしまう問題への暫定策
-puts ""
+tgt_date = identify_tgt_date(today)
+info_for_cal = processing_date(date:tgt_date,today:today)
+output_cal(info_for_cal)
