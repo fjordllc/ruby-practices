944169f650900cdbb67f8eeefe1bd77354dc831e
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2023-05-17 14:00:23 +0900

    Delete unnecessary conditional expression

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -3,11 +3,11 @@ while fizzbuzz < 20
   fizzbuzz = fizzbuzz + 1
   if fizzbuzz % 15 == 0
     puts "Fizzbuzz"
-  elsif fizzbuzz % 3 == 0 && fizzbuzz % 5 != 0
+  elsif fizzbuzz % 3 == 0
     puts "Fizz"
-  elsif fizzbuzz % 3 != 0 && fizzbuzz % 5 == 0
+  elsif fizzbuzz % 5 == 0
     puts "Buzz"
-  else fizzbuzz % 3 != 0 && fizzbuzz % 5 != 0
+  else
     puts fizzbuzz
   end
 end
