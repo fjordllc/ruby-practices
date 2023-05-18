4fc65c377bfb3adf4755f26ce62efd4450bfce80
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-05-18 22:47:06 +0900

    lsコマンドプラクティスを提出

--- 04.ls/my_ls.rb
+++ 04.ls/my_ls.rb
@@ -5,29 +5,31 @@ def line_up_files
   files_with_space = gets_files
   column = 3
   files_number = files_with_space.count
+  rows = files_number / column
   if files_number % column != 0
     (column - files_number % column).times { files_with_space << '' }
-    rows = files_number / column + 1
-  else
-    rows = files_number / column
+    rows += 1
   end
+  out_put(files_with_space, rows)
+end
 
+def gets_files
+  files = Dir.glob('*')
+  filename_max_length = files.map(&:size).max + 7
+  files.map do |file|
+    file + ' ' * (filename_max_length - file.size)
+  end
+end
+
+def out_put(files, rows)
   output_files = []
-  files_with_space.each_slice(rows) do |file|
+  files.each_slice(rows) do |file|
     output_files << file
   end
 
   output_files.transpose.each do |file|
     file.each { |filename| print filename }
     puts "\n"
-  end
-end
-
-def gets_files
-  files = Dir.glob('*')
-  filename_max_length = files.map(&:size).max + 4
-  files.map do |file|
-    file + ' ' * (filename_max_length - file.size)
   end
 end
 
