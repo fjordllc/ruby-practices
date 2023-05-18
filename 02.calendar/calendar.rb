f5b2f084743443f6f5fcd903bdcdbd93a5c721e0
ktgwShota <ktgw.shota.1998@gmail.com>
2023-05-18 15:04:58 +0900

    コメントに対する修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -7,24 +7,24 @@ opt.on('-m MONTH') { |v| options[:month] = v.to_i }
 opt.on('-y YEAR') { |v| options[:year] = v.to_i }
 opt.parse!(ARGV)
 
-year = options[:year] || Date.today.year
-month = options[:month] || Date.today.month
 today = Date.today
-first_day = Date.new(year, month)
-last_day = Date.new(year, month, -1)
+month = options[:month] || today.month
+year = options[:year] || today.year
+first_date = Date.new(year, month)
+last_date = Date.new(year, month, -1)
 
 puts "      #{month}月 #{year}年" 
 puts " 日 月 火 水 木 金 土"
 
 print ' ' * (first_day.wday * 3)
   
-(first_day..last_day).each do |day|
-  if day == today
+(first_date..last_date).each do |date|
+  if date == today
     print " \e[47m\e[30m#{today.day.to_s.rjust(2)}\e[0m"
   else
-    print " #{day.day.to_s.rjust(2)}"
+    print " #{date.day.to_s.rjust(2)}"
   end 
-  if day.saturday?
+  if date.saturday?
     print "\n"
   end
 end
