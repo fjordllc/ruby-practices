d2bde688c8e4a14eea964c4af1f074db49454c83
h-osawa <h-osawa@ruby-dev.jp>
2023-05-18 10:26:43 +0900

    ls.rbの修正1

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,43 +4,54 @@
 OUTPUT_COLUMN_NUMBER = 3
 
 def main
-  path = ARGV[0] || '.'
-  files = create_file_list(path)
+  files, max_filename_length = make_files
+  output_files(files, max_filename_length)
+end
+
+def make_files
+  absolute_path = make_absolute_path
+  files = create_file_list(absolute_path)
   sorted_files = files.compact.sort
   aligned_files = align_files(sorted_files)
   two_dimensional_files = make_two_dimensional_array(aligned_files)
-  max_file_length = generate_max_file_length(two_dimensional_files)
+  max_filename_length = generate_max_filename_length(two_dimensional_files)
   transposed_files = two_dimensional_files.transpose
-  output_files(transposed_files, max_file_length)
+  [transposed_files, max_filename_length]
 end
 
-def create_file_list(path)
-  Dir.open(path).each_child.map do |filename|
-    filename unless filename[0] == '.'                                            # 隠しファイルの除去
+def make_absolute_path
+  File.expand_path(ARGV[0] || '.')
+end
+
+def create_file_list(absolute_path)
+  Dir.chdir(absolute_path)
+  Dir.glob('*').map.to_a do |filename|
+    filename unless filename.chars.first == '.'
   end
 end
 
 def align_files(sorted_files)
-  sorted_files.push(' ') until (sorted_files.length % OUTPUT_COLUMN_NUMBER).zero? # 要素数の調整のための追加
+  sorted_files.push(' ') until (sorted_files.length % OUTPUT_COLUMN_NUMBER).zero?   # 要素数調整のため空白を追加
   sorted_files
 end
 
 def make_two_dimensional_array(aligned_files)
-  aligned_files.each_slice(aligned_files.length / OUTPUT_COLUMN_NUMBER).to_a      # 二次元配列に変換
+  aligned_files.each_slice(aligned_files.length / OUTPUT_COLUMN_NUMBER).to_a
 end
 
-def generate_max_file_length(two_dimensional_files)
-  max_file_length = {}
-  two_dimensional_files.each_with_index do |files, files_index|
-    max_file_length.store(files_index, files.map(&:length).max)                   # ハッシュに各列の最大文字数を格納
+def generate_max_filename_length(two_dimensional_files)
+  max_filename_length = 0
+  two_dimensional_files.each do |files|
+    filename_length = files.map(&:length).max
+    max_filename_length = filename_length if max_filename_length < filename_length  # ハッシュに各列の最大文字数を格納
   end
-  max_file_length
+  max_filename_length
 end
 
 def output_files(output_files, max_file_length)
   output_files.each do |files|
-    files.each_with_index do |file, file_index|
-      print file.ljust(max_file_length[file_index % OUTPUT_COLUMN_NUMBER] + 2)    # 最大文字数+2を出力
+    files.each do |file|
+      print file.ljust(max_file_length + 2)
     end
     print "\n"
   end
