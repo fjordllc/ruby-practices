ce79257fb3fd554bd1efb197553390422c3d66da
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-05-19 19:31:32 +0900

    bowling.rbを旧ルール仕様に変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,18 +12,55 @@ end
 
 frames = []
 shots.each_slice(2) do |s|
+  s << 0 if s.size == 1
   frames << s
 end
 
-point = 0
 frames.each do |frame|
   if frame[0] == 10
-    point += 30
+   frame << {sum: 10, mark: :strike}
   elsif frame.sum == 10
-    point += frame[0] + 10
+    frame << {sum: 10, mark: :spare}
   else
-    point += frame.sum
+    frame << {sum: frame.sum, mark: :none}
   end
 end
 
+(0..8).each do |number|
+  current= frames[number]
+  one_ahead=frames[number+1]
+  two_ahead= frames[number+2]
+  if current[2][:mark] == :strike
+    if one_ahead[2][:mark] == :strike
+      current[2][:sum] += 10 + two_ahead[0]
+    else 
+      current[2][:sum] += one_ahead[0]+one_ahead[1]
+    end
+  elsif current[2][:mark] == :spare
+    current[2][:sum] += one_ahead[0]
+  end
+end
+
+# if frames[10]
+#   frames[9][2][:sum] +=[] 
+# end
+
+point = 0
+frames.each do |frame|
+  point += frame[2][:sum]
+end
+
 puts point
+
+# point = 0
+# frames.each do |frame|
+#   if frame[0] == 10
+#     point += 30
+#   elsif frame.sum == 10
+#     point += frame[0] + 10
+#   else
+#     point += frame.sum
+#   end
+# end
+
+# puts point
