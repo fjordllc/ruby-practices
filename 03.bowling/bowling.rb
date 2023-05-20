d79f26f077bd1f8328cf5d3d53e6136760c7ca8f
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-05-20 18:33:07 +0900

    bowling.rbにrubocopをかけて、修正をしました

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == 'X' 
+  if s == 'X'
     shots << 10
     shots << 0
   else
     shots << s.to_i
   end
-end 
+end
 # ここまで新ルールと同様
 
-#配列shotsを2つずつに分割した配列sをつくる
-#配列framesに、キーを:score,値を配列sとしたハッシュを追加していく
-#第10フレームでストライクかスコアがあると、framesの要素数が11もしくは12になる
+# 配列shotsを2つずつに分割した配列sをつくる
+# 配列framesに、キーを:score,値を配列sとしたハッシュを追加していく
+# 第10フレームでストライクかスコアがあると、framesの要素数が11もしくは12になる
 frames = []
 shots.each_slice(2) do |s|
-  frames << {score: s}
+  frames << { score: s }
 end
 
-#framesの各要素の:scoreの値から:sumと:markの値を決定してハッシュに追加
-#ストライクかスペアの際は:sumを暫定的に10とする
+# framesの各要素の:scoreの値から:sumと:markの値を決定してハッシュに追加
+# ストライクかスペアの際は:sumを暫定的に10とする
 frames.each do |frame|
   if frame[:score][0] == 10
     frame[:sum] = 10
@@ -30,33 +32,31 @@ frames.each do |frame|
     frame[:mark] = :spare
   else
     frame[:sum] = frame[:score].sum
-    frame[:mark] = :none 
+    frame[:mark] = :none
   end
 end
 
-#1フレームから9フレームにおいて、ストライクとスペアのフレームの:sumを再計算
-#ストライクで、かつ次のフレームもストライクであれば「10 + 2つ次のフレームの1投目」を加算
-#ストライクで、次のフレームがストライクでなければ、次のフレームの合計値を加算
-#スペアであれば、次のフレームの1投目を加算
-(0..8).each do |n|
-  current= frames[n]
-  one_ahead=frames[n+1]
-  two_ahead= frames[n+2]
+# 1フレームから9フレームにおいて、ストライクとスペアのフレームの:sumを再計算
+# ストライクで、かつ次のフレームもストライクであれば「10 + 2つ次のフレームの1投目」を加算
+# ストライクで、次のフレームがストライクでなければ、次のフレームの合計値を加算
+# スペアであれば、次のフレームの1投目を加算
+9.times do |n|
+  current = frames[n]
+  one_ahead = frames[n + 1]
+  two_ahead = frames[n + 2]
   if current[:mark] == :strike
     if one_ahead[:mark] == :strike
       current[:sum] += 10 + two_ahead[:score][0]
-    else 
+    else
       current[:sum] += one_ahead[:score].sum
     end
   elsif current[:mark] == :spare
     current[:sum] += one_ahead[:score][0]
-  else
   end
 end
-#10フレーム目の得点はframesの10番目以降の要素の:sumの合計と等しくなるので再計算は不要
+# 10フレーム目の得点はframesの10番目以降の要素の:sumの合計と等しくなるので再計算は不要
 
-
-#framesの全ての要素の:sumを合計してpointとする
+# framesの全ての要素の:sumを合計してpointとする
 point = 0
 frames.each do |frame|
   point += frame[:sum]
