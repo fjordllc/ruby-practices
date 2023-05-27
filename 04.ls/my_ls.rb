3a7347ce7da66d66101983befc4cbf193eae4d2e
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-05-27 21:52:14 +0900

    メソッド名修正。戻り値利用を変数利用に修正

--- 04.ls/my_ls.rb
+++ 04.ls/my_ls.rb
@@ -3,14 +3,14 @@
 
 COLUMN_NUMBER = 3
 def line_up_files
-  array_of_files = files_with_space
-  files_number = files_with_space.count
+  files = files_with_space
+  files_number = files.count
   rows = files_number / COLUMN_NUMBER
   if files_number % COLUMN_NUMBER != 0
-    (COLUMN_NUMBER - files_number % COLUMN_NUMBER).times { array_of_files << '' }
+    (COLUMN_NUMBER - files_number % COLUMN_NUMBER).times { files << '' }
     rows += 1
   end
-  output(array_of_files, rows)
+  output(files, rows)
 end
 
 def files_with_space
