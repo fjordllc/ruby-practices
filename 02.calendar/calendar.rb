da8f386a4ad9239cbe97d5c27b8c79c30ea79c1a
oyuuhan_naninan <yumikoohta0710@gmail.com>
2023-05-28 19:55:58 +0900

    カレンダーを作成しました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,17 +1,44 @@
+#!/usr/bin/env ruby
+
 require 'optparse'
 require 'date'
 
-todaymonth = Date.today.month
-todayyear = Date.today.year
-puts "      " + todaymonth.to_s + "月" + " " + todayyear.to_s
-japanese_wdays = ["日", "月", "火", "水", "木", "金", "土"]
-puts japanese_wdays.join(" ")
+option ={}
+OptionParser.new do |opt|
+  opt.on('-m value'){|n| option[:m] = n}
+  opt.on('-y value'){|n| option[:y] = n}
+  opt.parse!(ARGV)
+end
 
-# 表示する年月を取得
+if option[:y]
+    year = option[:y].to_i
+else
+    year = Date.today.year
+end
 
-# 引数の年月から日付配列を作成
+if option[:m]
+  month = option[:m].to_i
+else
+  month = Date.today.month
+end
 
 
-# 引数をカレンダーに整形後、表示するstrを配列に入れて返す
+puts "#{month}月 #{year}年".center(20)
 
-# 処理実行
+puts ["日", "月", "火", "水", "木", "金", "土"].join(" ")
+
+# year = Date.today.year
+# mon = Date.today.mon
+firstday_wday = Date.new(year,month,1).wday
+lastday_date = Date.new(year,month,-1).day
+wday = firstday_wday
+
+print "   " * firstday_wday
+
+(1..lastday_date).each do |date|
+  print date.to_s.rjust(2) + " "
+  wday += 1
+  if wday % 7 == 0
+    print "\n"
+  end
+end
