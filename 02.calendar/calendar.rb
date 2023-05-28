01bcd9d49ebd3ace38841814e2bbdcd1b444b0c1
oyuuhan_naninan <yumikoohta0710@gmail.com>
2023-05-28 16:07:40 +0900

    コードを書き足しました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,6 +1,17 @@
-require "date"
-day = Date.today.wday
-days = ["日", "月", "火", "水", "木", "金", "土"]
-days.each do |week|
-    puts week
-end
+require 'optparse'
+require 'date'
+
+todaymonth = Date.today.month
+todayyear = Date.today.year
+puts "      " + todaymonth.to_s + "月" + " " + todayyear.to_s
+japanese_wdays = ["日", "月", "火", "水", "木", "金", "土"]
+puts japanese_wdays.join(" ")
+
+# 表示する年月を取得
+
+# 引数の年月から日付配列を作成
+
+
+# 引数をカレンダーに整形後、表示するstrを配列に入れて返す
+
+# 処理実行
