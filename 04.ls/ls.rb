dc7dc47045658445286c5d796767119bb2fb674c
h-osawa <h-osawa@ruby-dev.jp>
2023-05-29 10:59:52 +0900

    rオプションの機能を作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -11,9 +11,10 @@ def main
 end
 
 def make_files
-  files = create_file_list(make_absolute_path, parse_option)
-  sorted_files = files.compact.sort
-  two_dimensional_files = make_two_dimensional_array(align_files(sorted_files))
+  option = parse_option
+  files = create_file_list(make_absolute_path).compact.sort
+  files = files.reverse if option[:r]
+  two_dimensional_files = make_two_dimensional_array(align_files(files))
   max_filename_length = generate_max_filename_length(two_dimensional_files)
   transposed_files = two_dimensional_files.transpose
   [transposed_files, max_filename_length]
@@ -22,7 +23,7 @@ end
 def parse_option
   option = {}
   opt = OptionParser.new
-  opt.on('-a')
+  opt.on('-r')
   opt.parse!(ARGV, into: option)
   option
 end
@@ -31,9 +32,9 @@ def make_absolute_path
   File.expand_path(ARGV[0] || '.')
 end
 
-def create_file_list(absolute_path, option)
+def create_file_list(absolute_path)
   Dir.chdir(absolute_path)
-  option[:a] ? Dir.glob('*', File::FNM_DOTMATCH).map.to_a : Dir.glob('*').map.to_a
+  Dir.glob('*').map.to_a
 end
 
 def align_files(sorted_files)
@@ -49,10 +50,14 @@ def generate_max_filename_length(two_dimensional_files)
   two_dimensional_files.flatten.map(&:length).max
 end
 
+def count_fullwidth_character(file)
+  file.chars.count { |char| char.bytesize == 3 }
+end
+
 def output_files(output_files, max_file_length)
   output_files.each do |files|
     files.each do |file|
-      print file.ljust(max_file_length + 2)
+      print file.ljust((max_file_length - count_fullwidth_character(file)) + 2)
     end
     print "\n"
   end
