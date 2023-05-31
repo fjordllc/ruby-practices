db3709ffc75b1b87b8e5485096a384030928110f
ktgwShota <ktgw.shota.1998@gmail.com>
2023-05-31 15:13:05 +0900

    コメントに対する修正
    ・変数名をわかりやすいものに変更
    ・next_flameとnext_next_flame変数を追加し直感的に見やすいように
    ・ifの書き方を短略化
    ・加算処理を.sumを使って短略化

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,36 +12,39 @@ scores.each do |score|
     scores_change.push(10, 0)
   end
 end
-scores_flame = scores_change.each_slice(2).to_a # 変換したスコアをフレーム毎に分割
+flames = scores_change.each_slice(2).to_a # 変換したスコアをフレーム毎に分割
 
-if scores_flame[9][0] == 10 # 10フレーム目の1投目がストライクの場合
-  scores_flame[9].delete_at(1)
-  scores_flame[9].push(*scores_flame[10]) # 2投目と3投目を10フレーム目の配列に追加
-  scores_flame.delete_at(10)
-  if scores_flame[9][1] == 10 # 2投目もストライクの場合
-    scores_flame[9].delete_at(2)
-    scores_flame[9].push(scores_flame[10][0]) # 3投目を10フレーム目の配列に追加
-    scores_flame.delete_at(10)
+if flames[9][0] == 10 # 10フレーム目の1投目がストライクの場合
+  flames[9].delete_at(1)
+  flames[9].push(*flames[10]) # 2投目と3投目を10フレーム目の配列に追加
+  flames.delete_at(10)
+  if flames[9][1] == 10 # 2投目もストライクの場合
+    flames[9].delete_at(2)
+    flames[9].push(flames[10][0]) # 3投目を10フレーム目の配列に追加
+    flames.delete_at(10)
   end
-elsif scores_flame[9][0..1].sum == 10 # 2投目がスペアの場合
-  scores_flame[9].push(scores_flame[10][0]) # 3投目を10フレーム目の配列に追加
-  scores_flame.delete_at(10)
+elsif flames[9][0..1].sum == 10 # 2投目がスペアの場合
+  flames[9].push(flames[10][0]) # 3投目を10フレーム目の配列に追加
+  flames.delete_at(10)
 end
 
 # スコアの計算
-result = scores_flame.each_with_index.sum do |score, i|
-  if scores_flame[9][0] == 10 # 10フレーム目の処理 
-    scores_flame[9][0..2].sum
-  elsif score[0] == 10  # 1~9フレーム目の処理 # ストライクの場合
-    if scores_flame[i + 1][0] == 10 && i < 8 # 連続ストライクで次のフレーム2投目の値が存在しない（0）場合は、次の次のフレームの1投目の値を加算
-      add_score = scores_flame[i + 2][0]
-    end
-    10 + scores_flame[i + 1][0..1].sum + add_score.to_i  # 次の1,2投目とIFの追加スコアを加算
-  elsif score[0..1].sum == 10 # スペアの場合
-    10 + scores_flame[i + 1][0]
+
+result = flames.each_with_index.sum do |flame, i|
+  next_flame = flames[i + 1]
+  next_next_flame = flames[i + 2]
+  if i == 9 # 10フレーム目の処理 
+    flames[9][0..2].sum
+  elsif flame[0] == 10 # 1~9フレーム目の処理 # ストライクの場合
+    score = 10 + next_flame[0..1].sum
+    score += next_next_flame[0] if next_flame[0] == 10 && i < 8 # 連続ストライクで次のフレーム2投目の値が存在しない（0）場合
+    score
+  elsif flame[0..1].sum == 10 # 1~9フレーム目の処理 # スペアの場合
+    10 + next_flame[0]
   else # ストライク or スペア以外の場合
-    score[0] + score[1]
+    flame[0..1].sum
   end
 end
 
 puts result
+
