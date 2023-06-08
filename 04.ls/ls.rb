95cb6d5bb08c382a836212e5db28fb3378e270cd
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-08 14:01:50 +0900

    メソッドを作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,45 +10,68 @@ opts.on('-l') { |v| params[:l] = v }
 opts.parse!(ARGV)
 
 COLUMNS = 3
-
-
+BYTE_LENGTH = 5
 directory_path = ARGV[0] || '.'
 
 def get_file(path)
   Dir.glob('*', base: path, sort: true)
 end
 
-def get_details(files_and_directories, options)
-  byte_length = 5
+def check_file_type(file_type)
+  case file_type
+  when 'file'
+    '-'
+  when 'directory'
+    'd'
+  when 'characterSpecial'
+    'c'
+  when 'fifo'
+    'f'
+  when 'link'
+    'l'
+  when 'socket'
+    's'
+  end
+end
+
+def check_file_permission(file_permission)
+  case file_permission
+  when '0'
+    '---'
+  when '1'
+    '--x'
+  when '2'
+    '-w-'
+  when '3'
+    '-wx'
+  when '4'
+    'r--'
+  when '5'
+    'r-x'
+  when '6'
+    'rw-'
+  when '7'
+    'rwx'
+  end
+end
+
+def get_details(files_and_directories, options, byte_length)
   files_and_directories.map! do |files_and_directorie|
     details_files_and_directorie = {}
     if options[:l]
       permission = ''
       file_type = File::Stat.new(files_and_directorie).ftype
-      permission += '-' if file_type == 'file'
-      permission += 'd' if file_type == 'directory'
-      permission += 'c' if file_type == 'characterSpecial'
-      permission += 'f' if file_type == 'fifo'
-      permission += 'l' if file_type == 'link'
-      permission += 's' if file_type == 'socket'
+      permission += check_file_type(file_type)
       permission_base_eight = File::Stat.new(files_and_directorie).mode.to_s(8)
-      three_digit = permission_base_eight[(permission_base_eight.length.to_i - 3) ... permission_base_eight.to_i]
-      three_digit.each_char do |c|
-        permission += '---' if c == '0'
-        permission += '--x' if c == '1'
-        permission += '-w-' if c == '2'
-        permission += '-wx' if c == '3'
-        permission += 'r--' if c == '4'
-        permission += 'r-x' if c == '5'
-        permission += 'rw-' if c == '6'
-        permission += 'rwx' if c == '7'
-      end
+      three_digit = permission_base_eight[(permission_base_eight.length.to_i - 3)...permission_base_eight.to_i]
+      three_digit.each_char { |c| permission += check_file_permission(c) }
       details_files_and_directorie[:permission] = permission
-      details_files_and_directorie[:links] = " " + File::Stat.new(files_and_directorie).nlink.to_s
-      details_files_and_directorie[:owner] = " " + Etc.getpwuid(File::Stat.new(files_and_directorie).uid).name
-      details_files_and_directorie[:group] = " " + Etc.getgrgid(File::Stat.new(files_and_directorie).gid).name
-      details_files_and_directorie[:byte_size] = " " +  File::Stat.new(files_and_directorie).size.to_s.rjust(byte_length)
-      details_files_and_directorie[:time] = " " + File::Stat.new(files_and_directorie).mtime.strftime("%m %d %H:%M") + " "
+      file_stat = File::Stat.new(files_and_directorie)
+      details_files_and_directorie[:links] = file_stat.nlink.to_s.rjust(file_stat.nlink.to_s.length.to_i + 1)
+      details_files_and_directorie[:owner] = Etc.getpwuid(file_stat.uid).name.rjust(Etc.getpwuid(file_stat.uid).name.length.to_i + 1)
+      details_files_and_directorie[:group] = Etc.getgrgid(file_stat.gid).name.rjust(Etc.getgrgid(file_stat.gid).name.length.to_i + 1)
+      details_files_and_directorie[:byte_size] = file_stat.size.to_s.rjust(byte_length + 1)
+      details_files_and_directorie[:time] = file_stat.mtime.strftime('%m %d %H:%M').center(file_stat.mtime.strftime('%m %d %H:%M').length + 2)
     end
     details_files_and_directorie[:name] = files_and_directorie
     details_files_and_directorie
@@ -56,7 +79,7 @@ def get_details(files_and_directories, options)
 end
 
 def get_max_length(files_and_directories)
-  files_and_directories.map{ |files_and_directorie| files_and_directorie[:name].length }.max
+  files_and_directories.map { |files_and_directorie| files_and_directorie[:name].length }.max
 end
 
 def organizing_arrays(file_name_length, files_and_directories, columns, output_num)
@@ -70,12 +93,12 @@ def organizing_arrays(file_name_length, files_and_directories, columns, output_n
   outputs
 end
 
-def output_file(output_num, columns, files_and_directories, options)
+def output_file(output_num, files_and_directories, options)
   puts "total #{files_and_directories.flatten.length}" if options[:l]
   output_num.times do |time|
     files_and_directories[time].length.times do |column|
       if options[:l]
-        files_and_directories[column][time].each do |key, value|
+        files_and_directories[column][time].each do |_key, value|
           print value
         end
         puts "\n"
@@ -88,9 +111,9 @@ def output_file(output_num, columns, files_and_directories, options)
 end
 
 temporary_outputs = get_file(directory_path)
-temporary_outputs = get_details(temporary_outputs, params)
+temporary_outputs = get_details(temporary_outputs, params, BYTE_LENGTH)
 max_file_length = get_max_length(temporary_outputs)
 # # 一列に出力するファイルの数
 maximum_num = temporary_outputs.length / COLUMNS + 1
 outputs = organizing_arrays(max_file_length, temporary_outputs, COLUMNS, maximum_num)
-output_file(maximum_num, COLUMNS, outputs, params)
+output_file(maximum_num, outputs, params)
