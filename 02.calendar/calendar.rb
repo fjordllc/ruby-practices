89984d9915e06b1d24d94c368dbce23c31f45c3d
igarashi-naoki <naoki17igarashi@gmail.com>
2023-06-12 23:40:49 +0900

    コマンドライン引数及び例外処理に関する修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -7,48 +7,47 @@ TODAY = Date.today
 year = TODAY.year
 month = TODAY.month
 
+def get_arg_month(arg_month)
+  # 文字列を数値変換できない場合ArgumentError
+  raise ArgumentError unless (1..12).cover?(Integer(arg_month))
+
+  Integer(arg_month)
+rescue ArgumentError
+  puts "#{arg_month} is neither a month number (1..12) nor a name"
+  exit
+end
+
+def get_arg_year(arg_year)
+  # 文字列を数値変換できない場合ArgumentError
+  if (1..9999).cover?(Integer(arg_year))
+    Integer(arg_year)
+  else
+    puts "year '#{arg_year}' not in range 1..9999"
+    exit
+  end
+rescue ArgumentError
+  puts "not a valid year #{arg_year}"
+  exit
+end
+
+
 # コマンドライン引数の取得
-options = {}
 OptionParser.new do |o|
-  o.on('-m', '--month [ITEM]', 'set month') { |v| options[:month] = v }
-  o.on('-y', '--year [ITEM]', 'set year') { |v| options[:year] = v }
-  o.on('-h', '--help', 'show this help') { puts o; exit }
+  o.on('-m', '--month [ITEM]', 'set month') do |m|
+    month = get_arg_month(m) unless m.nil?
+  end
+
+  o.on('-y', '--year [ITEM]', 'set year') do |y|
+    year = get_arg_year(y) unless y.nil?
+  end
+  o.on('-h', '--help', 'show this help') do
+    puts o
+    exit
+  end
   o.parse!(ARGV)
 rescue OptionParser::InvalidOption => e
   puts e.message
   exit
-rescue SystemExit
-  exit
-end
-
-if !options[:month].nil?
-  if (1..12).cover?(options[:month].to_i)
-    month = options[:month].to_i
-  else
-    begin
-      puts "'#{options[:month]}' is neither a month number (1..12) nor a name"
-      exit
-    rescue SystemExit
-      exit
-    end
-  end
-end
-
-if !options[:year].nil?
-  if (1..9999).cover?(options[:year].to_i)
-    year = options[:year].to_i
-  else
-    begin
-      Integer(options[:year]) # 文字列を数値変換できない場合ArgumentError
-      puts " year '#{options[:year]}' not in range 1..9999"
-      exit
-    rescue ArgumentError
-      puts "not a valid year '#{options[:year]}'"
-      exit
-    rescue SystemExit
-      exit
-    end
-  end
 end
 
 print format("　　　　　%<m>2d月  %<y>4d \n", m: month, y: year)
