1e07fc4499b6f6074409a5f69ba87abfb19cf194
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-06-15 21:26:13 +0900

    optparseを用いた処理をコマンドからの入力を反映させるために追加

--- 02.calendar/my-calendar.rb
+++ 02.calendar/my-calendar.rb
@@ -1,12 +1,26 @@
 #!/usr/bin/env ruby
+require 'optparse'
 require 'date'
 
-date = Date.new(2023, 6, -1)
+display_year = Date.today.year
+display_month = Date.today.month
 
-printf("      \e[31m%d\e[0m月 \e[31m%d\e[0m     \n", date.month, date.year)
+OptionParser.new do |options|
+  options.on("-y YEAR", Integer) do |year|
+    display_year = year
+  end
+
+  options.on("-m MONTH", Integer) do |month|
+    display_month = month
+  end
+end.parse!
+
+display_date = Date.new(display_year, display_month, -1)
+
+printf("      \e[31m%d\e[0m月 \e[31m%d\e[0m     \n", display_date.month, display_date.year)
 puts '日 月 火 水 木 金 土'
 
-(1..date.mday).each do |each_date|
-  printf("\e[31m%2d \e[0m", each_date)
-  puts "\n" if (each_date % 7).zero?
+(1..display_date.mday).each do |date|
+  printf("\e[31m%2d \e[0m", date)
+  puts "\n" if (date % 7).zero?
 end
