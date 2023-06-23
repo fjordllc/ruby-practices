619f205ddfc9d372583494233d237cd8619f0a94
tyrrell-IH <gotothertm@gmail.com>
2023-06-23 10:33:07 +0900

    可読性の観点から使用するメソッド等を修正しました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,8 +5,8 @@ require 'date'
 require 'optparse'
 
 opt = OptionParser.new
-displayed_year = Date.today.strftime('%Y').to_i
-displayed_month = Date.today.strftime('%m').to_i
+displayed_year = Date.today.year
+displayed_month = Date.today.month
 opt.on('-y VAL') { |v| displayed_year = v.to_i }
 opt.on('-m VAL') { |v| displayed_month = v.to_i }
 opt.parse!(ARGV)
@@ -14,8 +14,8 @@ opt.parse!(ARGV)
 biginning_of_the_month = Date.new(displayed_year, displayed_month, 1)
 end_of_the_month = Date.new(displayed_year, displayed_month, -1)
 
-all_days_of_the_month = [*biginning_of_the_month..end_of_the_month].map { |date| date.strftime('%e') }
-biginning_of_the_month.strftime('%u').to_i.times { all_days_of_the_month.unshift('  ') } unless biginning_of_the_month.strftime('%u').to_i == 7
+all_days_of_the_month = (biginning_of_the_month..end_of_the_month).map { |date| date.strftime('%e') }
+biginning_of_the_month.cwday.times { all_days_of_the_month.unshift('  ') } unless biginning_of_the_month.sunday?
 
 puts "#{displayed_month}月 #{displayed_year}".center(20)
 puts '日 月 火 水 木 金 土'
