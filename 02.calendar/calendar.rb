288c7a769bbdbd1a588630aca3d56bc4b2caf6a9
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-06-23 17:23:11 +0900

    不要な赤い出力をなくすため、修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -18,10 +18,10 @@ end.parse!
 
 display_date = Date.new(display_year, display_month, -1)
 
-printf("      \e[31m%d\e[0m月 \e[31m%d\e[0m     \n", display_date.month, display_date.year)
+printf("      %d月 %d     \n", display_date.month, display_date.year)
 puts '日 月 火 水 木 金 土'
 
 (1..display_date.mday).each do |date|
-  printf("\e[31m%2d \e[0m", date)
+  printf("%2d ", date)
   puts "\n" if (date % 7).zero?
 end
