a108e08242869cc318103d481fc811c29398a3dc
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-06-23 17:40:05 +0900

    calコマンドを実行した際と、同じ文言で出力されるように修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -18,8 +18,8 @@ end.parse!
 
 display_date = Date.new(display_year, display_month, -1)
 
-printf("      %d月 %d     \n", display_date.month, display_date.year)
-puts '日 月 火 水 木 金 土'
+printf("      %s %d     \n", display_date.strftime("%B"), display_date.year)
+puts 'Su Mo Tu We Th Fr Sa'
 
 (1..display_date.mday).each do |date|
   printf("%2d ", date)
