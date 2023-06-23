dbaa66ba67b0e0a337a601357c84bf9b709e0c03
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-06-23 17:42:19 +0900

    最後は改行でおわるようにするため、修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -21,7 +21,7 @@ display_date = Date.new(display_year, display_month, -1)
 printf("      %s %d     \n", display_date.strftime("%B"), display_date.year)
 puts 'Su Mo Tu We Th Fr Sa'
 
-(1..display_date.mday).each do |date|
+(1..display_date.mday).each_with_index do |date, i|
   printf("%2d ", date)
-  puts "\n" if (date % 7).zero?
+  puts "\n" if (date % 7).zero? || i == display_date.mday - 1
 end
