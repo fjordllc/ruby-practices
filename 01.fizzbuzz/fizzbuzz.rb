9d81490d7ae9893783afd9b9f15720baaf67b606
YuSa0-6 <fuku.sugar@icloud.com>
2023-06-27 15:21:57 +0900

    [update]  return 取り消し

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,13 @@
 ## checked fizzbuzz
 def fizzbuzz(x)
   if x % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif x % 5 == 0
-    return "Buzz"
+    "Buzz"
   elsif x % 15 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
   else
-    return x.to_s
+    x.to_s
   end
 end
 
