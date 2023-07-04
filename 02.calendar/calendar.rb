882b46a856bd53aa5732e4d17b403b28aed9856a
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-07-04 21:08:32 +0900

    不要な一時変数への代入をなくすため、修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -22,11 +22,10 @@ last_day = Date.new(display_year, display_month, -1)
 printf("      %s %d     \n", last_day.strftime("%B"), last_day.year)
 puts 'Su Mo Tu We Th Fr Sa'
 
-first_day_wday = first_day.wday
-blank = '   ' * first_day_wday
+blank = '   ' * first_day.wday
 print blank
 
 (1..last_day.mday).each_with_index do |date, i|
   printf("%2d ", date)
-  puts "\n" if ((first_day_wday + date) % 7).zero? || i == last_day.mday - 1
+  puts "\n" if ((first_day.wday + date) % 7).zero? || i == last_day.mday - 1
 end
