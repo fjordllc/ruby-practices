763f1fb7a4b649cd631fc75c6cb04a62e6d4d05b
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-04 22:23:00 +0900

    メソッドを２つ以上作成条件をクリアしていなかったので作成した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,11 +3,14 @@
 current_directory = Dir.pwd
 file = Dir.entries(current_directory)
 files = []
-file.each do |x|
-  next if x.match?(/^\./)
+def ls_normal(file, files)
+  file.each do |x|
+    next if x.match?(/^\./)
 
-  files.push(x)
+    files.push(x)
+  end
 end
+ls_normal(file, files)
 
 col_num = 3
 row_num = (files.size / col_num.to_f).ceil
@@ -15,12 +18,15 @@ row_num = (files.size / col_num.to_f).ceil
 cols = [*0..col_num - 1]
 rows = [*0..row_num - 1]
 files_sorted = files.sort
-rows.each do |row|
-  cols.each do |col|
-    break if files_sorted[row + col * rows.size].nil?
+def make_cell(rows, cols, files_sorted)
+  rows.each do |row|
+    cols.each do |col|
+      break if files_sorted[row + col * rows.size].nil?
 
-    print files_sorted[row + col * rows.size].ljust(26)
+      print files_sorted[row + col * rows.size].ljust(26)
+    end
+
+    puts
   end
-
-  puts
 end
+make_cell(rows, cols, files_sorted)
