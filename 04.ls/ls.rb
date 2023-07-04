68f6049b6c41fb748b816bf65603bf88dc3812f7
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-04 13:35:10 +0900

    rubocopで修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,25 +1,25 @@
-require "debug"
+# frozen_string_literal: true
 
 current_directory = Dir.pwd
-file = Dir::entries("#{current_directory}")
+file = Dir.entries(current_directory)
 files = []
 file.each do |x|
   next if x.match?(/^\./)
+
   files.push(x)
 end
 
 col_num = 5
 row_num = (files.size / col_num.to_f).ceil
 
-cols = [*0..col_num-1]
-rows = [*0..row_num-1]
+cols = [*0..col_num - 1]
+rows = [*0..row_num - 1]
 nothing_option = files.sort
 rows.each do |row|
-  # debugger
   cols.each do |col|
-    break if nothing_option[ row + col * rows.size ] == nil
+    break if nothing_option[row + col * rows.size].nil?
 
-    print nothing_option[ row + col * rows.size ].ljust(26)
+    print nothing_option[row + col * rows.size].ljust(26)
   end
 
   puts
