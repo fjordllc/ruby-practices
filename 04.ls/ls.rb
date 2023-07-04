e8d3ffe1c1bfcca23d36647fba9814a9b6348298
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-04 15:32:03 +0900

    変数名変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -14,12 +14,12 @@ row_num = (files.size / col_num.to_f).ceil
 
 cols = [*0..col_num - 1]
 rows = [*0..row_num - 1]
-nothing_option = files.sort
+files_sorted = files.sort
 rows.each do |row|
   cols.each do |col|
-    break if nothing_option[row + col * rows.size].nil?
+    break if files_sorted[row + col * rows.size].nil?
 
-    print nothing_option[row + col * rows.size].ljust(26)
+    print files_sorted[row + col * rows.size].ljust(26)
   end
 
   puts
