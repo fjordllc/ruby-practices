16cb33ee8197f88ef6ab155efdb530ed5a7f08b7
tyrrell-IH <gotothertm@gmail.com>
2023-07-07 01:11:59 +0900

    表示されるファイル名の右側の余白の設定を変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,33 +2,46 @@
 # frozen_string_literal: true
 
 Acquired_file_names = Dir.glob('*')
-NUMBER_OF_COLUMN = 3
+desired_number_of_columns = 3
+NUMBER_OF_COLUMN = Acquired_file_names.find { |name| name.size >= 24 } ? 1 : desired_number_of_columns
 
-def get_filename_the_longest_number_of_characters(filenames)
-  longest_number_of_characters = 0
+def insert_empty_filename_and_sort_by_each_columns(filenames, columns)
+  copied_filenames = filenames.dup
+  copied_filenames << '' while copied_filenames.size % columns != 0
+  numbers_of_lines = copied_filenames.size / columns
+  copied_filenames.each_slice(numbers_of_lines).to_a.transpose
+end
+
+def get_maximum_number_of_characters_each_columns(filenames)
+  maximum_number_of_characters = 0
   filenames.each do |name|
-    longest_number_of_characters = name.size if longest_number_of_characters < name.size
+    maximum_number_of_characters = name.size if maximum_number_of_characters < name.size
   end
-  longest_number_of_characters
+  maximum_number_of_characters
 end
 
-def sort_files_for_display_with_some_columns(filenames, columns)
-  copied_filename = filenames.dup
-  copied_filename << '' while copied_filename.size % columns != 0
-  numbers_of_lines = copied_filename.size / columns
-  copied_filename.each_slice(numbers_of_lines).to_a.transpose
+def get_number_of_characters_of_filenames_including_margins(filenames)
+  case get_maximum_number_of_characters_each_columns(filenames)
+  when (0..7)
+    8
+  when (8..15)
+    16
+  when (16..23)
+    24
+  end
 end
 
-def display_with_some_columns(filename)
-  filename.each do |arr|
+def display_filenames_with_some_columns(filenames, columns)
+  insert_empty_filename_and_sort_by_each_columns(filenames, columns).each do |arr|
     arr.each_with_index do |name, i|
       if i == (NUMBER_OF_COLUMN - 1)
-        puts name&.ljust(get_filename_the_longest_number_of_characters(Acquired_file_names) + 5)
+        puts name
       else
-        print name&.ljust(get_filename_the_longest_number_of_characters(Acquired_file_names) + 5)
+        print name.ljust(Number_of_characters_of_filenames_including_margins)
       end
     end
   end
 end
 
-display_with_some_columns(sort_files_for_display_with_some_columns(Acquired_file_names, NUMBER_OF_COLUMN))
+Number_of_characters_of_filenames_including_margins = get_number_of_characters_of_filenames_including_margins(Acquired_file_names)
+display_filenames_with_some_columns(Acquired_file_names, NUMBER_OF_COLUMN)
