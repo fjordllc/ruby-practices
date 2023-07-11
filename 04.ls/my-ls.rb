b1ec70fc994570dd7483a03342ccc41e6349e63d
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-07-11 17:52:01 +0900

    変数を適当なものに変更するために修正

--- 04.ls/my-ls.rb
+++ 04.ls/my-ls.rb
@@ -2,9 +2,11 @@
 
 # frozen_string_literal: true
 
+require 'debug'
+
 SEGMENT_LENGTH = 3
 
-def divide_into_three(entities)
+def divide_into_segments(entities)
   entities.each_slice((entities.length + 2) / SEGMENT_LENGTH).to_a
 end
 
@@ -14,13 +16,15 @@ def transpose(entities)
   entities.transpose
 end
 
-divided_entities = divide_into_three(Dir.glob('*'))
+divided_entities = divide_into_segments(Dir.glob('*'))
+
+longest_entity_length = divided_entities.flatten.max_by { |element| element.length }.length
 
 transposed_entities = transpose(divided_entities)
 
 transposed_entities.each do |column_entitites|
   column_entitites.each do |column_entitiy|
-    print column_entitiy&.ljust(15)
+    print column_entitiy&.ljust(longest_entity_length + 1)
   end
   print("\n")
 end
