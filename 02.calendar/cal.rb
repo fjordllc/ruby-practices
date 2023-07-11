671a0d5493bd7b19c01e1e4d3c3ac40f45956de7
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-11 18:48:16 +0900

    2回目のレビューをもとに修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,69 +4,50 @@ require 'date'
 require 'optparse'
 
 def identify_target_date(today)
-  date = { year: today.year, month: today.mon, day: today.mday }
+  target_date = { year: today.year, month: today.mon, day: today.mday }
 
   opt = OptionParser.new
-  opt.on('-m VAL', Integer) { |m| date[:month] = m }
-  opt.on('-y VAL', Integer) { |y| date[:year] = y }
+  opt.on('-m VAL', Integer) { |m| target_date[:month] = m }
+  opt.on('-y VAL', Integer) { |y| target_date[:year] = y }
   opt.parse(ARGV)
 
-  date
+  target_date
 end
 
-def date_validation(date)
-  if date[:month] < 1 || date[:month] > 12
-    :invalid_month
-  elsif date[:year] < 1
-    :invalid_year
-  else
-    :valid
-  end
+def invalid_month?(target_date)
+  target_date[:month] < 1 || target_date[:month] > 12
 end
 
-# 日付ハッシュと今日のDateオブジェクトから、カレンダー出力に必要な情報へ加工するメソッド
-def processing_date(date:, today:)
-  info_for_cal = {}
-  info_for_cal[:month] = date[:month]
-  info_for_cal[:year] = date[:year]
-
-  info_for_cal[:day_of_the_week_of_first_day] = Date.new(date[:year], date[:month], 1).wday
-
-  info_for_cal[:last_day] = Date.new(date[:year], date[:month], -1).mday
-
-  # 出力する年月が今日の年月と一致していたら「:color_inverted_date」に今日の日付(整数)を設定
-  info_for_cal[:color_inverted_date] = today.mday if date[:year] == today.year && date[:month] == today.mon
-
-  info_for_cal
+def invalid_year?(target_date)
+  target_date[:year] < 1
 end
 
-def output_cal(info_for_cal)
-  puts "      #{info_for_cal[:month]}月 #{info_for_cal[:year]}"
+def output_cal(date:, today:)
+  puts "      #{date[:month]}月 #{date[:year]}"
   puts '日 月 火 水 木 金 土'
-  print '   ' * info_for_cal[:day_of_the_week_of_first_day]
+  print '   ' * Date.new(date[:year], date[:month], 1).wday
 
-  (1..info_for_cal[:last_day]).each do |day|
-    # color_inverted_dateがdayと一致していたら色反転
-    if info_for_cal[:color_inverted_date] == day
+  last_day = Date.new(date[:year], date[:month], -1).mday
+  
+  (1..last_day).each do |day|
+    output_date = Date.new(date[:year], date[:month], day)
+    if output_date == today
       print "\e[30m\e[47m#{day.to_s.rjust(2)}\e[0m "
     else
       print "#{day.to_s.rjust(2)} "
     end
-
-    print "\n" if Date.new(info_for_cal[:year], info_for_cal[:month], day).saturday?
+    print "\n" if output_date.saturday?
   end
-
   print "\n"
 end
 
 today = Date.today
 target_date = identify_target_date(today)
-case date_validation(target_date)
-when :invalid_month
+
+if invalid_month?(target_date)
   puts '指定された月の値は無効です。1～12の範囲で入力して下さい。'
-when :invalid_year
+elsif invalid_year?(target_date)
   puts '指定された西暦の値は無効です。1以上の値を入力して下さい'
-when :valid
-  info_for_cal = processing_date(date: target_date, today:)
-  output_cal(info_for_cal)
+else
+  output_cal(date: target_date, today:)
 end
