15d31a449a7445cf3538cd1c008ad9e4e131dc1b
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-12 17:06:54 +0900

    -aオプションの機能追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
-def make_cell(rows, cols, files_sorted)
+require 'debug'
+require 'optparse'
+# debugger
+opt = OptionParser.new
+option = {}
+opt.on('-a') {|a| option[:a] = a }
+argv = opt.parse(ARGV)
+
+def make_cell(rows, cols, files_sorted, option)
+  # debugger
+    rows.each do |row|
+      cols.each do |col|
+        break if files_sorted[row + col * rows.size].nil?
+
+        print files_sorted[row + col * rows.size].ljust(26)
+      end
+
+      puts
+      end
+end
+
+def option_a_make_cell(rows, cols, a_option_files_sorted)
   rows.each do |row|
     cols.each do |col|
-      break if files_sorted[row + col * rows.size].nil?
+      break if a_option_files_sorted[row + col * rows.size].nil?
 
-      print files_sorted[row + col * rows.size].ljust(26)
+      print a_option_files_sorted[row + col * rows.size].ljust(26)
     end
 
     puts
@@ -13,12 +34,18 @@ def make_cell(rows, cols, files_sorted)
 end
 
 files = Dir.glob('*')
+a_option_files = Dir.glob("*", File::FNM_DOTMATCH)
+files_sorted = files.sort
+a_option_files_sorted = a_option_files.sort
 
 COL_NUM = 3
 row_num = (files.size / COL_NUM.to_f).ceil
 
 cols = (0..COL_NUM)
 rows = (0..row_num - 1)
-files_sorted = files.sort
 
-make_cell(rows, cols, files_sorted)
+if option == {:a => true}
+  option_a_make_cell(rows, cols, a_option_files_sorted)
+else
+  make_cell(rows, cols, files_sorted, option)
+end
