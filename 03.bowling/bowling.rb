821abab7e8f6c6db9e2c01ea8bda53037e42f230
YuSa0-6 <fuku.sugar@icloud.com>
2023-07-14 16:49:27 +0900

    [delete] require'debug'

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,4 @@
-require 'debug'
+# 参考　新ルールの書き方(https://bootcamp.fjord.jp/pages/249)
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -19,15 +19,15 @@ end
 p frames
 point = 0
 
-frames.each_with_index do |frame, index| 
+frames.each_with_index do |frame, index|
   next_frame = frames[index+1]
   next_after_frame = frames[index+2]
 
-  if frame[0] == 10 && next_frame[0] == 10
+  if frame[0] == 10 && next_frame[0] == 10 # strike
     point += 10 + next_frame[0] + next_after_frame[0]
-  elsif frame[0] == 10 && next_frame[0] != 10
+  elsif frame[0] == 10 && next_frame[0] != 10 # strike
     point += 10 + next_frame.sum
-  elsif frame.sum == 10
+  elsif frame.sum == 10 # spare
     point += 10 + next_frame[0]
   else
     point += frame.sum
