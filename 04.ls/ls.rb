1e979f2ad2beaa9b011c2dff137e10b340a014c7
tyrrell-IH <gotothertm@gmail.com>
2023-07-16 21:30:38 +0900

    acquire_filesメソッドの作成及びその他の修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,28 +1,31 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-acquired_file = Dir.glob('*')
 NUMBER_OF_COLUMN = 3
+MULTIPLE_OF_COLUMN_WIDTH = 8
 
-def transposed_by_each_columns(files, number_of_columns)
+def acquire_files
+  Dir.glob('*')
+end
+
+def transpose_by_each_columns(files, number_of_columns)
   files << '' while files.size % number_of_columns != 0
   numbers_of_lines = files.size / number_of_columns
   files.each_slice(numbers_of_lines).to_a.transpose
 end
 
-def get_column_width(files)
-  i = 1
+def determine_column_width(files)
   maximum_number_of_characters = files.max_by(&:size).size
-  i += 1 until maximum_number_of_characters < (8 * i)
-  8 * i
+  (maximum_number_of_characters.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
 
 def display(files, number_of_columns)
-  transposed_files = transposed_by_each_columns(files, number_of_columns)
+  column_width = determine_column_width(files)
+  transposed_files = transpose_by_each_columns(files, number_of_columns)
   transposed_files.each do |files_each_lines|
-    files_with_mergins = files_each_lines.map { |file| file.ljust(get_column_width(files)) }
+    files_with_mergins = files_each_lines.map { |file| file.ljust(column_width) }
     puts files_with_mergins.join('')
   end
 end
 
-display(acquired_file, NUMBER_OF_COLUMN)
+display(acquire_files, NUMBER_OF_COLUMN)
