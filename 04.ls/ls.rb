c0fadb74484a95b43af96289c46bdfa2929acd67
tyrrell-IH <gotothertm@gmail.com>
2023-07-17 10:50:18 +0900

    メソッド名、定数名を一部修正しました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-NUMBER_OF_COLUMN = 3
+NUMBER_OF_COLUMNS = 3
 MULTIPLE_OF_COLUMN_WIDTH = 8
 
 def acquire_files
@@ -14,13 +14,13 @@ def transpose_by_each_columns(files, number_of_columns)
   files.each_slice(numbers_of_lines).to_a.transpose
 end
 
-def determine_column_width(files)
+def get_column_width(files)
   maximum_number_of_characters = files.max_by(&:size).size
   (maximum_number_of_characters.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
 
 def display(files, number_of_columns)
-  column_width = determine_column_width(files)
+  column_width = get_column_width(files)
   transposed_files = transpose_by_each_columns(files, number_of_columns)
   transposed_files.each do |files_each_lines|
     files_with_mergins = files_each_lines.map { |file| file.ljust(column_width) }
@@ -28,4 +28,4 @@ def display(files, number_of_columns)
   end
 end
 
-display(acquire_files, NUMBER_OF_COLUMN)
+display(acquire_files, NUMBER_OF_COLUMNS)
