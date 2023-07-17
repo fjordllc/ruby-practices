b74508a4981278e34b47a712cf68b389779c6bba
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-17 12:13:24 +0900

    -lオプションの際縦並びでファイルを表示するコード追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
-
+ require 'debug'
 require 'optparse'
 
 opt = OptionParser.new
 option = {}
 opt.on('-a') { |a| option[:a] = a }
 opt.on('-r') { |r| option[:r] = r }
+opt.on('-l') { |l| option[:l] = l }
 opt.parse(ARGV)
 
-def make_cell(rows, cols, files_ordered)
+def make_cell(rows, cols, files_ordered, option)
   rows.each do |row|
     cols.each do |col|
       break if files_ordered[row + col * rows.size].nil?
-
+    
       print files_ordered[row + col * rows.size].ljust(26)
     end
 
     puts
+  end
+end
+def option_l(files)
+  files.each do |file|
+    puts file
   end
 end
 
@@ -31,4 +37,9 @@ row_num = (files.size / COL_NUM.to_f).ceil
 cols = (0..COL_NUM - 1)
 rows = (0..row_num - 1)
 
-make_cell(rows, cols, files_ordered)
+if option[:l]
+  # debugger
+  option_l(files)
+else
+  make_cell(rows, cols, files_ordered, option)
+end
