659cc235e30d815ec82b010b4843e03be8ac71d6
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-20 19:12:25 +0900

    ls.rbのoutputメソッドをリファクタリング

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,24 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-NUMBER_OR_ROW = 3
+NUMBER_OF_ROW = 3
 target_path = ARGV[0] ||= './'
 
 def get_filenames(target_path)
   Dir.entries(target_path).sort
 end
 
+
+
 def output(filenames)
-  number_of_col = ((filenames.size - 1) / NUMBER_OR_ROW) + 1
-  filename_array = Array.new(number_of_col) { Array.new(NUMBER_OR_ROW) }
+  number_of_col = ((filenames.size - 1) / NUMBER_OF_ROW) + 1
+  filename_array = Array.new(number_of_col) { Array.new(NUMBER_OF_ROW) }
 
-  filenames.each_with_index do |filename, index|
-    filename_array[index % number_of_col][index / number_of_col] = filename
-  end
-
-  filename_array.each do |col|
-    col.compact.each do |filename|
-      print "#{filename}   "
+  number_of_col.times do |col|
+    NUMBER_OF_ROW.times do |row|
+      print "#{filenames[col + row * 5]}   "
     end
     print "\n"
   end
