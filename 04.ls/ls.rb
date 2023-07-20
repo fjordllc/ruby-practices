1d12573cbeaeb5dc69b54afcc1a5a5730def5230
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-20 16:21:12 +0900

    ls.rbのファイル名の表示が縦並びになるように変更
    各列の左端を揃える機能は未実装

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,15 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-target_path = ARGV[0] ||= "./"
+NUMBER_OR_ROW = 3
+target_path = ARGV[0] ||= './'
 
-def get_filenames (target_path)
+def get_filenames(target_path)
   Dir.entries(target_path).sort
 end
 
-def output (filenames)
-  filenames.each do |filename|
-    puts filename
+def output(filenames)
+  number_of_col = ((filenames.size - 1) / NUMBER_OR_ROW) + 1
+  filename_array = Array.new(number_of_col) { Array.new(NUMBER_OR_ROW) }
+
+  filenames.each_with_index do |filename, index|
+    filename_array[index % number_of_col][index / number_of_col] = filename
+  end
+
+  filename_array.each do |col|
+    col.compact.each do |filename|
+      print "#{filename}   "
+    end
+    print "\n"
   end
 end
 
