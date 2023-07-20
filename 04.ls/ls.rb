634e95f76c26544176c15a3ffc38fc565290efa3
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-20 23:22:15 +0900

    ls.rbを、左揃えで表示できるように変更しました。
    各列の最大の文字数+2の値でljustして出力するように変更しました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,16 +5,20 @@ NUMBER_OF_ROW = 3
 target_path = ARGV[0] ||= './'
 
 def get_filenames(target_path)
-  Dir.glob("*", base: target_path)
+  Dir.glob('*', base: target_path)
 end
 
 def output(filenames)
   number_of_col = ((filenames.size - 1) / NUMBER_OF_ROW) + 1
-  filename_array = Array.new(number_of_col) { Array.new(NUMBER_OF_ROW) }
+  ljust_widths = []
+
+  filenames.each_slice(number_of_col) do |row|
+    ljust_widths << row.map(&:size).max
+  end
 
   number_of_col.times do |col|
     NUMBER_OF_ROW.times do |row|
-      print "#{filenames[col + row * 5]}   "
+      print filenames[col + row * number_of_col].ljust(ljust_widths[row] + 2) if filenames[col + row * number_of_col]
     end
     print "\n"
   end
