7f2382dd424c274c43aa0d2f27e962c55b5df814
reckyy <srreki98@gmail.com>
2023-07-23 18:32:38 +0900

    得点の計算の処理追加
    9フレームまではスペアやストライクを考慮し
    10フレームではただ足し算するように変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -26,16 +26,21 @@ shots.each_slice(2).with_index do |s, index|
     break
   end
 end
-binding.break
 
 point = 0
-frames.each do |frame|
-  if frame[0] == 10 # strike
-    point += 30
-  elsif frame.sum == 10 # spare
-    point += frame[0] + 10
-  else
+frames.each_with_index do |frame, index|
+  if index < 9
+    if frame[0] == 10 # strike
+      point += 30
+    elsif frame.sum == 10 # spare
+      point += frame[0] + 10
+    else
+      point += frame.sum
+    end
+  elsif index == 9
     point += frame.sum
+  else
+    break
   end
 end
 puts point
