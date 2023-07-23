dfb8dd67f546d293250c234fa6fa31fbd9870c3a
reckyy <srreki98@gmail.com>
2023-07-23 21:48:54 +0900

    rubocop修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -15,33 +17,40 @@ shots.each_slice(2).with_index do |s, index|
   if index < 9
     frames << s
   elsif index == 9
-    frame_10 = []
-    18.upto(shots.length - 1) { |i|
-      frame_10 << shots[i] unless shots[i] == 0
-    }
-    frames << frame_10
+    frame10 = []
+    18.upto(shots.length - 1) do |i|
+      frame10 << shots[i] unless shots[i].zero?
+    end
+    frames << frame10
     break
+  end
+end
+
+def calc_strike_point(time, frames)
+  if frames[time + 1][0] == 10
+    if i < 8
+      20 + (frames[time + 2] ? frames[time + 2][0] : 0)
+    else
+      20 + frames[time + 1][1]
+    end
+  else
+    10 + frames[time + 1].sum
   end
 end
 
 point = 0
 frames.each_with_index do |frame, i|
-  if i < 9
-    if frame[0] == 10 # strike
-      if frames[(i + 1)][0] == 10
-        point += 20 + (frames[(i + 2)] ? frames[(i + 2)][0] : 0) if i < 8
-        point += 20 + frames[(i + 1)][1] if i == 8
-      else
-        point += 10 + frames[(i + 1)].sum
-      end
-    elsif frame.sum == 10 # spare
-      point += 10 + frames[(i + 1)][0]
-    else
-      point += frame.sum
-    end
-  elsif i == 9
-    point += frame.sum
-    break
-  end
+  point += if i < 9
+             if frame[0] == 10 # strike
+               calc_strike_point(i, frames)
+             elsif frame.sum == 10 # spare
+               10 + frames[(i + 1)][0]
+             else
+               frame.sum
+             end
+           elsif i == 9
+             frame.sum
+             break
+           end
 end
 puts point
