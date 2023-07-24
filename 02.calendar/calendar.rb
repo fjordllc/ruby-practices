846123b3edcc9fa709af334809704f70c5ce9345
reckyy <srreki98@gmail.com>
2023-07-24 11:33:22 +0900

    コードの簡素化
    lasy_dayに加えてfirsy_dayの変数を追加

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -13,17 +13,18 @@ year = year_and_month[:year] ? year_and_month[:year] : today.year
 month = year_and_month[:month] ? year_and_month[:month] : today.month
 
 entered_day  = Date.new(year, month)
-last_day = Date.new(entered_day.year, entered_day.month, -1).day
-first_day_of_the_week = Date.new(entered_day.year, entered_day.month, 1).cwday
+first_day = Date.new(entered_day.year, entered_day.month, 1)
+last_day = Date.new(entered_day.year, entered_day.month, -1)
+
 
 #calendarを表示
 week_day_box = Array.new(7)
 puts "#{entered_day.month}月 #{entered_day.year}"
 puts "日  月  火  水  木  金  土"
-if first_day_of_the_week != 7
-  print  "    " * first_day_of_the_week
+if first_day.cwday != 7
+  print  "    " * first_day.cwday
 end
-(Date.new(entered_day.year, entered_day.month, 1)..Date.new(entered_day.year, entered_day.month, -1)).each do |date|
+(first_day..last_day).each do |date|
   day = date.day
   if Date.today.day == day
     print "\e[7m#{day}\e[0m" + " " * (4 - day.to_s.length)
