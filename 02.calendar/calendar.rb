f6fb85d282a5d3d3249af73d10ea0763c59a7566
reckyy <srreki98@gmail.com>
2023-07-25 21:25:09 +0900

    コード修正
    左寄せではなく、右寄せに
    また、曜日間の空白を調整

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -20,16 +20,17 @@ last_day = Date.new(entered_day.year, entered_day.month, -1)
 #calendarを表示
 week_day_box = Array.new(7)
 puts "#{entered_day.month}月 #{entered_day.year}"
-puts "日  月  火  水  木  金  土"
+puts "日 月 火 水 木 金 土"
 if first_day.cwday != 7
-  print  "    " * first_day.cwday
+  print  "   " * (first_day.cwday)
 end
 (first_day..last_day).each do |date|
   day = date.day
   if Date.today.day == day
-    print "\e[7m#{day}\e[0m" + " " * (4 - day.to_s.length)
+    print " " * (2 - day.to_s.length) + "\e[7m#{day}\e[0m"
   else
-    print "#{day}".ljust(4)
+    print "#{day}".rjust(2)
   end
+  print " "
   puts "" if date.saturday?
 end
