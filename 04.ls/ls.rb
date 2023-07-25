c1262cce8d7b27b0f0c71d777432f72487f81200
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-26 08:36:49 +0900

    ls.rbのcolとrowが逆になっていたので修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-NUMBER_OF_ROW = 3
+NUMBER_OF_COL = 3
 
 def get_filenames(target_path)
   Dir.glob('*', base: target_path)
 end
 
 def output(filenames)
-  number_of_col = ((filenames.size - 1) / NUMBER_OF_ROW) + 1
+  number_of_row = ((filenames.size - 1) / NUMBER_OF_COL) + 1
   ljust_widths = []
 
-  split_filenames = filenames.each_slice(number_of_col).to_a
+  split_filenames = filenames.each_slice(number_of_row).to_a
 
-  split_filenames.each do |row|
-    ljust_widths << row.map(&:size).max + 2
+  split_filenames.each do |col|
+    ljust_widths << col.map(&:size).max + 2
   end
 
-  number_of_col.times do |col|
-    NUMBER_OF_ROW.times do |row|
+  number_of_row.times do |row|
+    NUMBER_OF_COL.times do |col|
       # split_filenamesのcolとrowを入れ替えて出力
-      print split_filenames[row][col].ljust(ljust_widths[row]) unless split_filenames[row][col].nil?
+      print split_filenames[col][row].ljust(ljust_widths[col]) unless split_filenames[col][row].nil?
     end
     print "\n"
   end
