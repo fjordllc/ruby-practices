d8d41bce8f65cea8774c7431ed869a78d1db8dc3
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-26 08:28:29 +0900

    ls.rbの出力部分のロジックを変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -11,13 +11,16 @@ def output(filenames)
   number_of_col = ((filenames.size - 1) / NUMBER_OF_ROW) + 1
   ljust_widths = []
 
-  filenames.each_slice(number_of_col) do |row|
-    ljust_widths << row.map(&:size).max
+  split_filenames = filenames.each_slice(number_of_col).to_a
+
+  split_filenames.each do |row|
+    ljust_widths << row.map(&:size).max + 2
   end
 
   number_of_col.times do |col|
     NUMBER_OF_ROW.times do |row|
-      print filenames[col + row * number_of_col].ljust(ljust_widths[row] + 2) if filenames[col + row * number_of_col]
+      # split_filenamesのcolとrowを入れ替えて出力
+      print split_filenames[row][col].ljust(ljust_widths[row]) unless split_filenames[row][col].nil?
     end
     print "\n"
   end
