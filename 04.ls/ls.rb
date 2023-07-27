287c6ac4af1308b0ec8c4b9223483fcebb185d0d
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-27 12:17:54 +0900

    ls.rbのrubocop指摘部分の修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -12,11 +12,11 @@ def output(filenames)
   number_of_row = ((filenames.size - 1) / NUMBER_OF_COL) + 1
   filenames_table = filenames.each_slice(number_of_row).to_a
 
-  ljust_widths = filenames_table.map{|cols| cols.map(&:size).max + SPACE_WIDTH}
+  ljust_widths = filenames_table.map { |cols| cols.map(&:size).max + SPACE_WIDTH }
 
   number_of_row.times do |row|
     NUMBER_OF_COL.times do |col|
-      target_filename = filenames_table[col][row] #colとrowを入れ替えて出力させる
+      target_filename = filenames_table[col][row] # colとrowを入れ替えて出力させる
       print target_filename.ljust(ljust_widths[col]) unless target_filename.nil?
     end
     print "\n"
