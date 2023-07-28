18b36118536bf19ae959767a76895ac4a4612abc
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-28 11:44:52 +0900

    ls.rbのコメントを修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,13 +10,15 @@ end
 
 def output(filenames)
   number_of_row = ((filenames.size - 1) / NUMBER_OF_COL) + 1
+  # NOTE: filenames_tableの要素は行と列が出力したい形とは逆で保存されている
   filenames_table = filenames.each_slice(number_of_row).to_a
 
   widths = filenames_table.map { |filenames| filenames.map(&:size).max + SPACE_WIDTH }
 
   number_of_row.times do |row_index|
     NUMBER_OF_COL.times do |col_index|
-      target_filename = filenames_table[col_index][row_index] # colとrowを入れ替えて出力させる
+      # filenames_tableの行と列が逆で保存されているので、col_indexとrow_indexを入れ替えて出力させている
+      target_filename = filenames_table[col_index][row_index] 
       print target_filename.ljust(widths[col_index]) unless target_filename.nil?
     end
     print "\n"
