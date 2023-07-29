8ca86f11bb724ac215efa0e8e7ec83db4f3c1f64
reckyy <srreki98@gmail.com>
2023-07-29 12:24:51 +0900

    コンパクトに修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require 'debug'
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -39,18 +39,18 @@ def calc_strike_point(time, frames)
   end
 end
 
-point = 0
-frames.each_with_index do |frame, i|
-  point += if i < 9
-             if frame[0] == 10 # strike
-               calc_strike_point(i, frames)
-             elsif frame.sum == 10 # spare
-               10 + frames[(i + 1)][0]
-             else
-               frame.sum
-             end
-             elsif i == 9
-             frame.sum
-           end
+point = frames.each_with_index do |frame, i|
+  if i < 9
+    if frame[0] == 10 # strike
+      calc_strike_point(i, frames)
+    elsif frame.sum == 10 # spare
+      10 + frames[(i + 1)][0]
+    else
+      frame.sum
+    end
+    elsif i == 9
+    frame.sum
+  end
 end
+
 puts point
