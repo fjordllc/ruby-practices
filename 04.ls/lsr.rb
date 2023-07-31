2f96ebcc2729a7436a6f0a3a228997059cb1c546
ko <kyokucho1989@gmail.com>
2023-07-31 19:19:16 +0900

    1回目作成

---
+++ 04.ls/lsr.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+file_name_list = Dir.glob("*")
+
+file_list = []
+display_col_size = 3
+fold_point = file_name_list.size/display_col_size - 1
+file_name_list.each do |name|
+  file_list << {name: name}
+end
+
+display_col_size.times {
+  file_list << file_list.slice!(0..fold_point)
+}
+
+num = fold_point - file_list.last.size + 1
+num.times{
+  file_list.last << {name: ''}
+}
+
+disp_file_name_array = file_list.transpose
+
+disp_file_name_array.each do |file_array|
+  file_array.each do |file|
+      print file[:name].ljust(40)
+  end
+  puts ''
+end
