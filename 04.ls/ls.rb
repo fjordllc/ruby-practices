956b453dc5f5ed470d1fc6f45b3317f85525973d
tyrrell-IH <gotothertm@gmail.com>
2023-08-01 04:56:13 +0900

    コード内で使用されている英単語を一部省略形にしました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,8 +4,8 @@
 require 'optparse'
 require 'debug'
 
-NUMBER_OF_COLUMNS = 3
-MULTIPLE_OF_COLUMN_WIDTH = 8
+NUMBER_OF_COL = 3
+MULTIPLE_OF_COL_WIDTH = 8
 
 def select_option
   params = {}
@@ -16,30 +16,30 @@ def select_option
   params
 end
 
-def acquire_files(selected_directory:, a_option: false, r_option: false)
-  files = a_option ? Dir.glob('*', File::FNM_DOTMATCH, base: selected_directory) : Dir.glob('*', base: selected_directory)
+def acquire_files(selected_dir:, a_option: false, r_option: false)
+  files = a_option ? Dir.glob('*', File::FNM_DOTMATCH, base: selected_dir) : Dir.glob('*', base: selected_dir)
   r_option ? files.reverse : files
 end
 
-def transpose_by_each_columns(files, number_of_columns)
-  files << '' while files.size % number_of_columns != 0
-  numbers_of_lines = files.size / number_of_columns
+def transpose_by_each_col(files, number_of_col)
+  files << '' while files.size % number_of_col != 0
+  numbers_of_lines = files.size / number_of_col
   files.each_slice(numbers_of_lines).to_a.transpose
 end
 
 def get_column_width(files)
   maximum_number_of_characters = files.max_by(&:size).size
-  (maximum_number_of_characters.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
+  (maximum_number_of_characters.next..).find { |n| (n % MULTIPLE_OF_COL_WIDTH).zero? }
 end
 
-def generate_files_for_display(files, number_of_columns)
+def generate_files_for_display(files, number_of_col)
   column_width = get_column_width(files)
-  transposed_files = transpose_by_each_columns(files, number_of_columns)
+  transposed_files = transpose_by_each_col(files, number_of_col)
   transposed_files.map do |files_each_lines|
     files_each_lines.map { |file| file.ljust(column_width) }.join('')
   end
 end
 
 selected_option = select_option
-acquired_files = acquire_files(selected_directory: selected_option[:dir], a_option: selected_option[:a], r_option: selected_option[:r])
-puts generate_files_for_display(acquired_files, NUMBER_OF_COLUMNS)
+acquired_files = acquire_files(selected_dir: selected_option[:dir], a_option: selected_option[:a], r_option: selected_option[:r])
+puts generate_files_for_display(acquired_files, NUMBER_OF_COL)
