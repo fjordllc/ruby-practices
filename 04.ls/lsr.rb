472b5f5770731b0417b94b92a641cad386e47843
ko <kyokucho1989@gmail.com>
2023-07-31 19:24:47 +0900

    rubocop 実行

--- 04.ls/lsr.rb
+++ 04.ls/lsr.rb
@@ -2,29 +2,29 @@
 # frozen_string_literal: true
 
 require 'optparse'
-file_name_list = Dir.glob("*")
+file_name_list = Dir.glob('*')
 
 file_list = []
 display_col_size = 3
-fold_point = file_name_list.size/display_col_size - 1
+fold_point = file_name_list.size / display_col_size - 1
 file_name_list.each do |name|
-  file_list << {name: name}
+  file_list << { name: }
 end
 
-display_col_size.times {
+display_col_size.times do
   file_list << file_list.slice!(0..fold_point)
-}
+end
 
 num = fold_point - file_list.last.size + 1
-num.times{
-  file_list.last << {name: ''}
-}
+num.times do
+  file_list.last << { name: '' }
+end
 
 disp_file_name_array = file_list.transpose
 
 disp_file_name_array.each do |file_array|
   file_array.each do |file|
-      print file[:name].ljust(40)
+    print file[:name].ljust(40)
   end
   puts ''
 end
