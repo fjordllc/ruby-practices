f250b33013d70cd06d0fa187fa2078d8fb2fdb39
a-terumoto <a-terumoto@genbasupport.com>
2023-08-01 12:02:16 +0900

    3回目

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,15 @@
 a = (1..20).to_a
 
 a.each do |x|
+    x.to_i
     case x
-        when x % 3 == 0 && x % 5 == 0
-            puts "FizzBuzz"
-        when x % 3 == 0
-            puts "Fizz"
-        when x % 5 == 0
-            puts "Buzz"
-        else
-            puts x
+    when x % 15 == 0 
+        puts "FizzBuzz"
+    when x % 3 == 0
+        puts "Fizz"
+    when x % 5 == 0
+        puts "Buzz"
+    else
+        puts x
     end
 end
