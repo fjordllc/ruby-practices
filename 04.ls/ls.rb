5a641a1c1ef724884ca7e54e7e3c90d15fc85ba1
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-01 13:02:29 +0900

    ls.rbでファイル名が2つ以下の場合にエラーとなるバグを修正
    ファイル名が1or2の時、col_indexが2や3になっていて、filenames_tableが参照できていなかったため修正
    ファイル名が0のとき、number_of_rowが0となりeach_sliceができていなかったため修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,7 +3,7 @@
 
 require 'optparse'
 
-NUMBER_OF_COL = 3
+NUMBER_OF_COL_MAX = 3
 SPACE_WIDTH = 2
 
 def search_option
@@ -23,14 +23,20 @@ def get_filenames(target_path, params)
 end
 
 def output(filenames)
-  number_of_row = ((filenames.size - 1) / NUMBER_OF_COL) + 1
+  number_of_row = ((filenames.size - 1) / NUMBER_OF_COL_MAX) + 1
+  number_of_col =
+    if filenames.size < NUMBER_OF_COL_MAX
+      filenames.size
+    else
+      NUMBER_OF_COL_MAX
+    end
+  
   # NOTE: filenames_tableの要素は行と列が出力したい形とは逆で保存されている
   filenames_table = filenames.each_slice(number_of_row).to_a
-
   widths = filenames_table.map { |col| col.map(&:size).max + SPACE_WIDTH }
 
   number_of_row.times do |row_index|
-    NUMBER_OF_COL.times do |col_index|
+    number_of_col.times do |col_index|
       # filenames_tableの行と列が逆で保存されているので、col_indexとrow_indexを入れ替えて出力させている
       target_filename = filenames_table[col_index][row_index]
       print target_filename.ljust(widths[col_index]) unless target_filename.nil?
@@ -41,5 +47,5 @@ end
 
 option_params = search_option
 target_path = ARGV[0] || './'
-filenames = get_filenames(target_path, option_params)
-output(filenames)
+filenames = get_filenames(target_path, option_params) 
+output(filenames) unless filenames.empty?
