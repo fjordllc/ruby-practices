47bdf2455b91c9ed8e0d7fc3cd8966d71b463308
ktgwShota <ktgw.shota.1998@gmail.com>
2023-08-01 13:39:49 +0900

    -aオプションを追加した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 def file_entries
-  Dir.glob('*')
+  if ARGV[0] == '-a'
+    Dir.glob('**/*', File::FNM_DOTMATCH)
+  else
+    Dir.glob('[^.]*')
+  end
 end
 
 def align_left(string, entries)
   max_length = entries.flatten.map { |entry| entry.to_s.size + 5 }.max
-  string.to_s.ljust(max_length) # 引数1を左揃え（取得した名称の最大文字数に+5した数値で揃える）
+  string.to_s.ljust(max_length)
 end
 
-entries = file_entries.each_slice((file_entries.size.to_f / 3).ceil).to_a 
-entries_max_size = entries.map(&:size).max 
+entries = file_entries.each_slice((file_entries.size.to_f / 3).ceil).to_a
+entries_max_size = entries.map(&:size).max
 
 new_entries = entries.map do |entry| # 二次元配列の要素数が均等じゃなかった場合、均等になるようにnilを追加
   entry.concat([nil] * (entries_max_size - entry.size)) if entries_max_size > entry.size
