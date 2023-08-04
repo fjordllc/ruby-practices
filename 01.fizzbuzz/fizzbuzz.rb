dba8c5deeb14987071e3bdfa101a425d3fd4af24
Goto-star <goyu620@gmail.com>
2023-08-04 17:12:21 +0900

    change fizzbuzz

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
-numbers = [1..20]
+numbers = (1..20).to_a
 
 numbers.each do |number|
-  if number % 3 == 0 && number 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     puts "FizzBuz"
   elsif number % 3 == 0
     puts "Fizz"
-  elsif
+  elsif number % 5 == 0
     puts "Buzz"
   else
     puts number
