fca5bc17ea9d6edb395484dca75892ba55900470
tyrrell-IH <gotothertm@gmail.com>
2023-08-06 22:36:51 +0900

    変数名、定数名でcol表記になっていたものをcolumn(s)へ変更しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,8 +4,8 @@
 require 'optparse'
 require 'debug'
 
-NUMBER_OF_COL = 3
-MULTIPLE_OF_COL_WIDTH = 8
+NUMBER_OF_COLUMNS = 3
+MULTIPLE_OF_COLUMN_WIDTH = 8
 
 def select_option
   params = {}
@@ -21,20 +21,20 @@ def acquire_files(selected_dir:, a_option: false, r_option: false)
   r_option ? files.reverse : files
 end
 
-def transpose_by_each_col(files, number_of_col)
+def transpose_by_each_columns(files, number_of_col)
   files << '' while files.size % number_of_col != 0
   numbers_of_lines = files.size / number_of_col
   files.each_slice(numbers_of_lines).to_a.transpose
 end
 
-def get_col_width(files)
+def get_column_width(files)
   maximum_number_of_characters = files.max_by(&:size).size
-  (maximum_number_of_characters.next..).find { |n| (n % MULTIPLE_OF_COL_WIDTH).zero? }
+  (maximum_number_of_characters.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
 
 def generate_files_for_display(files, number_of_col)
-  col_width = get_col_width(files)
-  transposed_files = transpose_by_each_col(files, number_of_col)
+  col_width = get_column_width(files)
+  transposed_files = transpose_by_each_columns(files, number_of_col)
   transposed_files.map do |files_each_lines|
     files_each_lines.map { |file| file.ljust(col_width) }.join('')
   end
@@ -42,4 +42,4 @@ end
 
 options = select_option
 acquired_files = acquire_files(selected_dir: options[:dir], a_option: options[:a], r_option: options[:r])
-puts generate_files_for_display(acquired_files, NUMBER_OF_COL)
+puts generate_files_for_display(acquired_files, NUMBER_OF_COLUMNS)
