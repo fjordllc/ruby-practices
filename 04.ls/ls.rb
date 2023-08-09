839156f1b7e56bbb49e75ea19cbb55403c0fcc09
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-09 10:49:36 +0900

    ls.rbのトップレベルの変数名を変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -47,7 +47,7 @@ def output(filenames)
   end
 end
 
-option_params, arguments_without_options = search_option(ARGV)
-target_path = arguments_without_options[0] || './'
+option_params, file_paths = search_option(ARGV)
+target_path = file_paths[0] || './'
 filenames = get_filenames(target_path, option_params)
 output(filenames)
