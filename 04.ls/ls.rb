47bdda5ce7aad939d7df37bef148e86099eedf53
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-09 11:02:09 +0900

    ls.rbの変数paramsをoptionsに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,14 +8,14 @@ SPACE_WIDTH = 2
 
 def search_option(argv)
   opt = OptionParser.new
-  params = {}
-  opt.on('-a') { |v| params[:a] = v }
+  options = {}
+  opt.on('-a') { |v| options[:a] = v }
   file_paths = opt.parse(argv)
-  return params, file_paths
+  return options, file_paths
 end
 
-def get_filenames(target_path, params)
-  if params[:a]
+def get_filenames(target_path, options)
+  if options[:a]
     Dir.glob('*', File::FNM_DOTMATCH, base: target_path, sort: true)
   else
     Dir.glob('*', base: target_path, sort: true)
@@ -27,7 +27,7 @@ def output(filenames)
 
   number_of_row = ((filenames.size - 1) / NUMBER_OF_COL_MAX) + 1
   number_of_col = filenames.size < NUMBER_OF_COL_MAX ? filenames.size : NUMBER_OF_COL_MAX
-  
+
   # NOTE: OS標準のlsコマンドは横並びではなく縦並びで出力される(転置して出力される)
   # NOTE: filenames_tableの要素は行と列が出力したい形(縦並び)とは逆で保存されている
   filenames_table = filenames.each_slice(number_of_row).to_a
@@ -43,7 +43,7 @@ def output(filenames)
   end
 end
 
-params, file_paths = search_option(ARGV)
+options, file_paths = search_option(ARGV)
 target_path = file_paths[0] || './'
-filenames = get_filenames(target_path, params)
+filenames = get_filenames(target_path, options)
 output(filenames)
