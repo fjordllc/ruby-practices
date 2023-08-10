8087ff3fd7bb59e0c667c5c6721dc01b90152345
GithubMuftah <muftah@i3-systems.com>
2023-08-10 13:51:23 +0900

    fix variable

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
-1.upto(20) do |bango|
-  if bango % 3 == 0 and bango % 5 == 0
+1.upto(20) do |number|
+  if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
-  elsif bango % 3 == 0
+  elsif number % 3 == 0
     puts "Fizz"
-  elsif bango % 5 == 0
+  elsif number % 5 == 0
     puts "Buzz"
   else
-    puts bango
+    puts number
   end
 end
