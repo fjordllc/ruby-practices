3a4954dcbd1cab0a2f98a40d1e102e29e5c5f82d
GithubMuftah <muftah@i3-systems.com>
2023-08-15 10:45:54 +0900

    fix loop and variable naming

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,37 +3,21 @@
 scores = ARGV[0].split(',').to_a
 
 total_score = 0
-index = 0
-frame = 1
+current_position = 0
 
-while frame < 11
-  if scores[index] == 'X'
-    total_score += 10
-    total_score += if scores[index + 1] == 'X'
-                     10
-                   else
-                     scores[index + 1].to_i
-                   end
+def check_if_x(score)
+  score == 'X' ? 10 : score.to_i
+end
 
-    total_score += if scores[index + 2] == 'X'
-                     10
-                   else
-                     scores[index + 2].to_i
-                   end
-
-    index += 1
+1.upto(10) do
+  if scores[current_position] == 'X'
+    total_score += 10 + check_if_x(scores[current_position + 1]) + check_if_x(scores[current_position + 2])
+    current_position += 1
   else
-    temp = scores[index].to_i + scores[index + 1].to_i
-    total_score += if temp < 10
-                     temp
-                   elsif scores[index + 2] == 'X'
-                     (temp + 10)
-                   else
-                     (temp + scores[index + 2].to_i)
-                   end
-    index += 2
+    frame_score = scores[current_position].to_i + scores[current_position + 1].to_i
+    total_score += frame_score < 10 ? frame_score : frame_score + check_if_x(scores[current_position + 2])
+    current_position += 2
   end
-  frame += 1
 end
 
 puts total_score
