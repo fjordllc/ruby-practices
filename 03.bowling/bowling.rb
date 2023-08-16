b83934e78e2ffaf09fccbce00c5d0d6f1c8e4a67
GithubMuftah <muftah@i3-systems.com>
2023-08-16 11:46:31 +0900

    change variable naming

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-scores = ARGV[0].split(',').to_a
+scoreboard = ARGV[0].split(',').to_a
 
 total_score = 0
 current_position = 0
 
-def check_if_x(score)
-  score == 'X' ? 10 : score.to_i
+def convert_ten_if_score_is_x(current_score)
+  current_score == 'X' ? 10 : current_score.to_i
 end
 
 1.upto(10) do
-  if scores[current_position] == 'X'
-    total_score += 10 + check_if_x(scores[current_position + 1]) + check_if_x(scores[current_position + 2])
+  if scoreboard[current_position] == 'X'
+    total_score += 10 + convert_ten_if_score_is_x(scoreboard[current_position + 1]) + convert_ten_if_score_is_x(scoreboard[current_position + 2])
     current_position += 1
   else
-    frame_score = scores[current_position].to_i + scores[current_position + 1].to_i
-    total_score += frame_score < 10 ? frame_score : frame_score + check_if_x(scores[current_position + 2])
+    frame_score = scoreboard[current_position].to_i + scoreboard[current_position + 1].to_i
+    total_score += frame_score < 10 ? frame_score : frame_score + convert_ten_if_score_is_x(scoreboard[current_position + 2])
     current_position += 2
   end
 end
