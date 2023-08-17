28a24a8365fdb525790ade4928bc568c5d99d51d
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-17 20:02:34 +0900

    オプションごとに、実行される処理を変更するように変更

--- 04.ls/my-ls.rb
+++ 04.ls/my-ls.rb
@@ -4,37 +4,16 @@
 
 require 'optparse'
 
-SEGMENT_LENGTH = 3
+options = {}
 
-def divide_into_segments(entities)
-  entities.each_slice((entities.length + 2) / SEGMENT_LENGTH).to_a
-end
-
-def transpose(entities)
-  max_size = entities.map(&:size).max
-  entities.map! { |items| items.values_at(0...max_size) }
-  entities.transpose
-end
-
-def list_filenames(params)
-  return Dir.glob('*').reverse if params[:r]
-
-  Dir.glob('*')
-end
-
-opt = OptionParser.new
-params = {}
-opt.on('-r') { |v| params[:r] = v }
-opt.parse!(ARGV)
-
-under_entities = list_filenames(params)
-divided_entities = divide_into_segments(under_entities)
-longest_entity_length = divided_entities.flatten.max_by(&:length).length
-transposed_entities = transpose(divided_entities)
-
-transposed_entities.each do |column_entitites|
-  column_entitites.each do |column_entitiy|
-    print column_entitiy&.ljust(longest_entity_length + 1)
+OptionParser.new do |opts|
+  opts.on("-l", "--list-details") do
+    options[:details] = true
   end
-  print("\n")
+end.parse!
+
+if options[:details]
+  list_entities_in_details(Dir.glob('*'))
+else
+  list_entities(Dir.glob('*'))
 end
