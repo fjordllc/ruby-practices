b0a9fe555946e05036453032b104d362768d5ec3
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-17 20:03:15 +0900

    コマンドがオプションなしに実行された時の処理の実装

--- 04.ls/my-ls.rb
+++ 04.ls/my-ls.rb
@@ -4,6 +4,31 @@
 
 require 'optparse'
 
+SEGMENT_LENGTH = 3
+
+def divide_into_segments(entities)
+  entities.each_slice((entities.length + 2) / SEGMENT_LENGTH).to_a
+end
+
+def transpose(entities)
+  max_size = entities.map(&:size).max
+  entities.map! { |items| items.values_at(0...max_size) }
+  entities.transpose
+end
+
+def list_entities(entities)
+  divided_entities = divide_into_segments(entities)
+  longest_entity_length = divided_entities.flatten.max_by(&:length).length
+  transposed_entities = transpose(divided_entities)
+
+  transposed_entities.each do |column_entitites|
+    column_entitites.each do |column_entitiy|
+      print column_entitiy&.ljust(longest_entity_length + 1)
+    end
+    print("\n")
+  end
+end
+
 options = {}
 
 OptionParser.new do |opts|
