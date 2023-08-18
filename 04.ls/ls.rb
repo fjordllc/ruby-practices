d55b7909c0b5d6f0f4a5a14dbdec6b1ea1af80eb
reckyy <srreki98@gmail.com>
2023-08-18 11:26:51 +0900

    ls_v1作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,8 +1,18 @@
+require 'debug'
 
-all_file = Dir.glob('*')
-div = all_file.size / 3
-amari = all_file.size % 3
-total_row = (amari == 0) ? div : (div + 1)
-max_length = all_file.max { |a, b| a.length <=> b.length }.size
-all_sort_file = all_file.each_slice(3).to_a.sort
-p all_sort_file
+def ls_v1
+  all_file = Dir.glob('*').sort
+  div = all_file.size / 3
+  amari = all_file.size % 3
+  total_row = (amari == 0) ? div : (div + 1)
+  width = all_file.max { |a, b| a.length <=> b.length }.size + 7
+  all_sort_file = all_file.each_slice(3).to_a
+  0.upto(total_row - 1) do |idx|
+    3.times do |time|
+      print all_sort_file[idx][time].ljust(width)
+    end
+    puts
+  end
+end
+
+ls_v1
