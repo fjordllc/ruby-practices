86e81364a5316b8e37c3e619b9e28563fc38e1ca
reckyy <srreki98@gmail.com>
2023-08-18 15:35:26 +0900

    共通処理をメソッド化

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,14 @@
 require 'debug'
-
-def ls_v1
+def parse_file
   all_file = Dir.glob('*').sort
   div = all_file.size / 3
   amari = all_file.size % 3
   total_row = (amari == 0) ? div : (div + 1)
   width = all_file.max { |a, b| a.length <=> b.length }.size + 7
+  [all_file, total_row, width]
+end
+
+def ls_v1(all_file, total_row, width)
   all_sort_file = all_file.each_slice(3).to_a
   total_row.times do |row|
     3.times do |col|
@@ -15,18 +18,15 @@ def ls_v1
   end
 end
 
-ls_v1
+all_file, total_row, width = parse_file
 
-def ls_v2
-  all_file = Dir.glob('*').sort
-  div = all_file.size / 3
-  amari = all_file.size % 3
-  total_row = (amari == 0) ? div : (div + 1)
-  width = all_file.max { |a, b| a.length <=> b.length }.size + 7
+ls_v1(all_file, total_row, width)
 
+def ls_v2(all_file, total_row, width)
   grouped_files = all_file.map.with_index do |file, idx|
     [idx % 3, file]
   end.sort.each_slice(3).to_a
+
   total_row.times do |row|
     3.times do |col|
       print grouped_files[row][col][1].ljust(width)
@@ -35,4 +35,4 @@ def ls_v2
   end
 end
 
-ls_v2
+ls_v2(all_file, total_row, width)
