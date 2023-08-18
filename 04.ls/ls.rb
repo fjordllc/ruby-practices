956807bb0346ffa0eca0c53eda152256efd5d84c
reckyy <srreki98@gmail.com>
2023-08-18 12:22:25 +0900

    ls_v2作成
    あまり、ls_v1と違いはないような気もするが、こっちの方がすっきり見える。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,4 +15,24 @@ def ls_v1
   end
 end
 
-ls_v1
+
+
+def ls_v2
+  all_file = Dir.glob('*').sort
+  div = all_file.size / 3
+  amari = all_file.size % 3
+  total_row = (amari == 0) ? div : (div + 1)
+  width = all_file.max { |a, b| a.length <=> b.length }.size + 7
+
+  grouped_files = all_file.map.with_index do |file, idx|
+    [idx % 3, file]
+  end.sort.each_slice(3).to_a
+  total_row.times do |row|
+    3.times do |col|
+      print grouped_files[row][col][1].ljust(width)
+    end
+    puts
+  end
+end
+
+ls_v2
