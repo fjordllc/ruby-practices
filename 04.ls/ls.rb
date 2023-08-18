a0a514af1126a73337773b8c5aac7b7a77a4fb34
reckyy <srreki98@gmail.com>
2023-08-18 10:50:32 +0900

    取得したファイルをsortして3つずつに分けた

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,2 +1,8 @@
 
 all_file = Dir.glob('*')
+div = all_file.size / 3
+amari = all_file.size % 3
+total_row = (amari == 0) ? div : (div + 1)
+max_length = all_file.max { |a, b| a.length <=> b.length }.size
+all_sort_file = all_file.each_slice(3).to_a.sort
+p all_sort_file
