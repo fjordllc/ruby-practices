d77409e4ed6de9fb86850c59449b8ede8a1971f7
GithubMuftah <muftah@i3-systems.com>
2023-08-18 17:26:41 +0900

    show folder list feature added

--- 04.ls/command.rb
+++ 04.ls/command.rb
@@ -1,23 +1,19 @@
 # frozen_string_literal: true
 
-current_path = if ARGV[0].nil? then __dir__ else ARGV[0] end
+current_path = ARGV[0].nil? ? __dir__ : ARGV[0]
 
 def empty_space_length(filename)
-  50 - filename.length
-end
-
-def contains_cjk?
-  !!(self =~ /\p{Han}|\p{Katakana}|\p{Hiragana}\p{Hangul}/)
+  40 - filename.length
 end
 
 def formatted_print(current_file)
   print(current_file + ' ' * empty_space_length(current_file))
 end
 
-file_list = Dir.entries(current_path).select { |f| File.file? File.join(current_path, f) }
+file_list = Dir.glob("#{current_path}/*")
 
 0.upto(file_list.length - 1) do |index|
-  formatted_print(file_list[index])
+  formatted_print(File.basename(file_list[index]))
   puts '' if ((index + 1) % 3).zero?
 end
 
