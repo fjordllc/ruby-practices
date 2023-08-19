df780c7cec8ed8add9b51676450b0cbc39ff5ddf
reckyy <srreki98@gmail.com>
2023-08-19 11:09:35 +0900

    ls_v1コマンドを修正
    ファイル名表示の順番を間違えていたので、配列をtotal_rowごとにsliceして、
    順番に表示されるように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,10 +9,10 @@ def parse_file
 end
 
 def ls_v1(all_file, total_row, width)
-  all_sort_file = all_file.each_slice(3).to_a
+  all_sort_file = all_file.each_slice(5).to_a
   total_row.times do |row|
     3.times do |col|
-      file_name = all_sort_file[row][col]
+      file_name = all_sort_file[col][row]
       print file_name.ljust(width) unless file_name.nil?
     end
     puts
@@ -27,7 +27,6 @@ def ls_v2(all_file, total_row, width)
   grouped_files = all_file.map.with_index do |file, idx|
     [idx % 3, file]
   end.sort.each_slice(3).to_a
-
   total_row.times do |row|
     3.times do |col|
       file_name = grouped_files[row][col]
