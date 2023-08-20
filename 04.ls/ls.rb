11b6a1dd900b0d0d3fc74ecd2d45a8a66f5711df
reckyy <srreki98@gmail.com>
2023-08-20 11:29:10 +0900

    rubocop修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,8 +5,8 @@ initial_row = 3
 def parse_file(initial_row)
   all_file = Dir.glob('*').sort
   div, mod = all_file.size.divmod(initial_row)
-  total_row = (mod == 0) ? div : (div + 1)
-  width = all_file.max { |a, b| a.length <=> b.length }.size + 7
+  total_row = mod.zero? ? div : (div + 1)
+  width = all_file.max_by(&:length).length + 7
   [all_file, total_row, width]
 end
 
@@ -18,7 +18,6 @@ def ls_v1(initial_row, all_file, total_row, width)
     initial_row.times do |row|
       file_name = all_sort_file[row][col]
       print file_name.ljust(width) unless file_name.nil?
-      binding.break
     end
     puts
   end
@@ -29,7 +28,8 @@ ls_v1(initial_row, all_file, total_row, width)
 def ls_v2(initial_row, all_file, total_row, width)
   grouped_files = all_file.map.with_index do |file, idx|
     [idx % total_row, file]
-  end.group_by { |div, file| div }
+  end
+  grouped_files = grouped_files.group_by { |div, _| div }
   total_row.times do |row|
     initial_row.times do |col|
       file_name = grouped_files[row][col]
