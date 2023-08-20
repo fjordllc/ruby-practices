dcecdeb65b039047508b8078d5689fde37d9cc0e
reckyy <srreki98@gmail.com>
2023-08-20 11:42:09 +0900

    メソッドを分岐
    parse_fileメソッドに行数を求めるファイル解析とは関係ない処理が混在していたので、
    それらをcalculateメソッドに分離

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,13 +4,18 @@ initial_row = 3
 
 def parse_file(initial_row)
   all_file = Dir.glob('*').sort
+  all_file
+end
+
+def calculate_row_and_space(initial_row, all_file)
   div, mod = all_file.size.divmod(initial_row)
   total_row = mod.zero? ? div : (div + 1)
   width = all_file.max_by(&:length).length + 7
-  [all_file, total_row, width]
+  [total_row, width]
 end
 
-all_file, total_row, width = parse_file(initial_row)
+all_file = parse_file(initial_row)
+total_row, width = calculate_row_and_space(initial_row, all_file)
 
 def ls_v1(initial_row, all_file, total_row, width)
   all_sort_file = all_file.each_slice(total_row).to_a
@@ -22,8 +27,6 @@ def ls_v1(initial_row, all_file, total_row, width)
     puts
   end
 end
-
-ls_v1(initial_row, all_file, total_row, width)
 
 def ls_v2(initial_row, all_file, total_row, width)
   grouped_files = all_file.map.with_index do |file, idx|
@@ -39,4 +42,5 @@ def ls_v2(initial_row, all_file, total_row, width)
   end
 end
 
+ls_v1(initial_row, all_file, total_row, width)
 ls_v2(initial_row, all_file, total_row, width)
