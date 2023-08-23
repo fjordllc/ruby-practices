04ad07aca5b7f947ffb1b7fdaced759fb723b158
GithubMuftah <muftah@i3-systems.com>
2023-08-23 18:03:23 +0900

    fix constant

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-strike = 10
+STRIKE = 10
 total_score = 0
 current_position = 0
 
-scoreboard = ARGV[0].split(',').collect { |score| score == 'X' ? strike : score.to_i }
+scoreboard = ARGV[0].split(',').collect { |score| score == 'X' ? STRIKE : score.to_i }
 
 1.upto(10) do
-  if scoreboard[current_position] == strike
-    total_score += strike + scoreboard[current_position + 1] + scoreboard[current_position + 2]
+  if scoreboard[current_position] == STRIKE
+    total_score += STRIKE + scoreboard[current_position + 1] + scoreboard[current_position + 2]
     current_position += 1
   else
     frame_score = scoreboard[current_position].to_i + scoreboard[current_position + 1].to_i
-    total_score += frame_score < strike ? frame_score : frame_score + scoreboard[current_position + 2]
+    total_score += frame_score < STRIKE ? frame_score : frame_score + scoreboard[current_position + 2]
     current_position += 2
   end
 end
