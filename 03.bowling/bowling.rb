8ba50fa18c0d5fdf7ad773c3db18a2a63383f26c
GithubMuftah <muftah@i3-systems.com>
2023-08-23 10:25:46 +0900

    Fix array splitting and variable naming

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
-scoreboard = ARGV[0].split(',').to_a.collect { |score| score.casecmp('X').zero? ? 10 : score.to_i }
-
+strike = 10
 total_score = 0
 current_position = 0
 
+scoreboard = ARGV[0].split(',').collect { |score| score == 'X' ? strike : score.to_i }
+
 1.upto(10) do
-  if scoreboard[current_position] == 10
-    total_score += 10 + scoreboard[current_position + 1] + scoreboard[current_position + 2]
+  if scoreboard[current_position] == strike
+    total_score += strike + scoreboard[current_position + 1] + scoreboard[current_position + 2]
     current_position += 1
   else
     frame_score = scoreboard[current_position].to_i + scoreboard[current_position + 1].to_i
-    total_score += frame_score < 10 ? frame_score : frame_score + scoreboard[current_position + 2]
+    total_score += frame_score < strike ? frame_score : frame_score + scoreboard[current_position + 2]
     current_position += 2
   end
 end
