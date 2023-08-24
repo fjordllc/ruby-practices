ca6dbcd194b04f79286cc78cc79b2df2b5495a61
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-24 17:45:36 +0900

    -aと-rオプションが与えられた時の処理を追加

--- 04.ls/my-ls.rb
+++ 04.ls/my-ls.rb
@@ -4,9 +4,40 @@
 
 require 'optparse'
 
+SEGMENT_LENGTH = 3
+
+def divide_into_segments(entities)
+  entities.each_slice((entities.length + 2) / SEGMENT_LENGTH).to_a
+end
+
+def transpose(entities)
+  max_size = entities.map(&:size).max
+  entities.map! { |items| items.values_at(0...max_size) }
+  entities.transpose
+end
+
+def list_filenames(params)
+  filenames = Dir.glob('*')
+  filenames = Dir.entries('.') if params[:a]
+  filenames = filenames.reverse if params[:r]
+  filenames
+end
+
 opt = OptionParser.new
 params = {}
 opt.on('-a') { |v| params[:a] = v }
 opt.on('-r') { |v| params[:r] = v }
 opt.on('-l') { |v| params[:l] = v }
 opt.parse!(ARGV)
+
+under_entities = list_filenames(params)
+divided_entities = divide_into_segments(under_entities)
+longest_entity_length = divided_entities.flatten.max_by(&:length).length
+transposed_entities = transpose(divided_entities)
+
+transposed_entities.each do |column_entitites|
+  column_entitites.each do |column_entitiy|
+    print column_entitiy&.ljust(longest_entity_length + 1)
+  end
+  print("\n")
+end
