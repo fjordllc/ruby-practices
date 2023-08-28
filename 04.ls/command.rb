dcea67b6fd6ac8c51535cbf139393c0d82f781f3
GithubMuftah <muftah@i3-systems.com>
2023-08-28 11:08:00 +0900

    fix looping logic

--- 04.ls/command.rb
+++ 04.ls/command.rb
@@ -1,22 +1,40 @@
 # frozen_string_literal: true
 
-current_path = ARGV[0].nil? ? __dir__ : ARGV[0]
+CURRENT_PATH = ARGV[0].nil? ? __dir__ : ARGV[0]
 MARGIN = 3
 
-def get_column_spacing(file_list)
-  file_list.max_by(&:length).length + MARGIN
+def add_spacing(filename)
+  filename.ljust(COLUMN_SPACING + MARGIN)
 end
 
-file_list = Dir.glob("#{current_path}/*").collect! { |file_path| File.basename(file_path) }
+def formatted_print(ordered_file_list)
+  0.upto(ROW_COUNT - 1) do |index|
+    first_column = ordered_file_list[0][index]
+    second_column = ordered_file_list[1][index].nil? ? ' ' : ordered_file_list[1][index]
+    third_column = ordered_file_list[2][index].nil? ? ' ' : ordered_file_list[2][index]
+
+    print(add_spacing(first_column) + add_spacing(second_column) + third_column)
+    puts('')
+  end
+end
+
+file_list = Dir.glob("#{CURRENT_PATH}/*")
 file_count = file_list.length
-row_count = (file_count / 3.to_f).ceil
-column_spacing = get_column_spacing(file_list)
+ROW_COUNT = (file_count / 3.to_f).ceil
 
-0.upto(row_count - 1) do |index|
-  print(File.basename(file_list[index]).ljust(column_spacing))
-  row_count + index <= file_count - 1 ? print(File.basename(file_list[row_count + index]).ljust(column_spacing)) : print(' '.ljust(column_spacing))
-  row_count * 2 + index <= file_count - 1 ? print(File.basename(file_list[row_count * 2 + index])) : print(' ')
-  puts ''
+max_length = 0
+ordered_file_list = []
+current_column = 0
+
+file_list.each_with_index do |file_path, index|
+  if (index % ROW_COUNT).zero? || index.zero?
+    ordered_file_list.append([])
+    current_column += 1 if index.positive?
+  end
+  file_name = File.basename(file_path)
+  max_length = file_name.length if file_name.length > max_length
+  ordered_file_list[current_column].append(file_name)
 end
 
-puts ''
+COLUMN_SPACING = max_length
+formatted_print(ordered_file_list)
