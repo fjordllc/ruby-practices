420ab91117b85c30ec0d69fca63115c78cdf6458
appsu-mi <natubate870@gmail.com>
2023-08-28 10:40:01 +0900

    FizzBuzz_issue

---
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -0,0 +1,14 @@
+num = 0
+
+20.times do
+  num += 1
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+  end
+end
