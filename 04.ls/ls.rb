9f39e72c4a6801102f75aeaccd616e9d01e42f2a
reckyy <srreki98@gmail.com>
2023-08-28 20:42:08 +0900

    inital_rowを変更
    3列に指定されているので、定数に変更、かつ
    initial_rowではなく列なので、INITIAL_COLUMNに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
+INITIAL_COLUMN = 3
+
 def parse_file
   Dir.glob('*').sort
 end
 
-def calculate_row_and_space(initial_row, all_files)
-  div, mod = all_files.size.divmod(initial_row)
+def calculate_row_and_space(all_files)
+  div, mod = all_files.size.divmod(INITIAL_COLUMN)
   total_row = mod.zero? ? div : (div + 1)
   width = all_files.max_by(&:length).length + 7
   [total_row, width]
 end
 
-initial_row = 3
 all_files = parse_file
-total_row, width = calculate_row_and_space(initial_row, all_files)
+total_row, width = calculate_row_and_space(all_files)
 argument = {
-  initial_row: 3,
   all_files: parse_file,
   total_row:,
   width:
 }
 
-def ls_v1(initial_row:, all_files:, total_row:, width:)
+def ls_v1(all_files:, total_row:, width:)
   all_sort_files = all_files.each_slice(total_row).to_a
   total_row.times do |col|
-    initial_row.times do |row|
+    INITIAL_COLUMN.times do |row|
       file_name = all_sort_files[row][col]
       print file_name.ljust(width) unless file_name.nil?
     end
@@ -32,13 +32,13 @@ def ls_v1(initial_row:, all_files:, total_row:, width:)
   end
 end
 
-def ls_v2(initial_row:, all_files:, total_row:, width:)
+def ls_v2(all_files:, total_row:, width:)
   grouped_files = all_files.map.with_index do |file, idx|
     [idx % total_row, file]
   end
   grouped_files = grouped_files.group_by { |div, _| div }
   total_row.times do |row|
-    initial_row.times do |col|
+    INITIAL_COLUMN.times do |col|
       file_name = grouped_files[row][col]
       print file_name[1].ljust(width) unless file_name.nil?
     end
