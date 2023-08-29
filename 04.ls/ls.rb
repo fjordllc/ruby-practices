10eb755353483d4a6865f2035027de9350f1c5e7
reckyy <srreki98@gmail.com>
2023-08-29 21:24:23 +0900

    .で始まるファイルを取得するように変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+require 'optparse'
 
 INITIAL_COLUMN = 3
 
 def parse_file
-  Dir.glob('*').sort
+  options = ARGV.getopts('a')
+  options['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
 end
 
 def calculate_row_and_space(all_files)
