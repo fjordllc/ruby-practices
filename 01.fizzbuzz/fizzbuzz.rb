7a4ab0d56610af53437a767612cf63b5c1c08800
shunhamm <shun.022885@gmail.com>
2023-09-06 15:39:54 +0900

    create fizzbuzz method

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,18 @@
+class Fizzbuzz
+
+    def puts_fizzbuzz()
+        for num in 1 .. 20
+            if num % 3 == 0 && num % 5 == 0
+                puts "FizzBuzz"
+            elsif num % 3 == 0
+                puts "Fizz"
+            elsif num % 5 == 0
+                puts "Buzz"
+            else
+                puts num
+            end
+        end 
+    end
+end
+
+fizzbuzz = Fizzbuzz.new().puts_fizzbuzz()
