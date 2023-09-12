7ab5bd9ce4651946748e7ee01838b0d72562f54b
shunhamm <shun.022885@gmail.com>
2023-09-12 16:13:10 +0900

    convert for func to each method

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,18 +1,14 @@
-class Fizzbuzz
+# !ruby
+# frozen_string_literal: true
 
-    def puts_fizzbuzz()
-        for num in 1 .. 20
-            if num % 3 == 0 && num % 5 == 0
-                puts "FizzBuzz"
-            elsif num % 3 == 0
-                puts "Fizz"
-            elsif num % 5 == 0
-                puts "Buzz"
-            else
-                puts num
-            end
-        end 
-    end
+(1...20).each do |num|
+  if (num % 3) == 0 && (num % 5) == 0
+    puts 'FizzBuzz'
+  elsif (num % 3) == 0
+    puts 'Fizz'
+  elsif (num % 5) == 0
+    puts 'Buzz'
+  else
+    puts num
+  end
 end
-
-Fizzbuzz.new().puts_fizzbuzz()
