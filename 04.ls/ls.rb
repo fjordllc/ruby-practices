52aabd566a13d75676f1d55339c7c33e56cd65c1
shunhamm <shun.022885@gmail.com>
2023-09-21 18:44:21 +0900

    Lsmethodsの中身を作成。オプションなしのlsメソッドを実装

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,12 +1,11 @@
 #!ruby
 
 require_relative 'ls_methods'
+def main
+  p LsMethods.new.ls_without_any_options
 
-# 現在のディレクトリ配下の全てのファイルシステムを取得
-pwd = fetch_present_working_directory
-contents_list = get_current_directory_contents(pwd)
-
-# ls の結果を表示
-contents_list.each do |content|
-  print "#{content} "
+  # 現在のディレクトリ配下の全てのファイルシステムを取得
+  # pwd = fetch_present_working_directory
 end
+
+main
--- 04.ls/ls_methods.rb
+++ 04.ls/ls_methods.rb
@@ -1,9 +1,24 @@
 class LsMethods
-  def self.fetch_present_working_directory
-    pass
+  def initialize(path = '.')
+    @all_contents = Dir.entries(path)
   end
 
-  def self.get_current_directory_contents(pwd)
-    pass
+  def ls_without_any_options
+    filtered_contents = @all_contents.filter { |content| content !~ /^\..*$/ }
+    show_ls(filtered_contents)
+  end
+
+  def show_ls(contents)
+    height = (contents.length / 3.0).ceil
+    # 行のナンバリング
+    (0...height).each do |h_num|
+      # 列のナンバリング
+      3.times do |w_num|
+        contents_index = h_num + (height * w_num)
+        print "#{contents[contents_index]}".ljust(20) if !contents[contents_index].nil? # rubocop:disable all
+      end
+      puts
+    end
+    nil
   end
 end
