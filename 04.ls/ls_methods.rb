40b59e2f24222c6e9a62da12e7dca0a069121007
shunhamm <shun.022885@gmail.com>
2023-09-21 18:49:29 +0900

    コメントを追加, modeを追加

--- 04.ls/ls_methods.rb
+++ 04.ls/ls_methods.rb
@@ -1,10 +1,10 @@
 class LsMethods
   def initialize(path = '.')
-    @all_contents = Dir.entries(path)
+    @all_contents = Dir.entries(path) # path配下にあるhidden fileを含む全てのファイルシステムを取得
   end
 
   def ls_without_any_options
-    filtered_contents = @all_contents.filter { |content| content !~ /^\..*$/ }
+    filtered_contents = @all_contents.filter { |content| content !~ /^\..*$/ } # hidden file をファイル
     show_ls(filtered_contents)
   end
 
@@ -12,7 +12,7 @@ class LsMethods
     height = (contents.length / 3.0).ceil
     # 行のナンバリング
     (0...height).each do |h_num|
-      # 列のナンバリング
+      # 列のナンバリング　今回は最大３列までなので最大３つまで表示したら次の行に折り返す。
       3.times do |w_num|
         contents_index = h_num + (height * w_num)
         print "#{contents[contents_index]}".ljust(20) if !contents[contents_index].nil? # rubocop:disable all
