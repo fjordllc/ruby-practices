b1a3c826c6f08506937c644649cceb5cd338bfe8
yota sato <jingangsihaiuei@gmail.com>
2023-09-25 14:34:29 +0900

    ファイルFizzBuzz.rbの追加

---
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -0,0 +1,11 @@
+(1..20).each do |num|
+    if(num % 3 == 0 && num % 5 == 0)
+        puts "FizzBuzz"
+    elsif(num % 5 == 0)
+        puts "Buzz"
+    elsif(num % 3 == 0)
+        puts "Fizz"
+    else
+        puts num
+    end
+end
