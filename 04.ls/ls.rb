1b9c8e20583715f71d18cac2637a22a04cffba0a
shunhamm <shun.022885@gmail.com>
2023-09-27 10:45:46 +0900

    shebangを修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,9 +1,9 @@
-#!ruby
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 require_relative 'ls_methods'
 def main
-  p LsMethods.new.ls_without_any_options
+  LsMethods.new.ls_without_any_options
 end
 
 main
