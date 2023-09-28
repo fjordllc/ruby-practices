4aad3047df6cbd2c832c208b3b832ace549ba3c7
shunhamm <shun.022885@gmail.com>
2023-09-28 10:50:18 +0900

    最大サイズのファイル名またはディレクトリ名によって文字間隔を自動調節するように改善

--- 04.ls/ls_methods.rb
+++ 04.ls/ls_methods.rb
@@ -10,13 +10,14 @@ def ls_without_any_options(file_system_arr)
 end
 
 def show_ls(contents)
-  height = (contents.length / 3.0).ceil
+  maximum_length = contents.max_by(&:length).length + 3
+  height = contents.length.ceildiv(3)
   # 行のナンバリング
   (0...height).each do |h_num|
     # 列のナンバリング　今回は最大３列までなので最大３つまで表示したら次の行に折り返す。
     3.times do |w_num|
       contents_index = h_num + (height * w_num)
-      print contents[contents_index].ljust(20) if !contents[contents_index].nil?
+      print contents[contents_index].ljust(maximum_length) if !contents[contents_index].nil?
     end
     puts # ターミナル上で見栄えが悪いので改行。
   end
