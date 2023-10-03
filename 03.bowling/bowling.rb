19f7d659d8094a93ebeedfdd2407f4d473e0a91d
triton27 <na54heart@gmail.com>
2023-10-04 02:01:07 +0900

    feat: 旧ルールのボウリングスコア計算プログラムを作成

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -23,13 +23,29 @@ end
 
 # ポイントを加算
 point = 0
-frames.each do |frame|
-    if frame[0] == 10 #strike
-        point += 30
-    elsif frame.sum == 10 #spare
-        point += frame[0] + 10
+for i in 0...10 do
+    # 10フレーム目
+    if (i == 9)
+        point += frames[i].sum
+        if (frames[i + 1] != nil)
+            point += frames[i + 1].sum
+        end
+        if (frames[i + 2] != nil)
+            point += frames[i + 2].sum
+        end
     else
-        point += frame.sum
+        if (frames[i][0] == 10) # strike
+            point += 10 + frames[i + 1].sum
+            # 次のフレームもstrikeだった場合、更に次のフレームの１投目も加算
+            if (frames[i + 1][0] == 10)
+                point += frames[i + 2][0]
+            end
+        elsif (frames[i].sum == 10) # spare
+            point += 10 + frames[i + 1][0]
+        else
+            point += frames[i].sum
+        end
     end
 end
+
 puts point
