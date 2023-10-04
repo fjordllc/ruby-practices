d0e67be2b2f0e0789de72a48ad145423787f6557
triton27 <na54heart@gmail.com>
2023-10-04 23:14:47 +0900

    fix: レビュー指摘対応

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,70 +8,49 @@ opt = OptionParser.new
 opt_year = nil
 opt_month = nil
 
-# オプションで年と月を設定
-opt.on('-y int') {|val| opt_year = val.to_i }
-opt.on('-m int') {|val| opt_month = val.to_i }
-
+# 年と月を取得
+opt.on('-y int') { |val| opt_year = val.to_i }
+opt.on('-m int') { |val| opt_month = val.to_i }
 opt.parse!(ARGV)
 
 # 引数を指定しない場合は、今年・今月のカレンダーを表示させる
-if (opt_year == nil and opt_month == nil)
-    opt_year = Date.today.year
-    opt_month = Date.today.month
-elsif (opt_year == nil)
-    opt_year = Date.today.year
-elsif (opt_month == nil)
-    opt_month = Date.today.month
-end
+opt_year = Date.today.year if opt_year.nil?
+opt_month = Date.today.month if opt_month.nil?
 
 # カレンダーの「dd月 yyyy年」部分を表示
-puts("      " + opt_month.to_s + "月" + " " + opt_year.to_s)
+puts("      #{opt_month}月 #{opt_year}")
 
 # 曜日を表示
-days = ["日", "月", "火", "水", "木", "金", "土"]
-days.each{|day|
-    if (days.index(day) < 6)
-        print(day + " ")
-    else
-        puts(day)
-    end
-}
+days = %w[日 月 火 水 木 金 土]
+puts(days.join(' '))
 
-# 対象月の１日目の曜日をintで取得
-date = Date.new(opt_year, opt_month, 1)
-first_date_of_day = date.wday
-
-# １日目の前に、全角スペース１つ（日にち分）と半角スペース１つ分（日にちの区切り分）追加
-for num in 1..first_date_of_day do
-    print("　" + " ")
+# 対象月の１日目の曜日を取得し、前にスペースを出力
+first_day = Date.new(opt_year, opt_month, 1)
+first_date_of_day = first_day.wday
+(1..first_date_of_day).each do
+  print(' ' * 3)
 end
 
-# 対象月の最終日を取得
-last_date = Date.new(opt_year, opt_month, -1).day;
-
+last_day = Date.new(opt_year, opt_month, -1)
 # 日にちを出力
-for date in 1..last_date do
-    # 曜日取得
-    target = Date.new(opt_year, opt_month, date)
-    day = target.wday
+(first_day..last_day).each do |date|
+  target = Date.new(opt_year, opt_month, date.day)
 
-    # 今日の日付の部分の色を反転させる
-    if (target == Date.today)
-        print("\e[7m")
-    else
-        print("\e[0m")
-    end
+  # 今日の日付の部分の色を反転させる
+  if target == Date.today
+    print("\e[7m")
+  else
+    print("\e[0m")
+  end
 
-    # 日にちが一桁だった場合は、日にちの前に半角スペースを入れる
-    if (date < 10)
-        print(" ")
-    end
-
-    # 土曜日の場合、改行する
-    if (day == 6)
-        puts(date)
-    else
-        print(date)
-        print(" ")
-    end
+  # 日曜か土曜だった場合は、出力方法を変える
+  if target.sunday?
+    print(date.day.to_s.center(3))
+  elsif target.saturday?
+    puts(date.day.to_s.center(3))
+  else
+    print(date.day.to_s.center(3))
+  end
 end
+
+puts
