5eca7c4bc948ba47d4ba5c52d5f359447f5fe373
ngtrrrn <negitorox149@gmail.com>
2023-10-08 11:38:27 +0900

    create fizzbuzz.rb

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizz(x)
+    return "Fizz" if x % 3 == 0
+    return ""
+end
+
+def buzz(x)
+    return "Buzz" if x % 5 == 0
+    return ""
+end
+
+(1..20).each do |x|
+    if x % 3 == 0 || x % 5 == 0
+        puts fizz(x) + buzz(x)
+    else
+        puts x
+    end
+end 
