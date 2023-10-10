b6fc6fc8041f8f020b7d7a6617e0b413f63ea863
triton27 <na54heart@gmail.com>
2023-10-11 00:34:42 +0900

    fix: レビュー指摘対応

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-require "date"
+require 'date'
 require 'optparse'
 
 opt = OptionParser.new
@@ -24,32 +24,24 @@ puts("      #{opt_month}月 #{opt_year}")
 days = %w[日 月 火 水 木 金 土]
 puts(days.join(' '))
 
-# 対象月の１日目の曜日を取得し、前にスペースを出力
+# 対象月の１日目の曜日を取得し、前に半角スペースを出力
 first_day = Date.new(opt_year, opt_month, 1)
 first_date_of_day = first_day.wday
-(1..first_date_of_day).each do
-  print(' ' * 3)
-end
+print(' ' * 3 * first_date_of_day)
 
 last_day = Date.new(opt_year, opt_month, -1)
 # 日にちを出力
-(first_day..last_day).each do |date|
-  target = Date.new(opt_year, opt_month, date.day)
-
+(first_day..last_day).each_with_index do |date, idx|
   # 今日の日付の部分の色を反転させる
-  if target == Date.today
-    print("\e[7m")
-  else
-    print("\e[0m")
-  end
+  print("\e[0m")
+  print(' ') if date.wday > 0 && idx > 0
+  print("\e[7m") if date == Date.today
 
-  # 日曜か土曜だった場合は、出力方法を変える
-  if target.sunday?
-    print(date.day.to_s.center(3))
-  elsif target.saturday?
-    puts(date.day.to_s.center(3))
+  # 土曜だった場合は、出力方法を変える
+  if date.saturday?
+    puts(date.day.to_s.rjust(2))
   else
-    print(date.day.to_s.center(3))
+    print(date.day.to_s.rjust(2))
   end
 end
 
