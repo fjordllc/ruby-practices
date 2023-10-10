57d863c5842dc586aa062a24f45a170a2da583fc
shunhamm <shun.022885@gmail.com>
2023-10-10 17:15:33 +0900

    コメントと定数を追加。

--- 04.ls/ls_methods.rb
+++ 04.ls/ls_methods.rb
@@ -2,6 +2,8 @@
 
 require 'etc'
 
+COLUMN_NUMBER = 3
+
 def get_files(path)
   Dir.entries(path).sort
 end
@@ -16,11 +18,10 @@ end
 
 def show(contents)
   maximum_length = contents.max_by(&:length).length + 3
-  height = contents.length.ceildiv(3)
-  # 行のナンバリング
+  height = contents.length.ceildiv(COLUMN_NUMBER)
+
   (0...height).each do |h_num|
-    # 列のナンバリング　今回は最大３列までなので最大３つまで表示したら次の行に折り返す。
-    3.times do |w_num|
+    COLUMN_NUMBER.times do |w_num|
       contents_index = h_num + (height * w_num)
       print contents[contents_index].ljust(maximum_length) if !contents[contents_index].nil?
     end
