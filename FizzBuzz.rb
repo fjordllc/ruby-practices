5ea03c3d470ac0f818a25a5b4b7ece27e896b73e
takayuki <takayuki12010218@gmail.com>
2023-10-16 08:52:35 +0900

    modify FizzBuzz

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,14 +1,13 @@
 number = 0
-while number < 20
-    number += 1
-    case
-    when number % 15 == 0
-        puts "FizzBuzz"
-    when number % 5 == 0
-        puts "Buzz"
-    when number % 3 == 0
-        puts "Fizz"
-    else
-        puts number
-    end
+(1..20).each do |number|
+  case
+  when number % 15 == 0
+    puts "FizzBuzz"
+  when number % 5 == 0
+    puts "Buzz"
+  when number % 3 == 0
+    puts "Fizz"
+  else
+    puts number
+  end
 end
