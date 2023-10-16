42d142c745c59572bb070e917e05cf842c6c0071
Goto-star <goyu620@gmail.com>
2023-10-16 15:53:00 +0900

    Dir.globメソッドの重複した呼び出しの修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,34 +1,31 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-require 'debug'
 
 SPLIT_NUMBER = 3
 
 def main
-  vertical_files = group_files.transpose
-  max_name_length = obtain_files.map(&:size).max
+  fetched_files = fetch_files
+  display_files(fetched_files)
+end
 
+def fetch_files
+  Dir.glob('*')
+end
+
+def display_files(fetched_files)
+  max_row_number = (fetched_files.length.to_f / SPLIT_NUMBER).ceil
+  grouped_files = fetched_files.each_slice(max_row_number).to_a
+  blank_numbers = grouped_files[0].length - grouped_files[-1].length
+  grouped_files[-1] += Array.new(blank_numbers, nil)
+
+  vertical_files = grouped_files.transpose
+  max_name_length = fetched_files.map(&:size).max
   vertical_files.each do |files|
     files.each do |file|
       print file.to_s.ljust(max_name_length + 1)
     end
     print("\n")
   end
-end
-
-def obtain_files
-  Dir.glob('*')
-end
-
-def group_files
-  obtained_files = obtain_files
-  max_row_number = (obtained_files.length.to_f / SPLIT_NUMBER).ceil
-  #binding.break
-
-  grouped_files = obtained_files.each_slice(max_row_number).to_a
-  blank_numbers = grouped_files[0].length - grouped_files[-1].length
-  grouped_files[-1] += Array.new(blank_numbers, nil)
-  grouped_files
 end
 
 main
