38a0d182baea2f54a1e300e80f5cd2b62afa9433
Goto-star <goyu620@gmail.com>
2023-10-16 16:04:19 +0900

    メソッド・変数名(fetch)の修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,22 +4,22 @@
 SPLIT_NUMBER = 3
 
 def main
-  fetched_files = fetch_files
-  display_files(fetched_files)
+  obtained_files = obtain_files
+  display_files(obtained_files)
 end
 
-def fetch_files
+def obtain_files
   Dir.glob('*')
 end
 
-def display_files(fetched_files)
-  max_row_number = (fetched_files.length.to_f / SPLIT_NUMBER).ceil
-  grouped_files = fetched_files.each_slice(max_row_number).to_a
+def display_files(obtained_files)
+  max_row_number = (obtained_files.length.to_f / SPLIT_NUMBER).ceil
+  grouped_files = obtained_files.each_slice(max_row_number).to_a
   blank_numbers = grouped_files[0].length - grouped_files[-1].length
   grouped_files[-1] += Array.new(blank_numbers, nil)
 
   vertical_files = grouped_files.transpose
-  max_name_length = fetched_files.map(&:size).max
+  max_name_length = obtained_files.map(&:size).max
   vertical_files.each do |files|
     files.each do |file|
       print file.to_s.ljust(max_name_length + 1)
