1dd46fac7f74b11a55ba91e27fcee30147c69501
funxxfun <23shirasawa@gmail.com>
2023-10-17 17:02:39 +0900

    その月の1日から最後の日を表示させました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,3 +4,7 @@ day = Date.today
 puts "#{day.strftime('%m')}月 #{day.strftime('%Y')}"
 puts "日 月 火 水 木 金 土"
 
+last_day = Date.new(day.year, day.month, -1).day
+(1..last_day).each do |day|
+  print day
+end
