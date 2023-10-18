f1ab82795ca1b4c6b0d363f46c5f987b71262a94
takayuki <takayuki12010218@gmail.com>
2023-10-18 22:49:56 +0900

    add bowling

---
+++ bowling.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'debug'
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
+end
+
+point = 0
+frames.each_with_index do |frame, n|
+  if n <= 8 # 7フレーム目まで
+    point += frame.sum
+    if frame[0] == 10 # ストライク
+      point += frames[n + 1].sum
+      point += frames[n + 2][0] if frames[n + 1][0] == 10 # 2連続ストライク
+    elsif frame[0] + frame[1] == 10 # スペア
+      point += frames[n + 1][0]
+    end
+  end
+
+  point += frame.sum if n > 8 # 最終フレーム
+end
+
+puts point
