ba69bddcefd3db5e02a1104c796dfa9909d970d2
funxxfun <23shirasawa@gmail.com>
2023-10-28 15:32:00 +0900

    土曜で改行する機能を記述した

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,6 +5,19 @@ puts day.strftime("%m月 %Y")
 puts "日 月 火 水 木 金 土"
 
 last_day = Date.new(day.year, day.month, -1).day
+first_day_wday = Date.new(day.year, day.month, 1).wday
+
+puts " " * first_day_wday
+
 (1..last_day).each do |day|
-  print day
+  if day <= 9
+    # 横並びに表示したいためputsではなくprintで出力する
+    print " " + "#{day}" + " "
+  else
+    print "#{day}" + " "
+  end
+
+  if (first_day_wday + day ) % 7 == 0
+    print "\n"
+  end
 end
