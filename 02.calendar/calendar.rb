e4876384ab5ff7f431a90b741059fb385de5a173
funxxfun <23shirasawa@gmail.com>
2023-10-28 16:34:54 +0900

    曜日の下の空白行を削除した

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,17 +1,17 @@
 require 'date'
 
 day = Date.today
-puts day.strftime("%m月 %Y")
+puts day.strftime("%m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
 
 last_day = Date.new(day.year, day.month, -1).day
 first_day_wday = Date.new(day.year, day.month, 1).wday
 
-puts " " * first_day_wday
+print " " * first_day_wday
 
 (1..last_day).each do |day|
+  # 横並びに表示したいためputsではなくprintで出力する
   if day <= 9
-    # 横並びに表示したいためputsではなくprintで出力する
     print " " + "#{day}" + " "
   else
     print "#{day}" + " "
