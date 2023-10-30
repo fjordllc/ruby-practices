affd2360e5f8bbe9291a7d38194c2e24d3bae82b
funxxfun <23shirasawa@gmail.com>
2023-10-30 23:17:30 +0900

    月初日の曜日を合わせる空白を半角3マス分に変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,13 +1,14 @@
 require 'date'
 
 day = Date.today
+
 puts day.strftime("%m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
 
 last_day = Date.new(day.year, day.month, -1).day
 first_day_wday = Date.new(day.year, day.month, 1).wday
 
-print " " * first_day_wday
+print "   " * first_day_wday
 
 (1..last_day).each do |day|
   # 横並びに表示したいためputsではなくprintで出力する
