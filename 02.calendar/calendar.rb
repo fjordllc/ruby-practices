7f94971080232916c2ce8b3394b6d9e5a05087d0
funxxfun <23shirasawa@gmail.com>
2023-10-31 21:58:48 +0900

    コマンド引数で指定の年月のカレンダーを表示できる機能を実装

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,17 +1,34 @@
 #!/usr/bin/env ruby
 
 require 'date'
+require 'optparse'
 
-day = Date.today
+params = ARGV.getopts('y:', 'm:')
+# puts params['y']
+# puts params['m']
+
+if params['y']
+  year = params['y'].to_i
+else
+  year = Date.today.year
+end
+
+if params['m']
+  month = params['m'].to_i
+else
+  month = Date.today.month
+end
+
+# puts year
+# puts month
+
+day = Date.new(year, month)
 
 puts day.strftime("%m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
-
 last_day = Date.new(day.year, day.month, -1).day
 first_day_wday = Date.new(day.year, day.month, 1).wday
-
 print "   " * first_day_wday
-
 (1..last_day).each do |day|
   # 横並びに表示したいためputsではなくprintで出力する
   if day <= 9
