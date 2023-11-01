f024c366f52377f55c4817d2fc430db21bad53ba
YuSa0-6 <fuku.sugar@icloud.com>
2023-11-01 20:07:52 +0900

    add option '-a'

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,35 +1,44 @@
 # frozen_string_literal: true
 
 def main(row, path)
-  files = find_files(path)
-  formatted_files = format_files(files)
-  vertical_lines = sort_files(formatted_files, row)
-  display_files(vertical_lines, row)
+  @row = row
+  find_files(path)
+  format_files(@files)
+  sort_files(@formatted_files)
+  display_files
+end
+
+def set_paramater
+  path = '*'
+  option = OptionParser.new
+  option.on('-a') { path = '{.,*}{.,*}' }
+  option.parse!(ARGV)
+  path
 end
 
 def find_files(path)
-  Dir.glob(path)
+  files = Dir.glob(path)
+  @files = files
 end
 
 def format_files(files)
   most_long_name = files.max_by(&:length)
-  files.map { |file| file.ljust(most_long_name.length + 4) }
+  @formatted_files = files.map { |file| file.ljust(most_long_name.length + 4) }
 end
 
-def sort_files(formatted_files, row)
-  sorted_files = formatted_files.sort
-  vertical_lines = []
-  number_of_lines = (sorted_files.length.to_f / row).ceil
-  number_of_lines.times do |col_index|
-    row.times do |row_index|
-      vertical_lines << sorted_files[col_index + number_of_lines * row_index]
+def sort_files(formatted_files)
+  @sorted_files = formatted_files.sort
+  @vertical_lines = []
+  number_of_lines = (@sorted_files.length.to_f / @row).ceil
+  number_of_lines.times do |col|
+    @row.times do |row|
+      @vertical_lines << @sorted_files[col + number_of_lines * row]
     end
   end
-  vertical_lines
 end
 
-def display_files(vertical_lines, row)
-  vertical_lines.each_with_index { |file, index| print ((index + 1) % row).zero? ? "#{file}\n" : file }
+def display_files
+  @vertical_lines.each_with_index { |file, index| print ((index + 1) % @row).zero? ? "#{file}\n" : file }
 end
 
-main(3, '*')
+main(3)
