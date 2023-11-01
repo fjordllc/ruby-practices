ac65e5863cb842786c70127731995908f8a34a90
YuSa0-6 <fuku.sugar@icloud.com>
2023-11-01 20:17:50 +0900

    update ls -a cmd

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-def main(row, path)
-  @row = row
-  find_files(path)
-  format_files(@files)
-  sort_files(@formatted_files)
-  display_files
+require 'optparse'
+
+def main(row)
+  path = set_paramater
+  files = find_files(path)
+  formatted_files = format_files(files)
+  vertical_lines = sort_files(formatted_files, row)
+  display_files(vertical_lines, row)
 end
 
 def set_paramater
@@ -17,28 +19,28 @@ def set_paramater
 end
 
 def find_files(path)
-  files = Dir.glob(path)
-  @files = files
+  Dir.glob(path)
 end
 
 def format_files(files)
   most_long_name = files.max_by(&:length)
-  @formatted_files = files.map { |file| file.ljust(most_long_name.length + 4) }
+  files.map { |file| file.ljust(most_long_name.length + 4) }
 end
 
-def sort_files(formatted_files)
-  @sorted_files = formatted_files.sort
-  @vertical_lines = []
-  number_of_lines = (@sorted_files.length.to_f / @row).ceil
-  number_of_lines.times do |col|
-    @row.times do |row|
-      @vertical_lines << @sorted_files[col + number_of_lines * row]
+def sort_files(formatted_files, row)
+  sorted_files = formatted_files.sort
+  vertical_lines = []
+  number_of_lines = (sorted_files.length.to_f / row).ceil
+  number_of_lines.times do |col_index|
+    row.times do |row_index|
+      vertical_lines << sorted_files[col_index + number_of_lines * row_index]
     end
   end
+  vertical_lines
 end
 
-def display_files
-  @vertical_lines.each_with_index { |file, index| print ((index + 1) % @row).zero? ? "#{file}\n" : file }
+def display_files(vertical_lines, row)
+  vertical_lines.each_with_index { |file, index| print ((index + 1) % row).zero? ? "#{file}\n" : file }
 end
 
 main(3)
