77e084c662719707b79bac23f1b7c973807c2e50
motohiro-mm <polarbear.dx@gmail.com>
2023-11-14 20:57:49 +0900

    ファイルの作成

---
+++ 06.bowling_object/test/bowling_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../lib/bowling'
+require_relative '../lib/game'
+require_relative '../lib/frame'
+require_relative '../lib/shot'
+
+class BowlingTest < Minitest::Test
+end
