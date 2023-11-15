4bc2bfead9768aaec77746ae882c4a1b7f0523b4
motohiro-mm <polarbear.dx@gmail.com>
2023-11-15 18:59:05 +0900

    Frameクラスとsum_shots_scoreメソッドを定義、それに伴うtest_frameテストを作成

--- 06.bowling_object/lib/frame.rb
+++ 06.bowling_object/lib/frame.rb
@@ -1,2 +1,19 @@
 class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+#  def initialize(first_mark, second_mark = nil, third_mark = nil)
+#    @first_shot = Shot.new(first_mark)
+#    @second_shot = Shot.new(second_mark)
+#    @third_shot = Shot.new(third_mark)
+#  end
+
+  def initialize(shots)
+    @first_shot = Shot.new(shots[0])
+    @second_shot = Shot.new(shots[1])
+    @third_shot = Shot.new(shots[2])
+  end
+
+  def sum_shots_score
+    first_shot.score + second_shot.score + third_shot.score
+  end
 end
--- 06.bowling_object/test/bowling_test.rb
+++ 06.bowling_object/test/bowling_test.rb
@@ -6,6 +6,12 @@ require_relative '../lib/shot'
 
 class BowlingTest < Minitest::Test
 
+  def test_frame
+    assert Frame.new(['1','2'])
+    assert_equal 12, Frame.new(['3','4','5']).sum_shots_score
+    assert_equal 10, Frame.new(['X']).sum_shots_score
+  end
+
   def test_shot
     assert Shot.new('1')
     assert_equal 2, Shot.new('2').score
