3c25a9eac5e26151998489593aa9e4085316af10
motohiro-mm <polarbear.dx@gmail.com>
2023-11-15 18:45:32 +0900

    shotクラスとscoreメソッドを定義、それに伴うtest_shotテストを作成

--- 06.bowling_object/lib/shot.rb
+++ 06.bowling_object/lib/shot.rb
@@ -1,2 +1,13 @@
 class Shot
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  def score
+    return 10 if mark == 'X'
+
+    mark.to_i
+  end
 end
--- 06.bowling_object/test/bowling_test.rb
+++ 06.bowling_object/test/bowling_test.rb
@@ -5,4 +5,10 @@ require_relative '../lib/frame'
 require_relative '../lib/shot'
 
 class BowlingTest < Minitest::Test
+
+  def test_shot
+    assert Shot.new('1')
+    assert_equal 2, Shot.new('2').score
+    assert_equal 10, Shot.new('X').score
+  end
 end
