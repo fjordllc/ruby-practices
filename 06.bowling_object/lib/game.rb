2947d9ea17489dcfcde738f2760f3cda2c3f9c86
motohiro-mm <polarbear.dx@gmail.com>
2023-11-16 12:42:59 +0900

    sum_frames_scoreメソッドをリファクタリング、initializeメソッド内の変数名を修正

--- 06.bowling_object/lib/game.rb
+++ 06.bowling_object/lib/game.rb
@@ -2,17 +2,17 @@ class Game
   attr_reader :frame1, :frame2, :frame3, :frame4, :frame5, :frame6, :frame7, :frame8, :frame9, :frame10
 
   def initialize(marks)
-    frames = Game.group_by_frame(marks.split(','))
-    @frame1 = Frame.new(frames[0])
-    @frame2 = Frame.new(frames[1])
-    @frame3 = Frame.new(frames[2])
-    @frame4 = Frame.new(frames[3])
-    @frame5 = Frame.new(frames[4])
-    @frame6 = Frame.new(frames[5])
-    @frame7 = Frame.new(frames[6])
-    @frame8 = Frame.new(frames[7])
-    @frame9 = Frame.new(frames[8])
-    @frame10 = Frame.new(frames[9])
+    marks_grouped = Game.group_by_frame(marks.split(','))
+    @frame1 = Frame.new(marks_grouped[0])
+    @frame2 = Frame.new(marks_grouped[1])
+    @frame3 = Frame.new(marks_grouped[2])
+    @frame4 = Frame.new(marks_grouped[3])
+    @frame5 = Frame.new(marks_grouped[4])
+    @frame6 = Frame.new(marks_grouped[5])
+    @frame7 = Frame.new(marks_grouped[6])
+    @frame8 = Frame.new(marks_grouped[7])
+    @frame9 = Frame.new(marks_grouped[8])
+    @frame10 = Frame.new(marks_grouped[9])
   end
 
   def total_score
@@ -20,7 +20,8 @@ class Game
   end
 
   def sum_frames_score
-    frame1.sum_shots_score + frame2.sum_shots_score + frame3.sum_shots_score + frame4.sum_shots_score + frame5.sum_shots_score + frame6.sum_shots_score + frame7.sum_shots_score + frame8.sum_shots_score + frame9.sum_shots_score + frame10.sum_shots_score
+    frames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10]
+    frames.map!(&:sum_shots_score).sum
   end
 
   def sum_bonus_score
