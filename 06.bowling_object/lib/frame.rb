20ed7b20d55f039f4d0dc78a2959294011a8371f
motohiro-mm <polarbear.dx@gmail.com>
2023-11-16 12:12:47 +0900

    不要なコメントアウトを削除

--- 06.bowling_object/lib/frame.rb
+++ 06.bowling_object/lib/frame.rb
@@ -1,12 +1,6 @@
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot
 
-#  def initialize(first_mark, second_mark = nil, third_mark = nil)
-#    @first_shot = Shot.new(first_mark)
-#    @second_shot = Shot.new(second_mark)
-#    @third_shot = Shot.new(third_mark)
-#  end
-
   def initialize(shots)
     @first_shot = Shot.new(shots[0])
     @second_shot = Shot.new(shots[1])
