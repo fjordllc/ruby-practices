92025ac440d07423b8886a219ee1779d0681733d
ryonamura <hansu0526@gmail.com>
2023-11-22 16:08:16 +0900

    FizzBuzz１回目の提出

---
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+(1..20).each do |i|
+if i % 3 == 0 && i % 5 ==0 then
+  puts "FizzBuzz"
+    elsif i % 3 == 0 then
+      puts "Fizz"
+    elsif i % 5 == 0 then
+      puts "Buzz"
+    elsif
+      puts i
+    end
+end
