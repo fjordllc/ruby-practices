1f5cc58975acc1db1fd6e89f8a220eb529c2ed26
takayuki <takayuki12010218@gmail.com>
2023-11-28 22:19:55 +0900

    modify ls.rb

--- 07.ls_object/lib/ls.rb
+++ 07.ls_object/lib/ls.rb
@@ -2,41 +2,40 @@
 
 items = []
 
+num_columns = 3 # 列幅の最大数
+
 def get_items(items)
   items.concat(Dir.glob('*'))
 end
 
-def slice_items(items)
+def slice_items(items, num_columns)
   get_items(items)
-  if (0..3).cover?(items)
+  if (0..num_columns).cover?(items.size)
     print items.join(' ') # 要素数が3つ以下は横並び出力
     puts
-  elsif (items.size % 3).zero?
-    order_id = items.size / 3.ceil # 3の倍数の時だけ、3で割り込む
-    sliced_items = items.each_slice(order_id).to_a
+  elsif (items.size % num_columns).zero?
+    order_id = items.size / num_columns.ceil # 3の倍数の時だけ、3で割り込む
+    items.each_slice(order_id).to_a
   else
-    order_id = items.size / 3.ceil + 1 # 最大3列に収める
-    sliced_items = items.each_slice(order_id).to_a
+    order_id = items.size / num_columns.ceil + 1 # 最大3列に収める
+    items.each_slice(order_id).to_a
   end
 end
 
-def sorting(items)
-  sliced_items = slice_items(items)
-  case items.size
-  when 0..3
-    # 何もしない
-  else
-    max_size = sliced_items.map(&:size).max
-    sliced_items.each do |item| # サブ配列の要素数を揃える
-      item << nil while item.size < max_size
+def sort_items(items, num_columns)
+  sliced_items = slice_items(items, num_columns)
+  return unless items.size > num_columns
+
+  max_size = sliced_items.map(&:size).max
+  sliced_items.each do |item| # サブ配列の要素数を揃える
+    item << nil while item.size < max_size
+  end
+  sliced_items.transpose.each do |elements|
+    elements.each do |element|
+      print element.to_s.ljust(20)
     end
-    sliced_items.transpose.each do |items|
-      items.each do |item|
-        print item.to_s.ljust(20)
-      end
-      puts
-    end
+    puts
   end
 end
 
-sorting(items)
+sort_items(items, num_columns)
