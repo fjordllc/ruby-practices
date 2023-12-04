e4fe2fd4e34813937eebfa47f5daaa8fd468f36d
taco <nnnantai-doubutsu@yahoo.co.jp>
2023-12-05 08:40:39 +0900

    rubocopの内容に基づき、ボウリングのプログラムを修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,33 +1,32 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 def convert_point(point)
   if point == 'X'
-    point = 10
+    10
   else
-    point = point.to_i
+    point.to_i
   end
-  point
 end
 
 points = ARGV[0].split(',')
 score = 0
-(1..10).each do |frame|
+10.times do
   frame_score = 0
-  strike_spare = ""
+  strike_spare = ''
   (1..2).each do |throwing|
     throwing_point = convert_point(points[0])
     frame_score += throwing_point
     if throwing == 1 && throwing_point == 10
-      strike_spare = "strike"
+      strike_spare = 'strike'
       frame_score += convert_point(points[1])
       frame_score += convert_point(points[2])
     elsif throwing == 2 && frame_score == 10 && strike_spare.empty?
-      strike_spare = "spare"
+      strike_spare = 'spare'
       frame_score += convert_point(points[1])
     end
     points.shift
-    if throwing == 2 || strike_spare == "strike" 
-      break
-    end
+    break if throwing == 2 || strike_spare == 'strike'
   end
   score += frame_score
 end
