89ff8b33fc84e5f4e0149412bbe169f4aebff3ee
guchi5 <zero19390901@gmail.com>
2023-12-06 11:32:34 +0900

    表示される並び順を変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,5 +2,19 @@
 # frozen_string_literal: true
 
 input = ARGV[0]&.to_s || '.'
-Dir.entries(input).reject { |file| /^\..*/.match(file) }.sort.map { |file| print "#{file}\s\s" }
-print "\n"
+files = Dir.entries(input).reject { |file| /^\..*/.match(file) }.sort
+matrix = []
+files.each_slice(4) do |file1, file2, file3, file4|
+  row = [file1, file2, file3, file4]
+  valid_row = row.compact
+  max_size = valid_row.max_by(&:length).length
+  matrix.push({ row: valid_row, size: max_size })
+end
+
+(0..matrix.length + 1).each do |i|
+  matrix.each do |value|
+    print value[:row][i].ljust(value[:size]) if !value[:row][i].nil?
+    print "\t"
+  end
+  print "\n"
+end
