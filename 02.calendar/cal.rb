31e34dfd77df90df223a13c28279b899492fc12d
taco <nnnantai-doubutsu@yahoo.co.jp>
2023-12-09 11:27:26 +0900

    レビューにて指摘された個所を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,40 +1,44 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'optparse'
-require "date"
+require 'date'
+
 opt = OptionParser.new
-month = ""
-year = ""
+month = nil
+year = nil
 now = Date.today
+
 # コマンドラインに入力されたオプション(月と年)の値を取得
-opt.on('-m int') {|v| month = v }
-opt.on('-y int') {|v| year = v }
+opt.on('-m int') { |v| month = v.to_i }
+opt.on('-y int') { |v| year = v.to_i }
 opt.parse!(ARGV)
-month = now.month if month.empty?
-year = now.year if year.empty?
-month = month.to_i
-year = year.to_i
-# 月の値に13以上、または1未満が入力されていた場合、処理を終了する
-if month > 12 || month < 1
-  puts "正しい月を入力してください"
+month ||= now.month
+year ||= now.year
+
+# 入力された月の値が1から12に収まらない場合、処理を終了する
+unless (1..12).cover?(month)
+  puts '正しい月を入力してください'
   exit
 end
-# 実行時の日付と、カレンダーに表示する月の最終日を取得
-last_day = Date.new(year,month,-1).day
+
+# カレンダーに表示する月の初日、最終日を取得
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
+
 # カレンダーの月と年の部分を表示する
-puts("   #{Date::MONTHNAMES[month]} #{year}")
+puts("   #{first_day.strftime('%B')} #{year}")
 # カレンダーの曜日の部分を表示する
-puts("Su Mo Tu We Th Fr Sa")
-# 表示するカレンダーの初日が、月曜日から離れている分だけ、一日ごとに3文字分の空白を出力する
-print " " * (Date.new(year, month, 1).wday * 3) 
+puts('Su Mo Tu We Th Fr Sa')
+# 表示するカレンダーの初日が月曜日から離れている場合、一日ごとに3文字分の空白を出力する
+print ' ' * (first_day.wday * 3)
+
 # カレンダーの日付の部分を表示する
-(1..last_day).each do |day|
-  # 今日の日付を表示する場合、色を反転して表示する
-  if day == now.day && month == now.month && year == now.year
-    print "\e[30m\e[47m#{day.to_s.rjust(2," ")}\e[0m "
-  else
-    print "#{day.to_s.rjust(2," ")} "
-  end
+(first_day..last_day).each do |date|
+  day_to_display = date.day.to_s.rjust(2, ' ')
+  # 今日の日付を表示する場合のみ、色を反転して表示する
+  print date == Date.today ? "\e[30m\e[47m#{day_to_display}\e[0m " : "#{day_to_display} "
   # 表示した日付が土曜日(カレンダーの右端)の場合、表示を改行する
-  puts if Date.new(year, month, day).saturday?
+  puts if date.saturday?
 end
 puts
