e81ade022dfcfec54e1ee71711b5f0fa432d2ed7
taco <nnnantai-doubutsu@yahoo.co.jp>
2023-12-10 16:39:36 +0900

    変数 strike_spareの変数名と関連するコードを修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -13,20 +13,19 @@ points = ARGV[0].split(',')
 score = 0
 10.times do
   frame_score = 0
-  strike_spare = ''
+  strike = false
   (1..2).each do |throwing|
     throwing_point = convert_point(points[0])
     frame_score += throwing_point
     if throwing == 1 && throwing_point == 10
-      strike_spare = 'strike'
+      strike = true
       frame_score += convert_point(points[1])
       frame_score += convert_point(points[2])
-    elsif throwing == 2 && frame_score == 10 && strike_spare.empty?
-      strike_spare = 'spare'
+    elsif throwing == 2 && frame_score == 10 && !strike
       frame_score += convert_point(points[1])
     end
     points.shift
-    break if throwing == 2 || strike_spare == 'strike'
+    break if throwing == 2 || strike
   end
   score += frame_score
 end
