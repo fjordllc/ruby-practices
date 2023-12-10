c44338635fa24249d0c58656a8bd2f9259652203
takayuki <takayuki12010218@gmail.com>
2023-12-10 13:15:45 +0900

    modify ls.rb

--- 07.ls_object/lib/ls.rb
+++ 07.ls_object/lib/ls.rb
@@ -2,40 +2,55 @@
 
 items = []
 
-num_columns = 3 # 列幅の最大数
+NUM_COLUMNS = 3 # 列幅の最大数
 
-def get_items(items)
+def main(items)
+  taken_items = take_items(items)
+  sliced_items = slice_items(taken_items)
+  sorted_items = sort_items(sliced_items)
+  display_items(sorted_items)
+end
+
+def take_items(items)
   items.concat(Dir.glob('*'))
 end
 
-def slice_items(items, num_columns)
-  get_items(items)
-  if (0..num_columns).cover?(items.size)
-    print items.join(' ') # 要素数が3つ以下は横並び出力
-    puts
-  elsif (items.size % num_columns).zero?
-    order_id = items.size / num_columns.ceil # 3の倍数の時だけ、3で割り込む
-    items.each_slice(order_id).to_a
-  else
-    order_id = items.size / num_columns.ceil + 1 # 最大3列に収める
-    items.each_slice(order_id).to_a
-  end
+def slice_items(taken_items)
+  order_id = if (taken_items.size % NUM_COLUMNS).zero?
+               taken_items.size / NUM_COLUMNS.ceil # NUM_COLUMNSの倍数の時だけ、NUM_COLUMNSで割り込む
+             else
+               taken_items.size / NUM_COLUMNS.ceil + 1 # 最大NUM_COLUMNS列に収める
+             end
+  taken_items.each_slice(order_id).to_a
 end
 
-def sort_items(items, num_columns)
-  sliced_items = slice_items(items, num_columns)
-  return unless items.size > num_columns
-
-  max_size = sliced_items.map(&:size).max
-  sliced_items.each do |item| # サブ配列の要素数を揃える
-    item << nil while item.size < max_size
+def sort_items(sliced_items)
+  if (0..NUM_COLUMNS).cover?(sliced_items.flatten.size)
+    sliced_items.flatten
+  else
+    max_size = sliced_items.map(&:size).max
+    sliced_items.each do |item| # サブ配列の要素数を揃える
+      item << nil if item.size < max_size
+    end
   end
-  sliced_items.transpose.each do |elements|
-    elements.each do |element|
-      print element.to_s.ljust(20)
+  sliced_items.transpose
+end
+
+def display_items(sorted_items)
+  max_word_count = sorted_items.flatten.compact.map(&:size).max
+  if (0..NUM_COLUMNS).cover?(sorted_items.flatten.size)
+    sorted_items.flatten.map do |item|
+      print item.to_s.ljust(max_word_count + 5)
     end
     puts
+  else
+    sorted_items.each do |items|
+      items.compact.each do |item|
+        print item.ljust(max_word_count + 5)
+      end
+      puts
+    end
   end
 end
 
-sort_items(items, num_columns)
+main(items)
