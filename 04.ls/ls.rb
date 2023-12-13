42fe02fad72c1d5aadfa8850f97c5113b5c93cde
guchi5 <zero19390901@gmail.com>
2023-12-13 22:22:02 +0900

    列が最大3となるように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,18 +1,29 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
+MAX_COL_SIZE = 3
+
 input = ARGV[0]&.to_s || '.'
 files = Dir.entries(input).reject { |file| /^\..*/.match(file) }.sort
 matrix = []
-files.each_slice(4) do |row|
-  valid_row = row.compact
-  max_size = valid_row.max_by(&:length).length
-  matrix.push({ row: valid_row, size: max_size })
+
+row_size = if files.empty?
+             return
+           elsif (files.length % MAX_COL_SIZE).zero?
+             files.length / MAX_COL_SIZE
+           else
+             files.length / (MAX_COL_SIZE - 1)
+           end
+
+files.each_slice(row_size) do |col|
+  valid_col = col.compact
+  max_size = valid_col.max_by(&:length).length
+  matrix.push({ col: valid_col, size: max_size })
 end
 
-(0..matrix.length).each do |i|
+matrix[0][:col].length.times do |i|
   matrix.each do |value|
-    print value[:row][i].ljust(value[:size]) if !value[:row][i].nil?
+    print value[:col][i].ljust(value[:size]) if !value[:col][i].nil?
     print "\s\s"
   end
   puts
