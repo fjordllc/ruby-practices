1085de9fe081c66bb79ce3084195dc73939829df
taco <nnnantai-doubutsu@yahoo.co.jp>
2023-12-17 19:12:24 +0900

    レビューで指摘を受けた個所を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,27 +4,33 @@
 COL_MAX = 3
 PADDING = 2
 
-def collect_files
-  path = ARGV[0]
+def main
+  path = ARGV[0] if ARGV[0]
+  files = if path
+            collect_files(path) if FileTest.directory?(path)
+          else
+            collect_files
+          end
+  files ? display_files(files) : puts('正しいパスを入力してください')
+end
+
+def collect_files(path = nil)
   path ||= '.'
   Dir.children(path).sort
-rescue Errno::ENOENT
-  puts '正しいパスを入力してください'
-  false
 end
 
 def display_files(files)
-  files = files.reject { |name| name.start_with?('.') }
-  total_files = files.size.to_f
+  preprocessed_files = files.reject { |name| name.start_with?('.') }
+  total_files = preprocessed_files.size.to_f
   row_size = (total_files / COL_MAX).ceil
   col_size = (total_files / row_size).ceil
-  widths = get_column_width(files, row_size, col_size)
+  widths = get_column_width(preprocessed_files, row_size, col_size)
 
   row_size.times do |row|
     col_size.times do |col|
-      file_name = files[row + col * row_size]
-      display_width = widths[col] + PADDING
-      print file_name.ljust(display_width, ' ') unless file_name.nil?
+      file_name = preprocessed_files[row + col * row_size]
+      width = widths[col] + PADDING
+      print file_name.ljust(width, ' ') if file_name
     end
     puts
   end
@@ -33,11 +39,10 @@ end
 def get_column_width(files, row_size, col_size)
   widths = Array.new(col_size, 0)
   files.each.with_index do |file, index|
-    column_number = index / row_size
-    widths[column_number] = file.length if file.length > widths[column_number]
+    column_index = index / row_size
+    widths[column_index] = file.length if file.length > widths[column_index]
   end
   widths
 end
 
-files = collect_files
-display_files(files) if files
+main
