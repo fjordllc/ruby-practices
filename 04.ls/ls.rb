6672705b32c4e57fca8c593d30a6bc88a33e6897
taco <nnnantai-doubutsu@yahoo.co.jp>
2023-12-18 18:46:23 +0900

    レビューで指摘を受けた個所を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,30 +5,22 @@ COL_MAX = 3
 PADDING = 2
 
 def main
-  path = ARGV[0] if ARGV[0]
-  files = if path
-            collect_files(path) if FileTest.directory?(path)
-          else
-            collect_files
-          end
-  files ? display_files(files) : puts('正しいパスを入力してください')
+  path = ARGV[0] || '.'
+  file_names = Dir.children(path).sort if FileTest.directory?(path)
+  file_names or return
+  display_file_names(file_names)
 end
 
-def collect_files(path = nil)
-  path ||= '.'
-  Dir.children(path).sort
-end
-
-def display_files(files)
-  preprocessed_files = files.reject { |name| name.start_with?('.') }
-  total_files = preprocessed_files.size.to_f
-  row_size = (total_files / COL_MAX).ceil
-  col_size = (total_files / row_size).ceil
-  widths = get_column_width(preprocessed_files, row_size, col_size)
+def display_file_names(file_names)
+  filtered_file_names = file_names.reject { |name| name.start_with?('.') }
+  total_file_names = filtered_file_names.size.to_f
+  row_size = (total_file_names / COL_MAX).ceil
+  col_size = (total_file_names / row_size).ceil
+  widths = get_column_widths(filtered_file_names, row_size, col_size)
 
   row_size.times do |row|
     col_size.times do |col|
-      file_name = preprocessed_files[row + col * row_size]
+      file_name = filtered_file_names[row + col * row_size]
       width = widths[col] + PADDING
       print file_name.ljust(width, ' ') if file_name
     end
@@ -36,11 +28,11 @@ def display_files(files)
   end
 end
 
-def get_column_width(files, row_size, col_size)
+def get_column_widths(file_names, row_size, col_size)
   widths = Array.new(col_size, 0)
-  files.each.with_index do |file, index|
+  file_names.each.with_index do |file_name, index|
     column_index = index / row_size
-    widths[column_index] = file.length if file.length > widths[column_index]
+    widths[column_index] = file_name.length if file_name.length > widths[column_index]
   end
   widths
 end
