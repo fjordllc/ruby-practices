231372ad06d4df4c64f20e9cf66baec41c3ebf7c
taco <nnnantai-doubutsu@yahoo.co.jp>
2023-12-25 15:44:42 +0900

    ファイルタイプを表示するメソッドを作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,26 +1,59 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 COL_MAX = 3
 PADDING = 2
+FILE_TYPE_TO_CHARACTER = {
+  'file' => '-',
+  'directory' => 'd',
+  'characterSpecial' => 'c',
+  'blockSpecial' => 'b',
+  'fifo' => 'p',
+  'link' => 'l',
+  'socket' => 's',
+  'unknown' => '?'
+}
 
 def main
-  path = ARGV[0] || '.'
-  FileTest.directory?(path) or return
-  file_names = Dir.children(path).sort
-  display_file_names(file_names)
+  options = { l: false }
+  opt = OptionParser.new
+  opt.on('-l') { options[:l] = true }
+  argv = opt.parse(ARGV)
+
+  path = argv[0] || './'
+  # pathの文字列が/で終わらない場合、文字列の末尾に/を追加する
+  corrected_path = path.sub(%r{([^/])\z}, '\1/')
+  FileTest.directory?(corrected_path) or return
+  file_names = Dir.children(corrected_path).sort
+  filtered_file_names = file_names.reject { |name| name.start_with?('.') }
+  options[:l] ? display_file_names_with_long(corrected_path, filtered_file_names) : display_file_names(filtered_file_names)
+end
+
+def display_file_names_with_long(path, file_names)
+  # column_name_to_width = {}
+  # column_name_to_width[:hard_link_numbers] = get_hard_link_numbers_width(file_names)
+  file_names.each do |file_name|
+    display_file_type(path, file_name)
+    puts
+  end
+end
+
+def display_file_type(path, file_name)
+  file_type = File.ftype(path + file_name)
+  print FILE_TYPE_TO_CHARACTER[file_type]
 end
 
 def display_file_names(file_names)
-  filtered_file_names = file_names.reject { |name| name.start_with?('.') }
-  total_file_names = filtered_file_names.size.to_f
+  total_file_names = file_names.size.to_f
   row_size = (total_file_names / COL_MAX).ceil
   col_size = (total_file_names / row_size).ceil
-  widths = get_column_widths(filtered_file_names, row_size, col_size)
+  widths = get_column_widths(file_names, row_size, col_size)
 
   row_size.times do |row|
     col_size.times do |col|
-      file_name = filtered_file_names[row + col * row_size]
+      file_name = file_names[row + col * row_size]
       width = widths[col] + PADDING
       print file_name.ljust(width, ' ') if file_name
     end
