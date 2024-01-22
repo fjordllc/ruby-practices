3907dd12107973a47884bcbfe54917d8eafbb756
Ryzhon <pekozono@email.com>
2024-01-23 07:48:06 +0900

    fix: 引数で定義しているものと実際の使い方が違う 引数名ではフレームを定義しているのに代入しているのはショットで違うため変更。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def calculate_bowling_score(frames)
+def calculate_bowling_score(shots)
   total_score = 0
-  frame_index = 0
+  shot_index = 0
 
-  10.times do |_frame|
-    if frames[frame_index] == 10
-      total_score += 10 + frames[frame_index + 1] + frames[frame_index + 2]
-      frame_index += 1
-    elsif frames[frame_index] + frames[frame_index + 1] == 10
-      total_score += 10 + frames[frame_index + 2]
-      frame_index += 2
+  10.times do
+    if shots[shot_index] == 10
+      total_score += 10 + shots[shot_index + 1] + shots[shot_index + 2]
+      shot_index += 1
+    elsif shots[shot_index] + shots[shot_index + 1] == 10
+      total_score += 10 + shots[shot_index + 2]
+      shot_index += 2
     else
-      total_score += frames[frame_index] + frames[frame_index + 1]
-      frame_index += 2
+      total_score += shots[shot_index] + shots[shot_index + 1]
+      shot_index += 2
     end
   end
 
@@ -28,4 +28,5 @@ shots = ARGV[0].split(',').map do |s|
     s.to_i
   end
 end
+
 puts calculate_bowling_score(shots)
