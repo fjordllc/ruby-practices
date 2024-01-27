cfedc7ce86d82b801bf950c698a2a0c2b4fecd65
Ogura Manabu <oguramanabu@gmail.com>
2024-01-27 22:36:57 +0900

    submit

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+# Represents a range of numbers from 1 to 20.
+number = (1..20).to_a
+number.each do |n|
+  if (n % 3).zero? && (n % 5).zero?
+    puts 'FizzBuzz'
+  elsif (n % 3).zero?
+    puts 'Fizz'
+  elsif (n % 5).zero?
+    puts 'Buzz'
+  else
+    puts n
+  end
+end
