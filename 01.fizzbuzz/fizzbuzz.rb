fa4918e522fca843e5b5b2cf83ce9ad35ed46979
yokomaru <cabochetoganlan@gmail.com>
2024-01-29 23:42:02 +0900

    fix: rubocopの指摘箇所を修正 - n % x == 0 を (n % x).zero?に修正 - "を’に修正 - 最後１行に空行を追加 - # frozen_string_literal: true を追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 def fizzbuzz
   (1..20).each do |n|
-    if n % 15 == 0
-      puts "FizzBuzz"
-    elsif n % 3 == 0
-      puts "Fizz"
-    elsif n % 5 == 0
-      puts "Buzz"
+    if (n % 15).zero?
+      puts 'FizzBuzz'
+    elsif (n % 3).zero?
+      puts 'Fizz'
+    elsif (n % 5).zero?
+      puts 'Buzz'
     else
       puts n
     end
