784718c78328286a466b955a786d32eeb0e09cc0
yokomaru <cabochetoganlan@gmail.com>
2024-01-29 23:16:05 +0900

    add:	fizzbuzz.rbの追加

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def fizzbuzz
+  (1..20).each do |n|
+    if n % 15 == 0
+      puts "FizzBuzz"
+    elsif n % 3 == 0
+      puts "Fizz"
+    elsif n % 5 == 0
+      puts "Buzz"
+    else
+      puts n
+    end
+  end
+end
+
+fizzbuzz
