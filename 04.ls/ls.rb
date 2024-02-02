b24406e3a388a9fc5c742694e2a95b0a9d5249c3
Ryzhon <pekozono@email.com>
2024-02-02 09:43:57 +0900

    fix: 表示方法

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,40 +1,24 @@
 # frozen_string_literal: true
 
-def pad_files(padded_files)
-  max_length = padded_files.map(&:length).max
-  padded_files.map { |column| column + [nil] * (max_length - column.length) }
-end
-
 def distribute_files(files, cols)
-  col_length = (files.length / cols.to_f).ceil
-  padded_files = files.each_slice(col_length).to_a
-  if files.length / cols <= 1 && cols < files.length
-    padded_files = Array.new(cols) { [] }
-    files.each_with_index do |file, index|
-      sublist_index = index / 2
-      break if sublist_index >= cols
-
-      padded_files[sublist_index] << file
-    end
-  else
-    padded_files = pad_files(padded_files)
-  end
-  padded_files
-end
-
-def calculate_columns(max_name_length, screen_width)
-  screen_width / (max_name_length + 2)
+  rows = (files.length / cols.to_f).ceil
+  files.each_slice(rows).to_a
 end
 
 def list_directory
-  file_names = Dir.glob('*')
-  max_file_name_length = file_names.map(&:length).max
-  screen_width = `tput cols`.to_i
+  file_names = Dir.glob('*').sort
+  file_count = file_names.size
 
-  cols = calculate_columns(max_file_name_length, screen_width) > 3 ? 3 : calculate_columns(max_file_name_length, screen_width)
+  cols = if file_count <= 3
+           file_count
+         else
+           3
+         end
+
   padded_files = distribute_files(file_names, cols)
-  padded_files[0].zip(*padded_files[1...]).each do |row|
-    puts row.map(&:to_s).join('  ')
+  p padded_files
+  padded_files[0].zip(*padded_files[1..]).each do |row|
+    puts row.join('  ')
   end
 end
 
