70fafcc1c5eb29b015915a606a21436c46b6ba0a
Ryzhon <pekozono@email.com>
2024-02-05 11:45:32 +0900

    add: 列を揃える

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
-def distribute_files(files, cols)
+def pad_files(files, cols)
   rows = (files.length / cols.to_f).ceil
-  files.each_slice(rows).to_a
+  max_length = files.max_by(&:length).length
+  files.map { |file| file.ljust(max_length) }
+       .each_slice(rows).to_a
+end
+
+def distribute_files(files, cols)
+  pad_files(files, cols)
 end
 
 def list_directory
   file_names = Dir.glob('*').sort
-  file_count = file_names.size
 
-  cols = if file_count <= 3
-           file_count
-         else
-           3
-         end
+  cols = 3
 
   padded_files = distribute_files(file_names, cols)
-  p padded_files
   padded_files[0].zip(*padded_files[1..]).each do |row|
     puts row.join('  ')
   end
