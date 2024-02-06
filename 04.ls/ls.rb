70b8fb8e9ef60219bb1fe2a69ba1c43639ea81c1
Ryzhon <pekozono@email.com>
2024-02-06 09:45:54 +0900

    fix: colsを定数で定義する

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
-def pad_files(files, cols)
+COLS = 3
+
+def distribute_files(files, cols)
   rows = (files.length / cols.to_f).ceil
   max_length = files.max_by(&:length).length
   files.map { |file| file.ljust(max_length) }
        .each_slice(rows).to_a
 end
 
-def distribute_files(files, cols)
-  pad_files(files, cols)
-end
-
 def list_directory
-  file_names = Dir.glob('*').sort
+  file_names = Dir.glob('*')
 
-  cols = 3
-
-  padded_files = distribute_files(file_names, cols)
+  padded_files = distribute_files(file_names, COLS)
   padded_files[0].zip(*padded_files[1..]).each do |row|
     puts row.join('  ')
   end
