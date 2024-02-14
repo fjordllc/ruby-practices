28fd424c59755f983d123d96dc3ebe9562fb2a5d
Ryzhon <pekozono@email.com>
2024-02-14 13:24:57 +0900

    fix: 複数のオプションを取得できるように引数の取得をoptparseに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 MAX_COLUMNS = 3
 
 def partition_filenames(files, cols)
@@ -17,13 +19,20 @@ def partition_filenames(files, cols)
   partitioned_files.each_slice(rows).to_a
 end
 
-def list_directory(include_hidden: false)
-  file_names = include_hidden ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+def list_directory(options)
+  file_names = options[:include_hidden] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
   partitioned_files = partition_filenames(file_names, MAX_COLUMNS)
   partitioned_files[0].zip(*partitioned_files[1..]).each do |row|
     puts row.join('  ')
   end
 end
 
-include_hidden = ARGV.include?('-a')
-list_directory(include_hidden:)
+options = { include_hidden: false, reverse: false }
+opt = OptionParser.new
+
+opt.on('-a') do
+  options[:include_hidden] = true
+end
+opt.parse!(ARGV)
+
+list_directory(options)
