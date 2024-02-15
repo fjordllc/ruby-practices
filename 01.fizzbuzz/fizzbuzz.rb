9df57731301c3b8d979a394f55dce6ce79f6d7db
Ogura Manabu <oguramanabu@gmail.com>
2024-02-15 22:36:24 +0900

    Edit file

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+number = (1..20).to_a
+number.each do |n|
+  if (n % 3).zero? && (n % 5).zero?
+    puts 'FizzBuzz'
+  elsif (n % 3).zero?
+    puts 'Fizz'
+  elsif (n % 5).zero?
+    puts 'Buzz'
+  else
+    puts n
+  end
+end
