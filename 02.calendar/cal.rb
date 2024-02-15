8073730623d4bf3d9e510352f03bf2d793e252d0
Ogura Manabu <oguramanabu@gmail.com>
2024-02-15 23:18:00 +0900

    rubocop

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'date'
 require 'optparse'
@@ -8,15 +9,15 @@ def calendar(year = Date.year, month = Date.month)
   end_date = Date.new(year, month, -1)
   day_array = Array.new(start_date.wday, nil).concat((start_date.day..end_date.day).to_a)
 
-  puts "#{start_date.strftime('%B %Y')}".center(20)
-  puts "Su Mo Tu We Th Fr Sa"
+  puts start_date.strftime('%B %Y').to_s.center(20)
+  puts 'Su Mo Tu We Th Fr Sa'
   day_array.each_slice(7) do |slice|
     formatted_slice = slice.map { |d| d.to_s.rjust(2) }
-    puts formatted_slice.join(" ")
+    puts formatted_slice.join(' ')
   end
 end
 
-options = { year: Date.today.year, month: Date.today.month}
+options = { year: Date.today.year, month: Date.today.month }
 OptionParser.new do |opts|
   opts.on('-y VAL', Integer) { |v| options[:year] = v }
   opts.on('-m VAL', Integer) { |v| options[:month] = v }
