3a15c0cefc31e04ddf3e3adf606741e3a1a59911
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-04 08:57:42 +0900

    fizzbuzz 修正点の改善

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -1,28 +1,13 @@
 num = 0
-20.times do
-    num += 1
-    if num % 15 == 0
-        puts "FizzBuzz"
-    elsif num % 5 == 0
-        puts "Buzz"
-    elsif num % 3 == 0
-        puts "Fizz"
-    else
-        puts num
-    end
+while num <= 20 do
+num += 1
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
 end
-
-# num = 0
-# 20.times do
-#     num += 1
-#     case num
-#     when num % 15 == 0
-#         puts "FizzBuzz"
-#     when num % 5 == 0
-#         puts "Buzz"
-#     when num % 3 == 0
-#         puts "Fizz"
-#     else
-#         puts num
-#     end
-# end
